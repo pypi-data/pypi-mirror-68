2fcca72b35eb35a06de8fe15111e9ce15b55bfba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:15:09 +0000

    Add package bin-distr-christoph2845/0.1/bin_distr_christoph2845-0.1.tar.gz

---
+++ packages/bin-distr-christoph2845/bin_distr_christoph2845-0.1.tar.gz/bin_distr_christoph2845-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='bin_distr_christoph2845',
+      version='0.1',
+      description='Test package binomial distribution',
+      packages=['bin_distr_christoph2845'],
+      zip_safe=False)
