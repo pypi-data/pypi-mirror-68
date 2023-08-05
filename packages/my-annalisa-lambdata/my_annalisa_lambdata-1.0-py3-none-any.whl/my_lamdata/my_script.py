e31439f0251449ed1d2d5d133a1652c31e3bff82
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:06 +0000

    Add package my-annalisa-lambdata/1.0/my_annalisa_lambdata-1.0-py3-none-any.whl

---
+++ packages/my-annalisa-lambdata/my_annalisa_lambdata-1.0-py3-none-any.whl/my_lamdata/my_script.py
@@ -0,0 +1,12 @@
+# my_script.py
+
+from pandas import DataFrame
+from my_lamdata.my_mod import enlarge
+
+print("HELLO")
+
+df = DataFrame({"a":[1,2,3], "b":[4,5,6]})
+print(df.head())
+
+x = 11
+print(enlarge(x))
