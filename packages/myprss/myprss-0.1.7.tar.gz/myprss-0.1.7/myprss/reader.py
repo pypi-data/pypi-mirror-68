85c8e66298b6eec378ef9aceb78182f28249e384
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:13:39 +0000

    Add package myprss/0.1.7/myprss-0.1.7.tar.gz

---
+++ packages/myprss/myprss-0.1.7.tar.gz/myprss-0.1.7/myprss/reader.py
@@ -0,0 +1,37 @@
+import click
+import requests
+
+from myprss import logger
+from myprss.config import Config
+from myprss.parser import parse_rss_content
+from myprss.utils import list_known_feeds
+
+
+@click.command(
+    "read", help="Read a rss feed", no_args_is_help=True, options_metavar=""
+)
+@click.argument("name", autocompletion=list_known_feeds)
+def read_feed(name):
+    config = Config()
+
+    if name not in config.data["registry"]:
+        logger.error(f"Unknown rss feed: {name}")
+        return
+
+    url = config.data["registry"][name]
+    response = requests.get(url)
+
+    if response.status_code != 200:
+        logger.error(f"{response.reason} -> {url}")
+        return
+
+    content = parse_rss_content(response.content)
+
+    logger.info(content["site"])
+    logger.info("")
+
+    for item in reversed(content["items"]):
+        logger.info(item["date"])
+        logger.link(item["url"], item["title"])
+        # logger.paragraph(item["description"])
+        logger.info("")
