b7453e328a93e2a2d2fabad29a4dbe61b758b194
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:36 +0000

    Add package env-file/2020.5.9/env-file-2020.5.9.tar.gz

---
+++ packages/env-file/env-file-2020.5.9.tar.gz/env-file-2020.5.9/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='env-file',
+    version='2020.5.9',
+    install_requires=[
+        'public',
+        'setuptools',
+        'values',
+    ],
+    packages=[
+        'env_file',
+    ],
+)
