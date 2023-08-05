9d119b3d1d2814ff5fcb7e79953709203e0f0bdf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:24 +0000

    Add package pyaefis/0.0.1.2/pyaefis-0.0.1.2.tar.gz

---
+++ packages/pyaefis/pyaefis-0.0.1.2.tar.gz/pyaefis-0.0.1.2/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pyaefis", # Replace with your own username
+    version="0.0.1.2",
+    author="Cedarville University",
+    author_email="techhelp@cedarville.edu",
+    description="Library for interacting with AEFIS",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/cedarville-university/pyaefis",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
