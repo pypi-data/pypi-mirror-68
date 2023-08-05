d5f012a874ef7bc5f52d0348d6469d379f92d4de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:45 +0000

    Add package chromosight/1.1.0/chromosight-1.1.0.tar.gz

---
+++ packages/chromosight/chromosight-1.1.0.tar.gz/chromosight-1.1.0/chromosight/__init__.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# coding: utf-8
+
+from .version import __version__ as version
+
+__author__ = "Axel Cournac, Axel Breuer, Lyam Baudry, Rémi Montagne, Cyril Matthey-Doret, Nadège Guielguimoni"
+__copyright__ = "Copyright © 2017-2018, Institut Pasteur, Paris, France"
+__credits__ = ["Hackathon ENGIE/Institut Pasteur teams"]
+__license__ = "GPLv3"
+__maintainer__ = "Axel Cournac, Lyam Baudry, Cyril Matthey-Doret, Nadège Guielguimoni"
+__email__ = "axel.cournac@pasteur.fr"
+__status__ = "Pre-Alpha"
+__version__ = version
