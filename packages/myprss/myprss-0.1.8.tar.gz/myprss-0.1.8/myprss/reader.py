003167da10d571e4ecbbdca5a8aaa86ed460d357
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:14:04 +0000

    Add package myprss/0.1.8/myprss-0.1.8.tar.gz

---
+++ packages/myprss/myprss-0.1.8.tar.gz/myprss-0.1.8/myprss/reader.py
@@ -0,0 +1,39 @@
+import click
+import requests
+
+from myprss import logger
+from myprss.config import Config
+from myprss.parser import parse_rss_content
+from myprss.utils import list_known_feeds
+
+
+@click.command(
+    "read", help="Read a rss feed", no_args_is_help=True
+)
+@click.option("-v", "--verbose", default=False, is_flag=True)
+@click.argument("name", autocompletion=list_known_feeds)
+def read_feed(name, verbose):
+    config = Config()
+
+    if name not in config.data["registry"]:
+        logger.error(f"Unknown rss feed: {name}")
+        return
+
+    url = config.data["registry"][name]
+    response = requests.get(url)
+
+    if response.status_code != 200:
+        logger.error(f"{response.reason} -> {url}")
+        return
+
+    content = parse_rss_content(response.content)
+
+    logger.info(content["site"])
+    logger.info("")
+
+    for item in reversed(content["items"]):
+        logger.info(item["date"])
+        logger.link(item["url"], item["title"])
+        if verbose:
+            logger.paragraph(item["description"])
+        logger.info("")
