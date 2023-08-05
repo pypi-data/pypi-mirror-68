3aec635b2465dd78d3ed109ab2aac7a94e6c3588
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:13:31 +0000

    Add package IOTAssignmentServerdorachua/0.0.48/IOTAssignmentServerdorachua-0.0.48.tar.gz

---
+++ packages/IOTAssignmentServerdorachua/IOTAssignmentServerdorachua-0.0.48.tar.gz/IOTAssignmentServerdorachua-0.0.48/IOTAssignmentServerdorachua/SocketFeedV2.py
@@ -0,0 +1,29 @@
+from time import sleep
+from IOTAssignmentUtilitiesdorachua.MySQLManager import MySQLManager
+from IOTAssignmentUtilitiesdorachua.MySQLManager import QUERYTYPE_DELETE,QUERYTYPE_INSERT,QUERYTYPE_RETRIEVE,QUERYTYPE_UPDATE
+from IOTAssignmentUtilitiesdorachua.OurCustomEncoder import OurCustomEncoder
+import json
+import sys
+from datetime import datetime,timedelta
+#from IOTAssignmentUtilitiesdorachua.MyDateTimeUtilities import MyDateTimeUtilities
+
+class SocketFeedV2:
+  
+    def __init__(self,record, u,pw,h,db):        
+        self.user = u
+        self.password = pw
+        self.host = h
+        self.database = db
+
+        self.id = record["id"]
+        self.bookingid = record["bookingid"]
+        self.bookingidwithtime = record['bookingidwithtime']
+        self.offsetseconds = record['offsetsecords']
+        self.startdatetime_value = record['startdatetime_value']
+        self.enddatetime_value = record['enddatetime_value']
+        self.timestamp_value = record['timestamp_value']
+
+        
+
+        
+    
