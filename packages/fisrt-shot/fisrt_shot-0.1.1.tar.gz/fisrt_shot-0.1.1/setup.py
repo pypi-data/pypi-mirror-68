2f690c32832c213699e428c1d5906f2d42ef2826
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:29 +0000

    Add package fisrt-shot/0.1.1/fisrt_shot-0.1.1.tar.gz

---
+++ packages/fisrt-shot/fisrt_shot-0.1.1.tar.gz/fisrt_shot-0.1.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='fisrt_shot',
+      version='0.1.1',
+      description='Gaussian distributions',
+      packages=['fisrt_shot'],
+      zip_safe=False)
+
+
