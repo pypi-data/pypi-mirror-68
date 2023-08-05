51a23bf7a3863a65441df01e09233e3a0e4bb2b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:13:48 +0000

    Add package python-chattrainer/0.0.2/python-chattrainer-0.0.2.tar.gz

---
+++ packages/python-chattrainer/python-chattrainer-0.0.2.tar.gz/python-chattrainer-0.0.2/README.md
@@ -0,0 +1,15 @@
+### Installation
+The aim of the project is to build a software solution to train people and employees to interact with the chatting and messaging consoles. This can also be used by people to train themselves how to chat with people without conveying false sentiments to the receiver. Customer care employees can be trained using this software to make them efficient communicators and agents who convey positive sentiment to the clients of the company.  
+
+Recommendation : Install ChatterBot module and nltk and sklearn modules before using this module to avoid issues while installation.
+
+Usages : python-chattrainer 
+	
+```sh
+$ pip install python-chattrainer
+$ python-chattrainer
+```
+### License
+MIT
+### Author
+Sahil Ahuja
---
+++ packages/python-chattrainer/python-chattrainer-0.0.2.tar.gz/python-chattrainer-0.0.2/setup.py
@@ -0,0 +1,41 @@
+from setuptools import setup
+
+def readme():
+  with open('README.md', encoding='utf-8') as f:
+    README = f.read()
+  return README
+
+setup(
+  name="python-chattrainer",
+  version="0.0.2",
+  description="A python package to do train people how to chat",
+  long_description=readme(),
+  long_description_content_type="text/markdown",
+  author="Sahil Ahuja",
+  author_email="sahil27ahuja1999@gmail.com",
+  license="MIT",
+  classifiers=[
+    'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+    'Intended Audience :: Developers',      # Define that your audience are developers
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',   # Again, pick a license
+    'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+  ],
+  packages=["chattrainer"],
+  include_package_data=True,
+  package_data={'python-chattrainer': ['*.pickle','*.spec','*.sqlite3']},
+  install_requires=[            # I get to this in a second
+    'nltk',
+    'statistics',
+    'ChatterBot',
+    'sklearn',
+  ],
+  entry_points={
+    "console_scripts": [
+      "python-chattrainer=chattrainer.chattrainer:main",    
+    ]
+  },
+)
