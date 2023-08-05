44dcc4c08dd7c83c59856c15dad93ba67e2989ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:11:23 +0000

    Add package zeus-mcmc/1.0.7/zeus-mcmc-1.0.7.tar.gz

---
+++ packages/zeus-mcmc/zeus-mcmc-1.0.7.tar.gz/zeus-mcmc-1.0.7/zeus/__init__.py
@@ -0,0 +1,11 @@
+name = "zeus-mcmc"
+__version__ = "1.0.7"
+__url__ = "https://zeus-mcmc.readthedocs.io"
+__author__ = "Minas Karamanis"
+__email__ = "minaskar@gmail.com"
+__license__ = "GPL-3.0"
+__description__ = "Lightning Fast MCMC"
+
+
+from .zeus import *
+from .autocorr import *
