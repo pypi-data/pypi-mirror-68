bc497bf67b34cd6f7138fb5a2acb5341babb17b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:41 +0000

    Add package irelease/1.0.0/irelease-1.0.0-py3-none-any.whl

---
+++ packages/irelease/irelease-1.0.0-py3-none-any.whl/irelease/examples.py
@@ -0,0 +1,14 @@
+# --------------------------------------------------
+# Name        : irelease.py
+# Author      : E.Taskesen
+# Contact     : erdogant@gmail.com
+# github      : https://github.com/erdogant/irelease
+# Licence     : MIT
+# --------------------------------------------------
+
+import irelease
+print(irelease.__version__)
+dir(irelease)
+
+# %%
+irelease.make_script()
