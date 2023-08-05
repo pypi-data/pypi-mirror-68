c22127698300652a690272a69d4e1a4b3546824a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:13:44 +0000

    Add package customdnn/1.1.2/customdnn-1.1.2.tar.gz

---
+++ packages/customdnn/customdnn-1.1.2.tar.gz/customdnn-1.1.2/customdnn/__init__.py
@@ -0,0 +1,10 @@
+# Authors: Manpreet Singh <singhmnprt01@gmail.com>
+# customdnn Copyright (C) 2020 singhmnprt01@gmail.com
+# License: GNU GENERAL PUBLIC LICENSE
+
+
+from customdnn import network_train
+from customdnn import network_predict
+
+__all__ = ('network_train')
+__all__ = ('network_predict')
