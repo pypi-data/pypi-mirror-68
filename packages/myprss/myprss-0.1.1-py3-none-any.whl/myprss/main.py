4b25490bb025873898304372e8f97b07d8c8ce03
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:13:10 +0000

    Add package myprss/0.1.1/myprss-0.1.1-py3-none-any.whl

---
+++ packages/myprss/myprss-0.1.1-py3-none-any.whl/myprss/main.py
@@ -0,0 +1,19 @@
+import click
+
+from myprss.reader import read_feed
+from myprss.registry import registry
+
+# echo -e '\e]8;;http://example.com\aThis is a link\e]8;;\a'
+
+
+@click.group(options_metavar="")
+def cli():
+    pass
+
+
+cli.add_command(registry)
+cli.add_command(read_feed)
+
+
+if __name__ == "__main__":
+    cli()
