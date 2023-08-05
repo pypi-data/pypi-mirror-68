fa19b7f5c442524f86b56f52abd29eb99fee9616
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:13:48 +0000

    Add package common-francis-francis95-han/0.0.0.1/common_francis-francis95-han-0.0.0.1.tar.gz

---
+++ packages/common-francis-francis95-han/common_francis-francis95-han-0.0.0.1.tar.gz/common_francis-francis95-han-0.0.0.1/setup.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+""" 
+@author zhangbohan.dell@gmail.com
+@function:
+@create 2020/5/10 19:11
+"""
+
+# !/usr/bin/env python
+from __future__ import print_function
+from setuptools import setup, find_packages
+import sys
+
+setup(
+    name = "common_francis-francis95-han",
+    version = "0.0.0.1",
+    author = "francis95-han",
+    author_email = "zhangbohan.dell@gmail.com",
+    description = "python common_francis package",
+    long_description = open("README.md").read(),
+    license = "MIT",
+    url = "https://github.com/francis95-han/common-francis95-han",
+    packages = ['common_francis'],
+    install_requires = [
+    ],
+    classifiers = [
+    "Environment :: Plugins",
+    "Intended Audience :: Developers",
+    "Topic :: Text Processing :: Indexing",
+    "Topic :: Utilities",
+    "Topic :: Internet",
+    "Topic :: Software Development :: Libraries :: Python Modules",
+    "Programming Language :: Python",
+    "Programming Language :: Python :: 3",
+    "Programming Language :: Python :: 3.6",
+    "Programming Language :: Python :: 3.7",
+    ],
+)
