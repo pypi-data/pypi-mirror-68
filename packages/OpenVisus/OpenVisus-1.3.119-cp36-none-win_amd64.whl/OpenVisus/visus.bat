9a75ba8fef2df8ab37bcf32ad04b980df1ba5506
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:34 +0000

    Add package OpenVisus/1.3.119/OpenVisus-1.3.119-cp36-none-win_amd64.whl

---
+++ packages/OpenVisus/OpenVisus-1.3.119-cp36-none-win_amd64.whl/OpenVisus/visus.bat
@@ -0,0 +1,5 @@
+
+cd %~dp0
+set PATH=C:\Python36-x64\python.exe\..;%PATH%;.\bin
+set PYTHONPATH=C:\projects\openvisus\build_win\Release\OpenVisus;C:\Python36-x64\python36.zip;C:\Python36-x64\DLLs;C:\Python36-x64\lib;C:\Python36-x64;C:\Python36-x64\lib\site-packages
+bin\visus.exe" %*
