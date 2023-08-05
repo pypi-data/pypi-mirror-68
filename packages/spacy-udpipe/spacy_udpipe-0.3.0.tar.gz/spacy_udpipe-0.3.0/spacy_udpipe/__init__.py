875eddeb9c91b3af61237ab4b68cd234acb97756
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:41 +0000

    Add package spacy-udpipe/0.3.0/spacy_udpipe-0.3.0.tar.gz

---
+++ packages/spacy-udpipe/spacy_udpipe-0.3.0.tar.gz/spacy_udpipe-0.3.0/spacy_udpipe/__init__.py
@@ -0,0 +1,5 @@
+from .language import UDPipeLanguage, UDPipeModel, load, load_from_path
+from .utils import download
+
+__all__ = ["UDPipeLanguage", "UDPipeModel",
+           "load", "load_from_path", "download"]
