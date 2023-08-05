6e045197dc5dcadc101dd78eb8441e344d54a74e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:41:40 +0000

    Add package cytosim-reader/0.0.1/cytosim_reader-0.0.1.tar.gz

---
+++ packages/cytosim-reader/cytosim_reader-0.0.1.tar.gz/cytosim_reader-0.0.1/setup.py
@@ -0,0 +1,15 @@
+
+
+from setuptools import setup
+
+setup(
+    name='cytosim_reader',
+    version='0.0.1',
+    description='Read data from cytosim simulations to numpy arrays.',
+    author='Ilyas Kuhlemann',
+    author_email='ilyasp.ku@gmail.com',
+    license='GNU GPLv3',
+    py_modules=['cytosim_reader'],
+    install_requires=['numpy',
+                      'pandas']
+)
