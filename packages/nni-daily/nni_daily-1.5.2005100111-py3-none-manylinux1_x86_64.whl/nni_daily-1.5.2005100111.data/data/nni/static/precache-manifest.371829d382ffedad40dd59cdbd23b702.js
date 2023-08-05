4c5a77a8563b34876577deb51c3e0e7bb6d31362
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:35 +0000

    Add package nni-daily/1.5.2005100111/nni_daily-1.5.2005100111-py3-none-manylinux1_x86_64.whl

---
+++ packages/nni-daily/nni_daily-1.5.2005100111-py3-none-manylinux1_x86_64.whl/nni_daily-1.5.2005100111.data/data/nni/static/precache-manifest.371829d382ffedad40dd59cdbd23b702.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ebf585767e040ff3c6b0273768f360ad",
+    "url": "/editor.worker.js"
+  },
+  {
+    "revision": "dcaa5131164eca320c4eacea44edf3e3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e4eab2f949b53eb818d3dd02482ab801",
+    "url": "/json.worker.js"
+  },
+  {
+    "revision": "5f3170a7aa2a6e6f6531",
+    "url": "/static/css/2.a2ee78b9.chunk.css"
+  },
+  {
+    "revision": "cf9b3b1326a61117385e",
+    "url": "/static/css/main.efc98c1f.chunk.css"
+  },
+  {
+    "revision": "5f3170a7aa2a6e6f6531",
+    "url": "/static/js/2.18c235e1.chunk.js"
+  },
+  {
+    "revision": "3b0c809b19f03e192486",
+    "url": "/static/js/3.2f96925d.chunk.js"
+  },
+  {
+    "revision": "cf9b3b1326a61117385e",
+    "url": "/static/js/main.2cd3e2be.chunk.js"
+  },
+  {
+    "revision": "1e4a1b3468604b8f9b6c",
+    "url": "/static/js/runtime~main.2a04e9a1.js"
+  },
+  {
+    "revision": "35bb8d560db5205616671eab8c4d0303",
+    "url": "/static/media/codicon.35bb8d56.ttf"
+  }
+]);
