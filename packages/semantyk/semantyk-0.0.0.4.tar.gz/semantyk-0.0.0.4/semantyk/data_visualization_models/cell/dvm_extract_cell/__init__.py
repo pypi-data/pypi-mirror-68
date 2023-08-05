4be84626666d5ecdd596913a3fcefbeec3a8e355
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:12 +0000

    Add package semantyk/0.0.0.4/semantyk-0.0.0.4.tar.gz

---
+++ packages/semantyk/semantyk-0.0.0.4.tar.gz/semantyk-0.0.0.4/semantyk/data_visualization_models/cell/dvm_extract_cell/__init__.py
@@ -0,0 +1,13 @@
+##############################################
+#
+# dvm_extract_cell
+# Package | Data Visualization Model Extract Cell Package.
+#
+# Daniel Bakas Amuchastegui
+# February, 2020
+# 
+# Copyright © Semantyk 2020. All rights reserved.
+#############################################
+
+from . import dvm_extract_cell
+from .dvm_extract_cell import *
---
+++ packages/semantyk/semantyk-0.0.0.4.tar.gz/semantyk-0.0.0.4/semantyk/data_visualization_models/graph/network/__init__.py
@@ -0,0 +1,16 @@
+##############################################
+#
+# network
+# Package | network Package.
+#
+# Daniel Bakas Amuchastegui
+# March 16, 2020
+# 
+# Copyright © Semantyk 2020. All rights reserved.
+#############################################
+
+from . import genealogical_tree
+from . import network
+
+from .genealogical_tree import *
+from .network import *
