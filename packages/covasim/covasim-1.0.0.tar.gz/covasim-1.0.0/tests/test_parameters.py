521a1bb03d91394e5a93a04bc95271cee8c608ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:00 +0000

    Add package covasim/1.0.0/covasim-1.0.0.tar.gz

---
+++ packages/covasim/covasim-1.0.0.tar.gz/covasim-1.0.0/tests/test_parameters.py
@@ -0,0 +1,59 @@
+'''
+Test that the parameters and data files are being created correctly.
+'''
+
+#%% Imports
+import os
+import pytest
+import sciris as sc
+import covasim as cv # NOTE: this is the only tests script that doesn't use base
+
+do_plot = False
+
+
+#%% Define the tests
+def test_parameters():
+    sc.heading('Model parameters')
+    pars = cv.make_pars()
+    sc.pp(pars)
+    return pars
+
+
+def test_data():
+    sc.heading('Data loading')
+    data = cv.load_data(os.path.join(sc.thisdir(__file__), 'example_data.csv'))
+    sc.pp(data)
+
+    # Check that it is looking for the right file
+    with pytest.raises(FileNotFoundError):
+        data = cv.load_data(filename='file_not_found.csv')
+
+    return data
+
+
+def test_location():
+    sc.heading('Population settings')
+
+    pars = dict(
+        pop_size = 1000,
+        pop_type = 'hybrid',
+        location = 'nigeria',
+        )
+    sim = cv.Sim(pars)
+    sim.initialize()
+
+    return sim
+
+
+#%% Run as a script
+if __name__ == '__main__':
+    sc.tic()
+
+    pars = test_parameters()
+    data = test_data()
+    sim = test_location()
+
+    sc.toc()
+
+
+print('Done.')
