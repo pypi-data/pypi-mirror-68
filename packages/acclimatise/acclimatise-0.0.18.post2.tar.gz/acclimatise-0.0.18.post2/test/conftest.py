7622af1c266b46000708b06d2e6012537948daa3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:02 +0000

    Add package acclimatise/0.0.18.post2/acclimatise-0.0.18.post2.tar.gz

---
+++ packages/acclimatise/acclimatise-0.0.18.post2.tar.gz/acclimatise-0.0.18.post2/test/conftest.py
@@ -0,0 +1,66 @@
+from textwrap import dedent
+
+import pytest
+from pkg_resources import resource_filename
+
+from acclimatise.flag_parser.parser import CliParser
+
+
+@pytest.fixture
+def samtools_help():
+    with open(resource_filename(__name__, "test_data/samtools.txt")) as fp:
+        return fp.read()
+
+
+@pytest.fixture
+def htseq_help():
+    with open(resource_filename(__name__, "test_data/htseq_count.txt")) as fp:
+        return fp.read()
+
+
+@pytest.fixture
+def bwamem_help():
+    with open(resource_filename(__name__, "test_data/bwa_mem.txt")) as fp:
+        return fp.read()
+
+
+@pytest.fixture
+def pisces_help():
+    with open(resource_filename(__name__, "test_data/pisces.txt")) as fp:
+        return fp.read()
+
+
+@pytest.fixture
+def bwa_help():
+    with open(resource_filename(__name__, "test_data/bwa.txt")) as fp:
+        return fp.read()
+
+
+@pytest.fixture
+def bedtools_help():
+    with open(resource_filename(__name__, "test_data/bedtools.txt")) as fp:
+        return fp.read()
+
+
+@pytest.fixture
+def bedtools_coverage_help():
+    with open(resource_filename(__name__, "test_data/bedtools_coverage.txt")) as fp:
+        return fp.read()
+
+
+@pytest.fixture
+def podchecker_help():
+    with open(resource_filename(__name__, "test_data/podchecker.txt")) as fp:
+        return fp.read()
+
+
+@pytest.fixture()
+def process():
+    def process_help_section(help):
+        """
+        Does some preprocessing on a help text segment to facilitate testing
+        """
+        help = help.strip("\n")
+        return dedent(help)
+
+    return process_help_section
