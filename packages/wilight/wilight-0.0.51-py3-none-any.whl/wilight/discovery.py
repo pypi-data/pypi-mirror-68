722801a57286b74d0c544a88385ec2b5fcd4da5a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:14:47 +0000

    Add package wilight/0.0.51/wilight-0.0.51-py3-none-any.whl

---
+++ packages/wilight/wilight-0.0.51-py3-none-any.whl/wilight/discovery.py
@@ -0,0 +1,119 @@
+"""Module to discover WiLight devices."""
+import logging
+import requests
+
+from . import ssdp
+from .wilight_device.api.xsd import device as deviceParser
+
+LOG = logging.getLogger(__name__)
+
+
+MANUFACTURER = 'All Automacao Ltda'
+
+
+def discover_devices(ssdp_st=None, max_devices=None,
+                     match_serial=None,
+                     rediscovery_enabled=True):
+    """Find WiLight devices on the local network."""
+    ssdp_st = ssdp_st or ssdp.ST
+    ssdp_entries = ssdp.scan(ssdp_st, max_entries=max_devices,
+                             match_serial=match_serial)
+
+    wilights = []
+
+    for entry in ssdp_entries:
+        if entry.match_device_description(
+                {'manufacturer': MANUFACTURER}):
+            serialNumber = entry.description.get('device').get('serialNumber')
+            device = device_from_description(
+                description_url=entry.location, serialNumber=serialNumber,
+                rediscovery_enabled=rediscovery_enabled)
+
+            if device is not None:
+                wilights.append(device)
+
+
+    print("WiLights :", wilights)
+
+    return wilights
+
+
+def device_from_description(description_url, serialNumber, rediscovery_enabled=True):
+    """Return object representing WiLight device running at host, else None."""
+    #print("description_url :", description_url)
+    xml = requests.get(description_url, timeout=10)
+    model = deviceParser.parseString(xml.content).device.modelName
+    #print("model :", model)
+    serial_number = serialNumber or deviceParser.parseString(xml.content).device.serialNumber
+    #print("serial_number :", serial_number)
+    key = deviceParser.parseString(xml.content).device.modelNumber
+    #print("serial_number :", serial_number)
+
+    if serial_number is None:
+        LOG.debug(
+            'No serial number was supplied or found in setup xml at: %s.',
+            description_url)
+
+    return wilight_from_model_serial_and_location(
+        description_url, model, serial_number, key,
+        rediscovery_enabled=rediscovery_enabled)
+
+def wilight_from_model_serial_and_location(location, model, serial_number, key,
+                                  rediscovery_enabled=True):
+    """Create device class based on the device input data."""
+    if model is None:
+        return None
+    if len(model) < 15:
+        return None
+    if serial_number is None:
+        return None
+    if len(serial_number) != 12:
+        return None
+    if location is None:
+        return None
+    #print("id :", id)
+    host = location.split('/', 3)[2].split(':', 1)[0]
+    #print("host :", host)
+    type_mode = model.split(' ', 1)[1].split('-', 1)
+    type = type_mode[0]
+    #print("type :", type)
+    mode = type_mode[1]
+    #print("mode :", mode)
+
+    return {"serial_number": serial_number, "host": host, "type": type, "mode": mode, "key": key}
+
+
+def device_from_uuid_and_location(uuid, mac, location,
+                                  rediscovery_enabled=True):
+    """Determine device class based on the device uuid."""
+    if uuid is None:
+        return None
+    if uuid.startswith('uuid:Socket'):
+        return Switch(url=location, mac=mac,
+                      rediscovery_enabled=rediscovery_enabled)
+    if uuid.startswith('uuid:Lightswitch'):
+        return LightSwitch(url=location, mac=mac,
+                           rediscovery_enabled=rediscovery_enabled)
+    if uuid.startswith('uuid:Dimmer'):
+        return Dimmer(url=location, mac=mac,
+                      rediscovery_enabled=rediscovery_enabled)
+    if uuid.startswith('uuid:Insight'):
+        return Insight(url=location, mac=mac,
+                       rediscovery_enabled=rediscovery_enabled)
+    if uuid.startswith('uuid:Sensor'):
+        return Motion(url=location, mac=mac,
+                      rediscovery_enabled=rediscovery_enabled)
+    if uuid.startswith('uuid:Maker'):
+        return Maker(url=location, mac=mac,
+                     rediscovery_enabled=rediscovery_enabled)
+    if uuid.startswith('uuid:Bridge'):
+        return Bridge(url=location, mac=mac,
+                      rediscovery_enabled=rediscovery_enabled)
+    if uuid.startswith('uuid:CoffeeMaker'):
+        return CoffeeMaker(url=location, mac=mac,
+                           rediscovery_enabled=rediscovery_enabled)
+    if uuid.startswith('uuid:Humidifier'):
+        return Humidifier(url=location, mac=mac,
+                          rediscovery_enabled=rediscovery_enabled)
+
+    return None
