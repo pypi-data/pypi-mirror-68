6763cb0cb2572adc699095958f8e99d941c50ce5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:36 +0000

    Add package vb2Py/0.4.1/vb2Py-0.4.1.tar.gz

---
+++ packages/vb2Py/vb2Py-0.4.1.tar.gz/vb2Py-0.4.1/vb2py/test/testmini.py
@@ -0,0 +1,48 @@
+#
+# Turn off logging in extensions (too loud!)
+import vb2py.extensions
+import unittest
+vb2py.extensions.disableLogging()
+
+from vb2py.vbparser import buildParseTree, VBParserError
+
+#
+# Set some config options which are appropriate for testing
+import vb2py.config
+Config = vb2py.config.VB2PYConfig()
+Config.setLocalOveride("General", "ReportPartialConversion", "No")
+
+
+tests = []
+
+
+# Continuation within a with
+tests.append(
+"""
+Sub a
+    b=12
+End Sub 
+"""
+)
+
+class ParsingTest(unittest.TestCase):
+    """Holder class which gets built into a whole test case"""
+
+
+def getTestMethod(vb):
+    """Create a test method"""
+    def testMethod(self):
+        try:
+            buildParseTree(vb)
+        except VBParserError:
+            raise Exception("Unable to parse ...\n%s" % vb)
+    return testMethod
+
+#
+# Add tests to main test class
+for idx in range(len(tests)):
+    setattr(ParsingTest, "test%d" % idx, getTestMethod(tests[idx]))
+
+
+if __name__ == "__main__":
+    unittest.main()
