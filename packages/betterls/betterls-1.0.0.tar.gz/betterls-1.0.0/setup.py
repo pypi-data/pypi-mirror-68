7f3f0e08e6f1ca2767422d534a4aa08554842866
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:39:43 +0000

    Add package betterls/1.0.0/betterls-1.0.0.tar.gz

---
+++ packages/betterls/betterls-1.0.0.tar.gz/betterls-1.0.0/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['betterls']
+
+package_data = \
+{'': ['*']}
+
+entry_points = \
+{'console_scripts': ['bls = betterls:main']}
+
+setup_kwargs = {
+    'name': 'betterls',
+    'version': '1.0.0',
+    'description': 'Better ls command.',
+    'long_description': None,
+    'author': 'Cyrus Yip',
+    'author_email': 'cyruscmyip1@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'entry_points': entry_points,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
