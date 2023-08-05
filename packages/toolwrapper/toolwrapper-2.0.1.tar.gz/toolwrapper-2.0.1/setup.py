b50cf3bbe24b369064f592f9eda1daaae31cc9a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:52 +0000

    Add package toolwrapper/2.0.1/toolwrapper-2.0.1.tar.gz

---
+++ packages/toolwrapper/toolwrapper-2.0.1.tar.gz/toolwrapper-2.0.1/setup.py
@@ -0,0 +1,45 @@
+from setuptools import setup
+from os import path
+import re
+
+
+def packagefile(*relpath):
+    return path.join(path.dirname(__file__), *relpath)
+
+
+def read(*relpath):
+    with open(packagefile(*relpath)) as f:
+        return f.read()
+
+
+def get_version(*relpath):
+    match = re.search(
+        r'''^__version__ = ['"]([^'"]*)['"]''',
+        read(*relpath),
+        re.M
+    )
+    if not match:
+        raise RuntimeError('Unable to find version string.')
+    return match.group(1)
+
+setup(
+    name='toolwrapper',
+    version=get_version('src', 'toolwrapper.py'),
+    description='A base class for wrapping text-processing tools',
+    long_description=read('README.rst'),
+    url='https://github.com/luismsgomes/toolwrapper',
+    author='Luís Gomes',
+    author_email='luismsgomes@gmail.com',
+    license='MIT',
+    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Topic :: Text Processing :: Filters',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3 :: Only',
+    ],
+    keywords='subprocess text tool wrapper',
+    package_dir={'': 'src'},
+    py_modules=['toolwrapper'],
+)
