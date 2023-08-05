f29f062c7810e98d99940d8b6336c54a4d04607a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:37 +0000

    Add package py-multi-pager/0.1.0/py-multi-pager-0.1.0.tar.gz

---
+++ packages/py-multi-pager/py-multi-pager-0.1.0.tar.gz/py-multi-pager-0.1.0/setup.py
@@ -0,0 +1,34 @@
+
+from setuptools import setup, find_packages
+
+from os import path
+
+def get_readme():
+    here = path.abspath(path.dirname(__file__))
+    with open(path.join(here, 'README.md'), encoding='utf-8') as readme_file:
+        readme = readme_file.read()
+    return readme
+
+def get_requirements():
+    here = path.abspath(path.dirname(__file__))
+    with open(path.join(here, 'requirements.txt'), encoding='utf-8') as \
+        requirements_file:
+        requirements = requirements_file.read().splitlines()
+        return requirements
+
+setup(
+    name="py-multi-pager",
+    version="0.1.0",
+    author='Doyeon Kim',
+    author_email='archdo0909@gmail.com',
+    description="Easy and fast python SNS notifier.",
+    long_description=get_readme(),
+    license="MIT",
+    install_requires=get_requirements(),
+    url='https://github.com/archdo0909/NotiPy',
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+    ]
+)
