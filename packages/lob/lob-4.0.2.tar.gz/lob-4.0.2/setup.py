7363d57aff86d15f53dd8dd6b3ea4f6d63ec3b33
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:42:01 +0000

    Add package lob/4.0.2/lob-4.0.2.tar.gz

---
+++ packages/lob/lob-4.0.2.tar.gz/lob-4.0.2/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup
+
+README = open('README.md').read()
+
+setup(
+    name='lob',
+    version='4.0.2',
+    author='Lob',
+    author_email='support@lob.com',
+    packages=['lob'],
+    description='Lob Python Bindings',
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url='https://github.com/lob/lob-python',
+    license='MIT',
+    install_requires=[
+        'requests'
+    ],
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Programming Language :: Python :: Implementation :: PyPy"
+    ]
+)
