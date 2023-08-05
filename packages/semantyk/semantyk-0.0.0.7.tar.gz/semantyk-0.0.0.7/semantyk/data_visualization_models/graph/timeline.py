0111a5125dc87c2d325d915b3fdc3c8c9581bb19
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:13 +0000

    Add package semantyk/0.0.0.7/semantyk-0.0.0.7.tar.gz

---
+++ packages/semantyk/semantyk-0.0.0.7.tar.gz/semantyk-0.0.0.7/semantyk/data_visualization_models/graph/timeline.py
@@ -0,0 +1,15 @@
+##############################################
+#
+# timeline
+# Module | Visual, linear arrangements of objects of type Event.
+#
+# Daniel Bakas Amuchastegui
+# September 3, 2019
+# 
+# Copyright © Semantyk 2020. All rights reserved.
+#############################################
+
+from .graph import Graph
+
+class Timeline(Graph):
+    pass
