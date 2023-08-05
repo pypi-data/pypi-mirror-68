b8ab461a20a53abc6ac172937575cc1093214832
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:34 +0000

    Add package hello20200509/1.0.0/hello20200509-1.0.0.tar.gz

---
+++ packages/hello20200509/hello20200509-1.0.0.tar.gz/hello20200509-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+setup(
+    name='hello20200509',
+    version='1.0.0',
+    py_modules=['hello20200509'],
+    author='viviliving',
+    author_email='vivisoft@yeah.net',
+    url='http://1.1.1',
+    description='test',
+    )
+    
