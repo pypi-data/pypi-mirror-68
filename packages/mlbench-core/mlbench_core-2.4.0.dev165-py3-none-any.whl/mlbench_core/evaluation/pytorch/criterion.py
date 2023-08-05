8f59bf8fd79692eea39f5df38f8f5f2b403b75bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:05 +0000

    Add package mlbench-core/2.4.0.dev165/mlbench_core-2.4.0.dev165-py3-none-any.whl

---
+++ packages/mlbench-core/mlbench_core-2.4.0.dev165-py3-none-any.whl/mlbench_core/evaluation/pytorch/criterion.py
@@ -0,0 +1,178 @@
+"""Customized Loss Functions."""
+
+import torch
+import torch.nn.functional as F
+from torch.nn.modules.loss import _Loss, _WeightedLoss
+
+try:
+    from apex.contrib.xentropy import SoftmaxCrossEntropyLoss
+
+    apex_installed = True
+except ImportError as e:
+    apex_installed = False
+
+
+class BCELossRegularized(_WeightedLoss):
+    """Binary Cross Entropy (BCE) with l1/l2 regularization.
+
+    Args:
+        weight (Tensor, optional): a manual rescaling weight given to each
+            class. If given, it has to be a Tensor of size `C`. Otherwise, it is
+            treated as if having all ones.
+        size_average (bool, optional): Deprecated (see :attr:`reduction`). By default,
+            the losses are averaged over each loss element in the batch. Note that for
+            some losses, there multiple elements per sample. If the field :attr:`size_average`
+            is set to ``False``, the losses are instead summed for each minibatch. Ignored
+            when reduce is ``False``. Default: ``True``
+        reduce (bool, optional): Deprecated (see :attr:`reduction`). By default, the
+            losses are averaged or summed over observations for each minibatch depending
+            on :attr:`size_average`. When :attr:`reduce` is ``False``, returns a loss per
+            batch element instead and ignores :attr:`size_average`. Default: ``True``
+        l1 (float, optional): The scale of the L1 regularization. Default: ``0.0``
+        l2 (float, optional): The scale of the L2 regularization. Default: ``0.0``
+        model (:obj:`torch.nn.Module`): a pytorch model to be trained and validated.
+        reduction (string, optional): Specifies the reduction to apply to the output:
+            'none' | 'elementwise_mean' | 'sum'. 'none': no reduction will be applied,
+            'elementwise_mean': the sum of the output will be divided by the number of
+            elements in the output, 'sum': the output will be summed. Note: :attr:`size_average`
+            and :attr:`reduce` are in the process of being deprecated, and in the meantime,
+            specifying either of those two args will override :attr:`reduction`. Default: 'elementwise_mean'
+    """
+
+    def __init__(
+        self,
+        weight=None,
+        size_average=None,
+        reduce=None,
+        l1=0.0,
+        l2=0.0,
+        model=None,
+        reduction="elementwise_mean",
+    ):
+        super(BCELossRegularized, self).__init__(
+            weight, size_average, reduce, reduction
+        )
+        self.l2 = l2
+        self.l1 = l1
+        self.model = model
+
+    def forward(self, input_, target):
+        output = F.binary_cross_entropy(
+            input_, target.float(), weight=self.weight, reduction=self.reduction
+        )
+        l2_loss = sum(param.norm(2) ** 2 for param in self.model.parameters())
+        output += self.l2 / 2 * l2_loss
+        l1_loss = sum(param.norm(1) for param in self.model.parameters())
+        output += self.l1 * l1_loss
+        return output
+
+
+class MSELossRegularized(_WeightedLoss):
+    """Mean Squared Error (MSE) with l1/l2 regularization.
+
+    Args:
+        weight (Tensor, optional): a manual rescaling weight given to each
+            class. If given, it has to be a Tensor of size `C`. Otherwise, it is
+            treated as if having all ones.
+        size_average (bool, optional): Deprecated (see :attr:`reduction`). By default,
+            the losses are averaged over each loss element in the batch. Note that for
+            some losses, there multiple elements per sample. If the field :attr:`size_average`
+            is set to ``False``, the losses are instead summed for each minibatch. Ignored
+            when reduce is ``False``. Default: ``True``
+        reduce (bool, optional): Deprecated (see :attr:`reduction`). By default, the
+            losses are averaged or summed over observations for each minibatch depending
+            on :attr:`size_average`. When :attr:`reduce` is ``False``, returns a loss per
+            batch element instead and ignores :attr:`size_average`. Default: ``True``
+        l1 (float, optional): The scale of the L1 regularization. Default: ``0.0``
+        l2 (float, optional): The scale of the L2 regularization. Default: ``0.0``
+        model (:obj:`torch.nn.Module`): a pytorch model to be trained and validated.
+        reduction (string, optional): Specifies the reduction to apply to the output:
+            'none' | 'elementwise_mean' | 'sum'. 'none': no reduction will be applied,
+            'elementwise_mean': the sum of the output will be divided by the number of
+            elements in the output, 'sum': the output will be summed. Note: :attr:`size_average`
+            and :attr:`reduce` are in the process of being deprecated, and in the meantime,
+            specifying either of those two args will override :attr:`reduction`. Default: 'elementwise_mean'
+    """
+
+    def __init__(
+        self,
+        weight=None,
+        size_average=None,
+        reduce=None,
+        l1=0.0,
+        l2=0.0,
+        model=None,
+        reduction="elementwise_mean",
+    ):
+        super(MSELossRegularized, self).__init__(
+            weight, size_average, reduce, reduction
+        )
+        self.l2 = l2
+        self.l1 = l1
+        self.model = model
+
+    def forward(self, input_, target):
+        output = F.mse_loss(input_, target, reduction=self.reduction)
+        l2_loss = sum(param.norm(2) ** 2 for param in self.model.parameters())
+        output += self.l2 / 2 * l2_loss
+        l1_loss = sum(param.norm(1) for param in self.model.parameters())
+        output += self.l1 * l1_loss
+        return output
+
+
+# Taken from MLPerf
+class LabelSmoothedCrossEntropy(_Loss):
+    def __init__(
+        self,
+        model,
+        label_smoothing,
+        padding_idx,
+        sentence_avg=False,
+        fast_xentropy=False,
+    ):
+        super(LabelSmoothedCrossEntropy, self).__init__()
+        self.eps = label_smoothing
+        self.padding_idx = padding_idx
+        self.sentence_avg = sentence_avg
+
+        if fast_xentropy and apex_installed:
+            self.xentropy_func = SoftmaxCrossEntropyLoss.apply
+        else:
+            self.xentropy_func = None
+
+        self.model = model
+
+    def forward(self, sample, output, reduce=True):
+
+        target = sample["target"]
+        target = target.view(-1, 1)
+        if self.xentropy_func is not None:
+            assert (output[0].dtype == torch.float16) or (
+                output[0].dtype == torch.float32
+            ), "Unsupported data types"
+            output = output[0].view(
+                output[0].size(0) * output[0].size(1), output[0].size(2)
+            )
+            labels = target.view(target.size(0) * target.size(1))
+            losses = self.xentropy_func(
+                output,
+                labels,
+                self.eps,
+                self.padding_idx,
+                output[0].dtype == torch.float16,
+            )
+            loss = losses.sum()
+        else:
+            lprobs = self.model.get_normalized_probs(output, log_probs=True)
+            lprobs = lprobs.view(-1, lprobs.size(-1))
+            non_pad_mask = target.ne(self.padding_idx)
+            nll_loss = -lprobs.gather(dim=-1, index=target)[non_pad_mask]
+            smooth_loss = -lprobs.sum(dim=-1, keepdim=True)[non_pad_mask]
+            if reduce:
+                nll_loss = nll_loss.sum()
+                smooth_loss = smooth_loss.sum()
+            eps_i = self.eps / lprobs.size(-1)
+            loss = (1.0 - self.eps) * nll_loss + eps_i * smooth_loss
+
+        sample_size = target.size(0) if self.sentence_avg else sample["ntokens"]
+        return loss, sample_size
---
+++ packages/mlbench-core/mlbench_core-2.4.0.dev165-py3-none-any.whl/mlbench_core/models/pytorch/transformer/decoder.py
@@ -0,0 +1,202 @@
+import math
+
+import torch
+import torch.nn.functional as F
+from mlbench_core.models.pytorch.transformer import utils
+from mlbench_core.models.pytorch.transformer.modules import (
+    AdaptiveSoftmax,
+    PositionalEmbedding,
+    SinusoidalPositionalEmbedding,
+    TransformerDecoderLayer,
+)
+from torch import nn
+
+
+# from apex.normalization.fused_layer_norm import FusedLayerNorm
+
+
+# Copied from mlperf.
+class TransformerDecoder(nn.Module):
+    """
+    Transformer decoder consisting of *args.decoder_layers* layers. Each layer
+    is a :class:`TransformerDecoderLayer`.
+
+    Args:
+        args (argparse.Namespace): parsed command-line arguments
+        dictionary (~fairseq.data.Dictionary): decoding dictionary
+        embed_tokens (torch.nn.Embedding): output embedding
+        no_encoder_attn (bool, optional): whether to attend to encoder outputs
+            (default: False).
+    """
+
+    def __init__(
+            self, args, dictionary, embed_tokens, no_encoder_attn=False, left_pad=False
+    ):
+        super().__init__()
+        self.dictionary = dictionary
+        self.dropout = args.dropout
+        self.share_input_output_embed = args.share_decoder_input_output_embed
+
+        embed_dim = embed_tokens.embedding_dim
+        padding_idx = embed_tokens.padding_idx
+        self.max_target_positions = args.max_target_positions
+
+        self.embed_tokens = embed_tokens
+        self.embed_scale = math.sqrt(embed_dim)
+        self.embed_positions = (
+            PositionalEmbedding(
+                args.max_target_positions,
+                embed_dim,
+                padding_idx,
+                left_pad=left_pad,
+                learned=args.decoder_learned_pos,
+            )
+            if not args.no_token_positional_embeddings
+            else None
+        )
+
+        self.layers = nn.ModuleList([])
+        self.layers.extend(
+            [
+                TransformerDecoderLayer(args, no_encoder_attn)
+                for _ in range(args.decoder_layers)
+            ]
+        )
+        self.adaptive_softmax = None
+
+        if args.adaptive_softmax_cutoff is not None:
+            self.adaptive_softmax = AdaptiveSoftmax(
+                len(dictionary),
+                args.decoder_embed_dim,
+                utils.eval_str_list(args.adaptive_softmax_cutoff, type=int),
+                dropout=args.dropout,
+            )
+        elif not self.share_input_output_embed:
+            self.embed_out = nn.Parameter(torch.Tensor(len(dictionary), embed_dim))
+            nn.init.normal_(self.embed_out, mean=0, std=embed_dim ** -0.5)
+        self.normalize = args.decoder_normalize_before
+
+        if self.normalize:
+            self.layer_norm = nn.LayerNorm(embed_dim)  # TODO change to FUsedLayerNorm
+
+    def forward(self, prev_output_tokens, encoder_out=None, incremental_state=None):
+        # embed positions
+        positions = (
+            self.embed_positions(
+                prev_output_tokens, incremental_state=incremental_state,
+            )
+            if self.embed_positions is not None
+            else None
+        )
+
+        if incremental_state is not None:
+            prev_output_tokens = prev_output_tokens[:, -1:]
+            if positions is not None:
+                positions = positions[:, -1:]
+
+        # embed tokens and positions
+        x = self.embed_scale * self.embed_tokens(prev_output_tokens)
+        if positions is not None:
+            x += positions
+        x = F.dropout(x, p=self.dropout, training=self.training)
+
+        # B x T x C -> T x B x C
+        x = x.transpose(0, 1)
+        attn = None
+
+        # decoder layers
+        for layer in self.layers:
+            x, attn = layer(
+                x,
+                encoder_out["encoder_out"] if encoder_out is not None else None,
+                encoder_out["encoder_padding_mask"]
+                if encoder_out is not None
+                else None,
+                incremental_state,
+            )
+
+        if self.normalize:
+            x = self.layer_norm(x)
+
+        # T x B x C -> B x T x C
+        x = x.transpose(0, 1)
+
+        if self.adaptive_softmax is None:
+            # project back to size of vocabulary
+            if self.share_input_output_embed:
+                x = F.linear(x, self.embed_tokens.weight)
+            else:
+                x = F.linear(x, self.embed_out)
+
+        return x, attn
+
+    def max_positions(self):
+        """Maximum output length supported by the decoder."""
+        if self.embed_positions is None:
+            return self.max_target_positions
+        return min(self.max_target_positions, self.embed_positions.max_positions())
+
+    def upgrade_state_dict(self, state_dict):
+        if isinstance(self.embed_positions, SinusoidalPositionalEmbedding):
+            if "decoder.embed_positions.weights" in state_dict:
+                del state_dict["decoder.embed_positions.weights"]
+            state_dict["decoder.embed_positions._float_tensor"] = torch.FloatTensor(1)
+
+        for i in range(len(self.layers)):
+            # update layer norms
+            layer_norm_map = {
+                "0": "self_attn_layer_norm",
+                "1": "encoder_attn_layer_norm",
+                "2": "final_layer_norm",
+            }
+            for old, new in layer_norm_map.items():
+                for m in ("weight", "bias"):
+                    k = "decoder.layers.{}.layer_norms.{}.{}".format(i, old, m)
+                    if k in state_dict:
+                        state_dict[
+                            "decoder.layers.{}.{}.{}".format(i, new, m)
+                        ] = state_dict[k]
+                        del state_dict[k]
+
+        return state_dict
+
+    def reorder_incremental_state(self, incremental_state, new_order):
+        """Reorder incremental state.
+
+        This should be called when the order of the input has changed from the
+        previous time step. A typical use case is beam search, where the input
+        order changes between time steps based on the selection of beams.
+        """
+
+        def apply_reorder_incremental_state(module):
+            if module != self and hasattr(module, "reorder_incremental_state"):
+                module.reorder_incremental_state(
+                    incremental_state, new_order,
+                )
+
+        self.apply(apply_reorder_incremental_state)
+
+    def set_beam_size(self, beam_size):
+        """Sets the beam size in the decoder and all children."""
+        if getattr(self, "_beam_size", -1) != beam_size:
+
+            def apply_set_beam_size(module):
+                if module != self and hasattr(module, "set_beam_size"):
+                    module.set_beam_size(beam_size)
+
+            self.apply(apply_set_beam_size)
+            self._beam_size = beam_size
+
+    def get_normalized_probs(self, net_output, log_probs, sample):
+        """Get normalized probabilities (or log probs) from a net's output."""
+
+        if hasattr(self, "adaptive_softmax") and self.adaptive_softmax is not None:
+            assert sample is not None and "target" in sample
+            out = self.adaptive_softmax.get_log_prob(net_output[0], sample["target"])
+            return out.exp_() if not log_probs else out
+
+        logits = net_output[0]
+        if log_probs:
+            return F.log_softmax(logits, dim=-1, dtype=torch.float32)
+        else:
+            return F.softmax(logits, dim=-1, dtype=torch.float32)
