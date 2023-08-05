858757cbe478a2a50bb2125e1466d5b600fc43bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:04 +0000

    Add package kiwoom-api-handler/1.0.11/kiwoom_api_handler-1.0.11.tar.gz

---
+++ packages/kiwoom-api-handler/kiwoom_api_handler-1.0.11.tar.gz/kiwoom_api_handler-1.0.11/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="kiwoom_api_handler",
+    version="1.0.11",
+    description="a package for hanlding Kiwoom OPEN API+ ActiveX Control with python",
+    author="Donghyung Ko",
+    author_email="koko8624@gmail.com",
+    license="MIT",
+    url="https://github.com/donghyungko/kiwoom_api_handler.git",
+    download_url="https://github.com/DonghyungKo/kiwoom_api_handler/archive/master.zip",
+    install_requires=["pandas==0.25.1", "PyQt5==5.14.1"],
+    packages=find_packages(exclude=[]),
+    keywords=["Kiwoom", "Kiwoom OPEN API+", "Kiwoom API", "키움증권"],
+    python_requires=">=3.6",
+    long_description=open("README.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    package_data={},
+    zip_safe=False,
+    classifiers=[
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+    ],
+)
