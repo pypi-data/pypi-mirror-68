10fa2bd8a397d497b253beff4d6fa3c4625784cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:26 +0000

    Add package devpi-web/4.0.4/devpi-web-4.0.4.tar.gz

---
+++ packages/devpi-web/devpi-web-4.0.4.tar.gz/devpi-web-4.0.4/devpi_web/doczip.py
@@ -0,0 +1,134 @@
+from __future__ import unicode_literals
+try:
+    from UserDict import DictMixin
+except ImportError:
+    from collections.abc import MutableMapping as DictMixin
+from bs4 import BeautifulSoup
+from devpi_common.archive import Archive
+from devpi_common.types import cached_property
+from devpi_common.validation import normalize_name
+from devpi_server.log import threadlog
+import json
+import os
+import py
+import shutil
+
+
+def get_unpack_path(stage, name, version):
+    path = stage.xom.config.args.documentation_path
+    if path is None:
+        path = stage.keyfs.basedir
+    else:
+        path = py.path.local(path)
+    return path.join(
+        stage.user.name, stage.index, normalize_name(name), version, "+doc")
+
+
+def unpack_docs(stage, name, version, entry):
+    # unpack, maybe a bit uncarefully but in principle
+    # we are not loosing the original zip file anyway
+    unpack_path = get_unpack_path(stage, name, version)
+    hash_path = unpack_path.join('.hash')
+    if hash_path.exists():
+        with hash_path.open() as f:
+            if f.read().strip() == entry.hash_spec:
+                return unpack_path
+    if unpack_path.exists():
+        unpack_path.remove()
+    with entry.file_open_read() as f:
+        with Archive(f) as archive:
+            archive.extract(unpack_path)
+    with hash_path.open('w') as f:
+        f.write(entry.hash_spec)
+    threadlog.debug("%s: unpacked %s-%s docs to %s",
+                    stage.name, name, version, unpack_path)
+    return unpack_path
+
+
+class Docs(DictMixin):
+    def __init__(self, stage, name, version):
+        self.stage = stage
+        self.name = name
+        self.version = version
+        self.entry = None
+        if version is not None:
+            linkstore = stage.get_linkstore_perstage(name, version)
+            links = linkstore.get_links(rel='doczip')
+            if links:
+                self.entry = links[0].entry
+
+    def exists(self):
+        return self.entry is not None and self.entry.file_exists()
+
+    @cached_property
+    def _entries(self):
+        if not self.exists():
+            # this happens on import, when the metadata is registered, but the docs
+            # aren't uploaded yet
+            threadlog.warn("Tried to access %s, but it doesn't exist.", self.unpack_path)
+            return {}
+        unpack_path = unpack_docs(self.stage, self.name, self.version, self.entry)
+        html = []
+        fjson = []
+        for entry in unpack_path.visit():
+            basename = entry.basename
+            if basename.endswith('.fjson'):
+                fjson.append(entry)
+            elif basename.endswith('.html'):
+                html.append(entry)
+        if fjson:
+            # if there is fjson, then we get structured data
+            # see http://www.sphinx-doc.org/en/master/usage/builders/index.html#serialization-builder-details
+            return {x.relto(unpack_path)[:-6]: x for x in fjson}
+        else:
+            return {x.relto(unpack_path)[:-5]: x for x in html}
+
+    def keys(self):
+        return self._entries.keys()
+
+    def __len__(self):
+        return len(self.keys())
+
+    def __iter__(self):
+        return iter(self.keys())
+
+    def __delitem__(self, name):
+        raise NotImplementedError
+
+    def __setitem__(self, name, value):
+        raise NotImplementedError
+
+    def __getitem__(self, name):
+        entry = self._entries[name]
+        if entry.basename.endswith('.fjson'):
+            info = json.loads(entry.read())
+            return dict(
+                title=BeautifulSoup(info.get('title', ''), "html.parser").text,
+                text=BeautifulSoup(info.get('body', ''), "html.parser").text,
+                path=info.get('current_page_name', name))
+        else:
+            soup = BeautifulSoup(entry.read(mode='rb'), "html.parser")
+            body = soup.find('body')
+            if body is None:
+                return
+            title = soup.find('title')
+            if title is None:
+                title = ''
+            else:
+                title = title.text
+            return dict(
+                title=title,
+                text=body.text,
+                path=name)
+
+
+def remove_docs(stage, project, version):
+    if stage is None:
+        # the stage was removed
+        return
+    directory = get_unpack_path(stage, project, version).strpath
+    if not os.path.isdir(directory):
+        threadlog.debug("ignoring lost unpacked docs: %s" % directory)
+    else:
+        threadlog.debug("removing unpacked docs: %s" % directory)
+        shutil.rmtree(directory)
---
+++ packages/devpi-web/devpi-web-4.0.4.tar.gz/devpi-web-4.0.4/tests/test_indexing.py
@@ -0,0 +1,45 @@
+from __future__ import unicode_literals
+
+from devpi_common.archive import zip_dict
+from devpi_web.doczip import get_unpack_path
+from devpi_web.indexing import ProjectIndexingInfo
+from devpi_web.indexing import preprocess_project
+import pytest
+
+
+def test_inheritance(xom):
+    with xom.keyfs.transaction(write=True):
+        user = xom.model.create_user("one", "one")
+        prod = user.create_stage("prod")
+        prod.set_versiondata({"name": "proj", "version": "1.0"})
+        dev = user.create_stage("dev", bases=(prod.name,))
+        dev.set_versiondata({"name": "proj", "version": "1.1"})
+
+    with xom.keyfs.transaction():
+        stage = xom.model.getstage(dev.name)
+        preprocess_project(ProjectIndexingInfo(stage=stage, name="proj"))
+
+
+@pytest.mark.with_notifier
+def test_doc_unpack_cleanup(mapp, testapp):
+    api = mapp.create_and_use()
+    content = zip_dict({
+        "index.html": "<html><body>2.6</body></html>",
+        "foo.html": "<html><body>Foo</body></html>"})
+    mapp.set_versiondata({"name": "pkg1", "version": "2.6"})
+    mapp.upload_doc("pkg1.zip", content, "pkg1", "2.6", code=200,
+                    waithooks=True)
+    with mapp.xom.keyfs.transaction(write=False):
+        stage = mapp.xom.model.getstage(api.stagename)
+        path = get_unpack_path(stage, 'pkg1', '2.6')
+    testapp.xget(200, api.index + '/pkg1/2.6/+doc/foo.html')
+    assert path.join('foo.html').exists()
+    content = zip_dict({
+        "index.html": "<html><body>2.6</body></html>"})
+    mapp.upload_doc("pkg1.zip", content, "pkg1", "2.6", code=200,
+                    waithooks=True)
+    with mapp.xom.keyfs.transaction(write=False):
+        stage = mapp.xom.model.getstage(api.stagename)
+        path = get_unpack_path(stage, 'pkg1', '2.6')
+    testapp.xget(404, api.index + '/pkg1/2.6/+doc/foo.html')
+    assert not path.join('foo.html').exists()
