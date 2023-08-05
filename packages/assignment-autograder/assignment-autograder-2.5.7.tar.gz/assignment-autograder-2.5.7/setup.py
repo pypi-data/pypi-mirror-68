b0f4831441e3fe1fb271c8cac168a0f3d5ea967c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:45 +0000

    Add package assignment-autograder/2.5.7/assignment-autograder-2.5.7.tar.gz

---
+++ packages/assignment-autograder/assignment-autograder-2.5.7.tar.gz/assignment-autograder-2.5.7/setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup  # type: ignore
+
+requires = ['sh']
+
+setup(
+    name="assignment-autograder",
+    packages=["autograder"],
+    version="2.5.7",
+    install_requires=requires,
+    package_data={
+        "autograder": [
+            "default_config.ini",
+            "test_helpers/test_helper.c"
+        ],
+    },
+    entry_points={
+        'console_scripts': ['autograder=autograder.__main__:main']
+    },
+
+    # metadata to display on PyPI
+    author="Stanislav Zmiev",
+    author_email="szmiev2000@gmail.com",
+    description="Automatic assignment grading for instructor use in programming courses",
+    license="MIT",
+    project_urls={"Source Code": "https://github.com/Ovsyanka83/autograder"},
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ]
+)
