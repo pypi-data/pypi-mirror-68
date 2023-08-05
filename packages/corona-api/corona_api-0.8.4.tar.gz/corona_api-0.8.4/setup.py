aecb20b5ab79619c81c0e9ed0d545717614b168d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:15:07 +0000

    Add package corona-api/0.8.4/corona_api-0.8.4.tar.gz

---
+++ packages/corona-api/corona_api-0.8.4.tar.gz/corona_api-0.8.4/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup
+
+with open('README.md', 'r') as f:
+  long_desc = f.read()
+
+setup(
+  name = 'corona_api',
+  packages = ['corona_api'],
+  version = '0.8.4',
+  license='MIT',
+  description = 'An asynchronous wrapper for the corona.lmao.ninja API written in Python.',
+  long_description= long_desc,
+  long_description_content_type = 'text/markdown',
+  author = 'Rob Wainwright',
+  author_email = 'wainwrightbobby@gmail.com',
+  url = 'https://github.com/apex2504/python-corona-api',
+  keywords = ['coronavirus', 'covid-19'],
+  install_requires=[
+          'aiohttp',
+      ],
+  classifiers=[
+    'Development Status :: 5 - Production/Stable',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
+  ],
+)
