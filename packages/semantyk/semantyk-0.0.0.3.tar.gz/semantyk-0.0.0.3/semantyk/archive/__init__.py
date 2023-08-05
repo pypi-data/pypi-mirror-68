5d1412e0cad300351e95fe58433ea97115fcb632
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:51 +0000

    Add package semantyk/0.0.0.3/semantyk-0.0.0.3.tar.gz

---
+++ packages/semantyk/semantyk-0.0.0.3.tar.gz/semantyk-0.0.0.3/semantyk/archive/__init__.py
@@ -0,0 +1,18 @@
+##############################################
+#
+# archive 
+# Package | File System Package.
+#
+# Author: Daniel Bakas Amuchastegui
+# September 3, 2019
+# 
+# Copyright © Semantyk 2020. All rights reserved.
+#############################################
+
+from . import graph
+from . import resource
+from . import user
+
+from .graph import *
+from .resource import *
+from .user import *
