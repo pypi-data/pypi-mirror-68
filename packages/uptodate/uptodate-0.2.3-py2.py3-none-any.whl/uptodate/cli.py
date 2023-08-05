6403613bb5a37cb925edb2fc3ddeed177f569afa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:50 +0000

    Add package uptodate/0.2.3/uptodate-0.2.3-py2.py3-none-any.whl

---
+++ packages/uptodate/uptodate-0.2.3-py2.py3-none-any.whl/uptodate/cli.py
@@ -0,0 +1,31 @@
+import os
+import click
+
+try:
+    from uptodate.core import UpToDate
+except Exception:
+    from core import UpToDate  # type: ignore
+
+
+DEFAULT_FILE_PATH = 'requirements.txt'
+
+
+@click.command()
+@click.argument('file_paths', nargs=-1, type=click.Path(exists=True))
+def up_to_date(file_paths):
+    """Scan requirements.txt file for dependences which are not up to date"""
+
+    if len(file_paths) == 0:
+        if os.path.isfile(DEFAULT_FILE_PATH):
+            file_paths = (DEFAULT_FILE_PATH,)
+        else:
+            click.secho('No requirements file found', fg='yellow')
+
+    uptodate = UpToDate()
+    for file_path in file_paths:
+        click.secho(file_path, fg='blue')
+        uptodate.check(file_path)
+
+
+if __name__ == '__main__':
+    up_to_date()
