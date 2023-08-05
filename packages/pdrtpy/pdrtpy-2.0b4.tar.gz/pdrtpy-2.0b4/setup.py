29521f1df2127d9174e1d15be7265e608915d9ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:39:47 +0000

    Add package pdrtpy/2.0b4/pdrtpy-2.0b4.tar.gz

---
+++ packages/pdrtpy/pdrtpy-2.0b4.tar.gz/pdrtpy-2.0b4/setup.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+# NOTE: if any required diretories are added, put them in MANIFEST.in or
+# readthedocs build will break
+
+from setuptools import setup, find_packages,find_namespace_packages
+
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
+#print(find_namespace_packages())
+
+#excludelist= ["build","dist"]
+excludelist= []
+print("Found packages ",find_packages(exclude=excludelist))
+
+
+setup(
+    name="pdrtpy",
+    version="2.0b4",
+    author = "Marc W. Pound",
+    author_email  = "mpound@umd.edu",
+    description="PhotoDissociation Region Toolbox (PDRT), astrophysics analysis tools",
+    keywords="PDR photodissociation",
+    long_description=readme(),
+    packages=find_packages(exclude=excludelist),
+    include_package_data = True,
+    install_requires = [
+        'astropy',
+        'numpy',
+        'scipy',
+        'matplotlib',
+    ],
+    url="https://dustem.astro.umd.edu",
+    project_urls={
+        "Documentation": "https://pdrtpy.readthedocs.io",
+        "Source Code": "https://github.com/mpound/pdrtpy",
+    },
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Topic :: Scientific/Engineering :: Astronomy",
+        "Intended Audience :: Science/Research",
+    ],
+    license = "GPLv3",
+    zip_safe=False,
+    python_requires='>=3.6'
+)
