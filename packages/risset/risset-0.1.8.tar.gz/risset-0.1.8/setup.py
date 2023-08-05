ed88d6f53f422c40a75ed03c4b1f2f2e3141076b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:11 +0000

    Add package risset/0.1.8/risset-0.1.8.tar.gz

---
+++ packages/risset/risset-0.1.8.tar.gz/risset-0.1.8/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup
+import subprocess
+import sys
+
+if (sys.version_info.major, sys.version_info.minor) < (3, 6):
+    print("Python >= 3.6 required")
+    sys.exit(-1)
+
+long_description = open("README.md").read()
+version = subprocess.getoutput("python3 ./risset.py --version")
+
+setup(
+    name = "risset",
+    version = version,
+    python_requires = ">=3.6",
+    description = "A package manager for csound",
+    author = "Eduardo Moguillansky",
+    author_email = "eduardo.moguillansky@gmail.com",
+    long_description = long_description,
+    long_description_content_type = 'text/markdown',
+    py_modules=["risset"],
+
+    url="https://github.com/csound-plugins/risset",
+
+    entry_points={
+        "console_scripts": [
+            "risset=risset:main",
+        ]
+    }
+
+)
