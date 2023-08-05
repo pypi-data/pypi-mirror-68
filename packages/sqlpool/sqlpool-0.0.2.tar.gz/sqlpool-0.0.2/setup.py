bcb0b33c7346921edcda914e9b77c2bef42f6b5a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:42:57 +0000

    Add package sqlpool/0.0.2/sqlpool-0.0.2.tar.gz

---
+++ packages/sqlpool/sqlpool-0.0.2.tar.gz/sqlpool-0.0.2/setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages, setup
+setup(
+    name='sqlpool',
+    version='0.0.2',
+    description='mysql,sqlserver,oracle poll',
+    author='zhaozhi',
+    author_email='13546120129@sina.cn',
+    url='https://github.com/szmgithubcode/',
+    #packages=find_packages(),
+    packages=['sqlpool'],
+    #install_requires=['requests'],
+)
