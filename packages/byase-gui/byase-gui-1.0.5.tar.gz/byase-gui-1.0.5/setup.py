d4bc3d13c6b8bc1fe6ab1d214312d5f887e4952b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:13:44 +0000

    Add package byase-gui/1.0.5/byase-gui-1.0.5.tar.gz

---
+++ packages/byase-gui/byase-gui-1.0.5.tar.gz/byase-gui-1.0.5/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="byase-gui",
+    version="1.0.5",
+    author="Lili Dong",
+    author_email="ncjllld@hit.edu.cn",
+    description="A GUI tool for BYASE software.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ncjllld/byase_gui",
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    entry_points={
+        'console_scripts': [
+            'byase-gui=byase_gui.gui:main'],
+    },
+)
