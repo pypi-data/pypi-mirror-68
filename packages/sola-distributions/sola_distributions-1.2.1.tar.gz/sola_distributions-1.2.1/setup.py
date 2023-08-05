1711045133f9a225e8e39d72b64ff66214fcae27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:48 +0000

    Add package sola-distributions/1.2.1/sola_distributions-1.2.1.tar.gz

---
+++ packages/sola-distributions/sola_distributions-1.2.1.tar.gz/sola_distributions-1.2.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='sola_distributions',
+      version='1.2.1',
+      description='Gaussian distributions',
+      packages=['sola_distributions'],
+      zip_safe=False)
