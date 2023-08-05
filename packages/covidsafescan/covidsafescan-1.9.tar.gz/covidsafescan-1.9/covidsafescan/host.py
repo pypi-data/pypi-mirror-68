8804f3d347985c1e2d708167d24eb4458b263771
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:39:21 +0000

    Add package covidsafescan/1.9/covidsafescan-1.9.tar.gz

---
+++ packages/covidsafescan/covidsafescan-1.9.tar.gz/covidsafescan-1.9/covidsafescan/host.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+import bleak
+import asyncio
+from twisted.internet.asyncioreactor import AsyncioSelectorReactor
+import traceback
+import argparse
+import sys
+import datetime
+import json
+
