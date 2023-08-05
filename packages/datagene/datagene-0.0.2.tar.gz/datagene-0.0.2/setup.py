e65996944c683561f22092923d079410e98a90cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:04 +0000

    Add package datagene/0.0.2/datagene-0.0.2.tar.gz

---
+++ packages/datagene/datagene-0.0.2.tar.gz/datagene-0.0.2/setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup, Command
+import os
+import sys
+
+
+setup(name='datagene',
+      version='0.0.2',
+      description='Data Comparison Toolbox with Transformation and Similarity Analysis',
+      url='https://github.com/firmai/datagene',
+      author='snowde',
+      author_email='d.snow@firmai.org',
+      license='MIT',
+      packages=['datagene'],
+      install_requires=[
+            "pandasvault",
+            "scikit-image==0.15"
+            "git+git://github.com/FirmAI-Research/ecopy.git",
+            "scikit-image",
+            "deltapy",
+            "esig",
+            "ImageHash",
+            "porespy"
+            "shap",
+            "pyts"
+            "tensorly",
+            "pandas",
+            "scikit-learn",
+            "scipy",
+            "statsmodels",
+            "numpy",
+            "google",
+            "tensorflow",
+            "keras",
+            "opencv-python"
+            
+            
+            ],
+      zip_safe=False)
