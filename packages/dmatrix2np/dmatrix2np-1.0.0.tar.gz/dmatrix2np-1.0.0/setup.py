fec1727269d73b60d15aaba5ac013423411d3ad5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:39 +0000

    Add package dmatrix2np/1.0.0/dmatrix2np-1.0.0.tar.gz

---
+++ packages/dmatrix2np/dmatrix2np-1.0.0.tar.gz/dmatrix2np-1.0.0/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['dmatrix2np']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['numpy']
+
+setup_kwargs = {
+    'name': 'dmatrix2np',
+    'version': '1.0.0',
+    'description': "Convert XGBoost's DMatrix to numpy array",
+    'long_description': None,
+    'author': 'Aporia technologies ltd',
+    'author_email': 'dev@aporia.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.6.1,<4.0.0',
+}
+
+
+setup(**setup_kwargs)
