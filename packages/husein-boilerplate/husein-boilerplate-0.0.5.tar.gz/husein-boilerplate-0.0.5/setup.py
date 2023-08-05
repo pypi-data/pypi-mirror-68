4d066c8d4327c02df5e27a97aab6f64453dcc7a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:26 +0000

    Add package husein-boilerplate/0.0.5/husein-boilerplate-0.0.5.tar.gz

---
+++ packages/husein-boilerplate/husein-boilerplate-0.0.5.tar.gz/husein-boilerplate-0.0.5/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+
+__packagename__ = 'husein-boilerplate'
+
+setuptools.setup(
+    name = __packagename__,
+    packages = setuptools.find_packages(),
+    version = '0.0.5',
+    python_requires = '>=3.6.*',
+    description = 'Boilerplate for Husein',
+    author = 'huseinzol05',
+    author_email = 'husein.zol05@gmail.com',
+    url = 'https://github.com/huseinzol05/husein-boilerplate',
+    install_requires = ['emoji', 'gensim'],
+    license = 'MIT',
+    classifiers = [
+        'Programming Language :: Python :: 3.7',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
