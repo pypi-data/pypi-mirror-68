0b5883d18c97faa2b63aafa31f91493b4b24d1cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:42:40 +0000

    Add package gtaasmysql/1.1.1/gtaasmysql-1.1.1.tar.gz

---
+++ packages/gtaasmysql/gtaasmysql-1.1.1.tar.gz/gtaasmysql-1.1.1/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name='gtaasmysql',
+    version='1.1.1',
+    py_modules=['gtaasmysql'],
+    author='WangFei',
+    author_email='qinexpire@gmail.com',
+    url='https://blog.csdn.net/andrew_wf',
+    description='Delete function update and rename function insert to save'
+)
