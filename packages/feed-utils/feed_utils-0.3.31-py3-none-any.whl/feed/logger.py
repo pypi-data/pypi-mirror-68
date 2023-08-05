4cf04d1b48c003b81b6c6fdca540f571fc428bec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:00 +0000

    Add package feed-utils/0.3.31/feed_utils-0.3.31-py3-none-any.whl

---
+++ packages/feed-utils/feed_utils-0.3.31-py3-none-any.whl/feed/logger.py
@@ -0,0 +1,28 @@
+import logging
+import sys
+import os
+
+logger = logging.getLogger(__name__)
+ch = logging.StreamHandler(stream=sys.stderr)
+ch.setLevel(os.getenv("LOG_LEVEL", "INFO"))
+
+logger.setLevel(os.getenv("LOG_LEVEL", "INFO"))
+
+def getLogger(name, toFile=False):
+    logger = logging.getLogger(name)
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s -%(message)s')
+    ch.setLevel(os.getenv("LOG_LEVEL", "DEBUG"))
+    ch.setFormatter(formatter)
+    logger.setLevel(os.getenv("LOG_LEVEL", "INFO"))
+
+    # logger.addHandler(ch)
+    if not os.path.exists('/tmp/logs/'):
+        os.mkdir('/tmp/logs')
+    if toFile:
+        fileHandler = logging.FileHandler(f'/tmp/logs/{name}.log')
+        fileHandler.setFormatter(formatter)
+        fileHandler.setLevel(os.getenv('FILE_LOGLEVEL', 'INFO'))
+        logger.addHandler(fileHandler)
+    return logger
+
+
