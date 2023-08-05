aaac0e99b36559573f63b5cd68f71a36bb7eed04
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:31 +0000

    Add package pymachinetalk/0.12.3/pymachinetalk-0.12.3.tar.gz

---
+++ packages/pymachinetalk/pymachinetalk-0.12.3.tar.gz/pymachinetalk-0.12.3/setup.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+import sys
+
+# We must use setuptools, not distutils, because we need to use the
+# namespace_packages option for the "google" package.
+try:
+    from setuptools import setup, Extension, find_packages
+except ImportError:
+    try:
+        from ez_setup import use_setuptools
+
+        use_setuptools()
+        from setuptools import setup, Extension, find_packages
+    except ImportError:
+        sys.stderr.write(
+            "Could not import setuptools; make sure you have setuptools or "
+            "ez_setup installed.\n"
+        )
+        raise
+
+from distutils.command.clean import clean
+
+if sys.version_info[0] == 3:
+    # Python 3
+    from distutils.command.build_py import build_py_2to3 as build_py
+else:
+    # Python 2
+    from distutils.command.build_py import build_py as build_py
+
+requirements = ['pyzmq', 'protobuf', 'machinetalk-protobuf', 'fysom', 'six>=1.12.0']
+if sys.version_info <= (3, 3):
+    requirements.append('zeroconf<=0.19.1')  # freeze version
+else:
+    requirements.append('zeroconf')
+
+if __name__ == '__main__':
+    setup(
+        name="pymachinetalk",
+        version="0.12.3",
+        description="Python bindings for Machinetalk",
+        author="Alexander Roessler",
+        author_email="alex@machinekoder.com",
+        url="https://github.com/machinekit/pymachinetalk",
+        namespace_packages=['pymachinetalk'],
+        packages=find_packages(),
+        install_requires=requirements,
+        extras_require={'dev': ['pytest', 'pytest-mock', 'pytest-pep8', 'pytest-cov']},
+        cmdclass={'clean': clean, 'build_py': build_py},
+    )
