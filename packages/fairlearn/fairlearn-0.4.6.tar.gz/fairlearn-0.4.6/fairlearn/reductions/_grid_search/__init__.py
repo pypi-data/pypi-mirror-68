35d05d5bd6ed6985b011321020dadcc03495e91c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:39 +0000

    Add package fairlearn/0.4.6/fairlearn-0.4.6.tar.gz

---
+++ packages/fairlearn/fairlearn-0.4.6.tar.gz/fairlearn-0.4.6/fairlearn/reductions/_grid_search/__init__.py
@@ -0,0 +1,10 @@
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License.
+
+"""Package for Grid Search."""
+
+from .grid_search import GridSearch  # noqa: F401
+
+__all__ = [
+    "GridSearch"
+]
