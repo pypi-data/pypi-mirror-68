e53d590afcf5139116aecf02f87d17cd94da4800
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:15:12 +0000

    Add package eniam/0.13/eniam-0.13.tar.gz

---
+++ packages/eniam/eniam-0.13.tar.gz/eniam-0.13/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+import setuptools
+
+# read the contents of your README file
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md')) as f:
+    long_description = f.read()
+
+setup(
+    name='eniam',
+    version='0.13',
+    license='MIT',
+    author='styczynski',
+    author_email='piotr@styczynski.in',
+    url='http://styczynski.in',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    packages=setuptools.find_packages(),
+    scripts=['bin/eniam-cli'],
+    include_package_data=True,
+    package_data={},
+    description="Cross-platform python wrapper around ENIAM (http://eniam.nlp.ipipan.waw.pl/)",
+)
