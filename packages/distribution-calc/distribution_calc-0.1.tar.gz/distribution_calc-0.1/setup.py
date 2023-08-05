a575515a74c1f5360c60da52d73ec8f5e8a2d31b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:14:57 +0000

    Add package distribution-calc/0.1/distribution_calc-0.1.tar.gz

---
+++ packages/distribution-calc/distribution_calc-0.1.tar.gz/distribution_calc-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='distribution_calc',
+      version='0.1',
+      description='Gaussian & Binomial distribution calculations',
+      packages=['distribution_calc'],
+      author = 'Helen Batson',
+      author_email = 'helenbatson@gmail.com',
+      zip_safe=False)
