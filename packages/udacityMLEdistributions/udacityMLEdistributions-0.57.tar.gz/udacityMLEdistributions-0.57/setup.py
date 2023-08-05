54aed8d44f0f344783de7bb6cbb1d64edea89356
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:13:42 +0000

    Add package udacityMLEdistributions/0.57/udacityMLEdistributions-0.57.tar.gz

---
+++ packages/udacityMLEdistributions/udacityMLEdistributions-0.57.tar.gz/udacityMLEdistributions-0.57/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='udacityMLEdistributions',
+      version='0.57',
+      description='Gaussian distributions',
+      packages=['udacityMLEdistributions'],
+      zip_safe=False)
