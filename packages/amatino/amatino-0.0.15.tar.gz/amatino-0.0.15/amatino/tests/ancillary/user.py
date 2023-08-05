91bb663e94ed85e9f521852b3638c09db698f4ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:44 +0000

    Add package amatino/0.0.15/amatino-0.0.15.tar.gz

---
+++ packages/amatino/amatino-0.0.15.tar.gz/amatino-0.0.15/amatino/tests/ancillary/user.py
@@ -0,0 +1,50 @@
+"""
+Amatino API Python Bindings
+User Test Module
+Author: hugh@amatino.io
+"""
+from amatino import User
+from amatino.tests.ancillary.session import SessionTest
+from urllib.error import HTTPError
+
+
+class UserTest(SessionTest):
+    """Test the User ancillary object"""
+
+    def __init__(self, name='Create, retrieve, delete Users') -> None:
+
+        super().__init__(name)
+        self.create_session()
+        return
+
+    def execute(self) -> None:
+
+        try:
+            user = User.retrieve(self.session, self.user_id)
+            assert isinstance(user, User), 'retrieved is User'
+
+            new_user = User.create(
+                session=self.session,
+                secret='what a great passphrase',
+                handle='Dick Cheney'
+            )
+            assert isinstance(new_user, User), 'new user is User'
+
+            new_user.delete()
+
+            was_deleted = False
+            try:
+                User.retrieve(self.session, new_user.id_)
+            except HTTPError as error:
+                if error.code == 404:
+                    was_deleted = True
+
+        except Exception as error:
+            self.record_failure(error)
+            return
+
+        if was_deleted is False:
+            self.record_failure('New user not deleted')
+
+        self.record_success()
+        return
