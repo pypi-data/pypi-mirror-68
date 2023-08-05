e144f13cd8c714e59a67042ca760563b88063727
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:35 +0000

    Add package dsnd-dsitributions-pri/1.0/dsnd_dsitributions_pri-1.0.tar.gz

---
+++ packages/dsnd-dsitributions-pri/dsnd_dsitributions_pri-1.0.tar.gz/dsnd_dsitributions_pri-1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='dsnd_dsitributions_pri',
+      version='1.0',
+      description='Gaussian distributions and Binomial distributions',
+      packages=['dsnd_dsitributions_pri'],
+      author="Priadi",
+      author_email="priaditeguhw@gmail.com",
+      zip_safe=False)
