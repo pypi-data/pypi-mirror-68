5efd8b4439e56dbdf1a85f461be1bc4a4278db03
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:41:01 +0000

    Add package search-engines/1.0.1/search_engines-1.0.1.tar.gz

---
+++ packages/search-engines/search_engines-1.0.1.tar.gz/search_engines-1.0.1/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+from pathlib import Path
+
+
+setuptools.setup(
+    name="search_engines",
+    version="1.0.1",
+    author="Dan Kelleher",
+    author_email="danielkelleher@protonmail.com",
+    maintainer='Dan Kelleher',
+    maintainer_email='danielkelleher@protonmail.com',
+    description="Query and scrape search engines.",
+    packages=["search_engines"],
+    url="https://github.com/djkelleher/search_engines",
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        'pyppeteer_spider',
+        'aiohttp'
+    ]
+)
