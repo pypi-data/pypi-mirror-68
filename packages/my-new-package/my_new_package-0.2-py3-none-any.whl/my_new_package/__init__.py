603aca8d008e368aaf09e1ad5b8cdd8cd4bfb746
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:28 +0000

    Add package my-new-package/0.2/my_new_package-0.2-py3-none-any.whl

---
+++ packages/my-new-package/my_new_package-0.2-py3-none-any.whl/my_new_package/__init__.py
@@ -0,0 +1,10 @@
+from . import hoge
+
+__copyright__    = 'Copyright (C) 2018 Your Name'
+__version__      = '1.0.0'
+__license__      = 'BSD-3-Clause'
+__author__       = 'Your Name'
+__author_email__ = 'Your@Email'
+__url__          = 'http://github.com/account/repository'
+
+__all__ = ['hoge']
