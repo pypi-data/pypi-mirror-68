4aa015a455549f26a7eeb5a3dfe7b141d722e6f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:00 +0000

    Add package craft-parser/0.1/craft-parser-0.1.tar.gz

---
+++ packages/craft-parser/craft-parser-0.1.tar.gz/craft-parser-0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='craft-parser', 
+      version='0.1',
+      description='A script to analyze parts of a Kerbal Space Program .craft file',
+      author='Rahul Menon',
+      url='https://github.com/rmeno12/craft-parser',
+      license='MIT',
+      packages=['craft-parser'],
+      zip_safe=False)
