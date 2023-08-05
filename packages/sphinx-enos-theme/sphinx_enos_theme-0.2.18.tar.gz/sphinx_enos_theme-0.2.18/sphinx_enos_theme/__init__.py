6218a94654b6e2bd0a026572ed02f3cdd36cdde0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:02 +0000

    Add package sphinx-enos-theme/0.2.18/sphinx_enos_theme-0.2.18.tar.gz

---
+++ packages/sphinx-enos-theme/sphinx_enos_theme-0.2.18.tar.gz/sphinx_enos_theme-0.2.18/sphinx_enos_theme/__init__.py
@@ -0,0 +1,19 @@
+"""Sphinx ENOS theme.
+
+From https://github.com/TRoam/sphinx-enos-theme.
+
+"""
+from os import path
+
+__version__ = '0.2.18'
+__version_full__ = __version__
+
+
+def get_html_theme_path():
+    """Return list of HTML theme paths."""
+    cur_dir = path.abspath(path.dirname(path.dirname(__file__)))
+    return cur_dir
+
+# See http://www.sphinx-doc.org/en/stable/theming.html#distribute-your-theme-as-a-python-package
+def setup(app):
+    app.add_html_theme('sphinx_enos_theme', path.abspath(path.dirname(__file__)))
