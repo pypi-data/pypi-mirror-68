7cb3dc6f9dfec9c0f5fe051ddc5633c8a1300cea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:04 +0000

    Add package jsys/1.0/jsys-1.0.tar.gz

---
+++ packages/jsys/jsys-1.0.tar.gz/jsys-1.0/my_app/main.py
@@ -0,0 +1,6 @@
+from time import sleep
+
+x = input('Podaj imie')
+
+print(f'Cxesc {x}')
+sleep(3)
---
+++ packages/jsys/jsys-1.0.tar.gz/jsys-1.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(
+name='jsys',
+version='1.0',
+description='Opis',
+packages=find_packages()
+)
