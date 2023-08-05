4082bc62f89e5a1bad79fe2f93f0adf78452d06f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:13:32 +0000

    Add package myprss/0.1.4/myprss-0.1.4.tar.gz

---
+++ packages/myprss/myprss-0.1.4.tar.gz/myprss-0.1.4/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['myprss']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['click>=7.1.2,<8.0.0', 'defusedxml>=0.6.0,<0.7.0', 'requests>=2.23.0,<3.0.0']
+
+setup_kwargs = {
+    'name': 'myprss',
+    'version': '0.1.4',
+    'description': 'rss reader for terminal',
+    'long_description': None,
+    'author': 'rarnal',
+    'author_email': 'arnal.romain@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
