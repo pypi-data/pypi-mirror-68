65a55239c33ec9d3631c24ac88ecd3766bf9ddea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:14:47 +0000

    Add package pepephone-data/1.0.5/pepephone-data-1.0.5.tar.gz

---
+++ packages/pepephone-data/pepephone-data-1.0.5.tar.gz/pepephone-data-1.0.5/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='pepephone-data',
+    version='1.0.5',
+    author='Carles Pina i Estany',
+    author_email='carles@pina.cat',
+    description='Shows Pepephone used and available data',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/cpina/pepephone-data',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+    install_requires=['requests'],
+    scripts=['bin/pepephone-data']
+)
