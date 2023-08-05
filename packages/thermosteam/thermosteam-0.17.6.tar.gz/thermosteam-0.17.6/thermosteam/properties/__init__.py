d0f7a83b9d88caecdaaf71d28871dad9917ccdae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:24 +0000

    Add package thermosteam/0.17.6/thermosteam-0.17.6.tar.gz

---
+++ packages/thermosteam/thermosteam-0.17.6.tar.gz/thermosteam-0.17.6/thermosteam/properties/__init__.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 30 12:07:13 2019
+
+@author: yoelr
+"""
+from . import (acentric,
+               combustion,
+               electrolyte_conductivity,
+               critical,
+               dipole,
+               dippr,
+               electrolytes,
+               elements,
+               eos,
+               heat_capacity,
+               identifiers,
+               lennard_jones,
+               miscdata,
+               permittivity,
+               phase_change,
+               reaction,
+               interface,
+               thermal_conductivity,
+               triple,
+               utils,
+               vapor_pressure,
+               virial,
+               viscosity,
+               volume)
+
+__all__ = ('combustion', 'electrolyte_conductivity', 'critical', 'dipole',
+           'electrolytes', 'elements', 
+           'eos', 'heat_capacity',  'identifiers', 
+           'lennard_jones', 'miscdata',
+           'permittivity', 'phase_change',  'reaction', 
+           'interface', 'thermal_conductivity', 'triple', 
+           'utils', 'vapor_pressure', 'virial',
+		   'viscosity', 'volume', 'acentric', 
+           'dippr',)
+
+
