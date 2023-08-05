97402a20be465c87efb81ccf6b227feae3708189
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:38 +0000

    Add package dkh-pixel/0.1/dkh_pixel-0.1.tar.gz

---
+++ packages/dkh-pixel/dkh_pixel-0.1.tar.gz/dkh_pixel-0.1/README.md
@@ -0,0 +1,7 @@
+# Тестовый пакет
+
+Включает в себя игру Астероид 1.0.
+Ссылка на GitHub:
+[Github-flavored Markdown](https://guides.github.com/features/mastering-markdown/)
+
+Проект создавался для обучения программированию.
---
+++ packages/dkh-pixel/dkh_pixel-0.1.tar.gz/dkh_pixel-0.1/setup.py
@@ -0,0 +1,23 @@
+
+import setuptools
+
+with open("README.md") as rdm:
+    longDiscr = rdm.read()
+    
+setuptools.setup(
+    name="dkh_pixel",
+    version="0.1",
+    author="Дмитрий Холостов",
+    author_email="dkh@ro.ru",
+    description="Библиотека для создания пиксельной графики в Python. Использует Tkinter.",
+    long_description=longDiscr,
+    long_description_content_type="text/markdown",
+    url="https://github.com/dkh-gh/pixel_python",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
