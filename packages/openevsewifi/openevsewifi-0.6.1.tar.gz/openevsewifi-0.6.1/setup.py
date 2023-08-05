42d7a8e045463245879f2f59dbfb9d7fe1934e16
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:02 +0000

    Add package openevsewifi/0.6.1/openevsewifi-0.6.1.tar.gz

---
+++ packages/openevsewifi/openevsewifi-0.6.1.tar.gz/openevsewifi-0.6.1/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['openevsewifi']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['pytest-cov>=2.8.1,<3.0.0', 'requests>=2.23.0,<3.0.0']
+
+setup_kwargs = {
+    'name': 'openevsewifi',
+    'version': '0.6.1',
+    'description': 'A python library for communicating with the ESP8266-based wifi module from OpenEVSE',
+    'long_description': '# python-openevse-wifi\nA python library for communicating with the ESP8266-based wifi module from OpenEVSE\n',
+    'author': 'Michelle Avery',
+    'author_email': 'dev@miniconfig.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://github.com/miniconfig/python-openevse-wifi',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.5,<4.0',
+}
+
+
+setup(**setup_kwargs)
