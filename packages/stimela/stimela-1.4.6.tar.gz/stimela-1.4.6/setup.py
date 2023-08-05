e8c4aba62fdc51aa591eacad2c7a4504b9f6640c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:20 +0000

    Add package stimela/1.4.6/stimela-1.4.6.tar.gz

---
+++ packages/stimela/stimela-1.4.6.tar.gz/stimela-1.4.6/setup.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+
+import os
+import sys
+from setuptools import setup
+import glob
+
+
+requirements = ["pyyaml",
+                "nose>=1.3.7",
+                "future-fstrings",
+                "udocker",
+                ],
+
+PACKAGE_NAME = "stimela"
+__version__ = "1.4.6"
+
+setup(name=PACKAGE_NAME,
+      version=__version__,
+      description="Dockerized radio interferometry scripting framework",
+      author="Sphesihle Makhathini",
+      author_email="sphemakh@gmail.com",
+      url="https://github.com/sphemakh/Stimela",
+      packages=["stimela", "stimela/cargo",
+                "stimela/utils", "stimela/cargo/cab",
+                "stimela/cargo/base"],
+      package_data={"stimela/cargo": [
+          "cab/*/Dockerfile",
+          "base/*/Dockerfile",
+          "cab/*/src/*.py",
+          "cab/*/src/*.sh",
+          "cab/*/src/*.json",
+          "base/*/xvfb.init.d",
+          "cab/*/xvfb.init.d",
+          "cab/*/parameters.json",
+          "cab/*/src/tdlconf.profiles",
+      ]},
+      install_requires=requirements,
+      scripts=["bin/" + i for i in os.listdir("bin")] + 
+                glob.glob("stimela/cargo/cab/stimela_runscript"),
+      classifiers=[],
+      )
