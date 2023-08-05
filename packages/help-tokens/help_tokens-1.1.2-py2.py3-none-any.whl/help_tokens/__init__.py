e2ac8c5a5ff6ff1c409a5f37c2873a3db61cb7cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:05 +0000

    Add package help-tokens/1.1.2/help_tokens-1.1.2-py2.py3-none-any.whl

---
+++ packages/help-tokens/help_tokens-1.1.2-py2.py3-none-any.whl/help_tokens/__init__.py
@@ -0,0 +1,11 @@
+"""
+Django app for linking to help pages with short tokens.
+"""
+
+from __future__ import absolute_import, unicode_literals
+
+from .context_processor import context_processor
+
+__version__ = '1.1.2'
+
+default_app_config = 'help_tokens.apps.HelpTokensConfig'  # pylint: disable=invalid-name
