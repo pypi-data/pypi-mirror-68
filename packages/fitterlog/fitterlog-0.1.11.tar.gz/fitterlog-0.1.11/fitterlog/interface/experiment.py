3d597638a68bc0e4773c58b513991c91bead11cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:42:35 +0000

    Add package fitterlog/0.1.11/fitterlog-0.1.11.tar.gz

---
+++ packages/fitterlog/fitterlog-0.1.11.tar.gz/fitterlog-0.1.11/fitterlog/interface/experiment.py
@@ -0,0 +1,63 @@
+from ..sql_proxy import Experiment 		as Core_Experiment
+from ..sql_proxy import ExperimentGroup as Core_Group
+from .others import Variable
+from ..quit import add_quit_process
+
+this_experiment = None
+
+def new_or_load_experiment(group_id = None, group_name = None, project_id = None, project_name = None , force_new = False):
+	global this_experiment
+	if this_experiment is not None:
+		if not force_new:
+			return this_experiment
+
+	new_exp = Experiment(group_id, group_name, project_id, project_name)
+	this_experiment = new_exp
+
+	return new_exp
+
+
+class Experiment:
+	def __init__(self , group_id = None, group_name = None, project_id = None, project_name = None):
+		
+		self.get_core(group_id , group_name , project_id , project_name)
+		self.variables = {}
+		self.add_line = self.write_log
+
+	def get_core(self , group_id = None, group_name = None, project_id = None, project_name = None):
+
+		if (group_id is None) and (group_name is None):
+			group_name = "default"
+		if (project_id is None) and (project_name is None):
+			project_name = "default"
+		self.core = Core_Experiment(group = Core_Group.find(group_id , group_name , project_id , project_name))
+
+	def finish(self):
+		from django.utils import timezone
+		self.core.state = 1
+		self.core.end_time = timezone.now()
+
+	def write_log(self , content = ""):
+		content = str(content) + "\n"
+		self.core.logs = self.core.logs + content
+
+	def new_variable(self , name , type = str , default = "None" , merge_func = None , editable = False):
+		self.variables[name] = Variable(self , name , type , default , merge_func , editable)
+
+	def use_argument_proxy(self , arg_prox , args = None):
+		C = arg_prox.assign_from_cmd(args)
+		for arg in arg_prox.args:
+			self.new_variable(arg.name , arg.type , str(C.__dict__[arg.name]) , editable = arg.editable)
+
+	def __getitem__(self , name):
+		return self.variables[name]
+
+
+
+def make_exp_state_on_quit():
+	if this_experiment is not None:
+		if this_experiment.core.state == 0:
+			print ("unexpected quit!!!")
+			this_experiment.core.state = 3 #unexpected quit
+
+add_quit_process(make_exp_state_on_quit)
---
+++ packages/fitterlog/fitterlog-0.1.11.tar.gz/fitterlog-0.1.11/fitterlog_server_module/experiment/views/displays.py
@@ -0,0 +1,59 @@
+from django.shortcuts import render
+from django.http import HttpResponse , Http404
+from ..models import Project , ExperimentGroup , Experiment
+from ..models import Variable , VariableTrack
+from .base import get_path
+from ..utils.str_opt import seped_s2list , seped_list2s
+
+def index(request):
+	context = {
+		"projects": Project.objects.all() , 
+	}
+	return render(request , get_path("index") , context)
+
+
+def project(request , project_id):
+	from .group_opt.group_sort import group_sort
+
+	project = Project.objects.get(id = project_id)
+
+	context = {
+		"project": project , 
+		"groups": group_sort(project.groups.all()) , 
+	}
+	return render(request , get_path("project") , context)
+
+
+
+def experiment(request , experiment_id):
+
+	experiment = Experiment.objects.get(id = experiment_id)
+
+	context = {
+		"experiment": experiment , 
+		"variables": experiment.variables.all() , 
+	}
+	return render(request , get_path("experiment/experiment") , context)
+
+
+
+def experiment_log(request , experiment_id):
+
+	experiment = Experiment.objects.get(id = experiment_id)
+
+	context = {
+		"experiment": experiment , 
+		"logs": experiment.logs , 
+	}
+	return render(request , get_path("experiment/logs") , context)
+
+
+def track(request , track_id):
+
+	track = VariableTrack.objects.get(id = track_id)
+
+	context = {
+		"track": track , 
+		"values": track.values.all() , 
+	}
+	return render(request , get_path("track") , context)
