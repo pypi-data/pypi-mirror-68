d4675574e1ec8133199d47f2813a7ce68e67f726
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:03 +0000

    Add package miepy/0.5.0/miepy-0.5.0-cp38-cp38-manylinux2010_x86_64.whl

---
+++ packages/miepy/miepy-0.5.0-cp38-cp38-manylinux2010_x86_64.whl/miepy/vsh/special.py
@@ -0,0 +1,1 @@
+from miepy.cpp.special import *
---
+++ packages/miepy/miepy-0.5.0-cp38-cp38-manylinux2010_x86_64.whl/miepy/vsh/vsh_translation.py
@@ -0,0 +1,9 @@
+"""
+VSH translation function
+"""
+
+import numpy as np
+from math import factorial
+from miepy import vsh
+from functools import partial
+from miepy.cpp.vsh_translation import vsh_translation
