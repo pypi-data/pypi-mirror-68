2077064cb4d442780c6c344222ac837582c9563d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:21 +0000

    Add package IOTAssignmentUtilitiesdorachua/0.0.12/IOTAssignmentUtilitiesdorachua-0.0.12.tar.gz

---
+++ packages/IOTAssignmentUtilitiesdorachua/IOTAssignmentUtilitiesdorachua-0.0.12.tar.gz/IOTAssignmentUtilitiesdorachua-0.0.12/IOTAssignmentUtilitiesdorachua/MyDateTimeUtilities.py
@@ -0,0 +1,7 @@
+from datetime import datetime,timedelta
+
+class MyDateTimeUtilities():
+
+    def date_diff_in_seconds(self,dt2, dt1):
+        timedelta = dt2 - dt1
+        return timedelta.days * 24 * 3600 + timedelta.seconds
