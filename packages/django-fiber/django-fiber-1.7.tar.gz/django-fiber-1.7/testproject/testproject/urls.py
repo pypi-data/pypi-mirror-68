28f49113a062ff251def3b309db95cff2ff14994
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:27:59 +0000

    Add package django-fiber/1.7/django-fiber-1.7.tar.gz

---
+++ packages/django-fiber/django-fiber-1.7.tar.gz/django-fiber-1.7/testproject/testproject/urls.py
@@ -0,0 +1,24 @@
+from django.conf import settings
+from django.conf.urls import include, url
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.http import HttpResponse
+
+from fiber.views import page
+
+admin.autodiscover()
+
+urlpatterns = [
+    url(r'^api/v2/', include('fiber.rest_api.urls')),
+    url(r'^admin/fiber/', include('fiber.admin_urls')),
+
+    url(r'^admin/', admin.site.urls),
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns += [
+    url(r'^empty/$', lambda request: HttpResponse('<!doctype html><html><head></head><body></body></html>')),
+    url(r'', page),
+]
