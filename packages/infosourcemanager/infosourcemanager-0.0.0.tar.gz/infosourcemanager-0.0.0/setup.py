1aa83bc91be2e60ea6df105c0037613ab7138769
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:39:47 +0000

    Add package infosourcemanager/0.0.0/infosourcemanager-0.0.0.tar.gz

---
+++ packages/infosourcemanager/infosourcemanager-0.0.0.tar.gz/infosourcemanager-0.0.0/setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="infosourcemanager",
+    version="0.0.0",
+    author="Tim",
+    author_email="",
+    description="infosourcemanager",
+    long_description="infosourcemanager",
+    long_description_content_type="text/markdown",
+    url="",
+    packages=setuptools.find_packages(),
+    classifiers=[],
+    python_requires='>=3.0',
+)
