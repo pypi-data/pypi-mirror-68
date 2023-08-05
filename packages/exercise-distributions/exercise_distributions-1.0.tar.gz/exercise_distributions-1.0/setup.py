837c476574eba3ed3cd54e6287c436f99e97bf6d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:14:47 +0000

    Add package exercise-distributions/1.0/exercise_distributions-1.0.tar.gz

---
+++ packages/exercise-distributions/exercise_distributions-1.0.tar.gz/exercise_distributions-1.0/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='exercise_distributions',
+      version='1.0',
+      description='Gaussian distributions',
+      packages=['exercise_distributions'],
+      zip_safe=False)
