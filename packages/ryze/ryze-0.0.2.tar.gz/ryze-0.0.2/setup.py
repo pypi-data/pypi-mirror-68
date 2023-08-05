8378739a702099584a42ffe2c99ab440aa27d1e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:03 +0000

    Add package ryze/0.0.2/ryze-0.0.2.tar.gz

---
+++ packages/ryze/ryze-0.0.2.tar.gz/ryze-0.0.2/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+from ryze import __version__
+
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
+
+setup(
+    name='ryze',
+    version=__version__,
+    author='André Sousa',
+    author_email='andrematosdesousa@gmail.com',
+    description='League of Legends game updates wrapper and scrapper',
+    license='MIT',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    packages=['ryze'],
+    install_requires=[
+        'requests',
+        'beautifulsoup4',
+        'lxml'
+    ],
+    project_urls={
+        'Source': 'https://github.com/sousa-andre/ryze'
+    }
+)
