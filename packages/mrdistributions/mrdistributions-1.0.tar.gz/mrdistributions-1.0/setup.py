dc87ef8faed8e1b79692048225938845e63b719b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:13:01 +0000

    Add package mrdistributions/1.0/mrdistributions-1.0.tar.gz

---
+++ packages/mrdistributions/mrdistributions-1.0.tar.gz/mrdistributions-1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='mrdistributions',
+      version='1.0',
+      description='Gaussian and Binomail distributions',
+      packages=['mrdistributions'],
+      author = 'MILIND RAJ',
+      author_email = 'milindraj003@gmail.com',
+      zip_safe=False)
