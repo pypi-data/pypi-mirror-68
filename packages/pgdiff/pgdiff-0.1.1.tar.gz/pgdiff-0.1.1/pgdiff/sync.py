5a1b6afacb61295f83b2943e006c47c8358f162f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:14:54 +0000

    Add package pgdiff/0.1.1/pgdiff-0.1.1.tar.gz

---
+++ packages/pgdiff/pgdiff-0.1.1.tar.gz/pgdiff-0.1.1/pgdiff/sync.py
@@ -0,0 +1,20 @@
+import contextlib
+from .diff import diff, inspect
+from .utils import temp_db, quick_cursor
+
+
+from psycopg2 import connect as db_connect  # type: ignore
+from psycopg2.extras import RealDictCursor  # type: ignore
+
+
+def sync(schema: str, dsn: str):
+    with temp_db(dsn) as temp_db_dsn:
+        with contextlib.ExitStack() as stack:
+            target = stack.enter_context(quick_cursor(temp_db_dsn, RealDictCursor))
+            current = stack.enter_context(quick_cursor(dsn, RealDictCursor))
+            target.execute(schema)
+            target_schema = inspect(target)
+            current_schema = inspect(current)
+            statements = diff(current_schema, target_schema)
+            script = ";\n".join(statements) + ";"
+            print(script)
