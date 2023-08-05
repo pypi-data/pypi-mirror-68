033485cfb076beed5bf1451bbdfa76ea13083c1f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:52 +0000

    Add package pyatag/0.3.1.1/pyatag-0.3.1.1.tar.gz

---
+++ packages/pyatag/pyatag-0.3.1.1.tar.gz/pyatag-0.3.1.1/setup.py
@@ -0,0 +1,24 @@
+"""Prepare for Pypi."""
+
+from distutils.core import setup
+
+setup(
+    name="pyatag",  # How you named your package folder (MyLib)
+    packages=["pyatag"],  # Chose the same as "name"
+    version="0.3.1.1",  # Sta2t with a small number
+    license="MIT",
+    description="Connection to ATAG One Thermostat",
+    author="Mats",  # Type in your name
+    author_email="mats.nelissen@gmail.com",  # Type in your E-Mail
+    url="https://github.com/MatsNl/pyatag",
+    install_requires=["asyncio", "aiohttp",],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        #    'Intended Audience :: Developers',    # Define developer audience
+        #    'Topic :: Software Development :: Build Tools',
+        "License :: OSI Approved :: MIT License",  # Again, pick a license
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+    ],
+)
