8b4c54fa4440acba21884cf7872306fd050c97b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:41:44 +0000

    Add package dash-leaflet/0.0.10rc4/dash_leaflet-0.0.10rc4.tar.gz

---
+++ packages/dash-leaflet/dash_leaflet-0.0.10rc4.tar.gz/dash_leaflet-0.0.10rc4/dash_leaflet/express.py
@@ -0,0 +1,32 @@
+import json
+import dash_leaflet as dl
+
+
+# Utils methods
+
+def categorical_colorbar(*args, categories, colorscale, **kwargs):
+    indices = list(range(len(categories) + 1))
+    return dl.Colorbar(*args, min=0, max=len(categories), classes=indices, colorscale=colorscale, tooltip=False,
+                       tickValues=[item + 0.5 for item in indices[:-1]], tickText=categories, **kwargs)
+
+
+def geojson(data, *args, style, **kwargs):
+    feature_id = "id"
+    # Check if id is present and unique.
+    ids = [f["id"] for f in data["features"] if "id" in f]
+    id_valid = len(list(set(ids))) == len(data["features"])
+    if not id_valid:
+        feature_id = "leaflet_id" if len(ids) > 0 else "id"
+        for i, f in enumerate(data["features"]):
+            f[feature_id] = i
+    # Setup options (for now assumed to be style only).
+    featureOptions = {f[feature_id]: dict(style=style(f)) for f in data["features"]}
+    return dl.GeoJSON(*args, data=data, featureOptions=featureOptions, featureId=feature_id, **kwargs)
+
+
+# Data samples.
+
+def us_states_population_density():
+    with open("us-states.json", 'r') as f:
+        data = json.load(f)
+    return data
