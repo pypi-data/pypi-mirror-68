a9a8e610bd081bbeb8bd5ce12993b62cb88d4333
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:41:20 +0000

    Add package opfunu/0.6.6/opfunu-0.6.6-py3-none-any.whl

---
+++ packages/opfunu/opfunu-0.6.6-py3-none-any.whl/examples/test_cec2013.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# ------------------------------------------------------------------------------------------------------%
+# Created by "Thieu Nguyen" at 22:21, 25/04/2020                                                        %
+#                                                                                                       %
+#       Email:      nguyenthieu2102@gmail.com                                                           %
+#       Homepage:   https://www.researchgate.net/profile/Thieu_Nguyen6                                  %
+#       Github:     https://github.com/thieunguyen5991                                                  %
+#-------------------------------------------------------------------------------------------------------%
+
+import numpy as np
+from opfunu.cec.cec2013.unconstraint import Model
+
+problem_size = 100
+solution = np.random.uniform(-1, 1, problem_size)
+func = Model(problem_size)
+
+print(func.F28(solution))
