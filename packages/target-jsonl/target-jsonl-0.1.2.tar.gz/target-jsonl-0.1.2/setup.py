1d2fd15f44fea2dd123d22538613eef11d741467
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:38:27 +0000

    Add package target-jsonl/0.1.2/target-jsonl-0.1.2.tar.gz

---
+++ packages/target-jsonl/target-jsonl-0.1.2.tar.gz/target-jsonl-0.1.2/setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+with open("README.md", "r", encoding="utf-8") as f:
+    readme = f.read()
+
+setup(
+    name='target-jsonl',
+    version='0.1.2',
+    description='Singer.io target for writing JSON Line files',
+    long_description=readme,
+    long_description_content_type='text/markdown',
+    author='Andy Huynh',
+    author_email="andy.huynh312@gmail.com",
+    url="https://github.com/andyh1203/target-jsonl",
+    keywords=["singer", "singer.io", "target", "etl"],
+    classifiers=['Programming Language :: Python :: 3 :: Only'],
+    py_modules=['target_jsonl'],
+    install_requires=['jsonschema==2.6.0', 'singer-python==2.1.4'],
+    entry_points='''
+          [console_scripts]
+          target-jsonl=target_jsonl:main
+      ''',
+)
