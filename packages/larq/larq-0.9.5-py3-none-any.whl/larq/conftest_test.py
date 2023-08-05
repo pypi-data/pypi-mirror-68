f23527f4286763deb2d196b5ec65caba34be9b2b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:07:42 +0000

    Add package larq/0.9.5/larq-0.9.5-py3-none-any.whl

---
+++ packages/larq/larq-0.9.5-py3-none-any.whl/larq/conftest_test.py
@@ -0,0 +1,28 @@
+import tensorflow as tf
+
+from larq import context
+
+
+def test_eager_and_graph_mode_fixture(eager_and_graph_mode):
+    if eager_and_graph_mode == "eager":
+        assert tf.executing_eagerly()
+    else:
+        assert not tf.executing_eagerly()
+        assert tf.compat.v1.get_default_session() is not None
+
+
+def test_eager_mode_fixture(eager_mode):
+    assert tf.executing_eagerly()
+
+
+def test_graph_mode_fixture(graph_mode):
+    assert not tf.executing_eagerly()
+    assert tf.compat.v1.get_default_session() is not None
+
+
+def test_distribute_scope(distribute_scope):
+    assert tf.distribute.has_strategy() is distribute_scope
+
+
+def test_quantize_scope(quantized):
+    assert context.should_quantize() == quantized
