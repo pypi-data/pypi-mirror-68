812fc27f646f8f2886bd63d9c24d77c5f91b4795
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:36 +0000

    Add package moonpdf/1.1/moonpdf-1.1.tar.gz

---
+++ packages/moonpdf/moonpdf-1.1.tar.gz/moonpdf-1.1/moonpdf/pdf2image.py
@@ -0,0 +1,15 @@
+""" This module provide the functions to convert pdf to image """
+
+
+def convert(path):
+    """
+    Convert the given pdf to image.
+
+    Parameter:
+    path (str) : The path of a pdf file.
+
+    Returns:
+    str: The content of the pdf file as string.   
+    """
+
+    print("Pdf to Image.")
