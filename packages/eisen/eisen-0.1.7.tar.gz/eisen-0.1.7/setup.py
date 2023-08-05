ae337e475da16342bc1bcf0fa40156857ef82edd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:43 +0000

    Add package eisen/0.1.7/eisen-0.1.7.tar.gz

---
+++ packages/eisen/eisen-0.1.7.tar.gz/eisen-0.1.7/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+
+VERSION = '0.1.7'
+
+with open('requirements.txt') as f:
+    install_requires = f.read().strip().split('\n')
+
+setup(
+    name='eisen',
+    version=VERSION,
+    description='Eisen Meta Package (installs all components of Eisen)',
+    packages=find_packages(),
+    install_requires=install_requires,
+)
