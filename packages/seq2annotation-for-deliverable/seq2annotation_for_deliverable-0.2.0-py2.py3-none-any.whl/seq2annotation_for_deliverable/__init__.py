a2f6a4f418037268236017ad5d7e9be673b6f144
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:43 +0000

    Add package seq2annotation-for-deliverable/0.2.0/seq2annotation_for_deliverable-0.2.0-py2.py3-none-any.whl

---
+++ packages/seq2annotation-for-deliverable/seq2annotation_for_deliverable-0.2.0-py2.py3-none-any.whl/seq2annotation_for_deliverable/__init__.py
@@ -0,0 +1,5 @@
+"""Top-level package for seq2annotation_for_deliverable."""
+
+__author__ = """Xiaoquan Kong"""
+__email__ = 'u1mail2me@gmail.com'
+__version__ = '0.2.0'
---
+++ packages/seq2annotation-for-deliverable/seq2annotation_for_deliverable-0.2.0-py2.py3-none-any.whl/seq2annotation_for_deliverable/cli.py
@@ -0,0 +1,16 @@
+"""Console script for seq2annotation_for_deliverable."""
+import sys
+import click
+
+
+@click.command()
+def main(args=None):
+    """Console script for seq2annotation_for_deliverable."""
+    click.echo("Replace this message by putting your code into "
+               "seq2annotation_for_deliverable.cli.main")
+    click.echo("See click documentation at https://click.palletsprojects.com/")
+    return 0
+
+
+if __name__ == "__main__":
+    sys.exit(main())  # pragma: no cover
