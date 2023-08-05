bab9e3e33c40f6c124d7bc0f6770c1b9cbc8b649
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:45 +0000

    Add package ai-graphics/0.1.37/ai_graphics-0.1.37-py3-none-any.whl

---
+++ packages/ai-graphics/ai_graphics-0.1.37-py3-none-any.whl/graphics/function/__init__.py
@@ -0,0 +1,8 @@
+from .Classifier import Classifier, train_test_split
+from .Detection import Detection
+from .ImageCrop import ImageCrop
+from .Matting import Matting
+from .MtcnnDetector import MtcnnDetector
+from .OcrDetector import OcrDetector
+from .Watermark import Watermark
+# from .Segmentation import Segmentation
