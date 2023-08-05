db051f79d689c58c8c0672b031649ada8a8af17c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:40 +0000

    Add package semantyk/0.0.0.5/semantyk-0.0.0.5.tar.gz

---
+++ packages/semantyk/semantyk-0.0.0.5.tar.gz/semantyk-0.0.0.5/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="semantyk",
+    version="0.0.0.5",
+    author="Daniel Bakas Amuchastegui",
+    author_email="danielbakas@gmail.com",
+    description="Semantyk | Ideas Wonder. | Visual Interactive Semantyk Intelligent Online Network.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/semantykcom/Semantyk",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3"
+    ],
+    python_requires='>=3.6',
+)
