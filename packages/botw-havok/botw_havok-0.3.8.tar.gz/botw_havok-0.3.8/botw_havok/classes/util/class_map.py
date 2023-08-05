28053769dd29996a8dc8ba4e6a2bad714f0a0fb2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:59 +0000

    Add package botw-havok/0.3.8/botw_havok-0.3.8.tar.gz

---
+++ packages/botw-havok/botw_havok-0.3.8.tar.gz/botw_havok-0.3.8/botw_havok/classes/util/class_map.py
@@ -0,0 +1,39 @@
+from ..hkpBoxShape import hkpBoxShape
+from ..hkpBvCompressedMeshShape import hkpBvCompressedMeshShape
+from ..hkpCapsuleShape import hkpCapsuleShape
+from ..hkpConvexVerticesShape import hkpConvexVerticesShape
+from ..hkpCylinderShape import hkpCylinderShape
+from ..hkpPhysicsData import hkpPhysicsData
+from ..hkpPhysicsSystem import hkpPhysicsSystem
+from ..hkpRigidBody import hkpRigidBody
+from ..hkpSphereShape import hkpSphereShape
+from ..hkpStaticCompoundShape import hkpStaticCompoundShape
+from ..hkRootLevelContainer import hkRootLevelContainer
+from ..StaticCompoundInfo import StaticCompoundInfo
+
+
+class HKClassMap:
+    hk_class_map = {
+        "hkpBoxShape": hkpBoxShape,
+        "hkpBvCompressedMeshShape": hkpBvCompressedMeshShape,
+        "hkpCapsuleShape": hkpCapsuleShape,
+        "hkpConvexVerticesShape": hkpConvexVerticesShape,
+        "hkpCylinderShape": hkpCylinderShape,
+        "hkpPhysicsData": hkpPhysicsData,
+        "hkpPhysicsSystem": hkpPhysicsSystem,
+        "hkpRigidBody": hkpRigidBody,
+        "hkpSphereShape": hkpSphereShape,
+        "hkpStaticCompoundShape": hkpStaticCompoundShape,
+        "hkRootLevelContainer": hkRootLevelContainer,
+        "StaticCompoundInfo": StaticCompoundInfo,
+    }
+
+    def __new__(cls):
+        raise Exception("This class shouldn't be instantiated")
+
+    @staticmethod
+    def get(name: str):
+        try:
+            return HKClassMap.hk_class_map[name]
+        except KeyError:
+            raise NotImplementedError(f"Class '{name}' is not implemented yet!")
