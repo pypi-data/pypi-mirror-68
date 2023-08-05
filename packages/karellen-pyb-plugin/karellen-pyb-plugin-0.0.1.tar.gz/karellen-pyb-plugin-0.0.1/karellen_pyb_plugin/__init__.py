6d06d4b742c82443137d53d110f13ca9ea427529
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:41:24 +0000

    Add package karellen-pyb-plugin/0.0.1/karellen-pyb-plugin-0.0.1.tar.gz

---
+++ packages/karellen-pyb-plugin/karellen-pyb-plugin-0.0.1.tar.gz/karellen-pyb-plugin-0.0.1/karellen_pyb_plugin/__init__.py
@@ -0,0 +1,128 @@
+#  -*- coding: utf-8 -*-
+#
+# (C) Copyright 2020 Karellen, Inc. (https://www.karellen.co/)
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+import textwrap
+
+from pybuilder.core import use_plugin, init, before
+
+use_plugin("python.core")
+use_plugin("copy_resources")
+use_plugin("python.unittest")
+use_plugin("python.integrationtest")
+use_plugin("python.flake8")
+use_plugin("python.distutils")
+use_plugin("python.pycharm")
+use_plugin("python.cram")
+use_plugin("python.sphinx")
+use_plugin("pypi:pybuilder_header_plugin")
+
+
+@init
+def configure(project):
+    project.build_depends_on("sphinx_rtd_theme", ">=0.0.1")
+
+    # Unit test Coverage Configuration
+    project.set_property("coverage_threshold_warn", 100)
+    project.set_property("coverage_branch_threshold_warn", 100)
+    project.set_property("coverage_branch_partial_threshold_warn", 100)
+    project.set_property("coverage_break_build", True)
+    project.set_property("coverage_exceptions", [])
+
+    # Flake8 Configuration
+    project.set_property("flake8_break_build", True)
+    project.set_property("flake8_include_test_sources", True)
+    project.set_property("flake8_include_scripts", True)
+    project.set_property("flake8_ignore", "E402")
+    project.set_property("flake8_exclude_patterns", ".svn,CVS,.bzr,.hg,.git,__pycache__,*.sh")
+
+    # Copyright Header
+    project.set_property("pybuilder_header_plugin_break_build", True)
+    project.set_property("pybuilder_header_plugin_expected_header",
+                         textwrap.dedent("""\
+                         #  -*- coding: utf-8 -*-
+                         #
+                         # (C) Copyright 2020 Karellen, Inc. (https://www.karellen.co/)
+                         #
+                         # Licensed under the Apache License, Version 2.0 (the "License");
+                         # you may not use this file except in compliance with the License.
+                         # You may obtain a copy of the License at
+                         #
+                         #     http://www.apache.org/licenses/LICENSE-2.0
+                         #
+                         # Unless required by applicable law or agreed to in writing, software
+                         # distributed under the License is distributed on an "AS IS" BASIS,
+                         # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+                         # See the License for the specific language governing permissions and
+                         # limitations under the License.
+                         #
+                         """))
+    project.set_property("pybuilder_header_plugin_exclude_patterns", [])
+
+    # Cram Configuration
+    project.set_property("cram_fail_if_no_tests", True)
+
+    # Distutils
+    project.set_property("distutils_classifiers", ["Development Status :: 5 - Production/Stable",
+                                                   "Environment :: Console",
+                                                   "Intended Audience :: Developers",
+                                                   "License :: OSI Approved :: Apache Software License",
+                                                   "Programming Language :: Python",
+                                                   ]
+                         )
+    project.set_property("distutils_commands", ["sdist", "bdist_wheel"])
+    project.set_property("distutils_upload_skip_existing", True)
+    project.set_property("distutils_upload_sign", True)
+    project.set_property("distutils_upload_sign_identity", "5F4AFAA3")
+    project.set_property("distutils_upload_repository_key", "pypi-karellen")
+    project.set_property("distutils_readme_description", True)
+    project.set_property("distutils_description_overwrite", True)
+    project.set_property("distutils_readme_file", "README.md")
+
+    # Sphinx
+    project.set_property("sphinx_output_per_builder", True)
+    project.set_property("sphinx_run_apidoc", True)
+    project.set_property("sphinx_apidoc_extra_args", ["-l", "-e"])
+    project.set_property("sphinx_build_extra_args", ["-E"])
+
+
+@before("sphinx_generate_documentation")
+def set_sphinx_html_path(project):
+    import sphinx_rtd_theme
+
+    sphinx_conf = project.get_property("sphinx_project_conf")
+    sphinx_conf["html_theme"] = "sphinx_rtd_theme"
+    sphinx_conf["html_theme_path"] = [sphinx_rtd_theme.get_html_theme_path()]
+
+    # Napoleon settings
+    sphinx_conf["extensions"].append("sphinx.ext.napoleon")
+    sphinx_conf["napoleon_google_docstring"] = True
+    sphinx_conf["napoleon_numpy_docstring"] = False
+    sphinx_conf["napoleon_include_init_with_doc"] = False
+    sphinx_conf["napoleon_include_private_with_doc"] = False
+    sphinx_conf["napoleon_include_special_with_doc"] = False
+    sphinx_conf["napoleon_use_admonition_for_examples"] = False
+    sphinx_conf["napoleon_use_admonition_for_notes"] = False
+    sphinx_conf["napoleon_use_admonition_for_references"] = False
+    sphinx_conf["napoleon_use_ivar"] = False
+    sphinx_conf["napoleon_use_param"] = True
+    sphinx_conf["napoleon_use_rtype"] = True
+    sphinx_conf["napoleon_use_keyword"] = True
+
+
+@before("verify")
+def run_header_check(project, logger, reactor):
+    reactor.execute_task_shortest_plan("check_source_file_headers")
---
+++ packages/karellen-pyb-plugin/karellen-pyb-plugin-0.0.1.tar.gz/karellen-pyb-plugin-0.0.1/setup.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+#   -*- coding: utf-8 -*-
+
+from setuptools import setup
+from setuptools.command.install import install as _install
+
+class install(_install):
+    def pre_install_script(self):
+        pass
+
+    def post_install_script(self):
+        pass
+
+    def run(self):
+        self.pre_install_script()
+
+        _install.run(self)
+
+        self.post_install_script()
+
+if __name__ == '__main__':
+    setup(
+        name = 'karellen-pyb-plugin',
+        version = '0.0.1',
+        description = 'Karellen PyBuilder Plugin',
+        long_description = '# Karellen PyBuilder Plugin\n\n[Karellen](https://www.karellen.co/karellen/) [PyBuilder](https://pybuilder.io/) Plugin\n\n[![Gitter chat](https://badges.gitter.im/karellen/gitter.svg)](https://gitter.im/karellen/lobby)\n\nThis is the main PyBuilder plugin for all of the projects under [Karellen](https://www.karellen.co/) umbrella.\nThis plugin governs the following PyBuilder project settings:\n\n1. Code style and copyright headers.\n2. Coverage passing threshold.\n3. Documentation building and style.\n4. Code packaging, packaging metadata and signing.\n\nIn order to make sure your project conforms to Karellen standards, use this plugin by adding the following to your `build.py`:\n\n```python\n\nfrom pybuilder.core import use_plugin\n\n# ... #\n\nuse_plugin("pypi:karellen_pyb_plugin", ">=0.0.1")\n\n```\n\n## For Developers\n\n[Karellen PyBuilder Plugin Issue Tracker](https://github.com/karellen/karellen-pyb-plugin/issues)\n\n[Karellen PyBuilder Plugin Source Code](https://github.com/karellen/karellen-pyb-plugin)\n',
+        long_description_content_type = 'text/markdown',
+        classifiers = [
+            'Development Status :: 5 - Production/Stable',
+            'Environment :: Console',
+            'Intended Audience :: Developers',
+            'License :: OSI Approved :: Apache Software License',
+            'Programming Language :: Python'
+        ],
+        keywords = '',
+
+        author = 'Karellen, Inc',
+        author_email = 'supervisor@karellen.co',
+        maintainer = 'Arcadiy Ivanov',
+        maintainer_email = 'arcadiy@karellen.co',
+
+        license = 'Apache License, Version 2.0',
+
+        url = 'https://github.com/karellen/karellen-pyb-plugin',
+        project_urls = {
+            'Bug Tracker': 'https://github.com/karellen/karellen-pyb-plugin/issues',
+            'Documentation': 'https://github.com/karellen/karellen-pyb-plugin/',
+            'Source Code': 'https://github.com/karellen/karellen-pyb-plugin/'
+        },
+
+        scripts = [],
+        packages = ['karellen_pyb_plugin'],
+        namespace_packages = [],
+        py_modules = [],
+        entry_points = {},
+        data_files = [],
+        package_data = {},
+        install_requires = [],
+        dependency_links = [],
+        zip_safe = True,
+        cmdclass = {'install': install},
+        python_requires = '',
+        obsoletes = [],
+    )
