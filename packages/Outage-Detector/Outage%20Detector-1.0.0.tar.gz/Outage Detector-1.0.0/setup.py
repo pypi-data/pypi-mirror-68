a4334dcff86deb859d6e2185d0ac40790c4bd5b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:14:49 +0000

    Add package Outage-Detector/1.0.0/Outage%20Detector-1.0.0.tar.gz

---
+++ packages/Outage-Detector/Outage%20Detector-1.0.0.tar.gz/Outage Detector-1.0.0/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+with open('requirements.txt') as req_files:
+    requirements = req_files.read().splitlines()
+
+with open("README.md", 'r') as readme:
+    long_description = readme.read()
+
+setup(
+    name='Outage Detector',
+    version='1.0.0',
+    description='A module helping you find out when internet and power outages happen.',
+    long_description=long_description,
+    license='MIT',
+    packages=['outagedetector'],
+    install_requires=requirements,
+    entry_points={
+        'console_scripts':[
+            'outage_detector = outagedetector.__main__:main'
+        ]
+    },
+    author='Butean Fabian',
+    author_email='buteanfabian@gmail.com'
+)
