4697c43f9fc4efca628803474f63db3af556cf28
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:14:57 +0000

    Add package parser-libraries/1.6/parser_libraries-1.6.tar.gz

---
+++ packages/parser-libraries/parser_libraries-1.6.tar.gz/parser_libraries-1.6/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='parser_libraries',
+    version='1.6',
+    packages=['parser_libraries'],
+    author_email='ivan.frinom@gmail.com',
+    install_requires=[
+        'pymysql',
+        'openpy',
+        'requests',
+	'bs4',
+	'selenium'
+    ]
+)
