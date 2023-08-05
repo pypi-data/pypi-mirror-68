94bc54ba9ec160d1b1e17200043a33995719b149
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:38 +0000

    Add package dist-probability/2.0/dist_probability-2.0.tar.gz

---
+++ packages/dist-probability/dist_probability-2.0.tar.gz/dist_probability-2.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name= 'dist_probability',
+      version= '2.0',
+      description= 'Gaussian and Binomial distributions',
+      packages= ['dist_probability'],
+      author= 'Aniket Sharma',
+      author_email= 'aniketsharma552@gmail.com',
+    zip_safe=False)
