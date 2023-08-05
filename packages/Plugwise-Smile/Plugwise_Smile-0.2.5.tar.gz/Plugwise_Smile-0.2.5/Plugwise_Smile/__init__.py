3e1f20aa4fc0f2e4949e949e184c3adb2fae1c38
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:42:23 +0000

    Add package Plugwise-Smile/0.2.5/Plugwise_Smile-0.2.5.tar.gz

---
+++ packages/Plugwise-Smile/Plugwise_Smile-0.2.5.tar.gz/Plugwise_Smile-0.2.5/Plugwise_Smile/__init__.py
@@ -0,0 +1,5 @@
+"""Plugwise Smile module."""
+
+__version__ = "0.2.5"
+
+from Plugwise_Smile import Smile
---
+++ packages/Plugwise-Smile/Plugwise_Smile-0.2.5.tar.gz/Plugwise_Smile-0.2.5/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup
+
+
+def readme():
+    with open("README.md") as f:
+        return f.read()
+
+
+setup(
+    name="Plugwise_Smile",
+    version="0.2.5",
+    description="Plugwise_Smile (Anna/Adam/P1) API to use in conjunction with Home Assistant.",
+    long_description="Plugwise Smile API to use in conjunction with Home Assistant, but it can also be used without Home Assistant as a module.",
+    keywords="HomeAssistant HA Home Assistant Anna Adam P1 Smile Plugwise",
+    url="https://github.com/plugwise/Plugwise-Smile",
+    author="Plugwise",
+    author_email="info@compa.nl",
+    license="MIT",
+    packages=["Plugwise_Smile"],
+    install_requires=[
+        "asyncio",
+        "aiohttp",
+        "async_timeout",
+        "datetime",
+        "lxml",
+        "pytz",
+        "python-dateutil",
+        "semver",
+    ],
+    zip_safe=False,
+)
