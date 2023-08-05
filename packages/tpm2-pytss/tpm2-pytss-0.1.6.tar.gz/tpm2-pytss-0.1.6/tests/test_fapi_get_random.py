b2211c91dee0230d31f115f777266dd0c604ccaf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:37:39 +0000

    Add package tpm2-pytss/0.1.6/tpm2-pytss-0.1.6.tar.gz

---
+++ packages/tpm2-pytss/tpm2-pytss-0.1.6.tar.gz/tpm2-pytss-0.1.6/tests/test_fapi_get_random.py
@@ -0,0 +1,21 @@
+import random
+from contextlib import ExitStack
+
+from tpm2_pytss.binding import *
+from tpm2_pytss.util.testing import BaseTestFAPI
+
+
+class TestGetRandom(BaseTestFAPI):
+    def test_random_length(self):
+        length = random.randint(8, 32)
+
+        self.fapi_ctx.Provision(None, None, None)
+
+        array = UINT8_PTR_PTR()
+
+        with array:
+            self.fapi_ctx.GetRandom(length, array)
+
+            value = to_bytearray(length, array.value)
+
+            self.assertEqual(length, len(value))
