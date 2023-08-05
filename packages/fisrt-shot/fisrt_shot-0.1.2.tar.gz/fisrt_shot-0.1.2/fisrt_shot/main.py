d46cd598d7bc8fca1bdbbeeda08d98f07798a67d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:33 +0000

    Add package fisrt-shot/0.1.2/fisrt_shot-0.1.2.tar.gz

---
+++ packages/fisrt-shot/fisrt_shot-0.1.2.tar.gz/fisrt_shot-0.1.2/fisrt_shot/main.py
@@ -0,0 +1,21 @@
+from fisrt_shot.sub_1.helper import helper
+from fisrt_shot.sub_1.mod_1 import fun_1_1
+from fisrt_shot.sub_2 import mod_2
+
+def kiritrain_function ():
+    print ('kiritaring function is just started ')
+    print ('kos e nane ramin moosavi ')
+    print ('++++calling helper form sub 1 helper ')
+    helper()
+    print('+++++helper is doen ')
+    print('+++++ calling func1-1 from sub 1 mod 1 ')
+    fun_1_1()
+    print('++++++ just done finc 1 1 ')
+    print('+++++ calling func 1- 2 from sub 2 mod 2 ')
+    mod_2.func_2_1()
+    print('++++++ just done finc 1 2')
+    print ('***** KOSE NANE RAMIN MOUSAVI *****')
+
+
+if __name__ == '__main__':
+    kiritrain_function()
---
+++ packages/fisrt-shot/fisrt_shot-0.1.2.tar.gz/fisrt_shot-0.1.2/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='fisrt_shot',
+      version='0.1.2',
+      description='Gaussian distributions',
+      packages=['fisrt_shot'],
+      zip_safe=False)
+
+
