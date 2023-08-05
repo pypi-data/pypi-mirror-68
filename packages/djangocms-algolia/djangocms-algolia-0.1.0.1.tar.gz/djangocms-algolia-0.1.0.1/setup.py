3b9d312119b3050fb35be32f5c19ba856c58e9bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:13:00 +0000

    Add package djangocms-algolia/0.1.0.1/djangocms-algolia-0.1.0.1.tar.gz

---
+++ packages/djangocms-algolia/djangocms-algolia-0.1.0.1.tar.gz/djangocms-algolia-0.1.0.1/setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+from setuptools import setup
+
+
+from djangocms_algolia import __version__
+
+
+setup(
+    name='djangocms-algolia',
+    version=__version__,
+    author='Victor Yunenko',
+    author_email='victor@what.digital',
+    url='https://gitlab.com/victor-yunenko/djangocms-algolia',
+    packages=[
+        'djangocms_algolia',
+    ],
+    include_package_data=True,
+    install_requires=[
+        'django >= 2.2, < 3',
+        'django-cms >= 3.7.2, < 4',
+        'algoliasearch-django',
+        'aldryn-search',
+    ],
+)
