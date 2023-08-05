55f80ab27f0aeb0bfecdad7a33b361c13b4f6950
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:09 +0000

    Add package OpenVisus/1.3.128/OpenVisus-1.3.128-cp38-none-win_amd64.whl

---
+++ packages/OpenVisus/OpenVisus-1.3.128-cp38-none-win_amd64.whl/OpenVisus/visusviewer.bat
@@ -0,0 +1,6 @@
+
+cd %~dp0
+set PATH=C:\hostedtoolcache\windows\Python\3.8.2\x64\python.exe\..;.\bin;bin\Qt\bin;%PATH%
+set PYTHONPATH=D:\a\OpenVisus\OpenVisus\build\Release\OpenVisus;C:\hostedtoolcache\windows\Python\3.8.2\x64\python38.zip;C:\hostedtoolcache\windows\Python\3.8.2\x64\DLLs;C:\hostedtoolcache\windows\Python\3.8.2\x64\lib;C:\hostedtoolcache\windows\Python\3.8.2\x64;C:\hostedtoolcache\windows\Python\3.8.2\x64\lib\site-packages;C:\hostedtoolcache\windows\Python\3.8.2\x64\lib\site-packages\win32;C:\hostedtoolcache\windows\Python\3.8.2\x64\lib\site-packages\win32\lib;C:\hostedtoolcache\windows\Python\3.8.2\x64\lib\site-packages\Pythonwin
+set QT_PLUGIN_PATH=bin\Qt\plugins
+"bin\visusviewer.exe" %*
