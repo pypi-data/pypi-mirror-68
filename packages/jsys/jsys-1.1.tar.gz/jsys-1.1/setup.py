577900c8271e811b32dda062ef48f8c30abae26d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:40 +0000

    Add package jsys/1.1/jsys-1.1.tar.gz

---
+++ packages/jsys/jsys-1.1.tar.gz/jsys-1.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+name='jsys',
+version='1.1',
+description='Opis',
+long_description='Dlugi opis',
+author='Marcin',
+author_email='m.bazydlo@jsystems.pl',
+url='http://jsystems.pl',
+packages=find_packages()
+)
