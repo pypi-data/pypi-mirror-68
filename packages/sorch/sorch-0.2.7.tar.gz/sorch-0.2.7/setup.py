930a6da79b91818c3df9cbe21db0ca88f783b4d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:40:08 +0000

    Add package sorch/0.2.7/sorch-0.2.7.tar.gz

---
+++ packages/sorch/sorch-0.2.7.tar.gz/sorch-0.2.7/setup.py
@@ -0,0 +1,20 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+long_desc = '''Simple PyTorch utils, yet another package.
+This one is mostly for my personal applications.'''
+
+# rm -rf dist build && python3 setup.py sdist bdist_wheel
+# twine upload dist/*
+setup(
+    name='sorch',
+    version='0.2.7',
+    packages=['sorch', 'sorch.trainer'],
+    url='https://github.com/PiotrDabkowski/sorch',
+    license='MIT',
+    author='Piotr Dabkowski',
+    author_email='piodrus@gmail.com',
+    description='Simple PyTorch utils, yet another package :)',
+    long_description=long_desc)
