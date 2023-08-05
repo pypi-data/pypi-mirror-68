ad5bdfc51ca735a4aaa29bb48049ce0057f9f2a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:37 +0000

    Add package colabexts/0.11000000000000001/colabexts-0.11000000000000001-py2.py3-none-any.whl

---
+++ packages/colabexts/colabexts-0.11000000000000001-py2.py3-none-any.whl/colabexts/__init__.py
@@ -0,0 +1,18 @@
+from Jupytils.jcommon import *
+
+def LoadJupytils(abspath=None, debug=False):
+    ip = get_ipython()
+    if(abspath is None):
+        abspath =os.path.dirname(Jupytils.__file__)
+    if(debug):
+        print("loading jupytils ... from: "+abspath);
+        
+    #ip.run_line_magic(magic_name="run", line=abspath+"/DBUtils.py")
+    #c = readFile(abspath+"/ajax.html")
+    #display(HTML(c))
+
+    
+try:
+    LoadJupytils()
+except:
+    pass
