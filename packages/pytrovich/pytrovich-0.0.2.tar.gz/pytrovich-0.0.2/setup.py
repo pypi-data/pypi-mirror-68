4265ff4913c40eb90c130f0084a6915550a00af2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:30 +0000

    Add package pytrovich/0.0.2/pytrovich-0.0.2.tar.gz

---
+++ packages/pytrovich/pytrovich-0.0.2.tar.gz/pytrovich-0.0.2/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+
+from distutils.core import setup
+
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="pytrovich",
+    packages=setuptools.find_packages(),
+    version="0.0.2",
+    description="pytrovich: a Python port of an inflector for Russian anthroponyms developed by petrovich team: "
+                "https://github.com/petrovich",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Anton Alekseev",
+    author_email="anton.m.alexeye@gmail.com",
+    url="https://github.com/petrovich/pytrovich",
+    keywords=["nlp", "morphology", "russian language"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    zip_safe=False,
+    include_package_data=True,
+    python_requires=">=3.6",
+)
