16fa7e764cf693f730fb2e81e92c23386fafed38
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:14:50 +0000

    Add package isogeo-scan-metadata-processor/1.0.1/isogeo-scan-metadata-processor-1.0.1.tar.gz

---
+++ packages/isogeo-scan-metadata-processor/isogeo-scan-metadata-processor-1.0.1.tar.gz/isogeo-scan-metadata-processor-1.0.1/scan_metadata_processor/__init__.py
@@ -0,0 +1,28 @@
+#! python3  # noqa: E265
+
+import logging
+
+# submodules
+from .__about__ import __version__  # noqa: F401
+
+# conditional import
+try:
+    from elasticsearch import Elasticsearch  # noqa: F401
+
+    ES_ENABLED = True
+    from .database import ElasticSearchManager  # noqa: F401
+except ImportError:
+    logging.warning(
+        "ElasticSearch package is not installed. Related functions cannot be used."
+    )
+    ES_ENABLED = False
+
+# subpackages
+from .parser import Listing, Lookup, Sign  # noqa: F401
+from .converter import (  # noqa: F401
+    MATCHER_FORMAT,
+    MATCHER_GEOMETRY,
+    LookupToIsogeo,
+    match_coordinate_system,
+)
+from .parser import MetadataJsonReader, Unzipper  # noqa: F401
---
+++ packages/isogeo-scan-metadata-processor/isogeo-scan-metadata-processor-1.0.1.tar.gz/isogeo-scan-metadata-processor-1.0.1/scan_metadata_processor/converter/__init__.py
@@ -0,0 +1,9 @@
+#! python3  # noqa: E265
+
+# submodules
+from .matchers import (
+    MATCHER_FORMAT,
+    MATCHER_GEOMETRY,  # noqa: F401
+    match_coordinate_system,
+)
+from .lookup_to_isogeo import LookupToIsogeo  # noqa: F401
