4d4971e776059d95390a45a7f97558e805a39172
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:38:55 +0000

    Add package tyrannosaurus/0.2.0/tyrannosaurus-0.2.0.tar.gz

---
+++ packages/tyrannosaurus/tyrannosaurus-0.2.0.tar.gz/tyrannosaurus-0.2.0/README.md
@@ -0,0 +1,76 @@
+# Tyrannosaurus Reqs
+
+[![Build status](https://img.shields.io/pypi/status/tyrannosaurus)](https://pypi.org/project/tyrannosaurus/)
+[![Latest version on PyPi](https://badge.fury.io/py/tyrannosaurus.svg)](https://pypi.org/project/tyrannosaurus/)
+[![Supported Python versions](https://img.shields.io/pypi/pyversions/tyrannosaurus.svg)](https://pypi.org/project/tyrannosaurus/)
+[![Documentation status](https://readthedocs.org/projects/tyrannosaurus/badge/?version=latest&style=flat-square)](https://readthedocs.org/projects/tyrannosaurus/stable)
+[![Build & test](https://github.com/dmyersturnbull/tyrannosaurus/workflows/Build%20&%20test/badge.svg)](https://github.com/dmyersturnbull/tyrannosaurus/actions)
+[![Travis](https://travis-ci.org/dmyersturnbull/tyrannosaurus.svg?branch=master)](https://travis-ci.org/dmyersturnbull/tyrannosaurus)
+[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)
+
+An opinionated 2020 Python template.
+
+I wrote this after making nearly 50 commits to configure
+readthedocs, PyPi, Tox, Docker, Travis, and Github actions.
+This avoids that struggle for 99% of projects.
+Just clone and modify or use `tyrannosaurus new`.
+Install with `pip install tyrannosaurus`.
+
+- _When you commit_, your code will be linted.
+- _When you push or make a pull request_, your code will be built and tested.
+  Metadata will be synced, security checks will be run, style will be checked,
+  documentation will be generated, and docker images, sdists, and wheels will be built.
+- _When you release on Github_, your code will be published on PyPi.
+  Just add `PYPI_TOKEN` as a Github repo secret.
+
+⚠ Status: Under development. The template works great.
+   Commands mostly work, but `sync` does little.
+
+Also comes with nice Github labels, a changelog template,
+Conda recipe generation, and various other integrations.
+Tyrannosaurus itself is included as a dependency to copy metadata between config files,
+such as the project version, description, copyright, and build and doc requirements.
+
+To run locally, install [Poetry](https://github.com/python-poetry/poetry)
+and [Tox](https://tox.readthedocs.io/en/latest/) (`pip install tox`).
+Then just type `tox` to build artifacts and run tests.
+
+For more information, [see the docs](https://tyrannosaurus.readthedocs.io/en/stable/).
+
+To make a new project, run:
+
+```bash
+pip install tyrannosaurus
+tyrannosaurus new projectname --license apache
+```
+
+### Building, extending, and contributing
+
+[New issues](https://github.com/dmyersturnbull/tyrannosaurus/issues) and pull requests are welcome.
+Tyrannosaurus is licensed under the [Apache License, version 2.0](https://www.apache.org/licenses/LICENSE-2.0).
+
+
+
+```text
+                                              .++++++++++++.
+                                           .++HHHHHHH^^HHH+.
+                                          .HHHHHHHHHH++-+-++.
+                                         .HHHHHHHHHHH:t~~~~~
+                                        .+HHHHHHHHHHjjjjjjjj.
+                                       .+NNNNNNNNN/++/:--..
+                              ........+NNNNNNNNNN.
+                          .++++BBBBBBBBBBBBBBB.
+ .tttttttt:..           .++BBBBBBBBBBBBBBBBBBB.
++tt+.      ``         .+BBBBBBBBBBBBBBBBBBBBB+++cccc.
+ttt.               .-++BBBBBBBBBBBBBBBBBBBBBB++.ccc.
++ttt++++:::::++++++BBBBBBBBBBBBBBBBBBBBBBB+..++.
+.+TTTTTTTTTTTTTBBBBBBBBBBBBBBBBBBBBBBBBB+.    .ccc.
+  .++TTTTTTTTTTBBBBBBBBBBBBBBBBBBBBBBBB+.      .cc.
+    ..:++++++++++++++++++BBBBBB++++BBBB.
+           .......      -LLLLL+. -LLLLL.
+                        -LLLL+.   -LLLL+.
+                        +LLL+       +LLL+
+                        +LL+         +ff+
+                        +ff++         +++:
+                        ++++:
+```
---
+++ packages/tyrannosaurus/tyrannosaurus-0.2.0.tar.gz/tyrannosaurus-0.2.0/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['tyrannosaurus']
+
+package_data = \
+{'': ['*'], 'tyrannosaurus': ['resources/*']}
+
+install_requires = \
+['importlib-metadata>=1.6,<2.0', 'tomlkit>=0.6.0,<0.7.0', 'typer>=0.2.1,<0.3.0']
+
+entry_points = \
+{'console_scripts': ['tyrannosaurus = tyrannosaurus.cli:cli']}
+
+setup_kwargs = {
+    'name': 'tyrannosaurus',
+    'version': '0.2.0',
+    'description': 'Opinionated 2020 Python template and metadata synchronizer.',
+    'long_description': '# Tyrannosaurus Reqs\n\n[![Build status](https://img.shields.io/pypi/status/tyrannosaurus)](https://pypi.org/project/tyrannosaurus/)\n[![Latest version on PyPi](https://badge.fury.io/py/tyrannosaurus.svg)](https://pypi.org/project/tyrannosaurus/)\n[![Supported Python versions](https://img.shields.io/pypi/pyversions/tyrannosaurus.svg)](https://pypi.org/project/tyrannosaurus/)\n[![Documentation status](https://readthedocs.org/projects/tyrannosaurus/badge/?version=latest&style=flat-square)](https://readthedocs.org/projects/tyrannosaurus/stable)\n[![Build & test](https://github.com/dmyersturnbull/tyrannosaurus/workflows/Build%20&%20test/badge.svg)](https://github.com/dmyersturnbull/tyrannosaurus/actions)\n[![Travis](https://travis-ci.org/dmyersturnbull/tyrannosaurus.svg?branch=master)](https://travis-ci.org/dmyersturnbull/tyrannosaurus)\n[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)\n\nAn opinionated 2020 Python template.\n\nI wrote this after making nearly 50 commits to configure\nreadthedocs, PyPi, Tox, Docker, Travis, and Github actions.\nThis avoids that struggle for 99% of projects.\nJust clone and modify or use `tyrannosaurus new`.\nInstall with `pip install tyrannosaurus`.\n\n- _When you commit_, your code will be linted.\n- _When you push or make a pull request_, your code will be built and tested.\n  Metadata will be synced, security checks will be run, style will be checked,\n  documentation will be generated, and docker images, sdists, and wheels will be built.\n- _When you release on Github_, your code will be published on PyPi.\n  Just add `PYPI_TOKEN` as a Github repo secret.\n\n⚠ Status: Under development. The template works great.\n   Commands mostly work, but `sync` does little.\n\nAlso comes with nice Github labels, a changelog template,\nConda recipe generation, and various other integrations.\nTyrannosaurus itself is included as a dependency to copy metadata between config files,\nsuch as the project version, description, copyright, and build and doc requirements.\n\nTo run locally, install [Poetry](https://github.com/python-poetry/poetry)\nand [Tox](https://tox.readthedocs.io/en/latest/) (`pip install tox`).\nThen just type `tox` to build artifacts and run tests.\n\nFor more information, [see the docs](https://tyrannosaurus.readthedocs.io/en/stable/).\n\nTo make a new project, run:\n\n```bash\npip install tyrannosaurus\ntyrannosaurus new projectname --license apache\n```\n\n### Building, extending, and contributing\n\n[New issues](https://github.com/dmyersturnbull/tyrannosaurus/issues) and pull requests are welcome.\nTyrannosaurus is licensed under the [Apache License, version 2.0](https://www.apache.org/licenses/LICENSE-2.0).\n\n\n\n```text\n                                              .++++++++++++.\n                                           .++HHHHHHH^^HHH+.\n                                          .HHHHHHHHHH++-+-++.\n                                         .HHHHHHHHHHH:t~~~~~\n                                        .+HHHHHHHHHHjjjjjjjj.\n                                       .+NNNNNNNNN/++/:--..\n                              ........+NNNNNNNNNN.\n                          .++++BBBBBBBBBBBBBBB.\n .tttttttt:..           .++BBBBBBBBBBBBBBBBBBB.\n+tt+.      ``         .+BBBBBBBBBBBBBBBBBBBBB+++cccc.\nttt.               .-++BBBBBBBBBBBBBBBBBBBBBB++.ccc.\n+ttt++++:::::++++++BBBBBBBBBBBBBBBBBBBBBBB+..++.\n.+TTTTTTTTTTTTTBBBBBBBBBBBBBBBBBBBBBBBBB+.    .ccc.\n  .++TTTTTTTTTTBBBBBBBBBBBBBBBBBBBBBBBB+.      .cc.\n    ..:++++++++++++++++++BBBBBB++++BBBB.\n           .......      -LLLLL+. -LLLLL.\n                        -LLLL+.   -LLLL+.\n                        +LLL+       +LLL+\n                        +LL+         +ff+\n                        +ff++         +++:\n                        ++++:\n```\n',
+    'author': 'Douglas Myers-Turnbull',
+    'author_email': None,
+    'maintainer': 'Douglas Myers-Turnbull',
+    'maintainer_email': None,
+    'url': 'https://github.com/dmyersturnbull/tyrannosaurus',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.7,<4',
+}
+
+
+setup(**setup_kwargs)
