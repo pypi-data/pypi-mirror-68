211ff4f071fc0241bf9c7af790b48455b4b9975f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:47 +0000

    Add package tap-mysql/1.17.4/tap-mysql-1.17.4.tar.gz

---
+++ packages/tap-mysql/tap-mysql-1.17.4.tar.gz/tap-mysql-1.17.4/setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='tap-mysql',
+      version='1.17.4',
+      description='Singer.io tap for extracting data from MySQL',
+      author='Stitch',
+      url='https://singer.io',
+      classifiers=['Programming Language :: Python :: 3 :: Only'],
+      py_modules=['tap_mysql'],
+      install_requires=[
+          'attrs==16.3.0',
+          'pendulum==1.2.0',
+          'singer-python==5.5.0',
+          'PyMySQL==0.9.3',
+          'backoff==1.3.2',
+          'mysql-replication==0.18',
+      ],
+      entry_points='''
+          [console_scripts]
+          tap-mysql=tap_mysql:main
+      ''',
+      packages=['tap_mysql', 'tap_mysql.sync_strategies'],
+)
