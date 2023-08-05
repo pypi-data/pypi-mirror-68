8998e96395ad45a955145fed8f0d7ccf10f42708
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:08 +0000

    Add package semantyk/0.0.0.17/semantyk-0.0.0.17.tar.gz

---
+++ packages/semantyk/semantyk-0.0.0.17.tar.gz/semantyk-0.0.0.17/semantyk/data_visualization_models/table.py
@@ -0,0 +1,15 @@
+##############################################
+#
+# table
+# Module | Displays an indexed, structured, table representating a given Resource's content.
+#
+# Daniel Bakas Amuchastegui
+# September 3, 2019
+# 
+# Copyright © Semantyk 2020. All rights reserved.
+#############################################
+
+from semantyk.core.model import Model
+
+class Table(Model):
+    pass
