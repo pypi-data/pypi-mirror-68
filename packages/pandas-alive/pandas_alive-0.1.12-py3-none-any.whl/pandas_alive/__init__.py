f9036605a0cfce8a70ab6b8cab922cc0ad84c456
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:26 +0000

    Add package pandas-alive/0.1.12/pandas_alive-0.1.12-py3-none-any.whl

---
+++ packages/pandas-alive/pandas_alive-0.1.12-py3-none-any.whl/pandas_alive/__init__.py
@@ -0,0 +1,12 @@
+from .plotting import plot, AnimatedAccessor, animate_multiple_plots
+from .base import load_dataset
+
+version = "0.1.12"
+
+# Register animated_plot accessor for Pandas DataFrames and Series:
+import pandas as pd
+from pandas.core.accessor import CachedAccessor
+
+plot_animated = CachedAccessor("plot_animated", AnimatedAccessor)
+pd.DataFrame.plot_animated = plot_animated
+pd.Series.plot_animated = plot
