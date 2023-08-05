1a6671e7bb945b2b9c2a2c9d6f28fe0d7c8ecd4a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:14:25 +0000

    Add package clubhouse-client/0.1.0/clubhouse_client-0.1.0.tar.gz

---
+++ packages/clubhouse-client/clubhouse_client-0.1.0.tar.gz/clubhouse_client-0.1.0/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['clubhouse_client', 'clubhouse_client.resources']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['requests>=2.23.0,<3.0.0']
+
+setup_kwargs = {
+    'name': 'clubhouse-client',
+    'version': '0.1.0',
+    'description': 'An unofficial python client for clubhouse.io',
+    'long_description': None,
+    'author': 'Michael J Burling',
+    'author_email': None,
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://gitlab.com/mjburling/clubhouse_client',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.6,<4.0',
+}
+
+
+setup(**setup_kwargs)
