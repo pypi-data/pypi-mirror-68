85d840925e9f31c3981b5a3ee4efca22ce3cdfcf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:09 +0000

    Add package chattest/0.0.9/chattest-0.0.9.tar.gz

---
+++ packages/chattest/chattest-0.0.9.tar.gz/chattest-0.0.9/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="chattest",
+    version="0.0.9",
+    author="simon",
+    author_email="simon@gmail.com",
+    description="Chat test.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/jshmSimon/chattest",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
