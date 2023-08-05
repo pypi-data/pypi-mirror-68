907b19c45aa5d47c9ea48c5929f0b42d274709c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:46 +0000

    Add package semantyk/0.0.0.8/semantyk-0.0.0.8.tar.gz

---
+++ packages/semantyk/semantyk-0.0.0.8.tar.gz/semantyk-0.0.0.8/README.md
@@ -0,0 +1,12 @@
+***
+<H1 align="center">Semantyk</H1>
+<p align="center">
+  <i>– Ideas Wonder –</i><br><br>
+  <img src="https://raw.githubusercontent.com/semantykcom/Semantyk/master/docs/Semantyk.png" alt="Semantyk" width="200"/><br><br>
+  <b>Author: Daniel Bakas Amuchástegui</b><br>
+  August 31, 2017<br>
+  <i>Copyright © 2020 Semantyk. All rights reserved.</i>
+</p>
+
+
+***
---
+++ packages/semantyk/semantyk-0.0.0.8.tar.gz/semantyk-0.0.0.8/semantyk/core/__init__.py
@@ -0,0 +1,18 @@
+##############################################
+#
+# core
+# Package | Core Package.
+#
+# Author: Daniel Bakas Amuchastegui
+# March 16, 2020
+# 
+# Copyright © Semantyk 2020. All rights reserved.
+#############################################
+
+from . import graph
+from . import model
+from . import node
+
+from .graph import *
+from .model import *
+from .node import *
