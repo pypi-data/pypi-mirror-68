696de2279b0a38d2470e579b741d25f8060695bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:44 +0000

    Add package fitloop/0.1.1/fitloop-0.1.1.tar.gz

---
+++ packages/fitloop/fitloop-0.1.1.tar.gz/fitloop-0.1.1/setup.py
@@ -0,0 +1,32 @@
+from pathlib import Path
+from setuptools import setup, find_packages
+
+HERE = Path(__file__).parent
+README = (HERE/"README.md").read_text()
+
+setup(
+  name="fitloop",
+  version="0.1.1",
+  description="fitloop trains Pytorch models",
+  packages=find_packages(),
+  long_description=README,
+  long_description_content_type="text/markdown",
+  license="MIT",
+  author="Alan",
+  author_email="2.alan.tom@gmail.com",
+  url="https://github.com/18alantom/fitloop",
+  classifiers= [
+    "License :: OSI Approved :: MIT License",
+    "Programming Language :: Python :: 3.7",
+    "Topic :: Scientific/Engineering :: Artificial Intelligence",
+    "Operating System :: OS Independent",
+    "Framework :: Jupyter"
+  ],
+  include_package_data=True,
+  install_requires=[
+    "torch>=1.4.0",
+    "matplotlib>=3.2.1",
+    "numpy>=1.18.0",
+    "tqdm"
+  ]
+)
