edba3de1f45365a2cc2d4a77a3d0fca2916df938
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:13:31 +0000

    Add package IOTAssignmentServerdorachua/0.0.49/IOTAssignmentServerdorachua-0.0.49.tar.gz

---
+++ packages/IOTAssignmentServerdorachua/IOTAssignmentServerdorachua-0.0.49.tar.gz/IOTAssignmentServerdorachua-0.0.49/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="IOTAssignmentServerdorachua", # Replace dorachua with your own username (no space)
+    version="0.0.49",
+    author="Dora Chua",
+    author_email="msdorachua@gmail.com",
+    description="A small example package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypa/sampleproject",
+    packages=setuptools.find_packages(),
+    install_requires=['numpy','mysql-connector-python','IOTAssignmentUtilitiesdorachua'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],    
+    python_requires='>=3.6',
+)
