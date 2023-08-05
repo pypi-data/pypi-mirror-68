8f558c86e6f7d6e2810789b2d3dc164d38fcd89d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:42:57 +0000

    Add package semantyk/0.0.0.10/semantyk-0.0.0.10.tar.gz

---
+++ packages/semantyk/semantyk-0.0.0.10.tar.gz/semantyk-0.0.0.10/semantyk/data_visualization_models/__init__.py
@@ -0,0 +1,22 @@
+##############################################
+#
+# data_visualization_models
+# Package | Interactive & Visual Models for Data Representation Package.
+#
+# Daniel Bakas Amuchastegui
+# September 3, 2019
+# 
+# Copyright © Semantyk 2020. All rights reserved.
+#############################################
+
+from . import article
+from . import cell
+from . import file
+from . import graph
+from . import table
+
+from .article import *
+from .cell import *
+from .file import *
+from .graph import *
+from .table import *
