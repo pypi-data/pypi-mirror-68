7969cf37538552b91cc3480940be8edc4a83d3b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:41:39 +0000

    Add package domdf-python-tools/0.3.1/domdf_python_tools-0.3.1.tar.gz

---
+++ packages/domdf-python-tools/domdf_python_tools-0.3.1.tar.gz/domdf_python_tools-0.3.1/README.rst
@@ -0,0 +1,58 @@
+**********************
+domdf_python_tools
+**********************
+
+.. image:: https://travis-ci.com/domdfcoding/domdf_python_tools.svg?branch=master
+    :target: https://travis-ci.com/domdfcoding/domdf_python_tools
+    :alt: Build Status
+.. image:: https://readthedocs.org/projects/domdf_python_tools/badge/?version=latest
+    :target: https://domdf_python_tools.readthedocs.io/en/latest/?badge=latest
+    :alt: Documentation Status
+.. image:: https://img.shields.io/pypi/v/domdf_python_tools.svg
+    :target: https://pypi.org/project/domdf_python_tools/
+    :alt: PyPI
+.. image:: https://img.shields.io/pypi/pyversions/domdf_python_tools.svg
+    :target: https://pypi.org/project/domdf_python_tools/
+    :alt: PyPI - Python Version
+.. image:: https://img.shields.io/conda/v/domdfcoding/domdf_python_tools
+    :alt: Conda
+    :target: https://anaconda.org/domdfcoding/domdf_python_tools
+.. image:: https://coveralls.io/repos/github/domdfcoding/domdf_python_tools/badge.svg?branch=master
+    :target: https://coveralls.io/github/domdfcoding/domdf_python_tools?branch=master
+    :alt: Coverage
+.. image:: https://img.shields.io/badge/License-LGPL%20v3-blue.svg
+    :alt: PyPI - License
+    :target: https://github.com/domdfcoding/domdf_python_tools/blob/master/LICENSE
+
+
+
+Helpful functions for Python
+
+|
+
+Installation
+--------------
+
+``domdf_python_tools`` can be installed from PyPI or Anaconda.
+
+To install with ``pip``:
+
+.. code-block:: bash
+
+    $ python -m pip install domdf_python_tools
+
+To install with ``conda``:
+
+    * First add the required channels
+
+    .. code-block:: bash
+
+        $ conda config --add channels http://conda.anaconda.org/domdfcoding
+        $ conda config --add channels http://conda.anaconda.org/conda-forge
+
+    * Then install
+
+    .. code-block:: bash
+
+        $ conda install domdf_python_tools
+
---
+++ packages/domdf-python-tools/domdf_python_tools-0.3.1.tar.gz/domdf_python_tools-0.3.1/__pkginfo__.py
@@ -0,0 +1,68 @@
+# Copyright (C) 2019-2020 Dominic Davis-Foster <dominic@davis-foster.co.uk>
+#
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+# This script based on https://github.com/rocky/python-uncompyle6/blob/master/__pkginfo__.py
+
+import pathlib
+
+copyright = """
+2019-2020 Dominic Davis-Foster <dominic@davis-foster.co.uk>
+"""
+
+VERSION = "0.3.1"
+
+modname = "domdf_python_tools"
+py_modules = None
+entry_points = None
+
+license = 'LGPLv3+'
+
+short_desc = 'Helpful functions for Python'
+
+author = "Dominic Davis-Foster"
+author_email = "dominic@davis-foster.co.uk"
+github_username = "domdfcoding"
+web = github_url = f"https://github.com/{github_username}/{modname}"
+project_urls = {
+		"Documentation": f"https://{modname}.readthedocs.io",  # TODO: Make this link match the package version
+		"Issue Tracker": f"{github_url}/issues",
+		"Source Code": github_url,
+		}
+
+repo_root = pathlib.Path(__file__).parent
+
+# Get info from files; set: long_description
+long_description = (repo_root / "README.rst").read_text() + '\n'
+install_requires = (repo_root / "requirements.txt").read_text().split('\n')
+extras_require = {"dates": ["pytz>=2019.1"]}
+
+classifiers = [
+		'Development Status :: 4 - Beta',
+		# "Development Status :: 4 - Beta",
+		# "Development Status :: 5 - Production/Stable",
+		# "Development Status :: 6 - Mature",
+		# "Development Status :: 7 - Inactive",
+		'Intended Audience :: Developers',
+		"License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)",
+		'Operating System :: OS Independent',
+		'Programming Language :: Python',
+		'Programming Language :: Python :: 3.6',
+		'Programming Language :: Python :: 3.7',
+		'Programming Language :: Python :: 3.8',
+		"Programming Language :: Python :: 3 :: Only",
+		"Programming Language :: Python :: Implementation :: CPython",
+		"Programming Language :: Python :: Implementation :: PyPy",
+		'Topic :: Software Development :: Libraries :: Python Modules',
+		]
