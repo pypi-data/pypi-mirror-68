df54dddd2d3ac62095f17706ee4c8a85d3cc6b25
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:35 +0000

    Add package sbp/2.8.0/sbp-2.8.0-cp38-cp38-linux_armv7l.whl

---
+++ packages/sbp/sbp-2.8.0-cp38-cp38-linux_armv7l.whl/sbp/_version.py
@@ -0,0 +1,7 @@
+# THIS FILE IS GENERATED FROM TRAITS SETUP.PY
+version = '2.8.0'
+full_version = '2.8.0'
+git_revision = '165fdafb'
+is_released = True
+if not is_released:
+    version = full_version
