129f302398d0ffb6591a11165c309861579754f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:56 +0000

    Add package spaceone-inventory/0.1.2.post85/spaceone_inventory-0.1.2.post85-py3-none-any.whl

---
+++ packages/spaceone-inventory/spaceone_inventory-0.1.2.post85-py3-none-any.whl/spaceone/inventory/conf/global_conf.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+
+DATABASES = {
+    'default': {
+        'db': 'inventory',
+        'host': 'localhost',
+        'port': 27017,
+        'username': '',
+        'password': ''
+    }
+}
+
+CACHES = {
+    'default': {},
+    'local': {
+        'backend': 'spaceone.core.cache.local_cache.LocalCache',
+        'max_size': 128,
+        'ttl': 86400
+    }
+}
+
+HANDLERS = {
+}
+
+CONNECTORS = {
+    'IdentityConnector': {
+    },
+    'SecretConnector': {
+    },
+    'PluginConnector': {
+    },
+    'CollectorPluginConnector': {
+    },
+
+}
+
+ENDPOINTS = {}
+LOG = {}
+QUEUES = {}
+SCHEDULERS = {}
+WORKERS = {}
+TOKEN = ""
+TOKEN_INFO = {}
