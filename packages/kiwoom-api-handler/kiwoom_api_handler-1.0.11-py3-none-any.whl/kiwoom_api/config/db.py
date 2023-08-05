6a3046df62ff6985fc8c574e2b9566f3c73b41b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:04 +0000

    Add package kiwoom-api-handler/1.0.11/kiwoom_api_handler-1.0.11-py3-none-any.whl

---
+++ packages/kiwoom-api-handler/kiwoom_api_handler-1.0.11-py3-none-any.whl/kiwoom_api/config/db.py
@@ -0,0 +1,10 @@
+
+class DBConfig:
+    config = {
+        'host' : 'db.qrft.io',
+        'port' : 3306,
+        'user' : 'financeteam',
+        'passwd' : 'legendfinance12!',
+        'db' : 'financeteam_stock_trading',
+        'charset' : 'utf8',
+    } 
