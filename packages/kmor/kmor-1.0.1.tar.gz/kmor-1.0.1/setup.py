aad93dae55050da968b45a51969c58e08a03ebf6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:12 +0000

    Add package kmor/1.0.1/kmor-1.0.1.tar.gz

---
+++ packages/kmor/kmor-1.0.1.tar.gz/kmor-1.0.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='kmor',
+    version='1.0.1',
+    description='Python wrapper for liquibase',
+    author='Sunho Kim',
+    author_email='ksunhokim123@naver.com',
+    url='https://github.com/sunho/kmor-np',
+    install_requires=['numpy'],
+    keywords=['kmor', 'anomality detection', 'cleaning', 'k-mean', 'data science'],
+    python_requires='>=3'
+)
+
