07ec9d28daad4e0a7f84f8ac93c6fb940fc74e63
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:40:46 +0000

    Add package ijr/0.0.15/ijr-0.0.15.tar.gz

---
+++ packages/ijr/ijr-0.0.15.tar.gz/ijr-0.0.15/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+
+__version__ = '0.0.15'
+
+setup(
+    name='ijr',
+    version=__version__,
+    packages=['ijr'],
+    url='https://github.com/IJsvogel/ijr',
+    license='MIT',
+    author='Herman Holterman',
+    author_email='herman.holterman@gmail.com',
+    description='IJsvogel Package',
+    install_requires=['pymongo', 'google-cloud-pubsub']
+)
