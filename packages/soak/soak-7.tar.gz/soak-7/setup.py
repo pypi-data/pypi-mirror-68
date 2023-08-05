d4182bc4770b85c724f824845d814a42d7d57aa9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:13 +0000

    Add package soak/7/soak-7.tar.gz

---
+++ packages/soak/soak-7.tar.gz/soak-7/setup.py
@@ -0,0 +1,42 @@
+import os, setuptools
+
+def long_description():
+    with open('README.md') as f:
+        return f.read()
+
+packages = setuptools.find_packages()
+
+def ext_modules():
+    def g():
+        suffix = '.pyx'
+        for package in packages:
+            dirpath = package.replace('.', os.sep)
+            for name in os.listdir(dirpath):
+                if name.endswith(suffix):
+                    path = os.path.join(dirpath, name)
+                    g = {}
+                    with open(path + 'bld') as f:
+                        exec(f.read(), g)
+                    yield g['make_ext'](package + '.' + name[:-len(suffix)], path)
+    paths = list(g())
+    if paths:
+        # XXX: Can cythonize be deferred?
+        from Cython.Build import cythonize
+        return dict(ext_modules = cythonize(paths))
+    return {}
+
+setuptools.setup(
+        name = 'soak',
+        version = '7',
+        description = 'Process aridity templates en masse, with secrets decryption',
+        long_description = long_description(),
+        long_description_content_type = 'text/markdown',
+        url = 'https://github.com/combatopera/soak',
+        author = 'Andrzej Cichocki',
+        packages = packages,
+        py_modules = [],
+        install_requires = ['aridity', 'diapyr', 'lagoon', 'PyYAML', 'splut'],
+        package_data = {'': ['*.pxd', '*.pyx', '*.pyxbld', '*.arid', '*.aridt']},
+        scripts = [],
+        entry_points = {'console_scripts': ['soak=soak.soak:main_soak']},
+        **ext_modules())
