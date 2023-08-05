9c032c45baa9c341d5c0df4544941ea92be17063
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:42:02 +0000

    Add package aics-dask-utils/0.2.0/aics_dask_utils-0.2.0.tar.gz

---
+++ packages/aics-dask-utils/aics_dask_utils-0.2.0.tar.gz/aics_dask_utils-0.2.0/aics_dask_utils/__init__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for AICS Dask Utils."""
+
+__author__ = "Jackson Maxfield Brown"
+__email__ = "jacksonb@alleninstitute.org"
+# Do not edit this string manually, always use bumpversion
+# Details in CONTRIBUTING.md
+__version__ = "0.2.0"
+
+
+def get_module_version():
+    return __version__
+
+
+from .distributed_handler import DEFAULT_MAX_THREADS, DistributedHandler  # noqa: F401
