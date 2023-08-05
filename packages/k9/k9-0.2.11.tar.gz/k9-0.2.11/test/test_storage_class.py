fdc9dad643e00c49ad0ceaabe3f30fb2bd253cda
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:11 +0000

    Add package k9/0.2.11/k9-0.2.11.tar.gz

---
+++ packages/k9/k9-0.2.11.tar.gz/k9-0.2.11/test/test_storage_class.py
@@ -0,0 +1,24 @@
+from unittest import TestCase
+from k9.storage import *
+import subprocess
+
+class TestStorage(TestCase):
+    def test_create_storage_class(self):
+        name = 'aws-storage-class'
+
+        print("")
+        create_storage_class('storage.yml')
+
+        self.assertTrue(storage_class_exists(name))
+
+        delete_storage_class(name)
+
+        self.assertFalse(storage_class_exists(name))
+
+    def test_create_storage_class_fail(self):
+        with self.assertRaisesRegex(subprocess.CalledProcessError, 'returned non-zero exit status 1.'):
+            create_storage_class('bogus')
+
+    def test_delete_storage_class_fail(self):
+        with self.assertRaisesRegex(subprocess.CalledProcessError, 'returned non-zero exit status 1.'):
+            delete_storage_class('bogus')
