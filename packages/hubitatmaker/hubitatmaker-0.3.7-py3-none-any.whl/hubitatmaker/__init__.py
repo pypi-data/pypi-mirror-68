806963d35a8c7a8dbc05ad992201d476d3d982c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:16:05 +0000

    Add package hubitatmaker/0.3.7/hubitatmaker-0.3.7-py3-none-any.whl

---
+++ packages/hubitatmaker/hubitatmaker-0.3.7-py3-none-any.whl/hubitatmaker/__init__.py
@@ -0,0 +1,6 @@
+from .const import *  # noqa
+from .error import ConnectionError, InvalidConfig, InvalidToken, RequestError  # noqa
+from .hub import Hub  # noqa
+from .types import Attribute, Device, Event  # noqa
+
+__version__ = "0.3.7"
