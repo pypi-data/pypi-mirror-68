0c4ef53f642a8ebfa41770981a06820bfdfcfecd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:40 +0000

    Add package qaseio/1.1.0b5/qaseio-1.1.0b5-py2.py3-none-any.whl

---
+++ packages/qaseio/qaseio-1.1.0b5-py2.py3-none-any.whl/qaseio/client/models/__init__.py
@@ -0,0 +1,11 @@
+# flake8: noqa
+from .base import *
+from .cases import *
+from .defects import *
+from .milestones import *
+from .plans import *
+from .projects import *
+from .results import *
+from .runs import *
+from .shared_steps import *
+from .suites import *
