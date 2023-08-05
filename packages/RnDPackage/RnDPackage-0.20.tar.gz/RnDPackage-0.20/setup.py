258ee4120dfce7b9dd6f68098b01a0d560f4debe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:39:12 +0000

    Add package RnDPackage/0.20/RnDPackage-0.20.tar.gz

---
+++ packages/RnDPackage/RnDPackage-0.20.tar.gz/RnDPackage-0.20/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name="RnDPackage",
+    version="0.20",
+    description="Shared packages for R&D projects",
+    url="https://github.com/mehtarjt/python",
+    author="RnD",
+    author_email="mehtarjt@gmail.com",
+    license="MIT",
+    packages=["RnDPackage"],
+    zip_safe=False,
+    include_package_data=True,
+)
