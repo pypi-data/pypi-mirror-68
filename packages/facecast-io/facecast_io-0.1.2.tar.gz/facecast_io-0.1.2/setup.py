dff6715d3dfcc9517f6e3c2b3e43a011979a6046
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:27 +0000

    Add package facecast-io/0.1.2/facecast_io-0.1.2.tar.gz

---
+++ packages/facecast-io/facecast_io-0.1.2.tar.gz/facecast_io-0.1.2/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['facecast_io']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['httpx>=0.12.1,<0.13.0', 'pyquery>=1.4.1,<2.0.0', 'yarl>=1.4.2,<2.0.0']
+
+setup_kwargs = {
+    'name': 'facecast-io',
+    'version': '0.1.2',
+    'description': '',
+    'long_description': None,
+    'author': 'Serhii Khalymon',
+    'author_email': 'sergiykhalimon@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.6,<4.0',
+}
+
+
+setup(**setup_kwargs)
