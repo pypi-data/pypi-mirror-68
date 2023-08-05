bb288d55e90be5b8cdba37aaa8915a45d2fc386c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:06 +0000

    Add package semantyk/0.0.0.15/semantyk-0.0.0.15.tar.gz

---
+++ packages/semantyk/semantyk-0.0.0.15.tar.gz/semantyk-0.0.0.15/semantyk/archive/user/editor.py
@@ -0,0 +1,17 @@
+##############################################
+#
+# editor
+# Module | Editor Module.
+#
+# Daniel Bakas Amuchastegui
+# March 18, 2020
+# 
+# Copyright © Semantyk 2020. All rights reserved.
+#############################################
+
+from semantyk.archive.user import User
+from rdflib import URIRef
+
+class Editor(User): 
+    def __init__(self, id:str = None, dictionary:dict = None, triples:set = None, uri:URIRef = None):
+        super().__init__(id, dictionary, triples, uri)
---
+++ packages/semantyk/semantyk-0.0.0.15.tar.gz/semantyk-0.0.0.15/semantyk/data_visualization_models/graph/timeline.py
@@ -0,0 +1,15 @@
+##############################################
+#
+# timeline
+# Module | Visual, linear arrangements of objects of type Event.
+#
+# Daniel Bakas Amuchastegui
+# September 3, 2019
+# 
+# Copyright © Semantyk 2020. All rights reserved.
+#############################################
+
+from semantyk.data_visualization_models.graph import Graph
+
+class Timeline(Graph):
+    pass
