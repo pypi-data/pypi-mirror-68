defa6b57342b34dc5be9ec9ef923b41e4797a59c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:14:27 +0000

    Add package x10-blackjack/0.3.1/x10_blackjack-0.3.1-py3-none-any.whl

---
+++ packages/x10-blackjack/x10_blackjack-0.3.1-py3-none-any.whl/blackjack/__init__.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+
+# This value is not the one controlling the version in the (GitLab) pipeline
+# This value is only necessary for automated pickup, and for manual deployments to PyPI
+version = '0.3.1'
