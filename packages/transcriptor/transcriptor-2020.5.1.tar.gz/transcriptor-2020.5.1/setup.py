69c291bb704955ff5cb29c619ea0dd0e433e965b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:37:42 +0000

    Add package transcriptor/2020.5.1/transcriptor-2020.5.1.tar.gz

---
+++ packages/transcriptor/transcriptor-2020.5.1.tar.gz/transcriptor-2020.5.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+        name='transcriptor',
+        version='2020.5.1',
+        description='A wrapper for transcription results.',
+        url='https://github.com/kjaymiller/transcriptor',
+        author='Jay Miller',
+        license='MIT',
+        packages=find_packages(),
+        zip_safe=False,
+        )
