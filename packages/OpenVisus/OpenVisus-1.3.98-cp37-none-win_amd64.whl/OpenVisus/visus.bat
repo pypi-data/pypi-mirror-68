d4f7ecb05d80fb8d82623e8c26e0dc5a87ee333f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:56 +0000

    Add package OpenVisus/1.3.98/OpenVisus-1.3.98-cp37-none-win_amd64.whl

---
+++ packages/OpenVisus/OpenVisus-1.3.98-cp37-none-win_amd64.whl/OpenVisus/visus.bat
@@ -0,0 +1,5 @@
+
+cd %~dp0
+set PATH=C:\Python37-x64\python.exe\..;%PATH%;.\bin
+set PYTHONPATH=C:\projects\openvisus\build_win\Release\OpenVisus;C:\Python37-x64\python37.zip;C:\Python37-x64\DLLs;C:\Python37-x64\lib;C:\Python37-x64;C:\Python37-x64\lib\site-packages
+bin\visus.exe" %*
