405c182d85db385c43c779111fc03086c1d92370
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:37 +0000

    Add package semantyk/0.0.0.28/semantyk-0.0.0.28.tar.gz

---
+++ packages/semantyk/semantyk-0.0.0.28.tar.gz/semantyk-0.0.0.28/semantyk/data_visualization_models/cell/text_cell/text_cell.py
@@ -0,0 +1,15 @@
+##############################################
+#
+# text_cell
+# Module | Text Cell Module.
+#
+# Daniel Bakas Amuchastegui
+# February 20, 2020
+# 
+# Copyright © Semantyk 2020. All rights reserved.
+#############################################
+
+from semantyk.data_visualization_models.cell.cell import Cell
+
+class TextCell(Cell):
+    pass
