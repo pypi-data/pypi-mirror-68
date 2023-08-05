914c68d4491c5b7f6be8e1a017afd2f25087d6e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:14:06 +0000

    Add package caelus/0.0.5/caelus-0.0.5.tar.gz

---
+++ packages/caelus/caelus-0.0.5.tar.gz/caelus-0.0.5/caelus/__init__.py
@@ -0,0 +1,13 @@
+__version__ = '0.0.5'
+
+import logging
+
+logging.basicConfig(level=logging.INFO,
+                    format='%(asctime)s | %(levelname)s | %(name)s | %(funcName)s | %(message)s',
+                    datefmt='%d-%m-%y %H:%M:%S')
+
+__all__ = [
+    'aws',
+    'az',
+    'gcp',
+]
