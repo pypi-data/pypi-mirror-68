5a94d1eb4d7b12ad0d0b8b0040da0048b211a045
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:11 +0000

    Add package raiden-services/0.9.0/raiden-services-0.9.0.tar.gz

---
+++ packages/raiden-services/raiden-services-0.9.0.tar.gz/raiden-services-0.9.0/src/raiden_libs/states.py
@@ -0,0 +1,13 @@
+from dataclasses import dataclass, field
+from typing import List, Optional
+
+from raiden.utils.typing import Address, BlockNumber, ChainID, TokenNetworkAddress
+
+
+@dataclass
+class BlockchainState:
+    chain_id: ChainID
+    token_network_registry_address: Address
+    latest_committed_block: BlockNumber
+    monitor_contract_address: Optional[Address] = None
+    token_network_addresses: List[TokenNetworkAddress] = field(default_factory=list)
