e4b74f66a7b77ab33932cfdb66d6f75233d18de9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:06 +0000

    Add package probability-estimation/0.1/probability_estimation-0.1.tar.gz

---
+++ packages/probability-estimation/probability_estimation-0.1.tar.gz/probability_estimation-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='probability_estimation',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['probability_estimation'],
+      author = 'Richu Thomas',
+      author_email = 'richuthomas98@gmail.com',
+      zip_safe=False)
