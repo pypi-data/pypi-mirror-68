656253cbb74980e81a888a9cf1e1908641d58b2f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:15:35 +0000

    Add package addmul/0.0.2/addmul-0.0.2.tar.gz

---
+++ packages/addmul/addmul-0.0.2.tar.gz/addmul-0.0.2/setup.py
@@ -0,0 +1,12 @@
+import setuptools
+
+setuptools.setup(
+  name="addmul",
+  version="0.0.2",
+  description="test Package",
+  author="bgjoe40",
+  packages=setuptools.find_packages(),
+  author_email="bgjoe40@gmail.com",
+  license="MIT",
+  classifiers=[]
+)
