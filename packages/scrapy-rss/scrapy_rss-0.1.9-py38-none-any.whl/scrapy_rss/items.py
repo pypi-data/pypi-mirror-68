f65e746b447800f8a2815dee057991c7a4835f20
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:03 +0000

    Add package scrapy-rss/0.1.9/scrapy_rss-0.1.9-py38-none-any.whl

---
+++ packages/scrapy-rss/scrapy_rss-0.1.9-py38-none-any.whl/scrapy_rss/items.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+
+import scrapy
+from scrapy.item import BaseItem
+from scrapy_rss.elements import *
+from scrapy_rss import meta
+import six
+
+
+@six.add_metaclass(meta.ItemMeta)
+class RssItem(BaseItem):
+    title = TitleElement()
+    link = LinkElement()
+    description = DescriptionElement()
+    author = AuthorElement()
+    category = meta.MultipleElements(CategoryElement)
+    comments = CommentsElement()
+    enclosure = EnclosureElement()
+    guid = GuidElement()
+    pubDate = PubDateElement()
+    source = SourceElement()
+
+
+class ExtendableItem(scrapy.Item):
+    def __setattr__(self, name, value):
+        if name in self.fields:
+            raise AttributeError("Use item[{!r}] = {!r} to set field value".format(name, value))
+        super(BaseItem, self).__setattr__(name, value)
+
+
+class RssedItem(ExtendableItem):
+    def __init__(self, *args, **kwargs):
+        super(RssedItem, self).__init__(*args, **kwargs)
+        self.rss = RssItem()
+
