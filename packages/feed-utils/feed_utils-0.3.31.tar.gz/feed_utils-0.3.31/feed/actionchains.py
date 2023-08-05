65e85ee81ec30aaf3fcd286646af1f0ca0511599
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:00 +0000

    Add package feed-utils/0.3.31/feed_utils-0.3.31.tar.gz

---
+++ packages/feed-utils/feed_utils-0.3.31.tar.gz/feed_utils-0.3.31/feed/actionchains.py
@@ -0,0 +1,337 @@
+import logging
+import traceback
+import requests
+import json
+from feed.actiontypes import ReturnTypes
+from bs4 import BeautifulSoup
+from feed.settings import kafka_params, routing_params
+import signal
+
+
+from kafka import KafkaConsumer, KafkaProducer
+
+
+
+class GracefulKiller:
+    kill_now = False
+    def __init__(self):
+        signal.signal(signal.SIGINT, self.exit_gracefully)
+        signal.signal(signal.SIGTERM, self.exit_gracefully)
+
+    def exit_gracefully(self,signum, frame):
+        self.kill_now = True
+
+
+
+class ObjectSearchParams:
+    def __init__(self, **kwargs):
+        #super().__init__(**kwargs)
+        self.isSingle = kwargs.get('isSingle', False)
+        self.returnType = kwargs.get('returnType', 'src')
+        self.attribute = kwargs.get('attribute', None)
+
+    def __dict__(self):
+        return self.kwargs
+
+    def _verifyResultLength(self, items):
+        if len(items) == 0:
+            return False
+        if self.isSingle and len(items) > 1:
+            self.backup = items
+            return False
+        else:
+            return True
+
+    def _returnItem(item, driver):
+        raise NotImplementedError
+    def search(item, driver):
+        raise NotImplementedError
+
+
+class BrowserSearchParams(ObjectSearchParams):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.kwargs = kwargs
+        self.css = kwargs.get('css')
+        self.xpath = kwargs.get('xpath')
+        self.text = kwargs.get('text')
+        #self.text = kwargs.get('class')
+        self.backup = None
+
+    def _returnItem(self, item, driver):
+        if self.returnType == 'text':
+            formatted = lambda item: item.text
+        elif self.returnType == 'src':
+            classes = set([element.get_attribute('class') for element in item])
+            soup = BeautifulSoup(driver.page_source)
+            out = []
+            for cls in classes:
+                out.extend(soup.findAll(attrs={'class': cls}))
+            formatted = lambda item: item
+        elif self.returnType == 'attr':
+            formatted = lambda element: element.get_attribute(self.attribute)
+        elif self.returnType == 'element':
+            formatted = lambda item: item
+        return list(map(formatted, item)) if len (item) > 1 else formatted(item[0])
+
+    def search(self, driver):
+
+        # first try css selector
+        logging.info(f'{type(self).__name__}::search(driver): searching for elemnent with css=[{self.css}]')
+        ret = driver.find_elements_by_css_selector(self.css)
+        if self._verifyResultLength(ret):
+            logging.debug(f'found element [{ret}] with css')
+            return self._returnItem(ret, driver)
+
+        # then try xpath
+        logging.info(f'{type(self).__name__}::search(driver): searching for elemnent with xpath=[{self.xpath}]')
+        ret = driver.find_elements_by_xpath(self.xpath)
+        if self._verifyResultLength(ret):
+            logging.debug(f'found element [{ret}] with xpath')
+            return self._returnItem(ret, driver)
+
+        # TODO: should search backup list with text at this point
+        if self.backup:
+            for res in filter(lambda item: item.text.upper() == self.text, self.backup):
+                logging.debug(f'using element [{ret}] from backup')
+                return self._returnItem([ret], driver)
+            return self._returnItem([self.backup[0]], driver)
+        else:
+            # TODO brute search with text at this point
+            return None
+
+
+class Action(BrowserSearchParams):
+
+    def __init__(self, position, **kwargs):
+        super().__init__(**kwargs)
+        self.kwargs = kwargs
+        self.position = position
+
+    @staticmethod
+    def execute(chain, action):
+        actionType = type(action).__name__
+        try:
+            ret = getattr(chain, f'on{actionType}')(action)
+            logging.debug(f'{actionType} has returned succesfully, name=[{chain.name}], position=[{action.position}]')
+            return ret
+        except Exception as ex:
+            traceback.print_exc()
+            logging.warning(f'{type(ex).__name__} thrown whilst processing name=[{chain.name}], position=[{action.position}], args=[{ex.args}]')
+            return False
+            # TODO Exception reporting callback called here
+            # OnClickException for example
+
+    def getActionableItem(self, action, driver):
+        item = self.search(driver)
+        logging.info(f'have item=[{item}], length=[{ 1 if not isinstance(item, list) else len(item)}]')
+        return item
+
+    @staticmethod
+    def get_params():
+        # TODO For UI-Server
+        return [self.__dict__().keys()]
+
+
+class CaptureAction(Action):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.returnType = 'src'
+        self.captureName = kwargs['captureName'] # mandatory
+        self.data = kwargs.get('data', None)
+
+    def __dict__(self):
+        return dict(**self.kwargs)
+
+class InputAction(Action):
+
+    def __init__(self, inputString, **kwargs):
+        super().__init__(**kwargs)
+        self.insputString = inputString
+        self.isSingle = True
+        self.returnType = 'element'
+
+    def __dict__(self):
+        return dict(inputString=self.insputString, **self.kwargs)
+
+class PublishAction(Action):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.urlStub = kwargs.get('urlStub')
+
+    def __dict__(self):
+        return dict(url=self.url, urlStub=self.urlStub, **self.kwargs)
+
+class ClickAction(Action):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.isSingle = True
+        self.returnType = 'element'
+
+ActionTypes = {
+    "ClickAction": ClickAction,
+    "InputAction": InputAction,
+    "CaptureAction": CaptureAction,
+    "PublishAction": PublishAction
+}
+
+class ActionChain:
+    actions= {}
+
+    def __init__(self, **kwargs):
+        self.kwargs = kwargs
+        self.name = kwargs.get('name')
+        self.startUrl = kwargs.get('startUrl')
+        self.repeating = kwargs.get('isRepeating', True)
+        actionParams = kwargs.get('actions', [])
+        self.failedChain = False
+        for order, params in enumerate(actionParams):
+            try:
+                action = ActionChain.actionFactory(position=order, actionParams=params)
+                self.actions.update({order: action})
+            except KeyError as ex:
+                # TODO: At this point we should pass this onto the user
+                logging.error(f'{type(self).__name__}::__init__(): chainName=[{self.name}], position=[{order}] actionType=[{params.get("actionType")}] is missing {ex.args} default parameter')
+
+
+    def recoverHistory(self):
+        req = requests.get('http://{host}:{port}/routingcontroller/getLastPage/{name}'.format(name=self.name, **routing_params))
+        logging.info(f'have {req} from routing.')
+        data = req.json()
+        return data
+
+    def saveHistory(self, url):
+        pass
+
+    @staticmethod
+    def actionFactory(position, actionParams):
+        logging.info(f'ActionChain::actionFactory: node=[{position}]: {", ".join(map(lambda key: "{}=[{}]".format(key, actionParams[key]), actionParams))}')
+        actionConstructor = ActionTypes.get(actionParams.get('actionType'))
+        return actionConstructor(position=position, **actionParams)
+
+    def initialise(self, caller):
+        pass
+
+    """
+    following methods correspond to module://feed.actiontypes.ActionTypes
+    """
+    def onClickAction(self, action):
+        raise NotImplementedError
+    def onInputAction(self, action):
+        raise NotImplementedError
+    def onPublishAction(self, action):
+        raise NotImplementedError
+    def onCaptureAction(self, action):
+        raise NotImplementedError
+
+    def execute(self, caller, initialise=True):
+        self.failedChain = False
+        if initialise:
+            self.initialise(caller)
+        for i in range(len(self.actions)):
+            self.current_pos = i
+            action = self.actions.get(i)
+            logging.info(f'executing action {type(action).__name__}')
+            success = Action.execute(self, action)
+            if not success:
+                logging.info(f'Detected failure: actionType={type(action).__name__}, position={i}, name={self.name}. Will go straight to next action. {"Will not re-evaluate" if self.repeating else ""}')
+                self.failedChain = True
+                continue
+            callBackMethod = getattr(caller, f'on{type(action).__name__}Callback')
+            for item in success:
+                callBackMethod(item)
+            self.saveHistory()
+        if self.repeating and not self.failedChain:
+            self.execute(caller, initialise=False)
+
+    def getRepublishRoute(self, action):
+        if isinstance(action, CaptureAction):
+            return 'summarizer-route'
+        if isinstance(action, PublishAction):
+            return 'worker-route'
+        if isinstance(action, ActionChain):
+            # TODO: If publisher wants to invoke a chain to be ran, they should set action to be a new action chain object
+            return 'leader-route'
+
+    def rePublish(self, action, *args, **kwargs):
+        pass
+
+
+class KafkaChainPublisher(ActionChain):
+    pass
+
+
+class KafkaActionPublisher(ActionChain):
+
+    def __init__(self):
+        self.producer = KafkaProducer(**kafka_params, value_serializer=lambda m: bytes(json.dumps(m).encode('utf-8')))
+
+    def rePublish(self, actionReturn):
+        topic = self.getRepublishRoute(actionReturn.action)
+        self.producer.send(topic=topic, value=actionReturn.__dict__())
+        logging.debug(f'republished items for {type(actionReturn.action).__name__} to {topic}')
+
+
+class ActionChainRunner:
+
+    driver = None
+    def __init__(self, implementation, **kwargs):
+        self.implementation = implementation
+
+    def subscription(self):
+        pass
+
+    def onClickActionCallback(self, *args, **kwargs):
+        logging.info(f'onClickActionCallback')
+
+    def onInputActionCallback(self, *args, **kwargs):
+        logging.info(f'onInputActionCallback')
+
+    def onPublishActionCallback(self, *args, **kwargs):
+        logging.info(f'onPublishActionCallback')
+
+    def onCaptureActionCallback(self, *args, **kwargs):
+        logging.info(f'onCaptureActionCallback')
+
+    def initialiseCallback(self, *args, **kwargs):
+        logging.info('initialiseCallback')
+
+    def main(self):
+        killer = GracefulKiller()
+        for actionChainParams in self.subscription():
+            if killer.kill_now:
+                self.cleanUp()
+                logging.info(f'cleaned up resources')
+                break
+            logging.debug(f'implementing action chain {actionChainParams.get("name")}: {json.dumps(actionChainParams, indent=4)}')
+            actionChain = self.implementation(driver=self.driver, **actionChainParams)
+            ret = actionChain.execute(self)
+
+    def cleanUp(self):
+        logging.warning(f'ActionChainRunner::cleanUp() No cleanup has been implemented')
+
+
+class KafkaActionSubscription(ActionChainRunner):
+
+    def __init__(self, topic,  **kwargs):
+        super().__init__(**kwargs)
+        self._consumer = KafkaConsumer(**kafka_params, value_deserializer=lambda m: json.loads(m.decode('utf-8')))
+        self._consumer.subscribe([topic])
+
+    def subscription(self):
+        for mes in self._consumer:
+            yield mes.value
+
+
+class CommandsActionSubscription(ActionChainRunner):
+
+    def __init__(self, endpoint, actionsImpl):
+        super().__init__(actionsImpl)
+        self.endpoint = endpoint
+
+    def subscription(self):
+        while True:
+            actionReq = requests.get(endpoint)
+            action = actionReq.json()
+            yield action
+
