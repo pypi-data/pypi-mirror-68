13c2241ef74ae5e614fc1c1a8f610ac5e1661b2c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:36:13 +0000

    Add package weather-reporter-1.1/1.0.1/weather-reporter-1.1-1.0.1.tar.gz

---
+++ packages/weather-reporter-1.1/weather-reporter-1.1-1.0.1.tar.gz/weather-reporter-1.1-1.0.1/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup
+
+def readme():
+    with open('README.md') as f:
+        README = f.read()
+    return README
+
+
+setup(
+    name="weather-reporter-1.1",
+    version="1.0.1",
+    description="A Python package to get weather reports for any location.",
+    long_description=readme(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/Raghu960/weather-reporter",
+    author="Raghavendra Deshmukh",
+    author_email="raghu.deshmukh55@gmail.com",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+    ],
+    packages=["weather_reporter"],
+    include_package_data=True,
+    install_requires=["requests"],
+    entry_points={
+        "console_scripts": [
+            "weather-reporter=weather_reporter.cli:main",
+        ]
+    },
+)
