012663d28d172b2b1049084d4473ad7d794c610a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:13:59 +0000

    Add package django-paddle/0.0.8/django_paddle-0.0.8-py3-none-any.whl

---
+++ packages/django-paddle/django_paddle-0.0.8-py3-none-any.whl/django_paddle/client.py
@@ -0,0 +1,119 @@
+from copy import copy
+import requests
+from django.conf import settings
+
+
+class PaddleClient:
+
+    def __init__(self):
+        self.base_url = 'https://vendors.paddle.com/api/2.0/'
+        self.vendor_id = settings.PADDLE_VENDOR_ID
+        self.vendor_auth_code = settings.PADDLE_AUTH_CODE
+        self.base_payload = {
+            'vendor_id': self.vendor_id,
+            'vendor_auth_code': self.vendor_auth_code
+        }
+
+    # Plans
+
+    def plans_list(self):
+        rsp = requests.post(
+            url=self.base_url + 'subscription/plans',
+            json=self.base_payload
+        )
+        return rsp.json()['response']
+
+    def plans_get(self, plan_id):
+        payload = copy(self.base_payload)
+        payload.update(plan_id=plan_id)
+        rsp = requests.post(
+            url=self.base_url + 'subscription/plans',
+            json=payload
+        )
+        return rsp.json()['response'][0]
+
+    # Subscriptions
+
+    def subscriptions_list(self, include_deleted=None):
+
+        subscriptions = []
+        max_results = 200
+        payload = copy(self.base_payload)
+        payload.update(page=1, results_per_page=max_results)
+        if include_deleted:
+            payload.update(state='deleted')
+
+        while True:
+            data = requests.post(
+                url=self.base_url + 'subscription/users',
+                json=payload
+            ).json()['response']
+            subscriptions += data
+            if len(data) < max_results:
+                break
+            else:
+                payload['page'] += 1
+
+        return subscriptions
+
+    def subscriptions_get(self, subscription_id):
+        payload = copy(self.base_payload)
+        payload.update(subscription_id=subscription_id)
+        return requests.post(
+            url=self.base_url + 'subscription/users',
+            json=payload
+        ).json()['response'][0]
+
+    def subscriptions_cancel(self, subscription_id):
+        payload = copy(self.base_payload)
+        payload.update(
+            subscription_id=subscription_id
+        )
+        requests.post(
+            url=self.base_url + 'subscription/users_cancel',
+            json=payload
+        )
+
+    def subscriptions_pause(self, subscription_id):
+        payload = copy(self.base_payload)
+        payload.update(
+            subscription_id=subscription_id,
+            pause=True
+        )
+        requests.post(
+            url=self.base_url + 'subscription/users/update',
+            json=payload
+        )
+
+    def subscriptions_unpause(self, subscription_id):
+        payload = copy(self.base_payload)
+        payload.update(
+            subscription_id=subscription_id,
+            pause=False
+        )
+        requests.post(
+            url=self.base_url + 'subscription/users/update',
+            json=payload
+        )
+
+    # Payments
+
+    def payments_list(self, subscription_id=None, is_paid=None):
+        payload = copy(self.base_payload)
+        if subscription_id:
+            payload.update(subscription_id=subscription_id)
+        if is_paid is not None:
+            payload.update(is_paid=is_paid)
+        return requests.post(
+            url=self.base_url + 'subscription/payments',
+            json=payload
+        ).json()['response']
+
+    # Transactions
+
+    def transactions_list(self, entity, id):
+        payload = copy(self.base_payload)
+        return requests.post(
+            url=self.base_url + '{}/{}/transactions'.format(entity, id),
+            json=payload
+        ).json()['response']
---
+++ packages/django-paddle/django_paddle-0.0.8-py3-none-any.whl/django_paddle/models.py
@@ -0,0 +1,187 @@
+from datetime import datetime
+
+from django.db import models
+from django.utils.timezone import make_aware
+from django_paddle.client import PaddleClient
+from django_paddle.utils import get_account_model, get_account_by_passthrough
+
+
+pc = PaddleClient()
+
+
+class PaddlePlan(models.Model):
+    id = models.PositiveIntegerField(
+        primary_key=True,
+        unique=True
+    )
+    name = models.CharField(max_length=255)
+    billing_type = models.CharField(max_length=255)
+    billing_period = models.PositiveIntegerField()
+    trial_days = models.PositiveIntegerField()
+
+    def initial_price_in(self, currency):
+        return self.initial_prices.get(currency=currency).amount
+
+    def recurring_price_in(self, currency):
+        return self.recurring_prices.get(currency=currency).amount
+
+    @staticmethod
+    def sync():
+        for plan in pc.plans_list():
+            plan_obj, _ = PaddlePlan.objects.update_or_create(
+                id=plan['id'],
+                defaults={
+                    'name': plan['name'],
+                    'billing_type': plan['billing_type'],
+                    'billing_period': plan['billing_period'],
+                    'trial_days': plan['trial_days']
+                }
+            )
+            for currency, amount in plan['initial_price'].items():
+                plan_obj.initial_prices.update_or_create(
+                    currency=currency,
+                    defaults={
+                        'amount': amount
+                    }
+                )
+            for currency, amount in plan['recurring_price'].items():
+                plan_obj.recurring_prices.update_or_create(
+                    currency=currency,
+                    defaults={
+                        'amount': amount
+                    }
+                )
+
+
+class PaddlePrice(models.Model):
+    plan = models.ForeignKey(
+        to=PaddlePlan,
+        on_delete=models.CASCADE,
+    )
+    currency = models.CharField(max_length=255)
+    amount = models.CharField(max_length=255)
+
+    class Meta:
+        abstract = True
+        unique_together = ['plan', 'currency']
+
+
+class PaddleInitialPrice(PaddlePrice):
+    class Meta:
+        default_related_name = 'initial_prices'
+
+
+class PaddleRecurringPrice(PaddlePrice):
+
+    class Meta:
+        default_related_name = 'recurring_prices'
+
+
+class PaddleSubscription(models.Model):
+    id = models.PositiveIntegerField(
+        primary_key=True,
+        unique=True
+    )
+    account = models.ForeignKey(
+        to=get_account_model(),
+        null=True,
+        on_delete=models.SET_NULL,
+        related_name='subscriptions'
+    )
+    plan = models.ForeignKey(
+        to=PaddlePlan,
+        null=True,
+        on_delete=models.SET_NULL,
+        related_name='subscriptions'
+    )
+    user_id = models.PositiveIntegerField()
+    user_email = models.EmailField()
+    marketing_consent = models.BooleanField()
+    update_url = models.CharField(max_length=255)
+    cancel_url = models.CharField(max_length=255)
+    state = models.CharField(max_length=255)
+    signup_date = models.DateTimeField()
+
+    def cancel(self):
+        pc.subscriptions_cancel(self.id)
+        self.state = 'deleted'
+        self.save()
+
+    def pause(self):
+        pc.subscriptions_pause(subscription_id=self.id)
+        self.state = 'paused'
+        self.save()
+
+    def unpause(self):
+        pc.subscriptions_unpause(subscription_id=self.id)
+        self.state = 'active'
+        self.save()
+
+    def sync_payments(self):
+        for payment in pc.payments_list(subscription_id=self.id):
+            defaults = {
+                'amount': payment['amount'],
+                'currency': payment['currency'],
+                'payout_date': make_aware(datetime.strptime(payment['payout_date'], '%Y-%m-%d')),
+                'is_paid': payment['is_paid'],
+                'is_one_off_charge': payment['is_one_off_charge'],
+            }
+            if 'receipt_url' in payment:
+                defaults['receipt_url'] = payment['receipt_url']
+            PaddlePayment.objects.update_or_create(
+                id=payment['id'],
+                subscription=self,
+                defaults=defaults
+            )
+
+    @staticmethod
+    def sync():
+        for sub in pc.subscriptions_list():
+            transaction = pc.transactions_list(entity='subscription', id=sub['subscription_id'])[0]
+            account = get_account_by_passthrough(transaction['passthrough'])
+
+            try:
+                plan = PaddlePlan.objects.get(id=sub['plan_id'])
+            except PaddlePlan.DoesNotExist:
+                plan = None
+
+            defaults = {
+                    'user_id': sub['user_id'],
+                    'user_email': sub['user_email'],
+                    'marketing_consent': sub['marketing_consent'],
+                    'update_url': sub['update_url'],
+                    'cancel_url': sub['cancel_url'],
+                    'state': sub['state'],
+                    'signup_date': make_aware(datetime.strptime(sub['signup_date'], '%Y-%m-%d %H:%M:%S'))
+            }
+
+            if plan:
+                defaults['plan'] = plan
+
+            if account:
+                defaults['account'] = account
+
+            PaddleSubscription.objects.update_or_create(
+                id=sub['subscription_id'],
+                defaults=defaults
+            )
+
+
+class PaddlePayment(models.Model):
+    id = models.PositiveIntegerField(
+        unique=True,
+        primary_key=True
+    )
+    subscription = models.ForeignKey(
+        to=PaddleSubscription,
+        on_delete=models.CASCADE,
+        related_name='payments'
+    )
+    amount = models.PositiveIntegerField()
+    currency = models.CharField(max_length=255)
+    payout_date = models.DateField(max_length=255)
+    is_paid = models.BooleanField()
+    is_one_off_charge = models.BooleanField()
+    receipt_url = models.CharField(max_length=255, null=True)
+
+
