7c53489881a3c1947ca3407e9db95cf8b3699675
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:44 +0000

    Add package digitaldraw/1.2/digitaldraw-1.2.tar.gz

---
+++ packages/digitaldraw/digitaldraw-1.2.tar.gz/digitaldraw-1.2/setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+setuptools.setup(
+    name="digitaldraw",
+    version="1.2",
+    author="zkr",
+    author_email="1290267206@qq.com",
+    description="Happy Mather Day",
+    url="http://www.haoqixincode.com/",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
