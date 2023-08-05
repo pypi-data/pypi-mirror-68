ef79e1b5b219c77a61a5e70b8bc97fd627000310
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:15:09 +0000

    Add package gmr/1.1/gmr-1.1.tar.gz

---
+++ packages/gmr/gmr-1.1.tar.gz/gmr-1.1/gmr/gmm.py
@@ -0,0 +1,296 @@
+import numpy as np
+from .utils import check_random_state
+from .mvn import MVN
+
+
+class GMM(object):
+    """Gaussian Mixture Model.
+
+    Parameters
+    ----------
+    n_components : int
+        Number of MVNs that compose the GMM.
+
+    priors : array, shape (n_components,), optional
+        Weights of the components.
+
+    means : array, shape (n_components, n_features), optional
+        Means of the components.
+
+    covariances : array, shape (n_components, n_features, n_features), optional
+        Covariances of the components.
+
+    verbose : int, optional (default: 0)
+        Verbosity level.
+
+    random_state : int or RandomState, optional (default: global random state)
+        If an integer is given, it fixes the seed. Defaults to the global numpy
+        random number generator.
+    """
+    def __init__(self, n_components, priors=None, means=None, covariances=None,
+                 verbose=0, random_state=None):
+        self.n_components = n_components
+        self.priors = priors
+        self.means = means
+        self.covariances = covariances
+        self.verbose = verbose
+        self.random_state = check_random_state(random_state)
+
+    def _check_initialized(self):
+        if self.priors is None:
+            raise ValueError("Priors have not been initialized")
+        if self.means is None:
+            raise ValueError("Means have not been initialized")
+        if self.covariances is None:
+            raise ValueError("Covariances have not been initialized")
+
+    def from_samples(self, X, R_diff=1e-4, n_iter=100):
+        """MLE of the mean and covariance.
+
+        Expectation-maximization is used to infer the model parameters. The
+        objective function is non-convex. Hence, multiple runs can have
+        different results.
+
+        Parameters
+        ----------
+        X : array-like, shape (n_samples, n_features)
+            Samples from the true function.
+
+        R_diff : float
+            Minimum allowed difference of responsibilities between successive
+            EM iterations.
+
+        n_iter : int
+            Maximum number of iterations.
+
+        Returns
+        -------
+        self : MVN
+            This object.
+        """
+        n_samples, n_features = X.shape
+
+        if self.priors is None:
+            self.priors = np.ones(self.n_components,
+                                  dtype=np.float) / self.n_components
+
+        if self.means is None:
+            # TODO k-means++
+            indices = self.random_state.choice(
+                np.arange(n_samples), self.n_components)
+            self.means = X[indices]
+
+        if self.covariances is None:
+            self.covariances = np.empty((self.n_components, n_features,
+                                         n_features))
+            for k in range(self.n_components):
+                self.covariances[k] = np.eye(n_features)
+
+        R = np.zeros((n_samples, self.n_components))
+        for _ in range(n_iter):
+            R_prev = R
+
+            # Expectation
+            R = self.to_responsibilities(X)
+
+            if np.linalg.norm(R - R_prev) < R_diff:
+                if self.verbose:
+                    print("EM converged.")
+                break
+
+            # Maximization
+            w = R.sum(axis=0) + 10.0 * np.finfo(R.dtype).eps
+            R_n = R / w
+            self.priors = w / w.sum()
+            self.means = R_n.T.dot(X)
+            for k in range(self.n_components):
+                Xm = X - self.means[k]
+                self.covariances[k] = (R_n[:, k, np.newaxis] * Xm).T.dot(Xm)
+
+        return self
+
+    def sample(self, n_samples):
+        """Sample from Gaussian mixture distribution.
+
+        Parameters
+        ----------
+        n_samples : int
+            Number of samples.
+
+        Returns
+        -------
+        X : array, shape (n_samples, n_features)
+            Samples from the GMM.
+        """
+        self._check_initialized()
+
+        mvn_indices = self.random_state.choice(
+            self.n_components, size=(n_samples,), p=self.priors)
+        mvn_indices.sort()
+        split_indices = np.hstack(
+            ((0,), np.nonzero(np.diff(mvn_indices))[0] + 1, (n_samples,)))
+        clusters = np.unique(mvn_indices)
+        lens = np.diff(split_indices)
+        samples = np.empty((n_samples, self.means.shape[1]))
+        for i, (k, n_samples) in enumerate(zip(clusters, lens)):
+            samples[split_indices[i]:split_indices[i + 1]] = MVN(
+                mean=self.means[k], covariance=self.covariances[k],
+                random_state=self.random_state).sample(n_samples=n_samples)
+        return samples
+
+    def to_responsibilities(self, X):
+        """Compute responsibilities of each MVN for each sample.
+
+        Parameters
+        ----------
+        X : array-like, shape (n_samples, n_features)
+            Data.
+
+        Returns
+        -------
+        R : array, shape (n_samples, n_components)
+        """
+        self._check_initialized()
+
+        n_samples = X.shape[0]
+        R = np.empty((n_samples, self.n_components))
+        for k in range(self.n_components):
+            R[:, k] = self.priors[k] * MVN(
+                mean=self.means[k], covariance=self.covariances[k],
+                random_state=self.random_state).to_probability_density(X)
+        R_norm = R.sum(axis=1)[:, np.newaxis]
+        R_norm[np.where(R_norm == 0.0)] = 1.0
+        R /= R_norm
+        return R
+
+    def to_probability_density(self, X):
+        """Compute probability density.
+
+        Parameters
+        ----------
+        X : array-like, shape (n_samples, n_features)
+            Data.
+
+        Returns
+        -------
+        p : array, shape (n_samples,)
+            Probability densities of data.
+        """
+        self._check_initialized()
+
+        p = [MVN(mean=self.means[k], covariance=self.covariances[k],
+                 random_state=self.random_state).to_probability_density(X)
+             for k in range(self.n_components)]
+        return np.dot(self.priors, p)
+
+    def condition(self, indices, x):
+        """Conditional distribution over given indices.
+
+        Parameters
+        ----------
+        indices : array, shape (n_new_features,)
+            Indices of dimensions that we want to condition.
+
+        x : array, shape (n_new_features,)
+            Values of the features that we know.
+
+        Returns
+        -------
+        conditional : GMM
+            Conditional GMM distribution p(Y | X=x).
+        """
+        self._check_initialized()
+
+        n_features = self.means.shape[1] - len(indices)
+        priors = np.empty(self.n_components)
+        means = np.empty((self.n_components, n_features))
+        covariances = np.empty((self.n_components, n_features, n_features))
+        for k in range(self.n_components):
+            mvn = MVN(mean=self.means[k], covariance=self.covariances[k],
+                      random_state=self.random_state)
+            conditioned = mvn.condition(indices, x)
+            priors[k] = (self.priors[k] *
+                         mvn.marginalize(indices).to_probability_density(x))
+            means[k] = conditioned.mean
+            covariances[k] = conditioned.covariance
+        priors /= priors.sum()
+        return GMM(n_components=self.n_components, priors=priors, means=means,
+                   covariances=covariances, random_state=self.random_state)
+
+    def predict(self, indices, X):
+        """Predict means of posteriors.
+
+        Same as condition() but for multiple samples.
+
+        Parameters
+        ----------
+        indices : array, shape (n_features_1,)
+            Indices of dimensions that we want to condition.
+
+        X : array, shape (n_samples, n_features_1)
+            Values of the features that we know.
+
+        Returns
+        -------
+        Y : array, shape (n_samples, n_features_2)
+            Predicted means of missing values.
+        """
+        self._check_initialized()
+
+        n_samples, n_features_1 = X.shape
+        n_features_2 = self.means.shape[1] - n_features_1
+        Y = np.empty((n_samples, n_features_2))
+        for n in range(n_samples):
+            conditioned = self.condition(indices, X[n])
+            Y[n] = conditioned.priors.dot(conditioned.means)
+        return Y
+
+    def to_ellipses(self, factor=1.0):
+        """Compute error ellipses.
+
+        An error ellipse shows equiprobable points.
+
+        Parameters
+        ----------
+        factor : float
+            One means standard deviation.
+
+        Returns
+        -------
+        ellipses : array, shape (n_components, 3)
+            Parameters that describe the error ellipses of all components:
+            angles, widths and heights.
+        """
+        self._check_initialized()
+
+        res = []
+        for k in range(self.n_components):
+            mvn = MVN(mean=self.means[k], covariance=self.covariances[k],
+                      random_state=self.random_state)
+            res.append((self.means[k], mvn.to_ellipse(factor)))
+        return res
+
+
+def plot_error_ellipses(ax, gmm, colors=None):
+    """Plot error ellipses of GMM components.
+
+    Parameters
+    ----------
+    ax : axis
+        Matplotlib axis.
+
+    gmm : GMM
+        Gaussian mixture model.
+    """
+    from matplotlib.patches import Ellipse
+    from itertools import cycle
+    if colors is not None:
+        colors = cycle(colors)
+    for factor in np.linspace(0.5, 4.0, 8):
+        for mean, (angle, width, height) in gmm.to_ellipses(factor):
+            ell = Ellipse(xy=mean, width=width, height=height,
+                          angle=np.degrees(angle))
+            ell.set_alpha(0.25)
+            if colors is not None:
+                ell.set_color(next(colors))
+            ax.add_artist(ell)
---
+++ packages/gmr/gmr-1.1.tar.gz/gmr-1.1/gmr/mvn.py
@@ -0,0 +1,239 @@
+import numpy as np
+from .utils import check_random_state, pinvh
+import scipy as sp
+
+
+def invert_indices(n_features, indices):
+    inv = np.ones(n_features, dtype=np.bool)
+    inv[indices] = False
+    inv, = np.where(inv)
+    return inv
+
+
+class MVN(object):
+    """Multivariate normal distribution.
+
+    Some utility functions for MVNs. See
+    http://en.wikipedia.org/wiki/Multivariate_normal_distribution
+    for more details.
+
+    Parameters
+    ----------
+    mean : array, shape (n_features), optional
+        Mean of the MVN.
+
+    covariance : array, shape (n_features, n_features), optional
+        Covariance of the MVN.
+
+    verbose : int, optional (default: 0)
+        Verbosity level.
+
+    random_state : int or RandomState, optional (default: global random state)
+        If an integer is given, it fixes the seed. Defaults to the global numpy
+        random number generator.
+    """
+    def __init__(self, mean=None, covariance=None, verbose=0,
+                 random_state=None):
+        self.mean = mean
+        self.covariance = covariance
+        self.verbose = verbose
+        self.random_state = check_random_state(random_state)
+        self.norm = None
+
+    def _check_initialized(self):
+        if self.mean is None:
+            raise ValueError("Mean has not been initialized")
+        if self.covariance is None:
+            raise ValueError("Covariance has not been initialized")
+
+    def from_samples(self, X, bessels_correction=True):
+        """MLE of the mean and covariance.
+
+        Parameters
+        ----------
+        X : array-like, shape (n_samples, n_features)
+            Samples from the true function.
+
+        Returns
+        -------
+        self : MVN
+            This object.
+        """
+        self.mean = np.mean(X, axis=0)
+        bias = 0 if bessels_correction else 1
+        self.covariance = np.cov(X, rowvar=0, bias=bias)
+        self.norm = None
+        return self
+
+    def sample(self, n_samples):
+        """Sample from multivariate normal distribution.
+
+        Parameters
+        ----------
+        n_samples : int
+            Number of samples.
+
+        Returns
+        -------
+        X : array, shape (n_samples, n_features)
+            Samples from the MVN.
+        """
+        self._check_initialized()
+        return self.random_state.multivariate_normal(
+            self.mean, self.covariance, size=(n_samples,))
+
+    def to_probability_density(self, X):
+        """Compute probability density.
+
+        Parameters
+        ----------
+        X : array-like, shape (n_samples, n_features)
+            Data.
+
+        Returns
+        -------
+        p : array, shape (n_samples,)
+            Probability densities of data.
+        """
+        self._check_initialized()
+
+        X = np.atleast_2d(X)
+        n_features = X.shape[1]
+
+        C = self.covariance
+        try:
+            L = sp.linalg.cholesky(C, lower=True)
+        except np.linalg.LinAlgError:
+            C = self.covariance + 1e-6 * np.eye(n_features)
+            L = sp.linalg.cholesky(C, lower=True)
+        D = X - self.mean
+        cov_sol = sp.linalg.solve_triangular(L, D.T, lower=True).T
+        if self.norm is None:
+            self.norm = 0.5 / np.pi ** (0.5 * n_features) / sp.linalg.det(L)
+
+        DpD = np.sum(cov_sol ** 2, axis=1)
+        return self.norm * np.exp(-0.5 * DpD)
+
+    def marginalize(self, indices):
+        """Marginalize over everything except the given indices.
+
+        Parameters
+        ----------
+        indices : array, shape (n_new_features,)
+            Indices of dimensions that we want to keep.
+
+        Returns
+        -------
+        marginal : MVN
+            Marginal MVN distribution.
+        """
+        self._check_initialized()
+        return MVN(mean=self.mean[indices],
+                   covariance=self.covariance[np.ix_(indices, indices)])
+
+    def condition(self, indices, x):
+        """Conditional distribution over given indices.
+
+        Parameters
+        ----------
+        indices : array, shape (n_new_features,)
+            Indices of dimensions that we want to condition.
+
+        x : array, shape (n_new_features,)
+            Values of the features that we know.
+
+        Returns
+        -------
+        conditional : MVN
+            Conditional MVN distribution p(Y | X=x).
+        """
+        self._check_initialized()
+        mean, covariance = self._condition(
+            invert_indices(self.mean.shape[0], indices), indices, x)
+        return MVN(mean=mean, covariance=covariance,
+                   random_state=self.random_state)
+
+    def predict(self, indices, X):
+        """Predict means and covariance of posteriors.
+
+        Same as condition() but for multiple samples.
+
+        Parameters
+        ----------
+        indices : array, shape (n_features_1,)
+            Indices of dimensions that we want to condition.
+
+        X : array, shape (n_samples, n_features_1)
+            Values of the features that we know.
+
+        Returns
+        -------
+        Y : array, shape (n_samples, n_features_2)
+            Predicted means of missing values.
+
+        covariance : array, shape (n_features_2, n_features_2)
+            Covariance of the predicted features.
+        """
+        self._check_initialized()
+        return self._condition(invert_indices(self.mean.shape[0], indices),
+                               indices, X)
+
+    def _condition(self, i1, i2, X):
+        cov_12 = self.covariance[np.ix_(i1, i2)]
+        cov_11 = self.covariance[np.ix_(i1, i1)]
+        cov_22 = self.covariance[np.ix_(i2, i2)]
+        prec_22 = pinvh(cov_22)
+        regression_coeffs = cov_12.dot(prec_22)
+
+        mean = self.mean[i1] + regression_coeffs.dot((X - self.mean[i2]).T).T
+        covariance = cov_11 - regression_coeffs.dot(cov_12.T)
+        return mean, covariance
+
+    def to_ellipse(self, factor=1.0):
+        """Compute error ellipse.
+
+        An error ellipse shows equiprobable points.
+
+        Parameters
+        ----------
+        factor : float
+            One means standard deviation.
+
+        Returns
+        -------
+        angle : float
+            Rotation angle of the ellipse.
+
+        width : float
+            Width of the ellipse.
+
+        height : float
+            Height of the ellipse.
+        """
+        self._check_initialized()
+        vals, vecs = sp.linalg.eigh(self.covariance)
+        order = vals.argsort()[::-1]
+        vals, vecs = vals[order], vecs[:, order]
+        angle = np.arctan2(*vecs[:, 0][::-1])
+        width, height = factor * np.sqrt(vals)
+        return angle, width, height
+
+
+def plot_error_ellipse(ax, mvn):
+    """Plot error ellipse of MVN.
+
+    Parameters
+    ----------
+    ax : axis
+        Matplotlib axis.
+
+    mvn : MVN
+        Multivariate normal distribution.
+    """
+    from matplotlib.patches import Ellipse
+    for factor in np.linspace(0.5, 4.0, 8):
+        angle, width, height = mvn.to_ellipse(factor)
+        ell = Ellipse(xy=mvn.mean, width=width, height=height,
+                      angle=np.degrees(angle))
+        ell.set_alpha(0.25)
+        ax.add_artist(ell)
