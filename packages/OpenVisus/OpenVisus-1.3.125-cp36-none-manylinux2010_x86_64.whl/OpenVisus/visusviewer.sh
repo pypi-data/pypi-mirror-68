5e790340dcb02bb58e3cb0be67e653e4e021d982
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:07 +0000

    Add package OpenVisus/1.3.125/OpenVisus-1.3.125-cp36-none-manylinux2010_x86_64.whl

---
+++ packages/OpenVisus/OpenVisus-1.3.125-cp36-none-manylinux2010_x86_64.whl/OpenVisus/visusviewer.sh
@@ -0,0 +1,7 @@
+
+#!/bin/bash
+cd $(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+export PYTHONPATH=/root/OpenVisus/build/Release/OpenVisus:/usr/local/lib/python36.zip:/usr/local/lib/python3.6:/usr/local/lib/python3.6/lib-dynload:/usr/local/lib/python3.6/site-packages
+export LD_LIBRARY_PATH=/usr/local/lib:${LD_LIBRARY_PATH}
+export QT_PLUGIN_PATH=$(pwd)/bin/qt/plugins 
+bin/visusviewer "$@"
