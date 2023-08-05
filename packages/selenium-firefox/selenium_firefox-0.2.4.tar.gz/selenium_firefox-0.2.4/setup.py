cba531755a925d9407d6b87604136aaef5c3a310
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:43:07 +0000

    Add package selenium-firefox/0.2.4/selenium_firefox-0.2.4.tar.gz

---
+++ packages/selenium-firefox/selenium_firefox-0.2.4.tar.gz/selenium_firefox-0.2.4/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="selenium_firefox",
+    version="0.2.4",
+    author="Kovács Kristóf-Attila",
+    description="selenium_firefox",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/kkristof200/selenium_firefox",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'selenium',
+        'fake-useragent',
+        'tldextract'
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
