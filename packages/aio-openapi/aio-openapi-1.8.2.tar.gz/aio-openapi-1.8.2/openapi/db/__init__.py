6874e5d87f198ab711532e099d158eac6e69e9ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:34 +0000

    Add package aio-openapi/1.8.2/aio-openapi-1.8.2.tar.gz

---
+++ packages/aio-openapi/aio-openapi-1.8.2.tar.gz/aio-openapi-1.8.2/openapi/db/__init__.py
@@ -0,0 +1,30 @@
+import os
+import typing as t
+
+from aiohttp.web import Application
+
+from ..db.dbmodel import CrudDB
+
+
+def get_db(app: Application, store_url: str = None) -> t.Optional[CrudDB]:
+    """Create an Open API db handler
+
+    This function
+    * add the database to the aiohttp application
+    * add the db command to the command line client (if command is True)
+    * add the close handler on shutdown
+
+    It returns the database object
+    """
+    store_url = store_url or os.environ.get("DATASTORE")
+    if not store_url:  # pragma: no cover
+        app.logger.warning("DATASTORE url not available")
+        return None
+    else:
+        app["db"] = CrudDB(store_url)
+        app.on_shutdown.append(close_db)
+        return app["db"]
+
+
+async def close_db(app: Application) -> None:
+    await app["db"].close()
