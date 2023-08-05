fa5cb3db9e03b529c6fa259c04dd317941c50f69
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:08 +0000

    Add package semantyk/0.0.0.18/semantyk-0.0.0.18.tar.gz

---
+++ packages/semantyk/semantyk-0.0.0.18.tar.gz/semantyk-0.0.0.18/semantyk/data_visualization_models/cell/cell.py
@@ -0,0 +1,15 @@
+##############################################
+#
+# cell
+# Module | Main component of a Data Visualization Model like Articles or FileCards.
+#
+# Daniel Bakas Amuchastegui
+# September 3, 2019
+# 
+# Copyright © Semantyk 2020. All rights reserved.
+#############################################
+
+from semantyk.core.model import Model
+
+class Cell(Model):
+    pass
