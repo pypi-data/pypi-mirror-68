643e92b4c0f44f310418a4f3e8ab68d11bdbcf9d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:10 +0000

    Add package sm-distributions/0.1/sm-distributions-0.1.tar.gz

---
+++ packages/sm-distributions/sm-distributions-0.1.tar.gz/sm-distributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='sm-distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['sm-distributions'],
+      author='Shipra Mahajan',
+      author_email='shipra.mahajan@gmail.com',
+      zip_safe=False)
