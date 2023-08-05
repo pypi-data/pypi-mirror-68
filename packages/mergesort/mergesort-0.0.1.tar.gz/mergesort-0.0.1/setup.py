1b1f94de892dd77dda7aa34644352e1353a81975
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:35 +0000

    Add package mergesort/0.0.1/mergesort-0.0.1.tar.gz

---
+++ packages/mergesort/mergesort-0.0.1.tar.gz/mergesort-0.0.1/setup.py
@@ -0,0 +1,11 @@
+
+from setuptools import setup, find_packages
+
+setup(
+    name="mergesort",
+    version="0.0.1",
+    description="merge sort module.",
+    author="khk37601",
+    author_email="khk37601@gmail.com",
+    url="https://github.com/khk37601/merge-sort",
+)
