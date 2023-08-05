36ecad08f265d5bd07ab2a25237ac65c374c8324
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:41:49 +0000

    Add package djangocms-cascade/1.3/djangocms-cascade-1.3.tar.gz

---
+++ packages/djangocms-cascade/djangocms-cascade-1.3.tar.gz/djangocms-cascade-1.3/setup.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+from cmsplugin_cascade import __version__
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+CLASSIFIERS = [
+    'Development Status :: 5 - Production/Stable',
+    'Environment :: Web Environment',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: MIT License',
+    'Operating System :: OS Independent',
+    'Programming Language :: Python',
+    'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Framework :: Django :: 1.11',
+    'Framework :: Django :: 2.0',
+    'Framework :: Django :: 2.1',
+    'Framework :: Django :: 2.2',
+]
+
+setup(
+    name='djangocms-cascade',
+    version=__version__,
+    description='Build Single Page Applications using the Django-CMS plugin system',
+    author='Jacob Rief',
+    author_email='jacob.rief@gmail.com',
+    url='https://github.com/jrief/djangocms-cascade',
+    packages=find_packages(exclude=['examples', 'docs', 'tests']),
+    install_requires=[
+        'django>=1.11,<3.0',
+        'django-classy-tags>=0.8',
+        'django-cms>=3.5,<4',
+        'django-entangled',
+        'djangocms-text-ckeditor>=3.7',
+        'jsonfield',
+        'requests',
+    ],
+    license='MIT',
+    platforms=['OS Independent'],
+    classifiers=CLASSIFIERS,
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    include_package_data=True,
+    zip_safe=False,
+)
