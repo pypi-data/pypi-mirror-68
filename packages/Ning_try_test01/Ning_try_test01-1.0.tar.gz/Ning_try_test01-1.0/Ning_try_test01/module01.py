b1fb2b305219db6647de819c6fb68e997d8b960a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:12:56 +0000

    Add package Ning_try_test01/1.0/Ning_try_test01-1.0.tar.gz

---
+++ packages/Ning_try_test01/Ning_try_test01-1.0.tar.gz/Ning_try_test01-1.0/Ning_try_test01/module01.py
@@ -0,0 +1,3 @@
+#coding=utf-8
+def add():
+    print("打印 NingHuiChao")
---
+++ packages/Ning_try_test01/Ning_try_test01-1.0.tar.gz/Ning_try_test01-1.0/setup.py
@@ -0,0 +1,11 @@
+#coding=utf-8
+from distutils.core import setup
+
+setup(
+    name='Ning_try_test01', # 对外我们模块的名字
+    version='1.0', # 版本号
+    description='这是第一个对外发布的模块，用于测试哦', #描述
+    author='ninghuichao', # 作者
+    author_email='838799336@qq.com',
+    py_modules=['Ning_try_test01.module01'] # 要发布的模块
+)
