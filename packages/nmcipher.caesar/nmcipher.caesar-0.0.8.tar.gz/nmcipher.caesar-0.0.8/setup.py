f10051d0b5427b24f4b7747a83ce3ab515e48556
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:11 +0000

    Add package nmcipher.caesar/0.0.8/nmcipher.caesar-0.0.8.tar.gz

---
+++ packages/nmcipher.caesar/nmcipher.caesar-0.0.8.tar.gz/nmcipher.caesar-0.0.8/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_namespace_packages
+
+setup(
+    name="nmcipher.caesar",
+    version="0.0.8",
+    author="Neil Marshall",
+    author_email="neil.marshall@dunelm.org.uk",
+    description="A command line parser package for use with basic encryption algorithms",
+    packages=find_namespace_packages(include=["nmcipher.*"], exclude=["nmcipher.tests"]),
+    python_requires='>=3.8',
+    entry_points={
+        "console_scripts": [
+            "caesar = nmcipher.caesar:main"
+        ]
+    }
+)
