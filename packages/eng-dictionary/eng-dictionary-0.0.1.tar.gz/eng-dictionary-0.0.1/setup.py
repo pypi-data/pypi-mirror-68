9cdb9ce91e0a4917358fbf8d710b3c48d1772fc3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:34 +0000

    Add package eng-dictionary/0.0.1/eng-dictionary-0.0.1.tar.gz

---
+++ packages/eng-dictionary/eng-dictionary-0.0.1.tar.gz/eng-dictionary-0.0.1/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(name='eng-dictionary',
+      version='0.0.1',
+      author='Sourab Sharma',
+      author_email='sharmasourab93@gmail.com',
+      url='https://github.com/sharmasourab93/eng_dictionary',
+      description='A BS4 based English Dictionary',
+      long_description=open('README.md').read(),
+      packages=find_packages(),
+      include_package_data=True,
+      install_requires=open('requirements.txt').read(),
+      classifiers=["Programming Language :: Python :: 3",
+                   "License :: OSI Approved :: MIT License",
+                   "Operating System :: OS Independent",
+                   ],
+      python_requires='>=3.6',
+      )
