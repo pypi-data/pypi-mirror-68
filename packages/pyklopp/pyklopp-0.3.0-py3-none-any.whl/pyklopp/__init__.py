2daf1319d5bccf15c6c4c59a9ac3a35fac8f7b99
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:56 +0000

    Add package pyklopp/0.3.0/pyklopp-0.3.0-py3-none-any.whl

---
+++ packages/pyklopp/pyklopp-0.3.0-py3-none-any.whl/pyklopp/__init__.py
@@ -0,0 +1,5 @@
+from .__version__ import __version__
+from .defaults import get_transform
+from .defaults import get_optimizer
+from .defaults import get_loss
+from pyklopp.loading import subpackage_import
