e84230a0e76a988cd21f9f0e99a5ac31db241c77
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:43 +0000

    Add package icinga2apic/0.7.3/icinga2apic-0.7.3.tar.gz

---
+++ packages/icinga2apic/icinga2apic-0.7.3.tar.gz/icinga2apic-0.7.3/README.md
@@ -0,0 +1,22 @@
+# <a id="about-icinga2"></a> About icinga2apic
+
+icinga2apic is a [Python](http://www.python.org) module to interact with the [Icinga 2 RESTful API](https://www.icinga.com/docs/icinga2/latest/doc/12-icinga2-api/).
+
+# Features
+
+1. [basic and certificate auth](doc/2-authentication.md)
+1. [config file support](doc/2-authentication.md#-config-file)
+1. [objects (zone support)](doc/3-objects.md)
+1. [actions](doc/4-actions.md)
+1. [events](doc/5-events.md)
+1. [status](doc/6-status.md)
+
+# Developing
+
+1. Code cleanup
+1. Tests
+1. Configuration Management
+
+# Usage
+
+See the [doc](doc) directory.
---
+++ packages/icinga2apic/icinga2apic-0.7.3.tar.gz/icinga2apic-0.7.3/icinga2apic/client.py
@@ -0,0 +1,91 @@
+# -*- coding: utf-8 -*-
+'''
+Copyright 2017 fmnisme@gmail.com, Copyright 2020 christian@jonak.org
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice, this
+list of conditions and the following disclaimer.
+
+2. Redistributions in binary form must reproduce the above copyright notice,
+this list of conditions and the following disclaimer in the documentation
+and/or other materials provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+Icinga 2 API client
+
+The Icinga 2 API allows you to manage configuration objects and resources in a simple,
+programmatic way using HTTP requests.
+'''
+
+from __future__ import print_function
+import logging
+
+import icinga2apic
+from icinga2apic.actions import Actions
+from icinga2apic.configfile import ClientConfigFile
+from icinga2apic.events import Events
+from icinga2apic.exceptions import Icinga2ApiException
+from icinga2apic.objects import Objects
+from icinga2apic.status import Status
+
+LOG = logging.getLogger(__name__)
+
+
+class Client(object):
+    '''
+    Icinga 2 Client class
+    '''
+
+    def __init__(self,
+                 url=None,
+                 username=None,
+                 password=None,
+                 timeout=None,
+                 certificate=None,
+                 key=None,
+                 ca_certificate=None,
+                 config_file=None):
+        '''
+        initialize object
+        '''
+        config_from_file = ClientConfigFile(config_file)
+        if config_file:
+            config_from_file.parse()
+        self.url = url or \
+            config_from_file.url
+        self.username = username or \
+            config_from_file.username
+        self.password = password or \
+            config_from_file.password
+        self.timeout = timeout or \
+            config_from_file.timeout
+        self.certificate = certificate or \
+            config_from_file.certificate
+        self.key = key or \
+            config_from_file.key
+        self.ca_certificate = ca_certificate or \
+            config_from_file.ca_certificate
+        self.objects = Objects(self)
+        self.actions = Actions(self)
+        self.events = Events(self)
+        self.status = Status(self)
+        self.version = icinga2apic.__version__
+
+        if not self.url:
+            raise Icinga2ApiException('No "url" defined.')
+        if not self.username and not self.password and not self.certificate:
+            raise Icinga2ApiException(
+                'Neither username/password nor certificate defined.'
+            )
