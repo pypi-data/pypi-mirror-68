bce1913048acd817a92b3c77c68649cde6d8d03e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:13:59 +0000

    Add package python-socialite/0.1.1/python_socialite-0.1.1.tar.gz

---
+++ packages/python-socialite/python_socialite-0.1.1.tar.gz/python_socialite-0.1.1/setup.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# fmt: off
+from setuptools import setup, find_packages  # type: ignore
+
+with open("README.md") as readme_file:
+    readme = readme_file.read()
+
+setup(
+    author="Evans Mwendwa",
+    author_email="evans@authenticvisualsmedia.com",
+    python_requires=">=3.6",
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3",
+    ],
+    description="Simple Python oAuth Login",
+    install_requires=["requests"],
+    license="MIT license",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    package_data={"python_socialite": ["py.typed"]},
+    include_package_data=True,
+    keywords="python_socialite",
+    name="python_socialite",
+    package_dir={"": "src"},
+    packages=find_packages(
+        include=["src/python_socialite", "src/python_socialite.*"]
+    ),
+    setup_requires=[],
+    url="https://github.com/evansmwendwa/python-socialite",
+    version="0.1.1",
+    zip_safe=False,
+)
+# fmt: on
