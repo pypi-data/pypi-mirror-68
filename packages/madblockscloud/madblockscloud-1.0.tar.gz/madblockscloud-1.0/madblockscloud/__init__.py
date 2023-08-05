99e645d9674ff4a3b33c3839aa284ad559be6a6d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:14 +0000

    Add package madblockscloud/1.0/madblockscloud-1.0.tar.gz

---
+++ packages/madblockscloud/madblockscloud-1.0.tar.gz/madblockscloud-1.0/madblockscloud/__init__.py
@@ -0,0 +1,6 @@
+from .thingspeak import upload_data_ts,read_data_ts
+from .mbcloud import device_push,device_pull,upload_data_mb,read_data_mb
+from .mbmail import send_email_with_attachment,send_email
+from .mbotp import otp
+from .mbaws import sendsms,face_reg,object_detect
+from .mbibm import classifyimage
---
+++ packages/madblockscloud/madblockscloud-1.0.tar.gz/madblockscloud-1.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+setup(name='madblockscloud',
+version='1.0',
+description='madBlocks Cloud Library',
+url='https://upload.pypi.org/legacy/',
+author='Surya Kumar V',
+author_email='orl.suryakumar@gmail.com',
+license='MIT',
+packages=['madblockscloud'],
+zip_safe=False)
