16e81eebbaa8ea8be510dae15d39357df9f4a0ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:18 +0000

    Add package satella/2.7.23/satella-2.7.23-py2.py3-none-any.whl

---
+++ packages/satella/satella-2.7.23-py2.py3-none-any.whl/satella/coding/structures/__init__.py
@@ -0,0 +1,33 @@
+from .dictionaries import DictObject, apply_dict_object, DictionaryView, TwoWayDictionary, \
+    DirtyDict, KeyAwareDefaultDict
+from .hashable_objects import HashableWrapper
+from .immutable import Immutable, frozendict
+from .singleton import Singleton, SingletonWithRegardsTo
+from .structures import SetHeap, OmniHashableMixin, TimeBasedHeap, Heap
+from .proxy import Proxy
+from .typednamedtuple import typednamedtuple
+from .ranking import Ranking
+from .sorted_list import SortedList, SliceableDeque
+
+__all__ = [
+    'KeyAwareDefaultDict',
+    'Proxy',
+    'DirtyDict',
+    'SortedList',
+    'SliceableDeque',
+    'Ranking',
+    'TwoWayDictionary',
+    'typednamedtuple',
+    'HashableWrapper',
+    'Heap',
+    'SetHeap',
+    'DictionaryView',
+    'frozendict',
+    'TimeBasedHeap',
+    'OmniHashableMixin',
+    'Singleton',
+    'SingletonWithRegardsTo',
+    'DictObject',
+    'apply_dict_object',
+    'Immutable'
+]
---
+++ packages/satella/satella-2.7.23-py2.py3-none-any.whl/satella/time.py
@@ -0,0 +1,90 @@
+import typing as tp
+import time
+from concurrent.futures import Future
+
+from functools import wraps
+
+__all__ = ['measure']
+
+
+class measure:
+    """
+    A class used to measure time elapsed. Use for example like this:
+
+    >>> with measure() as measurement:
+    >>>     time.sleep(1)
+    >>>     print('This has taken so far', measurement(), 'seconds')
+    >>>     time.sleep(1)
+    >>> print('A total of ', measurement(), 'seconds have elapsed')
+
+    You can also use the .start() method instead of context manager. Time measurement
+    will stop after exiting or calling .stop() depending on stop_on_stop flag.
+
+    You can also decorate your functions to have them keep track time of their execution, like that:
+
+    >>> @measure()
+    >>> def measuring(measurement_object: measure, *args, **kwargs):
+    >>>     ...
+
+    You can also measure how long does executing a future take, eg.
+
+    >>> future = get_my_future()
+    >>> measurement = measure(future)
+    >>> future.result()
+    >>> print('Executing the future took', measurement(), 'seconds')
+
+    In case a future is passed, the measurement will stop automatically as soon as the future
+    returns with a result (or exception)
+
+    :param stop_on_stop: stop elapsing time upon calling .stop()/exiting the context manager
+    :param adjust: interval to add to current time upon initialization
+    :param time_getter_callable: callable/0 -> float to get the time with
+    """
+    __slots__ = ('started_on', 'elapsed', 'stopped_on', 'stop_on_stop', 'time_getter_callable')
+
+    def __init__(self, future_to_measure: tp.Optional[Future] = None, stop_on_stop: bool = True,
+                 adjust: float = 0.0, time_getter_callable: tp.Callable[[], float] = time.monotonic):
+        self.time_getter_callable = time_getter_callable
+        self.started_on = time_getter_callable() + adjust
+        self.elapsed = None
+        self.stopped_on = None
+        self.stop_on_stop = stop_on_stop
+        if future_to_measure is not None:
+            future_to_measure.add_done_callback(lambda fut: self.stop())
+
+    def start(self) -> None:
+        """Start measuring time or update the internal counter"""
+        self.started_on = self.time_getter_callable()
+
+    def update(self) -> None:
+        """Alias for .start()"""
+        self.start()
+
+    def adjust(self, interval: float) -> None:
+        """Add given value to internal started_at counter"""
+        self.started_on += interval
+
+    def __call__(self, fun: tp.Optional[tp.Callable] = None) -> float:
+        if fun is None:
+            if self.stop_on_stop and self.elapsed is not None:
+                return self.elapsed
+            return self.time_getter_callable() - self.started_on
+        else:
+            @wraps(fun)
+            def inner(*args, **kwargs):
+                with self:
+                    return fun(self, *args, **kwargs)
+            return inner
+
+    def __enter__(self):
+        self.start()
+        return self
+
+    def stop(self) -> None:
+        """Stop counting time"""
+        self.stopped_on = self.time_getter_callable()
+        self.elapsed = self.stopped_on - self.started_on
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.stop()
+        return False
