88fea793669fb2eeb9cc786d1900d2bbc36348f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:47:54 +0000

    Add package zucked/0.1.5/zucked-0.1.5.tar.gz

---
+++ packages/zucked/zucked-0.1.5.tar.gz/zucked-0.1.5/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+VERSION = '0.1.5'
+
+setup(
+    name='zucked',
+    version=VERSION,
+    py_modules=['zucked'],
+    url='https://github.com/Jordan-Milne/zucked',
+    download_url='https://github.com/Jordan-Milne/zucked'.format(VERSION),
+    license='MIT',
+    author='Jordan Milne',
+    author_email='jordan.milne2@gmail.com',
+    description='Python library for parsing through facebook messenger data',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+
+    python_requires=">=3.6",
+    setup_requires=["setuptools>=38.6.0"],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+
+    ]
+)
