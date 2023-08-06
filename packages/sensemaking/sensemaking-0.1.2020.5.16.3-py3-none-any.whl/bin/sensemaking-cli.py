9a71dfc1439c678aec4d316032961cb9420b5cf0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:02:52 +0000

    Add package sensemaking/0.1.2020.5.16.3/sensemaking-0.1.2020.5.16.3-py3-none-any.whl

---
+++ packages/sensemaking/sensemaking-0.1.2020.5.16.3-py3-none-any.whl/bin/sensemaking-cli.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*
+
+print("s e n s e m a k i n g")
---
+++ packages/sensemaking/sensemaking-0.1.2020.5.16.3-py3-none-any.whl/deepsensemaking/__init__.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*
+
+"""
+sensemaking: Auxiliary Python Modules
+
+
+"""
+
+from sensemaking.testing import test
+
+
+if __name__ == "__main__":
+    pass
