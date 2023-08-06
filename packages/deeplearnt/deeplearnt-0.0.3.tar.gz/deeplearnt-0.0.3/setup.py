c8dd5e208d741a2aad8cbd276f08bc173d31fdda
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:29 +0000

    Add package deeplearnt/0.0.3/deeplearnt-0.0.3.tar.gz

---
+++ packages/deeplearnt/deeplearnt-0.0.3.tar.gz/deeplearnt-0.0.3/setup.py
@@ -0,0 +1,10 @@
+import setuptools
+setuptools.setup(name='deeplearnt',
+      version='0.0.3',
+      description='clinical trial information retriver',
+      url='https://github.com/pergrand/deeplearntool',
+      author='lowe.li',
+      author_email='alilzg@aliyun.com',
+      license='MIT',
+      packages=setuptools.find_packages(),
+      zip_safe=False)
