3fa7ef9b4c045f84659ac4c9d24c004960efed6b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:13:14 +0000

    Add package epsound/0.0.5/epsound-0.0.5.tar.gz

---
+++ packages/epsound/epsound-0.0.5.tar.gz/epsound-0.0.5/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(name='epsound',
+      version='0.0.5',
+      description='Sound player library',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      url='https://github.com/EPC-MSU/epsound',
+      author='EPC MSU',
+      author_email='goncharovush@physlab.ru',
+      license='MIT',
+      packages=['epsound'],
+      install_requires=[
+            'simpleaudio==1.0.2',
+      ],
+      classifiers=[
+            "Programming Language :: Python :: 3",
+            "License :: OSI Approved :: MIT License",
+            "Operating System :: OS Independent",
+      ],
+      python_requires='>=3.4',
+      zip_safe=False)
