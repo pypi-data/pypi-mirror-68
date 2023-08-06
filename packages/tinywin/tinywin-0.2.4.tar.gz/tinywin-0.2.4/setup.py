43df48963a1ff32eedaeb2020e6dad2c0b34de6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:12:14 +0000

    Add package tinywin/0.2.4/tinywin-0.2.4.tar.gz

---
+++ packages/tinywin/tinywin-0.2.4.tar.gz/tinywin-0.2.4/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+setuptools.setup(
+    name='tinywin',
+    version='0.2.4',
+    scripts=[],
+    author="Keenan Nicholson",
+    author_email="",
+    description="A tiny Curses window management library.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/knicholson32/tinywin/",
+    packages=setuptools.find_packages(),
+    classifiers=[
+         "Programming Language :: Python :: 3",
+         "License :: OSI Approved :: MIT License",
+         "Operating System :: Unix",
+         "Environment :: Console :: Curses"
+    ],
+)
