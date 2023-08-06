9ba1d3df5b6fb92c4e0ec5f59bf42cd6fd75c0ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:55:46 +0000

    Add package gracc-reporting/3.1.1/gracc-reporting-3.1.1.tar.gz

---
+++ packages/gracc-reporting/gracc-reporting-3.1.1.tar.gz/gracc-reporting-3.1.1/setup.py
@@ -0,0 +1,15 @@
+"""Setup file for gracc-reporting"""
+import sys
+from setuptools import setup
+
+setup(name='gracc-reporting',
+      version='3.1.1',
+      description='GRACC Email Reports',
+      author_email='sbhat@fnal.gov',
+      author='Shreyas Bhat',
+      url='https://github.com/opensciencegrid/gracc-reporting',
+      packages=['gracc_reporting'],
+      install_requires=['elasticsearch', 'elasticsearch_dsl',
+                        'python-dateutil', 'toml', 'tabulate',
+                        'pandas']
+     )
