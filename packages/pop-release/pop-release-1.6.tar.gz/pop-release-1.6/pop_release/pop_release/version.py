cefa227d6a3359c25cea8442d8ea9aa3bf0d5a89
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:52 +0000

    Add package pop-release/1.6/pop-release-1.6.tar.gz

---
+++ packages/pop-release/pop-release-1.6.tar.gz/pop-release-1.6/pop_release/pop_release/version.py
@@ -0,0 +1,43 @@
+# Import python libs
+import os
+
+
+def set_doc(hub):
+    """
+    Set the version on the docs
+    """
+    ver = hub.OPT["pop_release"]["ver"]
+    name = hub.pop_release.PATHNAME
+    lines = []
+    paths = [os.path.join("docs", "conf.py"), os.path.join("docs", "source", "conf.py")]
+    path = None
+    for check in paths:
+        if os.path.isfile(check):
+            path = check
+    if not path:
+        print("No docs set up for this project, use Sphinx to set up docs")
+        return
+    with open(path, "r") as rfh:
+        for line in rfh.readlines():
+            if line.startswith("ver"):
+                lines.append(f'version = "{ver}"\n')
+                continue
+            elif line.startswith("release"):
+                lines.append(f'release = "{ver}"\n')
+                continue
+            else:
+                lines.append(line)
+    with open(path, "w+") as wfh:
+        wfh.writelines(lines)
+
+
+def set_ver(hub):
+    """
+    Set the version to the <project name>/version.py
+    """
+    ver = hub.OPT["pop_release"]["ver"]
+    name = hub.pop_release.PATHNAME
+    ver_str = f'# -*- coding: utf-8 -*-\nversion = "{ver}"\n'
+    path = os.path.join(name, "version.py")
+    with open(path, "w+") as wfh:
+        wfh.write(ver_str)
