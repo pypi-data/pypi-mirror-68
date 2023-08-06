c5f0fdc707b2b91a038cfc325fa81578432f7c00
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:24:08 +0000

    Add package scidra-module-utils/0.2.1/scidra-module-utils-0.2.1.tar.gz

---
+++ packages/scidra-module-utils/scidra-module-utils-0.2.1.tar.gz/scidra-module-utils-0.2.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_namespace_packages
+
+setup(
+    name="scidra-module-utils",
+    description="Utilities and Classes to create scidra compatible modules",
+    version="0.2.1",
+    packages=find_namespace_packages(include=["scidra.*"]),
+    install_requires=["click", "humps", "pydantic", "requests", "clint", "loguru"],
+)
