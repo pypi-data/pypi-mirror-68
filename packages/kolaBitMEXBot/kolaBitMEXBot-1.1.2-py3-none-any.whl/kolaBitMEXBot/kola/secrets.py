b48c7cca9a875895058828b300c589970154770e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:25:18 +0000

    Add package kolaBitMEXBot/1.1.2/kolaBitMEXBot-1.1.2-py3-none-any.whl

---
+++ packages/kolaBitMEXBot/kolaBitMEXBot-1.1.2-py3-none-any.whl/kolaBitMEXBot/kola/secrets.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+File with secrets.
+
+Should find a better way to store them.
+"""
+
+LIVE_URL = "https://www.bitmex.com/api/v1/"
+LIVE_KEY = "x1UuRas6Xyw5IShX0GtFDT-X"
+LIVE_SECRET = "SOfiLEab2QDbciLrNHX2Or3Z4mX4iPPH6HUCz0pBPt55Npum"
+
+TEST_URL = "https://testnet.bitmex.com/api/v1/"
+TEST_KEY = "HdKn_XAllF0gXOcfMJnbuttz"
+TEST_SECRET = "CgLjmJ3tbNCz12bV3SnzJS7ksohrPKI3r4usb5pvOMU2C4h4"
