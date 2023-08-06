40f0640f135be247fd2487b33d9aefa8cb0c3dff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:41 +0000

    Add package COG-translator/0.0.2/COG-translator-0.0.2.tar.gz

---
+++ packages/COG-translator/COG-translator-0.0.2.tar.gz/COG-translator-0.0.2/setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+setuptools.setup(
+    name="COG-translator",
+    version="0.0.2",
+    author="Vini Salazar",
+    author_email="viniws@gmail.com",
+    description="COG-translator, a pure Python translator for COG names.",
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+)
