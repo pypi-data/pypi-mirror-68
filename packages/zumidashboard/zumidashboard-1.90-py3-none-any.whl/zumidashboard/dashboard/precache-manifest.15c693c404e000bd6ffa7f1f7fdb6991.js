7e5ed72d720486434c018babd44e198b11b5c32d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:52:58 +0000

    Add package zumidashboard/1.90/zumidashboard-1.90-py3-none-any.whl

---
+++ packages/zumidashboard/zumidashboard-1.90-py3-none-any.whl/zumidashboard/dashboard/precache-manifest.15c693c404e000bd6ffa7f1f7fdb6991.js
@@ -0,0 +1,206 @@
+self.__precacheManifest = [
+  {
+    "revision": "d13b90eaf0cfb16256cd",
+    "url": "/static/css/main.f3b27d9e.chunk.css"
+  },
+  {
+    "revision": "d13b90eaf0cfb16256cd",
+    "url": "/static/js/main.2fa574f1.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "517bcceaf3750137f20a",
+    "url": "/static/js/2.d7fdf8ab.chunk.js"
+  },
+  {
+    "revision": "47185233421201178030c97e0ed14f7e",
+    "url": "/static/media/back_button.47185233.svg"
+  },
+  {
+    "revision": "2bc78510732c987c6f2e9425cae65a23",
+    "url": "/static/media/zumiface_blank.2bc78510.svg"
+  },
+  {
+    "revision": "ee977ce6948f11e6942241bdfb1e31a5",
+    "url": "/static/media/user_general_icon.ee977ce6.svg"
+  },
+  {
+    "revision": "cd4822cd2da22385ea3273ec577d892e",
+    "url": "/static/media/dot_menu.cd4822cd.svg"
+  },
+  {
+    "revision": "97039d3805a0da2c06f35783539a76dc",
+    "url": "/static/media/spinner_2.97039d38.svg"
+  },
+  {
+    "revision": "775b12fd0edde2cec8be15a7b027b0dc",
+    "url": "/static/media/user_disabled_screen.775b12fd.svg"
+  },
+  {
+    "revision": "c0e32a2cf717d48c0dfa7cdafda387bd",
+    "url": "/static/media/check.c0e32a2c.svg"
+  },
+  {
+    "revision": "04ae3512e340206a65f8d17fb8cdc305",
+    "url": "/static/media/info.04ae3512.svg"
+  },
+  {
+    "revision": "0ea3f37669bb469de1c5977058f71c3e",
+    "url": "/static/media/drive_direction_active_witharrow.0ea3f376.svg"
+  },
+  {
+    "revision": "c336b12e94018273b6c7d9c1fa294423",
+    "url": "/static/media/zumiface_sad.c336b12e.svg"
+  },
+  {
+    "revision": "a11194fda4edc97d0b502d080327e8e3",
+    "url": "/static/media/camera_icon.a11194fd.svg"
+  },
+  {
+    "revision": "c12804589ba3a7e3ea07e608e061e19d",
+    "url": "/static/media/zumiface_xx_eyes.c1280458.svg"
+  },
+  {
+    "revision": "4eb689eee52c4894a7e675cc29159261",
+    "url": "/static/media/zumiface_closed_eyes.4eb689ee.svg"
+  },
+  {
+    "revision": "760254d92365dd21dd1a12040fe7c183",
+    "url": "/static/media/connect_network_no_pw.760254d9.svg"
+  },
+  {
+    "revision": "b90e7ca6fa6127fdef3ca2b9c8c25224",
+    "url": "/static/media/header_battery_green.b90e7ca6.svg"
+  },
+  {
+    "revision": "756c975af7a720c0167f5c07b014d259",
+    "url": "/static/media/codemode_block_icon.756c975a.svg"
+  },
+  {
+    "revision": "8031ddc5f60bdd57732fdd83047529a4",
+    "url": "/static/media/codemode_code_icon.8031ddc5.svg"
+  },
+  {
+    "revision": "ab5a3f1ab6af3900c70c9b73c0505175",
+    "url": "/static/media/header_battery_red.ab5a3f1a.svg"
+  },
+  {
+    "revision": "ba818cd1b5c37614bfe2ef3ab78c52f2",
+    "url": "/static/media/header_charging_electricity.ba818cd1.svg"
+  },
+  {
+    "revision": "043aa75d883ebd2deac64aa3ff5be3c7",
+    "url": "/static/media/stop_up.043aa75d.svg"
+  },
+  {
+    "revision": "c3bdf703dabf1349cb47be2bc95184db",
+    "url": "/static/media/thinking_icon.c3bdf703.svg"
+  },
+  {
+    "revision": "8695d705e399db7107dcd4a1e4236e7b",
+    "url": "/static/media/try_connect.8695d705.gif"
+  },
+  {
+    "revision": "70bc2179ea7d848e0baa442c829c630f",
+    "url": "/static/media/connected.70bc2179.png"
+  },
+  {
+    "revision": "a17619604cd5788e537fa96910bc7bd7",
+    "url": "/static/media/zumiface_neutral.a1761960.svg"
+  },
+  {
+    "revision": "fcd42c0f7e6e3070910cc51c01a8997c",
+    "url": "/static/media/connect_network_show_pw.fcd42c0f.svg"
+  },
+  {
+    "revision": "6e852638634e8123d0e22950821b2e35",
+    "url": "/static/media/spinner.6e852638.svg"
+  },
+  {
+    "revision": "0896842f941af3fb5f12a50246e95aeb",
+    "url": "/static/media/header_onnetwork_notonline.0896842f.svg"
+  },
+  {
+    "revision": "bac84c3a5c793993dc48f7b478573a15",
+    "url": "/static/media/header_onlineicon.bac84c3a.svg"
+  },
+  {
+    "revision": "cafb838b7133c2be147a951145422c39",
+    "url": "/static/media/header_settings_icon.cafb838b.svg"
+  },
+  {
+    "revision": "e4247433073e7d08446e6017663dfc87",
+    "url": "/static/media/header_user_icon.e4247433.svg"
+  },
+  {
+    "revision": "7f16cdca0e9f60c6c0f883a5af395e6e",
+    "url": "/static/media/footer_power_icon.7f16cdca.svg"
+  },
+  {
+    "revision": "5fa33fa237c71d10ed5407803cb6d267",
+    "url": "/static/media/zumiface_happy_eyes.5fa33fa2.svg"
+  },
+  {
+    "revision": "4add1746cbc31d5dc62744ed6c8eb373",
+    "url": "/static/media/overlay_logged_lesson_arrow.4add1746.svg"
+  },
+  {
+    "revision": "1c5373057362277d8424efe13d31aeda",
+    "url": "/static/media/main_explore_bg.1c537305.svg"
+  },
+  {
+    "revision": "2c0e623000c4666d9a54b4a223843329",
+    "url": "/static/media/main_learn_bg.2c0e6230.svg"
+  },
+  {
+    "revision": "283a11aba3530d81108bb46364d643e4",
+    "url": "/static/media/main_steeringwheel.283a11ab.svg"
+  },
+  {
+    "revision": "cbe4ce61265e89db0d7fd0d0e10f06b2",
+    "url": "/static/media/main_code_24px.cbe4ce61.svg"
+  },
+  {
+    "revision": "a86ede5f4730ce88f83aff652aec9dde",
+    "url": "/static/media/main_school_24px.a86ede5f.svg"
+  },
+  {
+    "revision": "d9071487bbe929dadbc71fc80ccf9a86",
+    "url": "/static/media/main_explore_24px.d9071487.svg"
+  },
+  {
+    "revision": "42b5470922f19d5d25b5c0bd108bcd1c",
+    "url": "/static/media/gesture_illustration.42b54709.svg"
+  },
+  {
+    "revision": "d641b032e7d4689bc01a22466ec76cb8",
+    "url": "/static/media/top_rendering_360.d641b032.png"
+  },
+  {
+    "revision": "6c8698be7b41d6132ef003b917b4e448",
+    "url": "/static/media/zumiface_happy.6c8698be.svg"
+  },
+  {
+    "revision": "b1c84e5ce85a09235bb5a7dbced349cc",
+    "url": "/static/media/close.b1c84e5c.svg"
+  },
+  {
+    "revision": "26c43c37020c8c23d4d8c9e138030607",
+    "url": "/static/media/gesture_stop.26c43c37.svg"
+  },
+  {
+    "revision": "3278d624e5dfdd45f46ee04fe69f0849",
+    "url": "/static/media/drive_directionpad_base_arrows.3278d624.svg"
+  },
+  {
+    "revision": "9b868b273f8b56dcef752d2b99613c91",
+    "url": "/static/media/stop_down.9b868b27.svg"
+  },
+  {
+    "revision": "c29fa0a3402ecfb1c6289f685608c3ce",
+    "url": "/index.html"
+  }
+];
