6be0646de3e77c281d80c0c2524946b87bab57ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:24 +0000

    Add package ISY994-Homie4-Bridge/0.4.4/ISY994-Homie4-Bridge-0.4.4.tar.gz

---
+++ packages/ISY994-Homie4-Bridge/ISY994-Homie4-Bridge-0.4.4.tar.gz/ISY994-Homie4-Bridge-0.4.4/isy_homie_start.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+import time
+import yaml 
+
+from isy_homie.bridge import Bridge
+
+
+with open("isy_homie.yml", 'r') as ymlfile:
+    cfg = yaml.full_load(ymlfile)
+
+bridge = None
+
+try:
+    if 'logging' in cfg:
+        logging = cfg ['logging']
+    else:
+        logging = None
+
+    bridge = Bridge (address=cfg['isy'] ['url'], username=cfg['isy'] ['username'],password=cfg['isy'] ['password'],mqtt_settings=cfg['mqtt'],log_settings=logging)
+
+    while True:
+        time.sleep(1)
+
+except (KeyboardInterrupt, SystemExit):
+    print("Quitting.")    
+
