3b50298658c496cd08258d75f3f9f9839a1fa7aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:56:17 +0000

    Add package NEWGB-probability/0.1/NEWGB_probability-0.1.tar.gz

---
+++ packages/NEWGB-probability/NEWGB_probability-0.1.tar.gz/NEWGB_probability-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='NEWGB_probability',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['NEWGB_probability'],
+      zip_safe=False)
