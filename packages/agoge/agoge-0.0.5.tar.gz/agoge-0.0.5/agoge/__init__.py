7c3cd83673a5e8accde278d67625d71cd6550c3e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:20:23 +0000

    Add package agoge/0.0.5/agoge-0.0.5.tar.gz

---
+++ packages/agoge/agoge-0.0.5.tar.gz/agoge-0.0.5/agoge/__init__.py
@@ -0,0 +1,16 @@
+from .utils import defaults_f
+DEFAULTS = defaults_f({
+    'ARTIFACTS_ROOT': '~/agoge/artifacts',
+    'TQDM_ENABLED': False,
+    'TRIAL_ROOT': 'Worker',
+    'BUCKET': 'nintorac_model_serving',
+    'BASE_URL': 'https://github.com/Nintorac/NeuralDX7-weights/raw/master'
+})
+
+from .tracker import Tracker
+from .data_handler import DataHandler
+
+from .model import AbstractModel
+from .solver import AbstractSolver
+from .train_worker import TrainWorker
+from .inference_worker import InferenceWorker
