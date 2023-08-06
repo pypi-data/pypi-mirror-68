434585d6a9d331c5aaa05283df7b408258e0dfb6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:25:22 +0000

    Add package fake-bpy-module-2.81/20200514/fake-bpy-module-2.81-20200514.tar.gz

---
+++ packages/fake-bpy-module-2.81/fake-bpy-module-2.81-20200514.tar.gz/fake-bpy-module-2.81-20200514/gpu_extras/presets.py
@@ -0,0 +1,18 @@
+import sys
+import typing
+
+
+def draw_circle_2d(position, color, radius, segments):
+    '''
+
+    '''
+
+    pass
+
+
+def draw_texture_2d(texture_id, position, width, height):
+    '''
+
+    '''
+
+    pass
