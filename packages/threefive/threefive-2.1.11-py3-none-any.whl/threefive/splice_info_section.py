62c8a0ae8a9eb765565048f684ea31f0570ee0e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:12:35 +0000

    Add package threefive/2.1.11/threefive-2.1.11-py3-none-any.whl

---
+++ packages/threefive/threefive-2.1.11-py3-none-any.whl/threefive/splice_info_section.py
@@ -0,0 +1,34 @@
+class Splice_Info_Section:
+
+    def __init__(self):
+         pass
+
+    def decode(self, bitbin):
+        self.table_id = bitbin.ashex(8)
+        if self.table_id != '0xfc':
+            raise ValueError('splice_info_section.table_id should be 0xfc')   
+        self.section_syntax_indicator = bitbin.asflag(1)
+        if self.section_syntax_indicator:
+            raise ValueError('splice_info_section.section_syntax_indicator should be False')    
+        self.private = bitbin.asflag(1)
+        if self.private:
+            raise ValueError('splice_info_section.private should be False')    
+        self.reserved = bitbin.ashex(2)
+        if self.reserved != '0x3':
+            raise ValueError('splice_info_section.reserved should be 0x3')
+        self.section_length = bitbin.asint(12)
+        if self.section_length > 4093:
+            raise ValueError('splice_info_section.section_length cannot be greater than 4093') 
+        self.protocol_version = bitbin.asint(8)
+        if self.protocol_version != 0:
+            raise ValueError('splice_info_section.protocol_version should be 0') 
+        self.encrypted_packet = bitbin.asflag(1)
+        self.encryption_algorithm = bitbin.asint(6)
+        self.pts_adjustment = bitbin.as90k(33)
+        self.cw_index = bitbin.ashex(8)
+        self.tier = bitbin.ashex(12)
+        if int(self.tier,16) > 4095:
+            raise ValueError('splice_info_section.tier should less than 0xfff')
+        self.splice_command_length = bitbin.asint(12)
+        self.splice_command_type = bitbin.asint(8)
+        self.descriptor_loop_length = False
