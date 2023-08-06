7b085148d27386de26079a39b9dd42dcd4d529dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:26:26 +0000

    Add package cpm-cli/0.5.2/cpm-cli-0.5.2.tar.gz

---
+++ packages/cpm-cli/cpm-cli-0.5.2.tar.gz/cpm-cli-0.5.2/cpm/domain/bit.py
@@ -0,0 +1,19 @@
+class Bit(object):
+    def __init__(self, name):
+        self.name = name
+        self.version = ""
+        self.sources = []
+        self.packages = []
+        self.include_directories = []
+
+    def __eq__(self, other):
+        return self.name == other.name
+
+    def add_include_directory(self, directory):
+        self.include_directories.append(directory)
+
+    def add_package(self, package):
+        self.packages.append(package)
+
+    def add_sources(self, sources):
+        self.sources.extend(sources)
