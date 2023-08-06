84270fbe3e99c71a18d5f115c70193458d0a77b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:23:05 +0000

    Add package superhelp/0.9.30/superhelp-0.9.30.tar.gz

---
+++ packages/superhelp/superhelp-0.9.30.tar.gz/superhelp-0.9.30/superhelp/__init__.py
@@ -0,0 +1,7 @@
+from . import ast_funcs, code_execution, conf, helper, messages
+from superhelp import helpers
+from superhelp import gen_utils
+from .helper import shelp, this  # @UnresolvedImport
+
+if __name__ == '__main__':
+    shelp()
