1c9b719a3139728bcdbc4a5559e5af22d450143a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:05:22 +0000

    Add package essm/0.4.3.dev2/essm-0.4.3.dev2.tar.gz

---
+++ packages/essm/essm-0.4.3.dev2.tar.gz/essm-0.4.3.dev2/CHANGES.rst
@@ -0,0 +1,80 @@
+Changes
+=======
+
+``v0.4.2``
+----------
+*released 2020-04-28*
+
+Bug Fixes
+~~~~~~~~~
+
+- **utils:** Improve markdown representation of units (`PR #79 <https://github.com/environmentalscience/essm/pull/79>`__)
+
+- **variables:**  Fix generate_metadata_table for selected variables (`PR #80 <https://github.com/environmentalscience/essm/pull/80>`__)
+
+
+``v0.4.1``
+----------
+*released 2019-11-20*
+
+Bug Fixes
+~~~~~~~~~
+
+- **equations:** Improve dimensional testing of equations and substitution (`PR #73 <https://github.com/environmentalscience/essm/pull/73>`__)
+
+- **equations:** Add support for Integral and Piecewise in Equation PR (`PR #76 <https://github.com/environmentalscience/essm/pull/76>`__)
+
+
+Features
+~~~~~~~~
+
+-  **utils:** subs_eq() for simultaneous substitutions.(`PR #75 <https://github.com/environmentalscience/essm/pull/75>`__)
+
+
+``v0.3.0``
+----------
+*released 2019-04-09*
+
+Bug Fixes
+~~~~~~~~~
+
+-  **equations:** improve substitutions with equations
+   (`79ac37d <https://github.com/environmentalscience/essm/commit/79ac37d>`__)
+
+Features
+~~~~~~~~
+
+-  **utils:** add definition to metadata table
+   (`3ceaa69 <https://github.com/environmentalscience/essm/commit/3ceaa69>`__)
+
+``v0.2.0``
+----------
+*released 2019-04-04*
+
+- global: adapt to Python 3 and Sympy >=1.3
+- global: removal of SageMath mentions
+- docs: fix latex representation of x_O2 as x_{O2}
+- equations: extend replace_variables
+- equations: make .subs() on equation return an equality
+- units: reverted missing dimension lookup
+- variables: behave as Symbols
+- variables: better markdown formatting of units
+- variables: changes base class to Symbol
+- variables: enableddictionaries with symbols in replace_variables
+- variables: fix derive_unit for dimensionless expression
+- variables: fix latex rendering
+- variables: generate_metadata_table with HTML
+- variables: include assumptions from cls attribute
+- variables: modify derive_unit to work with summations
+- variables: remove Dimension deprecation warnings
+- variables: remove internal SI and refer to sympy.physics.units.systems.si
+- variables: respect unit in variable with expr
+- variables: set dimension and scale factor using method
+- variables: support dimensionless variable expression
+- variables: support replacing variables by their default values
+
+``v0.1.0``
+----------
+ *released 2017-06-29*
+
+- Initial public release.
---
+++ packages/essm/essm-0.4.3.dev2.tar.gz/essm-0.4.3.dev2/tests/test_variables.py
@@ -0,0 +1,181 @@
+# -*- coding: utf-8 -*-
+"""Test variables."""
+
+import pytest
+
+from essm.variables import Variable
+from essm.variables.units import (derive_baseunit, derive_unit, joule,
+                                  kilogram, markdown, meter, second)
+from essm.variables.utils import generate_metadata_table
+from sympy import Eq, exp
+
+
+class demo_variable(Variable):
+    """Test variable."""
+
+    default = 1
+    unit = meter
+
+
+class demo_variable1(Variable):
+    """Test variable."""
+
+    default = 1
+    unit = meter
+
+
+class demo_expression_variable(Variable):
+    """Test expression variable."""
+
+    expr = 2 * demo_variable
+
+
+class lambda_E(Variable):
+    unit = joule / kilogram
+
+
+class E_lmass(Variable):
+    unit = kilogram / (meter ** 2 * second)
+
+
+class E_l(Variable):
+    unit = joule / (meter ** 2 * second)
+
+
+def test_variable_definition():
+    """Test variable definition."""
+    assert demo_variable.definition.default == 1
+    assert demo_expression_variable.subs(Variable.__expressions__) \
+        == 2 * demo_variable
+    assert demo_expression_variable.definition.unit == meter
+
+
+def test_local_definition():
+    """Test local variable definition."""
+
+    class local_definition(Variable):
+        """Local definition."""
+        unit = joule
+
+        class local_variable(Variable):
+            """Local variable."""
+            unit = kilogram * meter ** 2 / second ** 2
+            default = 2
+
+        expr = 1.5 * local_variable
+
+    assert local_definition.__doc__ == local_definition.definition.__doc__
+    assert local_definition.definition.expr.subs(Variable.__defaults__) == 3
+    assert local_definition.definition.unit == joule
+
+
+def test_unit_check():
+    """Test unit validation."""
+
+    class valid_unit(Variable):
+        expr = 3 * demo_variable
+        unit = meter
+
+    with pytest.raises(ValueError):
+
+        class invalid_unit(Variable):
+            expr = 4 * demo_variable
+            unit = second
+
+
+def test_symbolic():
+    """Test that variables behave like symbols"""
+
+    class l1(Variable):
+        """Length"""
+        unit = meter
+
+    class l2(Variable):
+        """Length"""
+        unit = meter
+
+    assert Eq(l1, -l2) is not False
+
+
+def test_assumption():
+    """Test that assumptions passed to Variable are stored"""
+    class x(Variable):
+        """Positive real variable."""
+
+        assumptions = {'positive': True, 'real': True}
+
+    assert x.is_positive is True
+
+
+def test_derive_unit():
+    """Test derive_unit from expression."""
+
+    assert derive_baseunit(2 * lambda_E * E_lmass) \
+        == kilogram / second ** 3
+    assert derive_unit(2 * lambda_E * E_lmass) \
+        == joule / (meter ** 2 * second)
+    assert derive_unit(E_l / E_l) == 1
+    assert(derive_unit(demo_variable - demo_variable1)) \
+        == meter
+    with pytest.raises(ValueError):
+        derive_unit(lambda_E + E_lmass)
+
+    class dimensionless(Variable):
+        expr = demo_variable / demo_expression_variable
+
+    assert dimensionless.definition.unit == 1
+
+    class var_joule(Variable):
+        unit = joule
+
+    class var_joule_base(Variable):
+        unit = kilogram * meter ** 2 / second ** 2
+
+    assert derive_baseunit(exp(var_joule / var_joule_base)) == 1
+
+
+def test_remove_variable_from_registry():
+    """Check is the variable is removed from registry."""
+
+    class removable(Variable):
+        """Should be removed."""
+
+    with pytest.warns(UserWarning):
+        del Variable[removable]
+
+    assert removable not in Variable.__registry__
+
+    with pytest.raises(KeyError):
+        del Variable[removable]
+
+
+def test_latex():
+    """Test latex representaiton of variables."""
+
+    from sympy import latex
+
+    class lmbda(Variable):
+        """Test variable."""
+        latex_name = 'L'
+
+    assert latex(lmbda) == 'L'
+
+
+def test_markdown():
+    """Check markdown representation of units."""
+    assert markdown(kilogram * meter / second ** 2) == 'kg m s$^{-2}$'
+    assert markdown(meter / second) == 'm s$^{-1}$'
+    assert markdown(second / meter) == 's m$^{-1}$'
+
+
+def test_generate_metadata_table():
+    """Check display of table of units."""
+    assert generate_metadata_table([demo_expression_variable, E_l, lambda_E]) \
+        == [('Symbol', 'Name', 'Description', 'Definition', 'Default value',
+             'Units'),
+            ('$\\lambda_E$', 'lambda_E', 'Latent heat of evaporation.', '',
+            '2450000.0', 'J kg$^{-1}$'),
+            ('$demo_expression_variable$', 'demo_expression_variable',
+            'Test expression variable.', '$2 demo_variable$', '-', 'm'),
+            ('$E_l$', 'E_l',
+            'Latent heat flux from leaf.', '', '-', 'J s$^{-1}$ m$^{-2}$')]
