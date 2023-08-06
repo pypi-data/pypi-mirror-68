bdde7aa0afde4ac3703aa4cc1e404593e83bb2ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:28:16 +0000

    Add package OpenVisus/1.5.16/OpenVisus-1.5.16-cp38-none-win_amd64.whl

---
+++ packages/OpenVisus/OpenVisus-1.5.16-cp38-none-win_amd64.whl/OpenVisus/visusviewer.bat
@@ -0,0 +1,6 @@
+
+set this_dir=%~dp0
+set PATH=C:\hostedtoolcache\windows\Python\3.8.2\x64\python.exe\..;%this_dir%\bin;%this_dir%\bin\Qt\bin;%PATH%
+set PYTHONPATH=D:\a\OpenVisus\OpenVisus\build\Release\OpenVisus;C:\hostedtoolcache\windows\Python\3.8.2\x64\python38.zip;C:\hostedtoolcache\windows\Python\3.8.2\x64\DLLs;C:\hostedtoolcache\windows\Python\3.8.2\x64\lib;C:\hostedtoolcache\windows\Python\3.8.2\x64;C:\hostedtoolcache\windows\Python\3.8.2\x64\lib\site-packages;C:\hostedtoolcache\windows\Python\3.8.2\x64\lib\site-packages\win32;C:\hostedtoolcache\windows\Python\3.8.2\x64\lib\site-packages\win32\lib;C:\hostedtoolcache\windows\Python\3.8.2\x64\lib\site-packages\Pythonwin
+set QT_PLUGIN_PATH=%this_dir%\bin\Qt\plugins
+"%this_dir%\bin\visusviewer.exe" %*
