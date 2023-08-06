69b28e665864ffadf60965f09fa49fc65ef8b67d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:25:10 +0000

    Add package ai-transformersx/0.2.1/ai-transformersx-0.2.1.tar.gz

---
+++ packages/ai-transformersx/ai-transformersx-0.2.1.tar.gz/ai-transformersx-0.2.1/ai_transformersx/configuration.py
@@ -0,0 +1,104 @@
+import os
+import sys
+from typing import Optional, Tuple
+
+from dataclasses import dataclass, fields, field
+from ai_harness import harnessutils as aiutils
+from transformers import HfArgumentParser
+
+from ai_transformersx.training_args import TrainingArguments
+from ai_transformersx.models import *
+
+log = aiutils.getLogger('task')
+
+
+@dataclass
+class ModelArguments:
+    """
+    Arguments pertaining to which model/config/tokenizer we are going to fine-tune from.
+    """
+
+    model_path: str = field(default='',
+                            metadata={"help": "Path to pretrained model or model identifier from huggingface.co/models"}
+                            )
+    config_name: Optional[str] = field(
+        default=None, metadata={"help": "Pretrained config name or path if not the same as model_name"}
+    )
+    tokenizer_name: Optional[str] = field(
+        default=None, metadata={"help": "Pretrained tokenizer name or path if not the same as model_name"}
+    )
+    cache_dir: Optional[str] = field(
+        default=None, metadata={"help": "Where do you want to store the pretrained models downloaded from s3"}
+    )
+
+    model_base_dir: str = field(default="./models/pretrained",
+                                metadata={"help": "the path base dir of models"})
+
+    model_type: str = field(default="base",
+                            metadata={
+                                "help": "the type of model: " + str(MODEL_TYPE_NAMES)})
+
+    model_mode: str = field(default="classification",
+                            metadata={"help": "the model of model: " + str(MODEL_MODEL_NAMES)})
+
+    model_name: str = field(default="Base.Bert.bert",
+                            metadata={"help": "the name of model: " + str(ALL_MODEL_NAMES)})
+
+    num_labels: str = field(default=2,
+                            metadata={"help": "the number of label"})
+
+    def validate(self):
+        if not self.model_base_dir:
+            raise ValueError("model_base_dir can not be empty.")
+
+
+@dataclass
+class DataArguments:
+    """
+    Arguments pertaining to what data we are going to input our model for training and eval.
+
+    Using `HfArgumentParser` we can turn this class
+    into argparse arguments to be able to specify them on
+    the command line.
+    """
+    task_name: str = field(default="",
+                           metadata={
+                               "help": "The input data dir. Should contain the .tsv files (or other data files) for the task."}
+                           )
+
+    model_mode_for_data: str = field(default="classification",
+                                     metadata={"help": "the model of model: classification or regression"})
+
+    max_seq_length: int = field(
+        default=128,
+        metadata={
+            "help": "The maximum total input sequence length after tokenization. Sequences longer "
+                    "than this will be truncated, sequences shorter will be padded."
+        },
+    )
+    overwrite_cache: bool = field(
+        default=False, metadata={"help": "Overwrite the cached training and evaluation sets"}
+    )
+
+
+@dataclass
+class TaskArguments:
+    model_args: ModelArguments
+    data_args: DataArguments
+    training_args: TrainingArguments
+
+
+def parse_args(*extensions) -> (TaskArguments,):
+    parser = HfArgumentParser((ModelArguments, DataArguments, TrainingArguments) + extensions)
+
+    if len(sys.argv) == 2 and sys.argv[1].endswith(".json"):
+        # If we pass only one argument to the script and it's the path to a json file,
+        # let's parse it to get our arguments.
+        args = parser.parse_json_file(json_file=os.path.abspath(sys.argv[1]))
+    else:
+        args = parser.parse_args_into_dataclasses()
+
+    if not args[2].output_dir:
+        args[2].output_dir = 'models/finetuning'
+    task_args = TaskArguments(args[0], args[1], args[2])
+    return (task_args,) + args[3:]
---
+++ packages/ai-transformersx/ai-transformersx-0.2.1.tar.gz/ai-transformersx-0.2.1/ai_transformersx/tasks.py
@@ -0,0 +1,219 @@
+import os
+import random
+import sys
+from typing import Dict
+
+from ai_transformersx.dataprocessor import DataProcessor
+
+from ai_transformersx.dataset import TaskDataset
+import numpy as np
+import torch
+from ai_harness.fileutils import join_path
+from transformers import AutoConfig, AutoTokenizer, Trainer, EvalPrediction, \
+    PreTrainedModel
+from transformers.data.metrics import acc_and_f1
+
+from ai_transformersx.configuration import ModelArguments, DataArguments, log, Model_Mode, TaskArguments, parse_args
+from ai_transformersx.models import Model_Tools, Model
+from ai_transformersx import models
+from ai_transformersx.training_args import TrainingArguments
+
+
+def set_seed(seed: int):
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+    # ^^ safe to call this function even if cuda is not available
+
+
+class TaskModel:
+    def __init__(self, modelArgs: ModelArguments, model_class=None):
+        self._model_args = modelArgs
+        self._model_class = model_class if model_class else modelArgs.model_name
+        self._model_path = self._make_model_path()
+        self._init()
+
+    def _init(self):
+        self._model_args.validate()
+        self.config = AutoConfig.from_pretrained(
+            self._model_path,
+            num_labels=self._model_args.num_labels
+        )
+
+        self.tokenizer = AutoTokenizer.from_pretrained(self._model_path)
+
+        self.model = self._model(self.config)
+
+    def _model(self, config):
+        model_class = self._get_model_class()
+
+        return model_class.from_pretrained(
+            self._model_path,
+            config=config
+        )
+
+    def _make_model_path(self):
+        if self._model_args.model_path:
+            return join_path(self._model_args.model_base_dir, self._model_args.model_path)
+
+        if isinstance(self._model_class, Model):
+            return join_path(self._model_args.model_base_dir, self._model_class.path)
+
+        if isinstance(self._model_class, str):
+            return join_path(self._model_args.model_base_dir, Model_Tools.model_by(self._model_class).path)
+
+        raise ValueError("Cannot get model path.")
+
+    def _get_model_class(self):
+        if isinstance(self._model_class, PreTrainedModel):
+            return self._model_class
+        model_class = models.model_class(self.config, self._model_args.model_type)
+        if not model_class:
+            if not self._model_class:
+                raise ValueError(
+                    "Cannot find the model class for model type {} and config {}.".format(self._model_args.model_type,
+                                                                                          self._model_args.model_path)
+                )
+        return model_class
+
+    def compute_metrics(self, p: EvalPrediction) -> Dict:
+        if self._model_args.model_mode == Model_Mode.classification:
+            preds = np.argmax(p.predictions, axis=1)
+        elif self._model_args.model_mode == Model_Mode.regression:
+            preds = np.squeeze(p.predictions)
+        return acc_and_f1(preds, p.label_ids)
+
+
+class TaskData:
+    def __init__(self, dataArgs: DataArguments,
+                 tokenizer, dataProcessor: DataProcessor, local_rank):
+        self._data_args = dataArgs
+        self._tokenizer = tokenizer
+        self._processor = dataProcessor
+        self._local_rank = local_rank
+        self._train_data, self._eval_data = None, None
+
+    def train_data(self):
+        if not self._train_data:
+            self._train_data = TaskDataset(self._data_args,
+                                           tokenizer=self._tokenizer,
+                                           processor=self._processor,
+                                           local_rank=self._local_rank)
+        return self._train_data
+
+    def eval_data(self):
+        if not self._eval_data:
+            self._eval_data = TaskDataset(self._data_args,
+                                          tokenizer=self._tokenizer,
+                                          processor=self._processor,
+                                          local_rank=self._local_rank,
+                                          evaluate=True)
+        return self._eval_data
+
+
+class TaskTrainer:
+    def __init__(self, trainingArgs: TrainingArguments,
+                 taskModel: TaskModel, taskData: TaskData, compute_metrics=None):
+        self._training_args = trainingArgs
+        self._taskModel = taskModel
+        self._taskData = taskData
+        self._compute_metrics = compute_metrics if compute_metrics else taskModel.compute_metrics
+        self._trainer = self._create_trainer()
+
+    def _check_train_args(self):
+        if (
+                os.path.exists(self._training_args.output_dir)
+                and os.listdir(self._training_args.output_dir)
+                and self._training_args.do_train
+                and not self._training_args.overwrite_output_dir
+        ):
+            raise ValueError(
+                f"Output directory ({self._training_args.output_dir}) already exists and is not empty. Use --overwrite_output_dir to overcome."
+            )
+
+    def _create_trainer(self):
+        return Trainer(
+            model=self._taskModel.model,
+            args=self._training_args,
+            train_dataset=self._taskData.train_data() if self._training_args.do_train else None,
+            eval_dataset=self._taskData.eval_data() if self._training_args.do_eval else None,
+            compute_metrics=self._compute_metrics,
+        )
+
+    def train(self):
+        if self._training_args.do_train:
+            self._trainer.train(
+                model_path=self._training_args.output_dir
+            )
+            self._trainer.save_model()
+            # For convenience, we also re-save the tokenizer to the same directory,
+            # so that you can share your model easily on huggingface.co/models =)
+            if self._trainer.is_world_master():
+                self._taskModel.tokenizer.save_pretrained(self._training_args.output_dir)
+        return self
+
+    def eval(self):
+        # Evaluation
+        results = {}
+        if self._training_args.do_eval and self._training_args.local_rank in [-1, 0]:
+            log.info("*** Evaluate ***")
+
+            eval_datasets = [self._taskData.eval_data()]
+
+            for eval_dataset in eval_datasets:
+                result = self._trainer.evaluate(eval_dataset=eval_dataset)
+
+                output_eval_file = join_path(
+                    self._training_args.output_dir, f"eval_results_{eval_dataset.args.task_name}.txt"
+                )
+                with open(output_eval_file, "w") as writer:
+                    log.info("***** Eval results {} *****".format(eval_dataset.args.task_name))
+                    for key, value in result.items():
+                        log.info("  %s = %s", key, value)
+                        writer.write("%s = %s\n" % (key, value))
+
+                results.update(result)
+        return results
+
+
+class TransformerTask:
+    def __init__(self, task_args: TaskArguments,
+                 dataProcessor, model_class=None, compute_metric=None):
+        log.info(task_args)
+        self.task_args = task_args
+        self._taskModel = TaskModel(task_args.model_args, model_class)
+        self._taskData = TaskData(task_args.data_args, self._taskModel.tokenizer, dataProcessor,
+                                  task_args.training_args.local_rank) if task_args.training_args.do_train else None
+        self._taskTrainer = TaskTrainer(task_args.training_args, self._taskModel,
+                                        self._taskData, compute_metric) if task_args.training_args.do_train else None
+        self._training_args = task_args.training_args
+
+    def _log_task_start(self):
+        log.warning(
+            "Process rank: %s, device: %s, n_gpu: %s, distributed training: %s, 16-bits training: %s",
+            self._training_args.local_rank,
+            self._training_args.device,
+            self._training_args.n_gpu,
+            bool(self._training_args.local_rank != -1),
+            self._training_args.fp16,
+        )
+        log.info("Training/evaluation parameters %s", self._training_args)
+
+    def train(self):
+        if not self._training_args.do_train:
+            log.warn("do_train argument is set for False")
+            return
+        self._log_task_start()
+
+        set_seed(self._training_args.seed)
+        results = self._taskTrainer.train().eval()
+        return results
+
+    def predict(self, *input):
+        return self._taskModel.model(*input)
+
+
+class DefaultTask(TransformerTask):
+    def __init__(self, dataProcessorClass, model_class=None, compute_metric=None):
+        super().__init__(parse_args(), dataProcessorClass, model_class, compute_metric)
