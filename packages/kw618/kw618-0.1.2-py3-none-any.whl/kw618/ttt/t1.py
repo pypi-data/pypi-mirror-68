e22b7c507f33b50187834c46d0d7e647f29412d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:13:25 +0000

    Add package kw618/0.1.2/kw618-0.1.2-py3-none-any.whl

---
+++ packages/kw618/kw618-0.1.2-py3-none-any.whl/kw618/ttt/t1.py
@@ -0,0 +1,13 @@
+
+grant all on *.* to root@'%' identified by '123456' with grant option;
+
+ALTER USER 'root'@'localhost' IDENTIFIED BY 'kw618';
+
+
+ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY '123456';
+
+
+
+
+
+#
