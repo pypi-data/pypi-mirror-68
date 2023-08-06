68ded68872a9926f9b4a7d3315d7eedceb182bd4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:22:44 +0000

    Add package ctns/0.2.3/ctns-0.2.3.tar.gz

---
+++ packages/ctns/ctns-0.2.3.tar.gz/ctns-0.2.3/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+import os
+
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
+with open('README.md') as readme_file:
+    README = readme_file.read()
+
+setup(
+  name = 'ctns',
+  packages = find_packages(),
+  version = '0.2.3',
+  scripts = ['bin/ctns', 'bin/ctns.bat','bin/ctns_run.py'],
+  description = 'CTNS, Contact Tracing Network Simulator: a tool to simulate contact tracing in a population where a disease is spreading',
+  long_description_content_type = "text/markdown",
+  long_description = README,
+  license = 'GNU GPLv3',
+  author = 'Matteo Mistri, Diego Miglio',
+  author_email = 'matteo.mistri1996@gmail.com',
+  install_requires = required,
+  url = "https://gitlab.com/mistrello96/ctns",
+  download_url='https://pypi.org/project/ctns/',
+)
