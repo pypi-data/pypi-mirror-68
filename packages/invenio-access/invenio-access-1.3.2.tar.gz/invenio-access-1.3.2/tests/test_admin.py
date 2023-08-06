e10387b1d9d1dddf4ecc00da45dc2d149c1e7c22
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:25:15 +0000

    Add package invenio-access/1.3.2/invenio-access-1.3.2.tar.gz

---
+++ packages/invenio-access/invenio-access-1.3.2.tar.gz/invenio-access-1.3.2/tests/test_admin.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+#
+# This file is part of Invenio.
+# Copyright (C) 2016-2018 CERN.
+#
+# Invenio is free software; you can redistribute it and/or modify it
+# under the terms of the MIT License; see LICENSE file for more details.
+
+"""Test for admin view."""
+
+from flask import current_app
+from invenio_accounts import InvenioAccounts
+from werkzeug.local import LocalProxy
+
+from invenio_access.admin import action_roles_adminview, \
+    action_system_roles_adminview, action_users_adminview
+
+_datastore = LocalProxy(
+    lambda: current_app.extensions['security'].datastore
+)
+
+
+def test_admin(app):
+    """Test flask-admin interace."""
+    assert isinstance(action_roles_adminview, dict)
+    assert isinstance(action_users_adminview, dict)
+    assert isinstance(action_system_roles_adminview, dict)
+
+    assert 'model' in action_roles_adminview
+    assert 'modelview' in action_roles_adminview
+    assert 'model' in action_users_adminview
+    assert 'modelview' in action_users_adminview
+    assert 'model' in action_system_roles_adminview
+    assert 'modelview' in action_system_roles_adminview
