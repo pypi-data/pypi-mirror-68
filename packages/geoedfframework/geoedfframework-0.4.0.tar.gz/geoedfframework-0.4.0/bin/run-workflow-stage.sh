2ba5df067ccf96841a4ccd499b5fcef66724a4a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:20:30 +0000

    Add package geoedfframework/0.4.0/geoedfframework-0.4.0.tar.gz

---
+++ packages/geoedfframework/geoedfframework-0.4.0.tar.gz/geoedfframework-0.4.0/bin/run-workflow-stage.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+#arguments:
+#workflow_fname: workflow filepath
+#workflow_stage: stage to be executed; encoded as : separated list (for connectors)
+#plugin_type: type of plugin to be executed; helps with validation of inputs
+#target_path: either path where outputs are to be stored or filepath for filter outputs
+#plugin_name; this is used to determine the environment to activate (only for connectors)
+
+#### the next three arguments need to be specified; use explicit 'none' in case no value
+#var_bindings: JSON string containing one set of variable bindings (only for connectors)
+#stage_bindings: JSON string containing one set of bindings for stage references
+#overridden_args: comma separated list of args that need to be overridden
+####
+
+#optional list of file paths to input files corresponding to overridden_args
+
+#simple validation on the minimum number of arguments required
+#this script is invoked for both connectors and processors
+#further validation needs to be performed in the respective scripts
+if [ "$#" -lt 6 ]; then
+   echo "At least six arguments need to be provided"
+   exit 1
+fi
+
+# if this is a connector plugin, we need to activate the environment first
+if [ "$3" == "Input" ] || [ "$3" == "Filter" ]; then
+    env="$5"
+    source activate $env
+    python3 /usr/local/bin/run-connector-plugin.py "$@"
+else # processor
+    python3 /usr/local/bin/run-processor-plugin.py "$@"
+fi
