bdba6187a36f3d7888457fd43ff6d8f1b056c8af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:23:26 +0000

    Add package leaguepedia-parser/0.3.0/leaguepedia_parser-0.3.0.tar.gz

---
+++ packages/leaguepedia-parser/leaguepedia_parser-0.3.0.tar.gz/leaguepedia_parser-0.3.0/setup.py
@@ -0,0 +1,20 @@
+import setuptools
+from os import path
+
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name='leaguepedia_parser',
+    version='0.3.0',
+    packages=['leaguepedia_parser'],
+    url='https://github.com/mrtolkien/leaguepedia_parser',
+    license='MIT',
+    author='Tolki',
+    install_requires=['mwclient'],
+    author_email='gary.mialaret+pypi@gmail.com',
+    description='A parser for the Leaguepedia website, focused on accessing esports data.',
+    long_description=long_description,
+    long_description_content_type='text/markdown'
+)
