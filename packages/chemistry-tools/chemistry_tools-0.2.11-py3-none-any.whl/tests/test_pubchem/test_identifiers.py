d26834c5bb9a2270f77bad0f26c32d5501eb6311
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:33 +0000

    Add package chemistry-tools/0.2.11/chemistry_tools-0.2.11-py3-none-any.whl

---
+++ packages/chemistry-tools/chemistry_tools-0.2.11-py3-none-any.whl/tests/test_pubchem/test_identifiers.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+test_indentifiers
+~~~~~~~~~~~~~~~~~
+
+Test identifiers requests.
+
+"""
+
+from chemistry_tools.pubchem.utils import get_aids, get_cids, get_sids
+
+
+def test_identifiers_from_name():
+	"""Use a name input to retrieve lists of identifiers."""
+	# Get CID for each compound linked to substances with name Aspirin
+	assert len(get_cids('Aspirin', 'name', 'substance')) >= 10
+	# Get CID for each compound with name Aspirin
+	assert len(get_cids('Aspirin', 'name', 'compound')) >= 1
+	# Get SID for substances linked to compound with name Aspirin
+	assert len(get_sids('Aspirin', 'name', 'substance')) >= 10
+	# Get AID for each assay linked to substances with name Aspirin
+	assert len(get_aids('Aspirin', 'name', 'substance')) >= 10
+	# Get AID for each assay linked to compound with name Aspirin
+	assert len(get_aids('Aspirin', 'name', 'compound')) >= 1
+
+
+def test_no_identifiers():
+	"""Test retrieving no identifier results."""
+	assert get_cids('asfgaerghaeirughae', 'name', 'substance') == []
+	assert get_cids('asfgaerghaeirughae', 'name', 'compound') == []
+	assert get_sids(999999999, 'cid', 'compound') == []
+	assert get_aids(150195, 'cid', 'compound') == []
