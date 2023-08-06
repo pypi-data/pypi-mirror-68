aa162b359d7119945aeb2a48dc337d2fb0dbc265
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:12:18 +0000

    Add package rigidanalytics-tracker/2.0.1/rigidanalytics_tracker-2.0.1-py3-none-any.whl

---
+++ packages/rigidanalytics-tracker/rigidanalytics_tracker-2.0.1-py3-none-any.whl/rigidanalytics_tracker/django/tracker.py
@@ -0,0 +1,48 @@
+import re
+
+from rigidanalytics_tracker import Tracker
+
+
+class DjangoTracker(Tracker):
+
+    def __init__(self):
+        self.init_tracker()
+
+    @property
+    def app_settings(self):
+        from django.conf import settings
+
+        return settings
+
+    def get_app_setting(self, setting, default=None):
+        if hasattr(self.app_settings, setting):
+            return getattr(self.app_settings, setting)
+        return default
+
+    def extract_request_headers(self):
+        regex_http_ = re.compile(r'^HTTP_.+$')
+        regex_content_type = re.compile(r'^CONTENT_TYPE$')
+        regex_content_length = re.compile(r'^CONTENT_LENGTH$')
+
+        request_headers = {}
+        for header in self.request.META:
+            if (
+                regex_http_.match(header)
+                or regex_content_type.match(header)
+                or regex_content_length.match(header)
+            ):
+                request_headers[header] = self.request.META[header]
+
+        return request_headers
+
+    def get_full_url(self):
+        return self.request.build_absolute_uri()
+
+    def get_session_id(self):
+        return self.request.session.session_key
+
+    def get_response_data(self):
+        return {
+            "status_code": self.response.status_code,
+            "cookies": self.response.cookies,
+        }
