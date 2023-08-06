0085adbad26905f233efbeabb7765b12e6d39759
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:24:39 +0000

    Add package inn/2020.5.19.16.11.20/inn-2020.5.19.16.11.20-py3-none-any.whl

---
+++ packages/inn/inn-2020.5.19.16.11.20-py3-none-any.whl/inn/models/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Project      : DeepTricks.
+# @File         : __init__.py
+# @Time         : 2020-02-14 20:40
+# @Author       : yuanjie
+# @Email        : yuanjie@xiaomi.com
+# @Software     : PyCharm
+# @Description  : 
+
+
+from .BaseModel import BaseModel
