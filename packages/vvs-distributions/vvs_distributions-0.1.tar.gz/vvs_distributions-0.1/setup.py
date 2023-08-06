825383decd0658a4eb45b597502509e0c9331ac0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:20:39 +0000

    Add package vvs-distributions/0.1/vvs_distributions-0.1.tar.gz

---
+++ packages/vvs-distributions/vvs_distributions-0.1.tar.gz/vvs_distributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='vvs_distributions',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['vvs_distributions'],
+      author = 'Vamsee Sundar',
+      author_email = 'vvippart@asu.edu',
+      zip_safe=False)
