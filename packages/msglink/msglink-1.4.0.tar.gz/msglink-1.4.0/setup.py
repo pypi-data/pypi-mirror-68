e80a966d77a8442e94f6945ff0ea8e4d706d0eee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:08:51 +0000

    Add package msglink/1.4.0/msglink-1.4.0.tar.gz

---
+++ packages/msglink/msglink-1.4.0.tar.gz/msglink-1.4.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='msglink',
+    version='1.4.0',
+    packages=['msglink'],
+    url='',
+    license='LGPL v3',
+    author='Robert Brünig',
+    author_email='',
+    description='msglink client library'
+)
