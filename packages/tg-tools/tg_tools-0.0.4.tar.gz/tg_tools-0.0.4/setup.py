9aef003df30531b8171dca6e34fd09350bea1415
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:04:43 +0000

    Add package tg-tools/0.0.4/tg_tools-0.0.4.tar.gz

---
+++ packages/tg-tools/tg_tools-0.0.4.tar.gz/tg_tools-0.0.4/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name='tg_tools',
+    version='0.0.4',
+    description='Standard toolset for development in TestGilde projects currently to simplify Config and MongoDB handling within docker container.',
+    py_modules=['tg_tools'],
+    package_dir={'': 'src'},
+    install_requires=[
+        "flask_pymongo",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
