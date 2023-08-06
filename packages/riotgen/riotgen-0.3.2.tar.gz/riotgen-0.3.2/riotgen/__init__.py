b7f855419886ff0236f98b008722fd634f2400a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:53:28 +0000

    Add package riotgen/0.3.2/riotgen-0.3.2.tar.gz

---
+++ packages/riotgen/riotgen-0.3.2.tar.gz/riotgen-0.3.2/riotgen/__init__.py
@@ -0,0 +1,13 @@
+"""RIOT generator is a helper library for bootstraping RIOT code.
+
+It can be used to bootstrap:
+- a new board support
+- a new external application
+- a new RIOT example application
+- a new test application
+- a new package for RIOT
+"""
+
+__version__ = '0.3.2'
+
+from .generator import cli  # noqa
