e75e4978931accd5267493168b0ea52ef203d97b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:35:10 +0000

    Add package Pyterate/2.0.1/Pyterate-2.0.1.tar.gz

---
+++ packages/Pyterate/Pyterate-2.0.1.tar.gz/Pyterate-2.0.1/doc/sphinx/source/examples/document-generator/full-test.py
@@ -0,0 +1,103 @@
+#r# .. note::
+#r#    You can see the source by clicking on the eye
+
+# A source code comment
+##
+###
+#?# A comment that must not appear in the documentation
+
+foo = 1
+
+#r# ==========================
+#r#  A Restructuredtext Title
+#r# ==========================
+
+foo = 1
+
+#r#
+#r# Some reStructuredText contents
+#r#
+
+#m#
+#m# Some Markdown contents
+#m#
+#m# [An inline-style link](https://www.python.org)
+#m#
+
+foo = 1
+
+# Insert the output of the following python code
+print(foo)
+#o#
+
+foo = 1
+
+# Hidden Python code
+#h# value = 123 * 3
+
+foo = 1
+
+#r# Format RST content with current locals dictionary using @@<<@@...@@>>@@ instead of {...}.
+#r#
+#r# .. math::
+#r#
+#r#     I_d = @<@value@>@ I_s \left( e^{\frac{V_d}{n V_T}} - 1 \right)
+
+# Add Python code as a literal block
+#l# for x in ():
+#l#   1 / 0 / 0
+
+# Interactive code
+#<i#
+1 + 1
+2 * 4 * 2
+a, b = 1, 2
+1, 2, 3
+#i>#
+
+# Guarded error
+#<e#
+1/0
+#e>#
+
+# Add a Python file as a literal block
+#f# getthecode('RingModulator.py')
+
+# Add the file content as literal block
+#f# literal_include('kicad-pyspice-example.cir')
+
+# Insert an image
+#f# image('kicad-pyspice-example.sch.svg')
+
+# Insert Circuit_macros diagram
+#f# foo = circuit_macros
+#f# foo('circuit.m4')
+
+# Insert Tikz figure
+#f# width = 3 * 200
+#f# tikz('diode.tex',
+#f#       width=width)
+
+import numpy as np
+import matplotlib.pyplot as plt
+figure = plt.figure(1, (20, 10))
+x = np.arange(1, 10, .1)
+y = np.sin(x)
+plt.plot(x, y)
+
+# Insert a Matplotlib figure
+#f# save_figure('figure', 'my-figure.png',
+#f#             width=1280)
+#f#
+
+# Insert a table
+N = 2
+x = np.arange(-N, N, 0.5)
+y = np.arange(-N, N, 0.5)
+xx, yy = np.meshgrid(x, y, sparse=True)
+z = np.sin(xx**2 + yy**2) / (xx**2 + yy**2)
+#f# export('z', grid_size='x.shape[0]')
+#f# table(z, str_format='{:.1f}')
+#f# table('z', columns=[chr(ord('A') + i) for i in range(grid_size)], str_format='{:.3f}')
+
+foo = 1
---
+++ packages/Pyterate/Pyterate-2.0.1.tar.gz/Pyterate-2.0.1/setup.py
@@ -0,0 +1,85 @@
+#! /usr/bin/env python3
+
+####################################################################################################
+#
+# Pyterate - Sphinx add-ons to create API documentation for Python projects
+# Copyright (C) 2017 Fabrice Salvaire
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+####################################################################################################
+
+####################################################################################################
+
+import glob
+import os
+import sys
+
+from setuptools import setup, find_packages
+setuptools_available = True
+
+####################################################################################################
+
+if sys.version_info < (3,):
+    print('Pyterate requires Python 3', file=sys.stderr)
+    sys.exit(1)
+
+exec(compile(open('setup_data.py').read(), 'setup_data.py', 'exec'))
+
+####################################################################################################
+
+def read_requirement():
+    return [requirement.strip() for requirement in open('requirements.txt').readlines()]
+
+####################################################################################################
+
+def glob_templates(*args):
+    path = os.path.join(*args)
+    return [os.path.relpath(x, path)
+            for x in glob.glob(os.path.join(path, 'templates', '*jinja2'))]
+
+####################################################################################################
+
+setup_dict.update(dict(
+    # include_package_data=True, # Look in MANIFEST.in
+    packages=find_packages(exclude=['unit-test']),
+    scripts=glob.glob('bin/*'),
+    package_data={
+        'Pyterate.Config': ['logging.yml'],
+        'Pyterate.ApiRstFactory': glob_templates('Pyterate', 'ApiRstFactory'),
+        'Pyterate.RstFactory': glob_templates('Pyterate', 'RstFactory'),
+    },
+
+    platforms='any',
+    zip_safe=False, # due to data files
+
+    # cf. http://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Framework :: Sphinx :: Extension',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Education',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3.4',
+        'Topic :: Software Development :: Documentation',
+        ],
+
+    install_requires=read_requirement(),
+))
+
+####################################################################################################
+
+setup(**setup_dict)
