57291176308a9156036acbf8f9954c9efd0adc00
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:27:07 +0000

    Add package peony/1.0.3/peony-1.0.3.tar.gz

---
+++ packages/peony/peony-1.0.3.tar.gz/peony-1.0.3/peony/proxy/stat_counter.py
@@ -0,0 +1,23 @@
+from collections import defaultdict
+
+
+class StatCounter(object):
+
+    """
+    统计计算类
+    """
+
+    # 客户端连接数
+    clients = 0
+    # 客户端请求数
+    client_req = 0
+    # 客户端回应数
+    client_rsp = 0
+    # worker请求数
+    worker_req_counter = None
+
+    def __init__(self):
+        self.worker_req_counter = defaultdict(int)
+
+    def add_worker_req(self, worker_id):
+        self.worker_req_counter[worker_id] += 1
