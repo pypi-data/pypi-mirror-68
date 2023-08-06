f4fb42856149b046ff8fa1a0b5b3f27eb987339d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:25:58 +0000

    Add package pysecuritytrails/0.1.2/pysecuritytrails-0.1.2.tar.gz

---
+++ packages/pysecuritytrails/pysecuritytrails-0.1.2.tar.gz/pysecuritytrails-0.1.2/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='pysecuritytrails',
+    version='0.1.2',
+    description='Python wrapper around the Security Trails API',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/Te-k/pysecuritytrails',
+    author='Tek',
+    author_email='tek@randhome.io',
+    keywords='security',
+    install_requires=['requests', 'configparser'],
+    license='GPLv3',
+    python_requires='>=3.5',
+    packages=['pysecuritytrails'],
+    entry_points= {
+        'console_scripts': [ 'securitytrails=pysecuritytrails.cli:main' ]
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ]
+
+)
