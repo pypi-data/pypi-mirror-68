61ca28bcb42dba1f14094bb7f847a05863b7410c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:00:11 +0000

    Add package steamsync/0.1.1/steamsync-0.1.1.tar.gz

---
+++ packages/steamsync/steamsync-0.1.1.tar.gz/steamsync-0.1.1/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="steamsync",
+    version="0.1.1",
+    author="Jayden Milne",
+    author_email="jaydenmilne@users.noreply.github.com",
+    description="Tool to automatically add games from the Epic Games Launcher to Steam",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/jaydenmilne/steamsync",
+    packages=setuptools.find_packages('src'),
+    package_dir={'': 'src'},
+    classifiers=[
+        "Operating System :: Microsoft :: Windows",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)",
+        "Topic :: Games/Entertainment",
+
+    ],
+    scripts=["src/steamsync.py"],
+    python_requires='>=3.6',
+)
