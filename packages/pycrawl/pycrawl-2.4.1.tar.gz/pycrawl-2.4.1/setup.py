c70a27289842e07dc71db2d7ae2ba5c0b68f9150
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:11:33 +0000

    Add package pycrawl/2.4.1/pycrawl-2.4.1.tar.gz

---
+++ packages/pycrawl/pycrawl-2.4.1.tar.gz/pycrawl-2.4.1/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='pycrawl',
+    packages=['pycrawl'],
+    install_requires=['mechanize', 'lxml', 'cssselect'],
+
+    version='2.4.1',
+    license='MIT',
+
+    author='Tatsuya Abe',
+    author_email='abe12@mccc.jp',
+
+    url='https://github.com/AjxLab/PyCrawl',
+
+    desription='A simple crawling utility for Python',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    keywords='crawling crawler scraping',
+
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.7',
+    ],
+)
