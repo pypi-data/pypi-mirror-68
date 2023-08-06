49261cf16bfa5cf4b8e46db2e3dcbd7b15494913
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:19:37 +0000

    Add package cobib/2.0.0b4/cobib-2.0.0b4.tar.gz

---
+++ packages/cobib/cobib-2.0.0b4.tar.gz/cobib-2.0.0b4/cobib/commands/init.py
@@ -0,0 +1,22 @@
+"""Cobib init command"""
+
+import os
+import sys
+
+from cobib.config import CONFIG
+from .base_command import Command
+
+
+class InitCommand(Command):
+    """Init Command"""
+
+    name = 'init'
+
+    def execute(self, args, out=sys.stdout):
+        """initialize database
+
+        Initializes the yaml database file at the configured location.
+        """
+        conf_database = CONFIG.config['DATABASE']
+        file = os.path.expanduser(conf_database['file'])
+        open(file, 'w').close()
