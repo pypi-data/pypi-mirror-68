f6f63a8dfc1d54c360ebc69b961f77f6f44395f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:11:33 +0000

    Add package hjw-distributions/0.1/hjw-distributions-0.1.tar.gz

---
+++ packages/hjw-distributions/hjw-distributions-0.1.tar.gz/hjw-distributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='hjw-distributions',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      author = 'Jingwen Huang',
+      author_email = 'jingwen.h@yahoo.com',
+      packages=['hjw-distributions'],
+      zip_safe=False)
