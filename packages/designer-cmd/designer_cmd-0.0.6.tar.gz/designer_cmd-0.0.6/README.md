4cf2886ce1d9c920c88a31bf65b911e607db7195
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:19:33 +0000

    Add package designer-cmd/0.0.6/designer_cmd-0.0.6.tar.gz

---
+++ packages/designer-cmd/designer_cmd-0.0.6.tar.gz/designer_cmd-0.0.6/README.md
@@ -0,0 +1,26 @@
+
+[![image](https://img.shields.io/pypi/v/designer_cmd.svg)](https://pypi.org/project/designer_cmd)
+[![image](https://img.shields.io/badge/License-MIT-green.svg)](https://opensource.org/licenses/MIT)
+
+# Пакет для автоматизации взаимодействия с пакетным режимом 1С.
+Формат команд поддерживается только > 8.3.12
+# Установка:
+    pip install designer_cmd    
+# Пример использования:    
+    from designer_cmd import api
+    
+    conn = api.Connection(user='User', password='Password', file_path='DB_Path')
+    designer = api.Designer('8.3.12.1254', self.conn)
+
+    designer.load_config_from_file('path_to_cf_file')
+# Функциональность:
+-- Работа в контексте Windows:
+- Выгрузка/Загрузка cf.
+- Выгрузка/Загрузка dt.
+- Выгрузка/Загрузка в/из xml
+- Сравнение конфигурации с файлом cf
+- Обновление структуры конфигурации
+#Планируемая фукциональность:
+- Работа в контексте linux
+- Работа с хранилищем
+- Работа с git
---
+++ packages/designer-cmd/designer_cmd-0.0.6.tar.gz/designer_cmd-0.0.6/setup.py
@@ -0,0 +1,54 @@
+import os
+from importlib.machinery import SourceFileLoader
+
+from pkg_resources import parse_requirements
+from setuptools import find_packages, setup
+
+module_name = 'designer_cmd'
+
+module = SourceFileLoader(
+    module_name, os.path.join(module_name, '__init__.py')
+).load_module()
+
+
+def load_requirements(fname: str) -> list:
+    requirements = []
+    with open(fname, 'r') as fp:
+        for req in parse_requirements(fp.read()):
+            extras = '[{}]'.format(','.join(req.extras)) if req.extras else ''
+            requirements.append(
+                '{}{}{}'.format(req.name, extras, req.specifier)
+            )
+    return requirements
+
+
+try:
+    import pypandoc
+    long_description = pypandoc.convert('Readme.md', 'rst')
+except(IOError, ImportError):
+    long_description = open('Readme.md', encoding='utf-8').read()
+
+setup(
+    name=module_name,
+    version=module.__version__,
+    author=module.__author__,
+    author_email=module.__email__,
+    license=module.__license__,
+    description="Пакет для автоматизации взаимодействия с пакетным режимом 1С.",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/AlexanderNiMo/designer_cmd',
+    platforms=['Windows'],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Natural Language :: Russian',
+        'Operating System :: Microsoft :: Windows',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: Implementation :: CPython'
+    ],
+    python_requires='>=3.6',
+    packages=find_packages(exclude=['tests']),
+    install_requires=load_requirements('requirements.txt'),
+    include_package_data=True,
+    test_suite='designer_cmd.tests',
+)
