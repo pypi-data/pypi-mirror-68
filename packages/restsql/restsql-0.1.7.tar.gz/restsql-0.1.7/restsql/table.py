8954429545f640f38fc67d613544e73055111049
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:54 +0000

    Add package restsql/0.1.7/restsql-0.1.7.tar.gz

---
+++ packages/restsql/restsql-0.1.7.tar.gz/restsql-0.1.7/restsql/table.py
@@ -0,0 +1,43 @@
+# -*- coding:utf-8 -*-
+
+
+class Field(object):
+    pass
+
+
+class IntegerField(Field):
+    '''
+    整数类型
+    '''
+    pass
+
+
+class DoubleField(Field):
+    '''
+    数字类型
+    '''
+    pass
+
+
+class StringField(Field):
+    '''
+    字符串类型
+    '''
+    pass
+
+
+class BoolField(Field):
+    '''
+    布尔类型
+    '''
+    pass
+
+
+class Table(object):
+    '''
+    Schema基类
+    '''
+
+    pass
+
+
