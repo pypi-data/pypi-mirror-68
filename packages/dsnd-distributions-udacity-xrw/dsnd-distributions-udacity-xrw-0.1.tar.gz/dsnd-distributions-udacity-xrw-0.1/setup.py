515542c640d0457ad0380ff348012957601e2c8a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:27 +0000

    Add package dsnd-distributions-udacity-xrw/0.1/dsnd-distributions-udacity-xrw-0.1.tar.gz

---
+++ packages/dsnd-distributions-udacity-xrw/dsnd-distributions-udacity-xrw-0.1.tar.gz/dsnd-distributions-udacity-xrw-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='dsnd-distributions-udacity-xrw',
+      version='0.1',
+      description='Gaussian and Bionomial Distributions',
+      author = 'Xinran Wang',
+      packages=['dsnd-distributions-udacity-xrw'],
+      author_email = 'xinranwang1997@outlook.com',
+      zip_safe=False)
