9ca7af11365259c5f15a42a577900eaf38a8329a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:13:51 +0000

    Add package TowelStuff10000/0.1.0/TowelStuff10000-0.1.0.tar.gz

---
+++ packages/TowelStuff10000/TowelStuff10000-0.1.0.tar.gz/TowelStuff10000-0.1.0/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+name='TowelStuff10000',
+version='0.1.0',
+author='pleasefeedme',
+author_email='myfakeemail@gmail.com',
+packages=['towelstuff'],
+scripts=[],
+url='http://pypi.python.org/pypi/TowelStuff10000',
+license='LICENSE.txt',
+description='Useful towel-related stuff.',
+install_requires=['requests'],
+)
