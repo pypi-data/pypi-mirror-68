bc64cbe60eb045418ed6d58bf91d811c14a919fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:01:41 +0000

    Add package thalesians.tsa/0.3/thalesians.tsa-0.3.tar.gz

---
+++ packages/thalesians.tsa/thalesians.tsa-0.3.tar.gz/thalesians.tsa-0.3/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(name='thalesians.tsa',
+      version='0.3',
+      description='The Thalesians\' Time Series Analysis (TSA) library',
+      url='https://github.com/thalesians/tsa',
+      author='Thalesians Ltd',
+      author_email='info@thalesians.com',
+      license='Apache-2.0',
+      packages=[
+            'thalesians',
+            'thalesians.tsa',
+            'thalesians.tsa.datasets',
+            'thalesians.tsa.filtering',
+            'thalesians.tsa.neural',
+            'thalesians.tsa.optimisation',
+            'thalesians.tsa.q'],
+      zip_safe=False)
