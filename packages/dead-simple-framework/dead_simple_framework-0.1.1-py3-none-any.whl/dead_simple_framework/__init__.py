d5b5d7a57f9244e150bae8409159f07502f79b01
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:05:27 +0000

    Add package dead-simple-framework/0.1.1/dead_simple_framework-0.1.1-py3-none-any.whl

---
+++ packages/dead-simple-framework/dead_simple_framework-0.1.1-py3-none-any.whl/dead_simple_framework/__init__.py
@@ -0,0 +1,10 @@
+''' Dead Simple Framework - Specify APIs with a Python dict and run with one line
+
+    Author: Peter Swanson
+'''
+
+from .main import Application
+from .database import Database
+from .tasks import Task_Manager
+
+from .sample import sample_config
