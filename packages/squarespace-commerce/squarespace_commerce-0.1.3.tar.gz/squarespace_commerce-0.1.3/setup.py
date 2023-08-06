3cb050116e26d0f8c7cea9c211077b0e83bee0c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:20:43 +0000

    Add package squarespace-commerce/0.1.3/squarespace_commerce-0.1.3.tar.gz

---
+++ packages/squarespace-commerce/squarespace_commerce-0.1.3.tar.gz/squarespace_commerce-0.1.3/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['squarespace_commerce']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['requests>=2.23.0,<3.0.0']
+
+entry_points = \
+{'console_scripts': ['my-script = squarespace_commerce.app:start']}
+
+setup_kwargs = {
+    'name': 'squarespace-commerce',
+    'version': '0.1.3',
+    'description': 'Pythonic access to the Squarespace Commerce API',
+    'long_description': "# Squarespace_Commerce Python Module\n\nThe Squarespace_Commerce module attempts to provide easy access to [Squarespace's Commerce API](http://developers.squarespace.com/commerce-api\n).\n\n## Usage\n````\n#Instantiate the squarespace class to get access to your store:\n#Version defaults to 1.5\norder = Squarespace('APIKEY')\n\n#Optional Parameters include:\norder = Squarespace('APIKEY','APIVERSION','APIBASEURL')\n````\n## Orders API\n````\n#Get the first page of orders, returns 50:\norder.get_orders()\n\n#Optional Parameters include:\norder.get_orders(cursor='{Token}',modified_after='{ISO 8601 Date}'',modified_before='{ISO 8601 Date}',fulfillment_status='{PENDING | FULFILLED | CANCELLED}')\n\n#Get a specific order\norder.get_order('order_id')\n\n#Fulfill a specific order\norder.fulfill_order('order_id')\n\n#Optional Parameters include:\norder.fulfill_order('order_id', send_notification={True | FALSE}, ship_date={ISO 8601 Date}, tracking_number='',\n                      carrier_name='', service='', tracking_url='{valid_url}'):\n````\n## Transactions API\n#WIP\n\n\n## Inventory API\n#WIP\n",
+    'author': 'Casey Dierking',
+    'author_email': 'casey@dierking.dev',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://https://github.com/caseydierking/squarespace-commerce-python',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
---
+++ packages/squarespace-commerce/squarespace_commerce-0.1.3.tar.gz/squarespace_commerce-0.1.3/squarespace_commerce/__init__.py
@@ -0,0 +1,2 @@
+from .app import Squarespace
+__version__ = '0.1.3'
