4225135345394272fadd0f824b0df47e9f90c89e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:11:55 +0000

    Add package labvision/0.2.12/labvision-0.2.12.tar.gz

---
+++ packages/labvision/labvision-0.2.12.tar.gz/labvision-0.2.12/labvision/__init__.py
@@ -0,0 +1,7 @@
+from . import auto
+from . import datasets
+from . import functional
+from . import transforms
+from . import dock
+
+__all__ = ['auto', 'datasets', 'functional', 'transforms', 'dock']
