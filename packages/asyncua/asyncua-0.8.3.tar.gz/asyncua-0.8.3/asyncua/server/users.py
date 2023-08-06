91c0d3c7b7acbb0d4d130f27344822ec462e0dd2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:22:38 +0000

    Add package asyncua/0.8.3/asyncua-0.8.3.tar.gz

---
+++ packages/asyncua/asyncua-0.8.3.tar.gz/asyncua-0.8.3/asyncua/server/users.py
@@ -0,0 +1,14 @@
+"""
+Implement user management here.
+"""
+
+from enum import Enum
+
+
+class User(Enum):
+    """
+    Define some default users.
+    """
+    Admin = 0
+    Anonymous = 1
+    User = 3
