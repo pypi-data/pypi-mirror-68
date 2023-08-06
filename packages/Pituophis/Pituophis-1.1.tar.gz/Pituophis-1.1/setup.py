072d55bf39afd72872ad54199c2424aa7938a6b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:25:44 +0000

    Add package Pituophis/1.1/Pituophis-1.1.tar.gz

---
+++ packages/Pituophis/Pituophis-1.1.tar.gz/Pituophis-1.1/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='Pituophis',
+    version='1.1',
+    install_requires=['natsort'],
+    packages=['pituophis'],
+    url='https://github.com/dotcomboom/Pituophis',
+    license='BSD 2-Clause License',
+    author='dotcomboom',
+    author_email='dotcomboom@somnolescent.net',
+    description='Python 3 library for building Gopher clients and servers',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Information Technology",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent"
+    ],
+)
