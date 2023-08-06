d06ddfa912a84d3a20d478c487c28faa020b819c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:23:59 +0000

    Add package twitter-nlp-toolkit/0.1.4/twitter_nlp_toolkit-0.1.4.tar.gz

---
+++ packages/twitter-nlp-toolkit/twitter_nlp_toolkit-0.1.4.tar.gz/twitter_nlp_toolkit-0.1.4/setup.py
@@ -0,0 +1,32 @@
+import sys , os
+import setuptools # USe to build pckg
+
+with open("README.md","r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name = "twitter_nlp_toolkit", # name shown on pypi and used with import
+    version ="0.1.4",
+    author = "Eric M. Schibli , Mohamad (Moe) Antar",
+    author_email = "moe.antar14@gmail.com",
+    description = "Tools for collecting , processing and analyzing twitter data",
+    long_description = long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/eschibli/twitter-toolbox", # url of github repo
+    keywords = "twitter sentiment analysis",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3.6",
+        "Operating System :: OS Independent" 
+    ],
+    install_requires=[
+          'numpy',
+          'pandas',
+          'scikit-learn',
+          
+      ],
+    python_requires='>=3.6',
+    include_package_data=True
+
+
+)
