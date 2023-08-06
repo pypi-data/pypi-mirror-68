785e99bdc8da3f94ac6d9fe29bca6368ee5dfb7f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:21:32 +0000

    Add package inventree/0.0.5/inventree-0.0.5.tar.gz

---
+++ packages/inventree/inventree-0.0.5.tar.gz/inventree-0.0.5/inventree/part.py
@@ -0,0 +1,72 @@
+# -*- coding: utf-8 -*-
+
+
+from inventree import base
+from inventree import stock
+from inventree import company
+from inventree import build
+
+
+class PartCategory(base.InventreeObject):
+    """ Class representing the PartCategory database model """
+
+    URL = 'part/category'
+    FILTERS = ['parent']
+
+    def getParts(self):
+        return Part.list(self._api, category=self.pk)
+    
+
+class Part(base.InventreeObject):
+    """ Class representing the Part database model """
+
+    URL = 'part'
+    FILTERS = [
+        'category',
+        'cascade',
+        'has_stock',
+        'low_stock',
+        'is_template',
+        'variant_of',
+        'assemply',
+        'component',
+        'trackable',
+        'salable',
+        'active',
+        'buildable',
+        'purchaseable',
+    ]
+
+    def getSupplierParts(self):
+        """ Return the supplier parts associated with this part """
+        return company.SupplierPart.list(self._api, part=self.pk)
+
+    def getBomItems(self):
+        """ Return the items required to make this part """
+        return BomItem.list(self._api, part=self.pk)
+
+    def getBuilds(self):
+        """ Return the builds associated with this part """
+        return build.Build.list(self._api, part=self.pk)
+
+    def getStockItems(self):
+        """ Return the stock items associated with this part """
+        return stock.StockItem.list(self._api, part=self.pk)
+
+    def getAttachments(self):
+        """ Return attachments associated with this part """
+        return PartAttachment.list(self._api, part=self.pk)
+
+
+class PartAttachment(base.Attachment):
+    """ Class representing a file attachment for a Part """
+
+    URL = 'part/attachment'
+    FILTERS = ['part']
+    
+
+class BomItem(base.InventreeObject):
+    """ Class representing the BomItem database model """
+
+    URL = 'bom'
+    FILTERS = ['part', 'sub_part']
---
+++ packages/inventree/inventree-0.0.5.tar.gz/inventree-0.0.5/inventree/stock.py
@@ -0,0 +1,134 @@
+# -*- coding: utf-8 -*-
+
+import os
+import logging
+
+from inventree import base
+
+
+class StockLocation(base.InventreeObject):
+    """ Class representing the StockLocation database model """
+
+    URL = 'stock/location'
+    FILTERS = ['parent']
+
+    def getStockItems(self):
+        return StockItem.list(self._api, location=self.pk)
+
+
+class StockItem(base.InventreeObject):
+    """ Class representing the StockItem database model.
+    
+    Stock items can be filtered by:
+    
+    - location: Where the stock item is stored
+    - category: The category of the part this stock item points to
+    - supplier: Who supplied this stock item
+    - part: The part referenced by this stock item
+    - supplier_part: Matching SupplierPart object
+    """
+
+    URL = 'stock'
+    FILTERS = [
+        'location',
+        'category',
+        'cascade',
+        'supplier',
+        'part',
+        'supplier_part'
+        'build',
+        'build_order',
+        'belongs_to',
+        'sales_order',
+        'customer',
+        'serialized',
+        'serial_number',
+        'allocated',
+        'active',
+        'ancestor',
+        'status',
+        'company',
+        'supplier',
+        'manufacturer',
+    ]
+
+    def getAttachments(self):
+        return StockItemAttachment.list(
+            self._api,
+            stock_item=self.pk
+        )
+
+    def getTestResults(self, **kwargs):
+
+        kwargs['stock_item'] = self.pk
+
+        return StockItemTestResult.list(
+            self._api,
+            **kwargs
+        )
+
+
+class StockItemAttachment(base.Attachment):
+    """ Class representing a file attachment for a StockItem """
+
+    URL = 'stock/attachment'
+    FILTERS = ['stock_item']
+
+
+class StockItemTracking(base.InventreeObject):
+    """ Class representing a StockItem tracking object """
+
+    URL = 'stock/track'
+    FILTERS = ['item', 'user']
+
+
+class StockItemTestResult(base.InventreeObject):
+    """ Class representing a StockItemTestResult object """
+
+    URL = 'stock/test'
+    FILTERS = ['stock_item', 'test', 'result', 'value', 'user']
+
+    @classmethod
+    def upload_result(cls, api, stock_item, test, result, **kwargs):
+        """
+        Upload a test result.
+
+        args:
+            api: Authenticated InvenTree API object
+            stock_item: pk of the StockItem object to upload the test result against
+            test: Name of the test (string)
+            result: Test result (boolean)
+
+        kwargs:
+            attachment: Optionally attach a file to the test result
+            notes: Add extra notes
+            value: Add a "value" to the test (e.g. an actual measurement made during the test)
+        """
+
+        attachment = kwargs.get('attachment', None)
+
+        files = {}
+
+        if attachment:
+            if os.path.exists(attachment):
+                f = os.path.basename(attachment)
+                files['attachment'] = (f, open(attachment, 'rb'))
+            else:
+                logging.error("File does not exist: '{f}'".format(f=attachment))
+
+        notes = kwargs.get('notes', '')
+        value = kwargs.get('value', '')
+
+        data = {
+            'stock_item': stock_item,
+            'test': test,
+            'result': result,
+            'notes': notes,
+            'value': value,
+        }
+
+        # Send the data to the serever
+        if api.post(cls.URL, data, files=files):
+            logging.info("Uploaded test result: '{test}'".format(test=test))
+        else:
+            logging.warning("Test upload failed")
