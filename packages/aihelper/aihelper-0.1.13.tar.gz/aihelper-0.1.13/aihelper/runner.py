55ff4222eea54a9189bff7b4c98a1e93323c8de0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:19:01 +0000

    Add package aihelper/0.1.13/aihelper-0.1.13.tar.gz

---
+++ packages/aihelper/aihelper-0.1.13.tar.gz/aihelper-0.1.13/aihelper/runner.py
@@ -0,0 +1,12 @@
+path = r'C:\Users\mjaquier\PycharmProjects\AIdirectory\tests\Project VERY NICE'
+
+import aihelper.aifile as afile
+
+def main():
+    allthedata = afile.topic_directories(path)
+    print('h')
+
+
+
+if __name__ == '__main__':
+    main()
