5478bf967379576d230e90b5580d6142f7d16b4c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:03:33 +0000

    Add package subset/0.1.1/subset-0.1.1.tar.gz

---
+++ packages/subset/subset-0.1.1.tar.gz/subset-0.1.1/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup
+import setuptools
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setup(name="subset",
+      version="0.1.1",
+      description="A cli-based word game",
+      long_description_content_type="text/markdown",
+      long_description=long_description,
+      url="https://github.com/rightbrace/subset-game.git",
+      author="Josef Duchesne",
+      author_email="josefduchesne@outlook.com",
+      license="MIT",
+      packages=setuptools.find_packages(),
+      scripts=["bin/subset"],
+      zip_safe=False,
+      install_requires=[
+          "colorama"
+      ],
+      include_package_data=True,
+      classifiers = [
+          "Programming Language :: Python :: 3",
+          "License :: OSI Approved :: MIT License",
+          "Operating System :: OS Independent",
+      ],
+      python_requires=">=3.5"
+      )
+
