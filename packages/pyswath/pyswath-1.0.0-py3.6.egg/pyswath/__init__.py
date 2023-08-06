1e55e9093436bbef7b62890e16dfa83d79ff24f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:35:14 +0000

    Add package pyswath/1.0.0/pyswath-1.0.0-py3.6.egg

---
+++ packages/pyswath/pyswath-1.0.0-py3.6.egg/pyswath/__init__.py
@@ -0,0 +1,24 @@
+######!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import  division
+# This to be sure that the result of the division of 2 integers is a real, not an integer
+from __future__ import absolute_import
+from __future__ import print_function
+
+__version__ = "1.0.0"
+
+# Import modules
+import sys
+import os
+#import copy
+import numpy as np
+
+# Import all the functions
+__all__ = ['raster_tools', 'profiles', 'swath', 'plotgraph', 'checks']
+
+from .raster_tools import *
+from .profiles import *
+from .swath import *
+from .plotgraph import *
+from .checks import *
