7f189c22caacdfa9d850b8a8987adf94c6939b6e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:00:01 +0000

    Add package throttler/1.1.2/throttler-1.1.2-py3-none-any.whl

---
+++ packages/throttler/throttler-1.1.2-py3-none-any.whl/throttler/__init__.py
@@ -0,0 +1,11 @@
+"""Zero-dependency Python package for simple throttling with asyncio support"""
+from .execution_timer import ExecutionTimer
+from .throttler import Throttler
+from .throttler_simultaneous import ThrottlerSimultaneous
+from .timer import Timer
+
+from .decorators import execution_timer, execution_timer_async, throttle, throttle_simultaneous, timer, timer_async
+
+__author__ = 'uburuntu'
+__license__ = 'MIT'
+__version__ = '1.1.2'
