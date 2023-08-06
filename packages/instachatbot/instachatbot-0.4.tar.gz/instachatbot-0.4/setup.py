13dec0e9bee0eacc55002abbe0c44a039b969f2e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:26:53 +0000

    Add package instachatbot/0.4/instachatbot-0.4.tar.gz

---
+++ packages/instachatbot/instachatbot-0.4.tar.gz/instachatbot-0.4/setup.py
@@ -0,0 +1,40 @@
+import codecs
+import sys
+
+from setuptools import setup
+import instachatbot
+
+
+def long_description():
+    try:
+        return codecs.open('README.md', 'r', 'utf-8').read()
+    except IOError:
+        return 'Long description error: Missing README.rst file'
+
+
+description = (
+    'Simple framework for building Instagram chat bots '
+    'with menu driven interface'
+)
+
+if sys.version_info < (3, 5):
+    sys.exit('instachatbot requires Python 3.5+')
+
+setup(
+    name='instachatbot',
+    description=description,
+    keywords='chatbot, instagram',
+    author='Artem Vasilyev',
+    author_email='artem.v.vasilyev@gmail.com',
+    long_description=long_description(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/art-vasilyev/instachatbot',
+    version=instachatbot.__version__,
+    packages=['instachatbot'],
+    python_requires=">=3.6",
+    platforms=['any'],
+    license='MIT',
+    install_requires=[
+        'instabot>=0.78'
+    ]
+)
