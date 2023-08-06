937d56229b9ad8c56bc0a88539bd0208fca012e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:22:55 +0000

    Add package distributions-unique/0.1/distributions-unique-0.1.tar.gz

---
+++ packages/distributions-unique/distributions-unique-0.1.tar.gz/distributions-unique-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributions-unique',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions-unique'],
+      zip_safe=False)
