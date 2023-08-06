3e263a87cc520ed70b1fb297006de8f980ead9c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:03:41 +0000

    Add package supy-driver/2018rc7/supy_driver-2018rc7-cp37-cp37m-macosx_10_14_x86_64.whl

---
+++ packages/supy-driver/supy_driver-2018rc7-cp37-cp37m-macosx_10_14_x86_64.whl/supy_driver/__init__.py
@@ -0,0 +1,14 @@
+###########################################################################
+# SUEWS for Python
+# Authors:
+# Ting Sun, ting.sun@reading.ac.uk
+# History:
+# 20 Jan 2018: first alpha release
+# 01 Feb 2018: performance improvement
+# 03 Feb 2018: improvement in output processing
+# 08 Mar 2018: pypi packaging
+###########################################################################
+
+from supy_driver.suews_driver import suews_driver
+from supy_driver.version import __version__
+
