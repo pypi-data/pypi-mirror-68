4ae8105cfab8e80f5563677bd3cf94d7aa2c8898
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:12:48 +0000

    Add package ngsolve-jupyterlab-widgets/0.1.10/ngsolve_jupyterlab_widgets-0.1.10.tar.gz

---
+++ packages/ngsolve-jupyterlab-widgets/ngsolve_jupyterlab_widgets-0.1.10.tar.gz/ngsolve_jupyterlab_widgets-0.1.10/README.md
@@ -0,0 +1,23 @@
+
+# ngsolve_jupyterlab_widgets
+
+[![Build Status](https://travis-ci.org/NGSolve/ngsolve_jupyterlab_widgets.svg?branch=master)](https://travis-ci.org/NGSolve/ngsolve_jupyterlab_widgets)
+[![codecov](https://codecov.io/gh/NGSolve/ngsolve_jupyterlab_widgets/branch/master/graph/badge.svg)](https://codecov.io/gh/NGSolve/ngsolve_jupyterlab_widgets)
+
+
+NGSolve Jupyter Widget Library
+
+## Installation
+
+You can install using `pip`:
+
+```bash
+pip install ngsolve_jupyterlab_widgets
+```
+
+If you are using Jupyter Notebook 5.2 or earlier, you may also need to enable
+the nbextension:
+
+```bash
+jupyter nbextension enable --py [--sys-prefix|--user|--system] ngsolve_jupyterlab_widgets
+```
---
+++ packages/ngsolve-jupyterlab-widgets/ngsolve_jupyterlab_widgets-0.1.10.tar.gz/ngsolve_jupyterlab_widgets-0.1.10/ngsolve_jupyterlab_widgets/example.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# Copyright (c) Matthias Hochsteger.
+# Distributed under the terms of the Modified BSD License.
+
+"""
+TODO: Add module docstring
+"""
+
+from ipywidgets import DOMWidget, register
+from traitlets import Unicode
+from ._frontend import module_name, module_version
+
+@register
+class NGSWebGuiWidget(DOMWidget):
+    from traitlets import Dict, Unicode
+    _view_name = Unicode('NGSolveView').tag(sync=True)
+    _view_module = Unicode(module_name).tag(sync=True)
+    _view_module_version = Unicode(module_version).tag(sync=True)
+    value = Dict({"ngsolve_version":'0.0.0'}).tag(sync=True)
+
+
+@register
+class ExampleWidget(DOMWidget):
+    """TODO: Add docstring here
+    """
+
+    _model_name = Unicode("ExampleModel").tag(sync=True)
+    _model_module = Unicode(module_name).tag(sync=True)
+    _model_module_version = Unicode(module_version).tag(sync=True)
+    _view_name = Unicode("ExampleView").tag(sync=True)
+    _view_module = Unicode(module_name).tag(sync=True)
+    _view_module_version = Unicode(module_version).tag(sync=True)
+
+    value = Unicode("Hello World").tag(sync=True)
