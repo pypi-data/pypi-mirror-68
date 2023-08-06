a13518e61ac434a2a3bc71d2d5fd24d4d60270a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:09:32 +0000

    Add package KDE-diffusion/1.0.0/KDE_diffusion-1.0.0-py3-none-any.whl

---
+++ packages/KDE-diffusion/KDE_diffusion-1.0.0-py3-none-any.whl/kde_diffusion/__init__.py
@@ -0,0 +1,20 @@
+﻿"""
+Kernel density estimation via diffusion in 1d and 2d.
+
+This module holds meta information and exposes the library's public
+interface. The actual functionality is implemented in separate
+modules.
+"""
+
+# Meta information
+__title__     = 'KDE-diffusion'
+__synopsis__  = 'Kernel density estimation via diffusion.'
+__version__   = '1.0.0'
+__date__      = '2020–05–14'
+__author__    = 'John Hennig'
+__copyright__ = 'John Hennig'
+__license__   = 'MIT'
+
+# Public interface
+from .kde1d import kde1d
+from .kde2d import kde2d
