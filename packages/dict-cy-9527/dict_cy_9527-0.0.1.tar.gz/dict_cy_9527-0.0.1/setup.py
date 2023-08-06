bba3ac6f54562c17b8dc190ecceb09e274e0c6aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:07 +0000

    Add package dict-cy-9527/0.0.1/dict_cy_9527-0.0.1.tar.gz

---
+++ packages/dict-cy-9527/dict_cy_9527-0.0.1.tar.gz/dict_cy_9527-0.0.1/setup.py
@@ -0,0 +1,9 @@
+import setuptools
+
+setuptools.setup(name='dict_cy_9527',
+                 version='0.0.1',
+                 description='test',
+                 author='cy',
+                 author_email='18556134754@163.com',
+                 license='MIT',
+                 packages=setuptools.find_packages())
