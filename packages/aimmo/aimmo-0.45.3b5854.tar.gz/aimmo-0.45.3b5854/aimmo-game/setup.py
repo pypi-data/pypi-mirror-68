4a5670db63eafff64f10b55862e3e57e0dd59576
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:18 +0000

    Add package aimmo/0.45.3b5854/aimmo-0.45.3b5854.tar.gz

---
+++ packages/aimmo/aimmo-0.45.3b5854.tar.gz/aimmo-0.45.3b5854/aimmo-game/setup.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+import os
+
+from setuptools import find_packages, setup
+
+withcoverage = os.environ.get("WITH_COVERAGE")
+
+setup(
+    name="aimmo-game",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        "aiohttp",
+        "aiohttp-cors",
+        "aiohttp-wsgi",
+        "eventlet",
+        "python-socketio==4.4.0",
+        "requests",
+        "six",
+        "kubernetes",
+        "prometheus_client",
+        "docker",
+    ],
+    setup_requires=["pytest-runner"],
+    tests_require=[
+        "socketIO-client",
+        "pytest>=5.2, <6",
+        "pytest-asyncio==0.10.0",
+        "pytest-mock",
+        "asynctest",
+        "httmock",
+        "mock",
+        "hypothesis",
+        "pytest-aiohttp",
+        "aioresponses",
+    ],
+    test_suite="tests",
+    zip_safe=False,
+)
