eed56f1d77ba59082b7494974fbb4e446f29c11d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:03:35 +0000

    Add package scrapy-proxy/0.3/scrapy-proxy-0.3.tar.gz

---
+++ packages/scrapy-proxy/scrapy-proxy-0.3.tar.gz/scrapy-proxy-0.3/setup.py
@@ -0,0 +1,20 @@
+'''
+@Author: apuyuseng
+@Date: 2020-04-20 17:13:19
+@LastEditTime: 2020-05-17 14:25:05
+@LastEditors: Please set LastEditors
+@Description: In User Settings Edit
+@FilePath: /scrapy-proxy/setup.py
+'''
+import sys
+from distutils.core import setup
+
+setup(name='scrapy-proxy',
+        version='0.3',
+        description='Scrapy Proxy: 从redis 中随机获取代理',
+        author='apuyuseng',
+        author_email='apuyuseng@gmail.com',
+        url='https://github.com/apuyuseng/scrapy-proxy',
+        packages=['scrapy_proxy'],
+        install_requires=['redis',],
+        )
