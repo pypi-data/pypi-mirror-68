4f7c43d99c409dd30f16d47bf6a9a01fb01b9d9f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:43 +0000

    Add package aithermal/0.1.2/aithermal-0.1.2.tar.gz

---
+++ packages/aithermal/aithermal-0.1.2.tar.gz/aithermal-0.1.2/setup.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['aithermal']
+
+package_data = \
+{'': ['*'], 'aithermal': ['out/*']}
+
+install_requires = \
+['PyYAML>=5.3.1,<6.0.0',
+ 'pandas>=1.0.3,<2.0.0',
+ 'simplelogging>=0.10.0,<0.11.0',
+ 'xlsxwriter>=1.2.8,<2.0.0']
+
+setup_kwargs = {
+    'name': 'aithermal',
+    'version': '0.1.2',
+    'description': '',
+    'long_description': None,
+    'author': 'mjaquier',
+    'author_email': 'mjaquier@me.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
