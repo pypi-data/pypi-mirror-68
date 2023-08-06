6c953f0ec8787d2c5100914a76b88ba1a7400a3f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:04:28 +0000

    Add package clusttool/0.0.8/clusttool-0.0.8-py3-none-any.whl

---
+++ packages/clusttool/clusttool-0.0.8-py3-none-any.whl/clusttool/toolbox.py
@@ -0,0 +1,35 @@
+
+def clustering(N,k):
+	"""
+	K-means clustering algorithm 
+	(c) Marko Niemelä, May 15, 2020
+	Inputs:
+	- N: Number of data samples
+	- k: Number of clusters
+	Ouputs:
+	- Plotted clustering result
+	"""
+	
+	# Generate fake data
+	X, L = sklearn.datasets.make_blobs(n_samples=N, n_features=2, centers=k)
+
+	# Perform clustering
+	kmeans = sklearn.cluster.KMeans(k, max_iter=100)
+	kmeans.fit(X)
+	means = kmeans.cluster_centers_
+
+	# Show results
+	plot.scatter(data[:, 0], data[:, 1], c=L)
+	plot.scatter(means[:, 0], means[:, 1], linewidths=2)
+	plot.show()
+
+
+
+
+
+
+
+
+
+
+
