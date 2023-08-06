a8ffb06e1ca5a51175529244a3fe323884518aa6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:22:37 +0000

    Add package simpler-ansi2html/0.1.0/simpler-ansi2html-0.1.0.tar.gz

---
+++ packages/simpler-ansi2html/simpler-ansi2html-0.1.0.tar.gz/simpler-ansi2html-0.1.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='simpler-ansi2html',
+    version='0.1.0',
+    url='https://github.com/volodymyrss/pystatuscake.git',
+    author='M. Bravo (Kronuz), Volodymyr Savchenko',
+    author_email='vladimir.savchenko@gmail.com',
+    description='an alternative ansi2html',
+    py_modules=['ansi2html'],
+    install_requires=[],
+)
