992aa28afccad154ce9a0a59e42939bb93cfce46
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:12:10 +0000

    Add package diario/0.2.0/diario-0.2.0.tar.gz

---
+++ packages/diario/diario-0.2.0.tar.gz/diario-0.2.0/setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+with open("README.rst", "r") as fh:
+    long_description = fh.read()
+
+EXCLUDE_FROM_PACKAGES = []
+
+install_requires = ["sdklib==1.10.3", "PySocks"]
+
+setuptools.setup(
+    name="diario",
+    version="0.2.0",
+    author="ElevenPaths",
+    author_email="innovationlab@11paths.com",
+    description="DIARIO SDK",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://diario.elevenpaths.com",
+    install_requires=install_requires,
+    keywords=["sdk", "api", "diario"],
+    classifiers=[
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+    ],
+    packages=setuptools.find_packages(exclude=EXCLUDE_FROM_PACKAGES),
+    zip_safe=False,
+)
