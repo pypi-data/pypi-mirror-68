0aaafb16c0d41467bf72d231c98977c13b3c3b84
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:26:10 +0000

    Add package edc-action-item/0.1.73/edc_action_item-0.1.73-py3-none-any.whl

---
+++ packages/edc-action-item/edc_action_item-0.1.73-py3-none-any.whl/edc_action_item/forms/action_item_form_mixin.py
@@ -0,0 +1,10 @@
+from django import forms
+
+
+class ActionItemFormMixin(forms.ModelForm):
+
+    action_identifier = forms.CharField(
+        label="Action Identifier",
+        required=False,
+        widget=forms.TextInput(attrs={"readonly": "readonly"}),
+    )
