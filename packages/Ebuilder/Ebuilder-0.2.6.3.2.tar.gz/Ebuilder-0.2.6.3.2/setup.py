62e4dee328d151e9aaf7e9de50701b60f839827c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:22:30 +0000

    Add package Ebuilder/0.2.6.3.2/Ebuilder-0.2.6.3.2.tar.gz

---
+++ packages/Ebuilder/Ebuilder-0.2.6.3.2.tar.gz/Ebuilder-0.2.6.3.2/setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="Ebuilder",
+    version="0.2.6.3.2",
+    author="Will F",
+    author_email="forsbergw82@gmail.com",
+    description="An upgraded version of good ol' Eebuilder, a static site generator that 'converts' Python 3 code to HTML 5.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=['ebuilder'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+    ],
+    url='https://github.com/RhinoCodes/Ebuilder',
+)
