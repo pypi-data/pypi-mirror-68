541992306136a91e9e76bf247720f3eacabc3035
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:36:42 +0000

    Add package pomace/0.0.10/pomace-0.0.10.tar.gz

---
+++ packages/pomace/pomace-0.0.10.tar.gz/pomace-0.0.10/pomace/browser.py
@@ -0,0 +1,54 @@
+import sys
+
+import log
+from splinter import Browser
+from splinter.exceptions import DriverNotFoundError
+from webdriver_manager import chrome, firefox
+
+from .config import settings
+
+
+NAMES = ['Firefox', 'Chrome']
+
+WEBDRIVER_MANAGERS = {
+    "chromedriver": chrome.ChromeDriverManager,
+    "geckodriver": firefox.GeckoDriverManager,
+}
+
+
+def launch() -> Browser:
+    options = {"headless": settings.browser.headless, "wait_time": 1.0}
+
+    if not settings.browser.name:
+        sys.exit("No browser specified")
+
+    # TODO: Only log when not IPython
+    # log.info(f'Launching browser: {settings.browser.name}')
+
+    try:
+        return Browser(settings.browser.name, **options)
+    except DriverNotFoundError:
+        sys.exit(f"Unsupported browser: {settings.browser.name}")
+    except Exception as e:  # pylint: disable=broad-except
+        log.debug(str(e))
+
+        for driver, manager in WEBDRIVER_MANAGERS.items():
+            if driver in str(e).lower():
+                options["executable_path"] = manager().install()
+                return Browser(settings.browser.name, **options)
+
+        raise e from None
+
+
+def resize(browser: Browser):
+    browser.driver.set_window_size(settings.browser.width, settings.browser.height)
+    browser.driver.set_window_position(0, 0)
+    size = browser.driver.get_window_size()
+    log.debug(f'Resized browser: {size}')
+
+
+def save_size(browser: Browser):
+    size = browser.driver.get_window_size()
+    settings.browser.width = size['width']
+    settings.browser.height = size['height']
+    log.debug(f'Saved new browser size: {size}')
---
+++ packages/pomace/pomace-0.0.10.tar.gz/pomace-0.0.10/pomace/tests/conftest.py
@@ -0,0 +1,43 @@
+# pylint: disable=no-self-use,unused-argument
+
+"""Unit tests configuration file."""
+
+
+import datafiles
+import log
+import pytest
+
+from pomace import shared
+
+
+class MockLinks:
+    def find_by_partial_text(self, value):
+        return [f'<mockelement: links.partial_text={value}>']
+
+
+class MockBrowser:
+
+    html = "Hello, world!"
+
+    def find_by_name(self, value):
+        return [f'<mockelement: name={value}>']
+
+    links = MockLinks()
+
+
+@pytest.fixture
+def mockbrowser(monkeypatch):
+    monkeypatch.setattr(shared, 'browser', MockBrowser())
+
+
+def pytest_configure(config):
+    """Disable verbose output when running tests."""
+    log.init(debug=True)
+
+    terminal = config.pluginmanager.getplugin('terminal')
+    terminal.TerminalReporter.showfspath = False
+
+
+def pytest_runtest_setup(item):
+    """Disable file storage during unit tests."""
+    datafiles.settings.HOOKS_ENABLED = False
