0f332ca0ce00ab48467d4156ea86f46ce3b59e29
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:17:59 +0000

    Add package ypyapi/0.0.2/ypyapi-0.0.2.tar.gz

---
+++ packages/ypyapi/ypyapi-0.0.2.tar.gz/ypyapi-0.0.2/setup.py
@@ -0,0 +1,31 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setup(name="ypyapi",
+      version="0.0.2",
+      description="A collection of tools for Python",
+      long_description=long_description,
+      classifiers=[
+          'Development Status :: 4 - Beta',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: MIT License',
+          'Natural Language :: English',
+          'Programming Language :: Python :: 3',
+          'Topic :: Utilities',
+          "Operating System :: OS Independent",
+          ],
+
+      install_requires=[
+          "selenium",
+          ],
+
+      author="PingyanYang",
+      url="http://pypi.python.org/pypi/ypyapi",
+      author_email="yangpingyan@gmail.com",
+      license="MIT",
+      packages=["ypyapi"])
