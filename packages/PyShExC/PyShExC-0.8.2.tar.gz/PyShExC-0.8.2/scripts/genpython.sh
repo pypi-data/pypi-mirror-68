cc1f68dc3925336910ffe5cebc3bb1fe95a14cb1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:08:44 +0000

    Add package PyShExC/0.8.2/PyShExC-0.8.2.tar.gz

---
+++ packages/PyShExC/PyShExC-0.8.2.tar.gz/PyShExC-0.8.2/scripts/genpython.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+# Note: need antlr4.8  to run this!
+# see: http://www.antlr.org/download.html for details
+cp ../../grammar/ShExDoc.g4 .
+antlr4 -Dlanguage=Python3 -package parser -o ../pyshexc/parser -no-listener -visitor ShExDoc.g4
+rm ShExDoc.g4
+
