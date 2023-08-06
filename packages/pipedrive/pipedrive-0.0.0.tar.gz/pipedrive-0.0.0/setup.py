cf4dec4c6b60978ae6a0b015f514145d30711c3e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:45 +0000

    Add package pipedrive/0.0.0/pipedrive-0.0.0.tar.gz

---
+++ packages/pipedrive/pipedrive-0.0.0.tar.gz/pipedrive-0.0.0/setup.py
@@ -0,0 +1,12 @@
+import setuptools
+
+setuptools.setup(
+    name="pipedrive",
+    version="0.0.0",
+    author="Ondrej Sika",
+    author_email="ondrej@ondrejsika.com",
+    description="Pipedrive Client",
+    url="https://github.com/ondrejsika/pipedrive",
+    packages=setuptools.find_packages(),
+    python_requires=">=3.6",
+)
