02737135b99426ca3e85187bf98e689b98827e92
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:02:58 +0000

    Add package fipt/0.2.0/fipt-0.2.0-py3-none-any.whl

---
+++ packages/fipt/fipt-0.2.0-py3-none-any.whl/fipt/impedance_data.py
@@ -0,0 +1,41 @@
+import numpy as np
+
+class ImpedanceData(object):
+    def __init__(self, name, base_filename, *, w_data=None, f_data=None, z_real_data=None, z_imag_data=None, z_data=None):
+        # self.logger = module_logger
+
+        self.name = name
+        self.base_filename = base_filename
+
+        if f_data is not None:
+            self.w_data = 2 * np.pi * f_data
+        elif w_data is not None:
+            self.w_data = w_data
+        else:
+            raise ValueError('Either w_data or f_data has to be given!')
+
+        if z_data:
+            self.z_data = z_data
+        elif z_real_data is not None and z_imag_data is not None:
+            self.z_data = z_real_data + 1j*z_imag_data
+        else:
+            raise ValueError('Either z_data or z_real_data and z_imag_data have to be given!')
+
+        if len(self.z_data) != len(self.w_data):
+            raise ValueError('Data for z_data and w_data need to have same length!')            
+
+          
+
+            
+
+def load_test_data_csv(fn):
+    import pandas as pd
+
+    df = pd.read_csv(fn)
+    df.head()
+
+    ipdata =  ImpedanceData(fn, fn, 
+                       f_data = df['Frequency (Hz)'].values,  
+                       z_real_data = df['Z\' (Ohms)'].values, 
+                       z_imag_data = df['Z\" (Ohms)'].values)    
+    return ipdata
