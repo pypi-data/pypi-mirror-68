9389db659c3aa83d45f07eb04f3ec72705a8ca3f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:23:45 +0000

    Add package fixturepy/0.1/fixturepy-0.1.tar.gz

---
+++ packages/fixturepy/fixturepy-0.1.tar.gz/fixturepy-0.1/setup.py
@@ -0,0 +1,39 @@
+from setuptools import setup
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+test_dependencies = ['assertpy']
+
+setup(
+    name='fixturepy',
+    packages=['fixturepy'],
+    version='0.1',
+    license='MIT',
+    description='Create random data to be used in tests',
+    author='Exentrique Solutions',
+    author_email='rs@exentriquesolutions.com',
+    url='https://github.com/exentriquesolutions/fixturepy',
+    keywords=['UnitTest', 'Fixture'],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    install_requires=[],
+    tests_require=test_dependencies,
+    test_suite='tests',
+    extras_require={
+        'test': test_dependencies
+    },
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Testing :: Unit',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ],
+)
