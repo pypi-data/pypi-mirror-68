449edf755ef9d219c6b85a260edacde3dbfaec2c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:12:35 +0000

    Add package tbcalc/1.0/tbcalc-1.0-py3.8.egg

---
+++ packages/tbcalc/tbcalc-1.0-py3.8.egg/tbcalc/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+from .analyser import Analyser
+from pyTTE import Quantity
+from .tensor_transform import cartesian_tensors_to_cylindrical
+
+__all__ = ['Analyser','Quantity','cartesian_tensors_to_cylindrical']
