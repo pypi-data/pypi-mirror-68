3a1eed2eb5b6353494ca75a611b18108f98ffcf3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:52 +0000

    Add package bauh/0.9.3/bauh-0.9.3-py3-none-any.whl

---
+++ packages/bauh/bauh-0.9.3-py3-none-any.whl/bauh/view/core/downloader.py
@@ -0,0 +1,128 @@
+import logging
+import os
+import re
+import time
+import traceback
+
+from bauh.api.abstract.download import FileDownloader
+from bauh.api.abstract.handler import ProcessWatcher
+from bauh.api.http import HttpClient
+from bauh.commons.html import bold
+from bauh.commons.system import run_cmd, new_subprocess, ProcessHandler, SystemProcess, SimpleProcess
+from bauh.view.util.translation import I18n
+
+RE_HAS_EXTENSION = re.compile(r'.+\.\w+$')
+
+
+class AdaptableFileDownloader(FileDownloader):
+
+    def __init__(self, logger: logging.Logger, multithread_enabled: bool, i18n: I18n, http_client: HttpClient):
+        self.logger = logger
+        self.multithread_enabled = multithread_enabled
+        self.i18n = i18n
+        self.http_client = http_client
+
+    def is_aria2c_available(self) -> bool:
+        return bool(run_cmd('which aria2c', print_error=False))
+
+    def _get_aria2c_process(self, url: str, output_path: str, cwd: str) -> SystemProcess:
+        cmd = ['aria2c', url,
+               '--no-conf',
+               '--max-connection-per-server=16',
+               '--split=16',
+               '--enable-color=false',
+               '--stderr=true',
+               '--summary-interval=0',
+               '--disable-ipv6',
+               '--min-split-size=1M',
+               '--allow-overwrite=true',
+               '--continue=true',
+               '--timeout=5',
+               '--max-file-not-found=3',
+               '--remote-time=true']
+
+        if output_path:
+            output_split = output_path.split('/')
+            cmd.append('--dir=' + '/'.join(output_split[:-1]))
+            cmd.append('--out=' + output_split[-1])
+
+        return SystemProcess(new_subprocess(cmd=cmd, cwd=cwd),
+                             skip_stdout=True,
+                             check_error_output=False,
+                             success_phrases=['download completed'],
+                             output_delay=0.001)
+
+    def _get_wget_process(self, url: str, output_path: str, cwd: str) -> SimpleProcess:
+        cmd = ['wget', url, '--continue', '--retry-connrefused', '--tries=10', '--no-config']
+
+        if output_path:
+            cmd.append('-O')
+            cmd.append(output_path)
+
+        return SimpleProcess(cmd=cmd, cwd=cwd)
+
+    def _rm_bad_file(self, file_name: str, output_path: str, cwd):
+        to_delete = output_path if output_path else '{}/{}'.format(cwd, file_name)
+
+        if to_delete and os.path.exists(to_delete):
+            self.logger.info('Removing downloaded file {}'.format(to_delete))
+            os.remove(to_delete)
+
+    def download(self, file_url: str, watcher: ProcessWatcher, output_path: str = None, cwd: str = None) -> bool:
+        self.logger.info('Downloading {}'.format(file_url))
+        handler = ProcessHandler(watcher)
+        file_name = file_url.split('/')[-1]
+
+        final_cwd = cwd if cwd else '.'
+
+        success = False
+        ti = time.time()
+        try:
+            if output_path and os.path.exists(output_path):
+                self.logger.info('Removing old file found before downloading: {}'.format(output_path))
+                os.remove(output_path)
+                self.logger.info("Old file {} removed".format(output_path))
+
+            if self.is_multithreaded():
+                ti = time.time()
+                process = self._get_aria2c_process(file_url, output_path, final_cwd)
+                downloader = 'aria2c'
+            else:
+                ti = time.time()
+                process = self._get_wget_process(file_url, output_path, final_cwd)
+                downloader = 'wget'
+
+            file_size = self.http_client.get_content_length(file_url)
+
+            name = file_url.split('/')[-1]
+
+            if output_path and not RE_HAS_EXTENSION.match(name) and RE_HAS_EXTENSION.match(output_path):
+                name = output_path.split('/')[-1]
+
+            msg = bold('[{}] ').format(downloader) + self.i18n['downloading'] + ' ' + bold(name) + (' ( {} )'.format(file_size) if file_size else '')
+
+            if watcher:
+                watcher.change_substatus(msg)
+
+            if isinstance(process, SimpleProcess):
+                success = handler.handle_simple(process)
+            else:
+                success = handler.handle(process)
+        except:
+            traceback.print_exc()
+            self._rm_bad_file(file_name, output_path, final_cwd)
+
+        tf = time.time()
+        self.logger.info(file_name + ' download took {0:.2f} minutes'.format((tf - ti) / 60))
+
+        if not success:
+            self.logger.error("Could not download '{}'".format(file_name))
+            self._rm_bad_file(file_name, output_path, final_cwd)
+
+        return success
+
+    def is_multithreaded(self) -> bool:
+        return self.multithread_enabled and self.is_aria2c_available()
+
+    def get_default_client_name(self) -> str:
+        return 'aria2c' if self. is_multithreaded() else 'wget'
---
+++ packages/bauh/bauh-0.9.3-py3-none-any.whl/bauh/view/qt/info.py
@@ -0,0 +1,101 @@
+from PyQt5.QtCore import QSize, Qt
+from PyQt5.QtGui import QIcon, QCursor
+from PyQt5.QtWidgets import QDialog, QVBoxLayout, QGroupBox, \
+    QLineEdit, QLabel, QGridLayout, QPushButton, QPlainTextEdit, QToolBar
+
+from bauh.api.abstract.cache import MemoryCache
+from bauh.view.util.translation import I18n
+
+IGNORED_ATTRS = {'name', '__app__'}
+
+
+class InfoDialog(QDialog):
+
+    def __init__(self, app: dict, icon_cache: MemoryCache, i18n: I18n, screen_size: QSize()):
+        super(InfoDialog, self).__init__()
+        self.setWindowTitle(str(app['__app__']))
+        self.screen_size = screen_size
+        self.i18n = i18n
+        layout = QVBoxLayout()
+        self.setLayout(layout)
+
+        self.toolbar_field = QToolBar()
+        self.bt_back = QPushButton(i18n['back'].capitalize())
+        self.bt_back.clicked.connect(self.back_to_info)
+        self.bt_back.setCursor(QCursor(Qt.PointingHandCursor))
+        self.toolbar_field.addWidget(self.bt_back)
+        self.layout().addWidget(self.toolbar_field)
+        self.toolbar_field.hide()
+
+        # shows complete field string
+        self.text_field = QPlainTextEdit()
+        self.text_field.setReadOnly(True)
+        self.layout().addWidget(self.text_field)
+        self.text_field.hide()
+
+        self.gbox_info = QGroupBox()
+        self.gbox_info.setMaximumHeight(self.screen_size.height() - self.screen_size.height() * 0.1)
+        self.gbox_info_layout = QGridLayout()
+        self.gbox_info.setLayout(self.gbox_info_layout)
+
+        layout.addWidget(self.gbox_info)
+
+        # THERE ARE CRASHES WITH SOME RARE ICONS ( like insomnia ). IT CAN BE A QT BUG. IN THE MEANTIME, ONLY THE TYPE ICON WILL BE RENDERED
+        #
+        # icon_data = icon_cache.get(app['__app__'].model.icon_url)
+        #
+        # if icon_data and icon_data.get('icon'):
+        #     self.setWindowIcon(icon_data.get('icon'))
+        self.setWindowIcon(QIcon(app['__app__'].model.get_type_icon_path()))
+
+        for idx, attr in enumerate(sorted(app.keys())):
+            if attr not in IGNORED_ATTRS and app[attr]:
+                i18n_key = app['__app__'].model.gem_name + '.info.' + attr.lower()
+
+                if isinstance(app[attr], list):
+                    val = ' '.join([str(e).strip() for e in app[attr] if e])
+                    show_val = '\n'.join(['* ' + str(e).strip() for e in app[attr] if e])
+                else:
+                    val = str(app[attr]).strip()
+                    show_val = val
+
+                i18n_val = i18n.get('{}.{}'.format(i18n_key, val.lower()))
+
+                if i18n_val:
+                    val = i18n_val
+                    show_val = val
+
+                text = QLineEdit()
+                text.setToolTip(show_val)
+                text.setText(val)
+                text.setCursorPosition(0)
+                text.setStyleSheet("width: 400px")
+                text.setReadOnly(True)
+
+                label = QLabel(i18n.get(i18n_key, i18n.get(attr.lower(), attr)).capitalize())
+                label.setStyleSheet("font-weight: bold")
+
+                self.gbox_info_layout.addWidget(label, idx, 0)
+                self.gbox_info_layout.addWidget(text, idx, 1)
+                self._gen_show_button(idx, show_val)
+
+        self.adjustSize()
+
+    def _gen_show_button(self, idx: int, val):
+
+        def show_full_field():
+            self.gbox_info.hide()
+            self.toolbar_field.show()
+            self.text_field.show()
+            self.text_field.setPlainText(val)
+
+        bt_full_field = QPushButton(self.i18n['show'].capitalize())
+        bt_full_field.setCursor(QCursor(Qt.PointingHandCursor))
+        bt_full_field.clicked.connect(show_full_field)
+        self.gbox_info_layout.addWidget(bt_full_field, idx, 2)
+
+    def back_to_info(self):
+        self.text_field.setPlainText("")
+        self.text_field.hide()
+        self.toolbar_field.hide()
+        self.gbox_info.show()
