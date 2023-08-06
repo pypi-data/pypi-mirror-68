d1cdc8b6d3071663da1c6a8edca8fabbe5d37047
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:07:15 +0000

    Add package semantyk/0.0.0.43/semantyk-0.0.0.43.tar.gz

---
+++ packages/semantyk/semantyk-0.0.0.43.tar.gz/semantyk-0.0.0.43/semantyk/namespace/__init__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+##################################################
+#
+# namespace
+# Package | Namespace
+#
+# Daniel Bakas Amuchastegui
+# Nay 10, 2020
+# 
+# Copyright © Semantyk 2020. All rights reserved.
+##################################################
+
+from . import namespace
+
+from .namespace import *
