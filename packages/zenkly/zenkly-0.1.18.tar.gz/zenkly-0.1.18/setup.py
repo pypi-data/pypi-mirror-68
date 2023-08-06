7c47da35817d626241891f659860de34884c18a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:54:57 +0000

    Add package zenkly/0.1.18/zenkly-0.1.18.tar.gz

---
+++ packages/zenkly/zenkly-0.1.18.tar.gz/zenkly-0.1.18/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='zenkly',
+    version='0.1.18',
+    packages=find_packages(),
+    description='CLI for Zendesk admin tasks',
+    author='Zena Hirsch',
+    author_email='hirsch.zena@gmail.com',
+    url='https://github.com/zenahirsch/zenkly',
+    include_package_data=True,
+    package_data={'zenkly': ['schemas/*.schema']},
+    install_requires=[
+        'Click',
+        'simplejson',
+        'requests',
+        'jsonschema',
+        'colorama',
+        'configparser',
+        'gitpython',
+        'tabulate',
+    ],
+    entry_points='''
+        [console_scripts]
+        zenkly=zenkly.scripts.zenkly:cli
+    ''',
+)
