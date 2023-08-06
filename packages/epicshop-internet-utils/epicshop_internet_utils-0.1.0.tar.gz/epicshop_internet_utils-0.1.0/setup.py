536e138b57a1f37b525e2b368515c0536a9ac0c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:39 +0000

    Add package epicshop-internet-utils/0.1.0/epicshop_internet_utils-0.1.0.tar.gz

---
+++ packages/epicshop-internet-utils/epicshop_internet_utils-0.1.0.tar.gz/epicshop_internet_utils-0.1.0/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+name='epicshop_internet_utils',
+version='0.1.0',
+author='pleasefeedme',
+author_email='myfakeemail@gmail.com',
+packages=['epicshop_internet_utils'],
+scripts=[],
+url='http://pypi.python.org/pypi/epicshop_internet_utils',
+license='LICENSE.txt',
+description='Useful towel-related stuff.',
+install_requires=['requests'],
+)
