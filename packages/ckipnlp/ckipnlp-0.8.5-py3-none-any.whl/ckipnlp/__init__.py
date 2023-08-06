a740e1d4be61506eb5c82ade164476752bfd6304
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:23:56 +0000

    Add package ckipnlp/0.8.5/ckipnlp-0.8.5-py3-none-any.whl

---
+++ packages/ckipnlp/ckipnlp-0.8.5-py3-none-any.whl/ckipnlp/__init__.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+
+"""
+The Official CKIP CoreNLP Toolkits.
+"""
+
+__author_name__ = 'Mu Yang'
+__author_email__ = 'emfomy@gmail.com'
+__copyright__ = '2018-2020 CKIP Lab'
+
+__title__ = 'CKIPNLP'
+__version__ = '0.8.5'
+__description__ = 'CKIP CoreNLP'
+__license__ = 'CC BY-NC-SA 4.0'
+
+__url__ = 'https://ckipnlp.readthedocs.io'
+__download_url__ = __url__+'/tarball/'+__version__
