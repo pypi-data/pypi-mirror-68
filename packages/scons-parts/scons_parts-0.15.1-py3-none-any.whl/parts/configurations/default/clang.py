9d82074a30bbf059fe3d3cb2ce8b2c8dc5d65dbd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:22:57 +0000

    Add package scons-parts/0.15.1/scons_parts-0.15.1-py3-none-any.whl

---
+++ packages/scons-parts/scons_parts-0.15.1-py3-none-any.whl/parts/configurations/default/clang.py
@@ -0,0 +1,29 @@
+######################################
+# clang compiler configurations default
+######################################
+
+
+from parts.config import *
+
+
+def map_default_version(env):
+    return env['CLANG_VERSION']
+
+
+config = configuration(map_default_version)
+
+config.VersionRange(
+    "*",
+    # RUN_PATH setting for this platform toolchain
+    replace=ConfigValues(
+        _RPATHSTR='${JOIN("$RUNPATHS",":")}',
+        RPATHLINK=[],
+        _RPATHLINK='${_concat("-Wl,-rpath-link=", RPATHLINK, "", __env__, RDirs, TARGET, SOURCE)}',
+        _ABSRPATHLINK='${_concat("-Wl,-rpath-link=", RPATHLINK, "", __env__, ABSDir, TARGET, SOURCE)}',
+        _RUNPATH='${_concat(RPATHPREFIX, _RPATHSTR, RPATHSUFFIX, __env__)}',
+        _RPATH='$_RUNPATH $_RPATHLINK',
+        _ABSRPATH='$_RUNPATH $_ABSRPATHLINK',
+        RUNPATHS='${GENRUNPATHS()}',
+        RPATHSUFFIX=",--enable-new-dtags",
+    ),
+)
---
+++ packages/scons-parts/scons_parts-0.15.1-py3-none-any.whl/parts/configurations/default/gcc.py
@@ -0,0 +1,28 @@
+######################################
+# gcc compiler configurations default
+######################################
+
+
+from parts.config import *
+
+
+def map_default_version(env):
+    return env['GCC_VERSION']
+
+
+config = configuration(map_default_version)
+
+config.VersionRange(
+    "*",
+    replace=ConfigValues(
+        _RPATHSTR='${JOIN("$RUNPATHS",":")}',
+        RPATHLINK=[],
+        _RPATHLINK='${_concat("-Wl,-rpath-link=", RPATHLINK, "", __env__, RDirs, TARGET, SOURCE)}',
+        _ABSRPATHLINK='${_concat("-Wl,-rpath-link=", RPATHLINK, "", __env__, ABSDir, TARGET, SOURCE)}',
+        _RUNPATH='${_concat(RPATHPREFIX, _RPATHSTR, RPATHSUFFIX, __env__)}',
+        _RPATH='$_RUNPATH $_RPATHLINK',
+        _ABSRPATH='$_RUNPATH $_ABSRPATHLINK',
+        RUNPATHS='${GENRUNPATHS()}',
+        RPATHSUFFIX=",--enable-new-dtags",
+    ),
+)
