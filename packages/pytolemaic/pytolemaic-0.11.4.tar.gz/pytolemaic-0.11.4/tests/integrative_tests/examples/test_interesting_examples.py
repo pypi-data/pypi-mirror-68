8aa3d56342d07186ed002e5884ecab9d6a37353e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:55:19 +0000

    Add package pytolemaic/0.11.4/pytolemaic-0.11.4.tar.gz

---
+++ packages/pytolemaic/pytolemaic-0.11.4.tar.gz/pytolemaic-0.11.4/tests/integrative_tests/examples/test_interesting_examples.py
@@ -0,0 +1,32 @@
+import unittest
+
+from examples.interesting_examples import adult_dataset, california_housing_dataset, kddcup99_dataset, \
+    prediction_uncertainty_adult_dataset, simple_example_classification, simple_example_regression, \
+    prediction_uncertainty_california_dataset
+
+
+class TestInterestingExamples(unittest.TestCase):
+
+    def test_simple_example_cla(self):
+        simple_example_classification.run()
+
+    def test_simple_example_reg(self):
+        simple_example_regression.run()
+
+    # @unittest.skip("adult_dataset example - Takes time")
+    def test_adult_dataset(self):
+        adult_dataset.run(fast=True)
+
+    def test_adult_dataset_uncertainty(self):
+        prediction_uncertainty_adult_dataset.run()
+
+    def test_california_dataset_uncertainty(self):
+        prediction_uncertainty_california_dataset.run()
+
+    # @unittest.skip("california_housing_dataset example - Takes time")
+    def test_california_housing(self):
+        california_housing_dataset.run(fast=True)
+
+    # @unittest.skip("kddcup99 example - Takes time")
+    def test_kddcup99(self):
+        kddcup99_dataset.run(fast=True)
