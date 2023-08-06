afa530b15db43d69d1f72283f3d5921621257490
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:24:39 +0000

    Add package poetrytestsqeddy/0.2.0/poetryTestSQEddy-0.2.0.tar.gz

---
+++ packages/poetrytestsqeddy/poetryTestSQEddy-0.2.0.tar.gz/poetryTestSQEddy-0.2.0/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['poetrytestsqeddy']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['matplotlib>=3.2.1,<4.0.0']
+
+entry_points = \
+{'console_scripts': ['run = testEddySemQuest.py']}
+
+setup_kwargs = {
+    'name': 'poetrytestsqeddy',
+    'version': '0.2.0',
+    'description': '',
+    'long_description': None,
+    'author': 'Eddy Ferrara Jr.',
+    'author_email': 'you@example.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
