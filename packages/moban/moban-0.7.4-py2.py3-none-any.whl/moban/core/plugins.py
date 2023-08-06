1aa038c228ad4174ce590a85b7662c706050978a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:23:36 +0000

    Add package moban/0.7.4/moban-0.7.4-py2.py3-none-any.whl

---
+++ packages/moban/moban-0.7.4-py2.py3-none-any.whl/moban/core/plugins.py
@@ -0,0 +1,14 @@
+from lml.loader import scan_plugins_regex
+
+from moban import constants
+
+BUILTIN_EXENSIONS = [
+    "moban.plugins.jinja2.engine",
+    "moban.plugins.yaml_loader",
+    "moban.plugins.json_loader",
+    "moban.plugins.copy",
+]
+
+
+def make_sure_all_pkg_are_loaded():
+    scan_plugins_regex(constants.MOBAN_ALL, "moban", None, BUILTIN_EXENSIONS)
