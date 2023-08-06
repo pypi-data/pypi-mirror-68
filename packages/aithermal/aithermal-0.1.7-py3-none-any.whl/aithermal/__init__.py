d7748fb4b5e1abbc3f8c45e4124751712f90c85b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:13:08 +0000

    Add package aithermal/0.1.7/aithermal-0.1.7-py3-none-any.whl

---
+++ packages/aithermal/aithermal-0.1.7-py3-none-any.whl/aithermal/__init__.py
@@ -0,0 +1,7 @@
+__version__ = '0.1.0'
+from aithermal.frame_runner import *
+import aithermal.utils as utils
+from aithermal.utils import *
+from aithermal.utils import topic_directories, method_directories
+
+
