a8cc64410063bee7a4efccfbbe85d14409bc0bbf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:12:11 +0000

    Add package dameopencv/0.0.2/dameopencv-0.0.2.tar.gz

---
+++ packages/dameopencv/dameopencv-0.0.2.tar.gz/dameopencv-0.0.2/setup.py
@@ -0,0 +1,65 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# Copyright (C) 2020  David Arroyo Menéndez
+
+# Author: David Arroyo Menéndez <davidam@gnu.org>
+# Maintainer: David Arroyo Menéndez <davidam@gnu.org>
+
+# This file is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3, or (at your option)
+# any later version.
+
+# This file is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with GNU Emacs; see the file COPYING.  If not, write to
+# the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+# Boston, MA 02110-1301 USA,
+
+from setuptools import setup
+from os import path
+
+# def readme():
+#     with open('README.org') as f:
+#         return f.read()
+
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md')) as f:
+    long_description = f.read()
+
+setup(name='dameopencv',
+      version='0.0.2',
+      description='Learning OpenCV from Tests by David Arroyo Menéndez',
+      long_description=long_description,
+      long_description_content_type='text/markdown',
+      classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Scientific/Engineering',
+      ],
+      keywords='opencv tests',
+#      scripts=['bin/damenumpy-sum.py'],
+      url='http://github.com/davidam/dameopencv',
+      author='David Arroyo Menéndez',
+      author_email='davidam@gnu.org',
+      license='GPLv3',
+      packages=['dameopencv', 'dameopencv.tests'],
+      package_dir={'dameopencv': 'dameopencv', 'dameopencv.tests': 'dameopencv/tests'},
+      data_files=[('dameopencv', ['README.md'])],      
+      install_requires=[
+          'markdown',
+          'numpy',
+      ],
+      test_suite='nose.collector',
+      tests_require=['nose', 'nose-cover3'],
+      entry_points={
+          'console_scripts': ['dameopencv=dameopencv'],
+      },
+      include_package_data=True,
+      zip_safe=False)
