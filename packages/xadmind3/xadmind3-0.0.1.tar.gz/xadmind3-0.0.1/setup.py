07e9abea06ae3e3f3f24546a96a5d9d9b61ef7fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:03:31 +0000

    Add package xadmind3/0.0.1/xadmind3-0.0.1.tar.gz

---
+++ packages/xadmind3/xadmind3-0.0.1.tar.gz/xadmind3-0.0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages, setup
+setup(
+    name='xadmind3',
+    version='0.0.1',
+    description='xadmin fit django3',
+    author='wychmod',#作者
+    author_email='17376512950@163.com',
+    url='https://github.com/wychmod/xadmind3',
+    #packages=find_packages(),
+    packages=['xadmind3'],  #这里是所有代码所在的文件夹名称
+    install_requires=[],
+)
