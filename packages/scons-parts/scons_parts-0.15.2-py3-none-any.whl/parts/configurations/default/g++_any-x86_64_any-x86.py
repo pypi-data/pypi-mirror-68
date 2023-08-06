f5b0d0914665003706939ba1965b31852534713d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:22:50 +0000

    Add package scons-parts/0.15.2/scons_parts-0.15.2-py3-none-any.whl

---
+++ packages/scons-parts/scons_parts-0.15.2-py3-none-any.whl/parts/configurations/default/g++_any-x86_64_any-x86.py
@@ -0,0 +1,34 @@
+######################################
+# g++ compiler configurations debug
+######################################
+
+
+from parts.config import *
+
+
+def map_default_version(env):
+    return env['GXX_VERSION']
+
+
+config = configuration(map_default_version)
+
+config.VersionRange(
+    "3-*",
+    prepend=ConfigValues(
+        CCARCHFLAGS=['-m32'],
+        CCFLAGS=['-m32'],
+        LINKFLAGS=['-m32'],
+    ),
+    # RUN_PATH setting for this platform toolchain
+    replace=ConfigValues(
+        _RPATHSTR='${JOIN("$RUNPATHS",":")}',
+        RPATHLINK=[],
+        _RPATHLINK='${_concat("-Wl,-rpath-link=", RPATHLINK, "", __env__, RDirs, TARGET, SOURCE)}',
+        _ABSRPATHLINK='${_concat("-Wl,-rpath-link=", RPATHLINK, "", __env__, ABSDir, TARGET, SOURCE)}',
+        _RUNPATH='${_concat(RPATHPREFIX, _RPATHSTR, RPATHSUFFIX, __env__)}',
+        _RPATH='$_RUNPATH $_RPATHLINK',
+        _ABSRPATH='$_RUNPATH $_ABSRPATHLINK',
+        RUNPATHS='${GENRUNPATHS()}',
+        RPATHSUFFIX=",--enable-new-dtags",
+    ),
+)
