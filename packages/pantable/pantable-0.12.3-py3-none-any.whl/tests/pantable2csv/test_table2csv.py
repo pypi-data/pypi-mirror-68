d2b72a17a511750855c77a530883b00bc444712f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:24:22 +0000

    Add package pantable/0.12.3/pantable-0.12.3-py3-none-any.whl

---
+++ packages/pantable/pantable-0.12.3-py3-none-any.whl/tests/pantable2csv/test_table2csv.py
@@ -0,0 +1,33 @@
+"""
+test panflute ast to markdown conversion
+"""
+from panflute import *
+
+from .context import table2csv
+
+
+def test_table2csv():
+    # get_table_body
+    markdown = """| 1 | 2 | 3 | 4 |
+|:--|--:|:-:|---|
+| 1 | 2 | 3 | 4 |
+
+: *abcd*
+"""
+    Panflute = convert_text(markdown)
+    code_block_converted = table2csv(*Panflute, doc=None)
+    code_block_referenced = CodeBlock('''---
+alignment: LRCD
+caption: '*abcd*'
+header: true
+markdown: true
+table-width: 0
+width:
+- 0
+- 0
+- 0
+- 0
+---
+1,2,3,4\r\n1,2,3,4\r\n''', classes=['table'])
+    assert repr(code_block_converted) == repr(code_block_referenced)
+    return
