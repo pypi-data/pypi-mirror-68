ecc86843db093b45a38a1f9e995f973e42d4a13b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:12:25 +0000

    Add package bingdog/0.1.1/bingdog-0.1.1-py3.5.egg

---
+++ packages/bingdog/bingdog-0.1.1-py3.5.egg/bingdog/executor.py
@@ -0,0 +1,29 @@
+from bingdog.task import TaskExecutionException
+from bingdog.appconfig import Configurator, ConfiguredTaskUtil
+from bingdog.logger import Logger
+
+class TaskExecutor():
+        
+    def execute(self,taskId):
+        try:
+            taskUtil = ConfiguredTaskUtil(Configurator.getConfigurator().flowDiagramPath)
+            task = taskUtil.getTask(taskId)
+            if (task):
+                self.__execute(task)
+            else:
+                raise TaskExecutionException("None Root Task")
+        except TaskExecutionException as e:
+            logging.exception("Root TaskExecutionException has Thrown.")
+        
+    def __execute(self, task):
+        if (task):
+            try:
+                task.run()
+                while task.hasNextChild():
+                    childTask = task.getNextChild()
+                    self.__execute(childTask)
+                nextTask = task.getNextTask()
+                if (nextTask):
+                    self.__execute(nextTask)
+            except TaskExecutionException as e:
+                logging.exception("TaskExecutionException has Thrown.")
