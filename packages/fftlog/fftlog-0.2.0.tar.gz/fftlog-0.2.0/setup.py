a23ab9bcf9ed93ae0c1d435a5730efe971591302
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:21:42 +0000

    Add package fftlog/0.2.0/fftlog-0.2.0.tar.gz

---
+++ packages/fftlog/fftlog-0.2.0.tar.gz/fftlog-0.2.0/setup.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+import os
+import re
+from numpy.distutils.core import setup, Extension
+
+# Get README and remove badges.
+readme = open('README.rst').read()
+readme = re.sub('.*`fftlog` - A', '`fftlog` - A', readme, flags=re.DOTALL)
+
+setup(
+    name='fftlog',
+    description='Logarithmic Fast Fourier Transform',
+    long_description=readme,
+    author='Dieter Werthmüller',
+    author_email='dieter@werthmuller.org',
+    url='https://github.com/prisae/fftlog',
+    license='CC0-1.0',
+    packages=['fftlog', ],
+    include_package_data=True,
+    ext_modules=[
+        Extension(
+            name="fftlog._fftlog",
+            sources=['fftlog/fftlog.pyf', ] +
+                    [
+                        'fftlog/src/cdgamma.f', 'fftlog/src/drfftb.f',
+                        'fftlog/src/drfftf.f', 'fftlog/src/drffti.f',
+                        'fftlog/src/fftlog.f'
+                    ],
+                 )
+        ],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'License :: CC0 1.0 Universal (CC0 1.0) Public Domain Dedication',
+    ],
+    install_requires=[
+        'scipy',
+    ],
+    version='0.2.0',
+    # use_scm_version={
+    #     'root': '.',
+    #     'relative_to': __file__,
+    #     'write_to': os.path.join('fftlog', 'version.py'),
+    # },
+    # setup_requires=['setuptools_scm'],
+)
