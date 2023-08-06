1c85af9324ce18d49da85aac09cb15dd65103671
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:26:15 +0000

    Add package sho/0.4.9/sho-0.4.9-py2.py3-none-any.whl

---
+++ packages/sho/sho-0.4.9-py2.py3-none-any.whl/sho/__init__.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+from sho.constants import OUTPUT_TYPE
+from sho.sho import output, output_detail
+
+"""Top-level package for sho."""
+
+__author__ = """Dave Dawson"""
+__email__ = 'davedawson.co@gmail.com'
+__version__ = '0.1.0'
+
+"""Main module."""
+def w(obj, output_type=OUTPUT_TYPE.DEFAULT.value):
+    """
+    Main function called, defaults to 
+        :param obj: the variable to show
+        :param output_type=OUTPUT_TYPE.DEFAULT.value: ability to force output types under certain scenarios
+    """
+    output(obj,output_type)
+
+"""Main module."""
+def ck(obj, output_type=OUTPUT_TYPE.DEFAULT.value):
+    """
+    Main function called, defaults to 
+        :param obj: the variable to show
+        :param output_type=OUTPUT_TYPE.DEFAULT.value: ability to force output types under certain scenarios
+    """
+    output_detail(obj,output_type)
