7b905bb97f15a26c8efb7dc24bddfe1c3cab1294
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:11:23 +0000

    Add package hack-assembler/0.4.2/hack-assembler-0.4.2.tar.gz

---
+++ packages/hack-assembler/hack-assembler-0.4.2.tar.gz/hack-assembler-0.4.2/README.md
@@ -0,0 +1,7 @@
+# Python implementation of Hack assembler
+
+![Linter](https://github.com/ITD27M01/hack-assembler/workflows/Linter/badge.svg)
+
+[nand2tetris project6](https://www.nand2tetris.org/project06)
+
+    assembler Prog.asm
---
+++ packages/hack-assembler/hack-assembler-0.4.2.tar.gz/hack-assembler-0.4.2/assembler/parser.py
@@ -0,0 +1,59 @@
+import sys
+import logging
+import re
+import string
+from os.path import realpath as path_realpath
+
+_log = logging.getLogger(name=__name__)
+
+# Regexps for Hack instructions
+#
+# group(1) of A instruction match returns address or label
+#
+# group(1) of C instruction match returns destination specification
+# group(2) of C instruction match returns computation specification
+# group(3) of C instruction match returns jump specification
+A_INSTRUCTION = re.compile("^@([0-9]+|[a-zA-Z_.$:]+[0-9a-zA-Z_.$:]*)$")
+C_INSTRUCTION = re.compile("^([AMD]+=)*([AMD!\-+|&01]+)(;[JGTEQLNMP]*)*$")
+L_INSTRUCTION = re.compile("^\(([a-zA-Z_.$:]+[0-9a-zA-Z_.$:]*)*\)$")
+COMMENT = re.compile("//.*")
+
+
+def _cleanup_instruction(instruction):
+    instruction = instruction.translate({ord(whitespace): None for whitespace in string.whitespace})
+    return re.sub(COMMENT, "", instruction)
+
+
+def _instruction_type(instruction):
+    if (match_instruction_object := re.fullmatch(A_INSTRUCTION, instruction)) is not None:
+        _log.debug(f"{instruction} is A instruction")
+        return {"type": "A_INSTRUCTION", "obj": match_instruction_object}
+    elif (match_instruction_object := re.fullmatch(C_INSTRUCTION, instruction)) is not None:
+        _log.debug(f"{instruction} is C instruction")
+        return {"type": "C_INSTRUCTION", "obj": match_instruction_object}
+    elif (match_instruction_object := re.fullmatch(L_INSTRUCTION, instruction)) is not None:
+        _log.debug(f"{instruction} is L (label) instruction")
+        return {"type": "L_INSTRUCTION", "obj": match_instruction_object}
+    else:
+        sys.exit(f"{instruction} is unknown")
+
+
+def parse(asm_file):
+    asm_file_path = path_realpath(asm_file)
+
+    parsed_code = list()
+    try:
+        with open(asm_file_path, "r") as assembly_file_descriptor:
+            _log.debug(f"Start to process {asm_file_path}")
+            instruction = assembly_file_descriptor.readline()
+            while instruction:
+                instruction = _cleanup_instruction(instruction)
+                if instruction:
+                    # If not empty string or comment let's parse it
+                    parsed_code.append(_instruction_type(instruction))
+
+                instruction = assembly_file_descriptor.readline()
+
+        return parsed_code
+    except FileNotFoundError:
+        sys.exit(f"File {asm_file_path} not found.")
