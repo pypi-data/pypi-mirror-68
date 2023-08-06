1c76f68137421a917dd142236443f2066f15586f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:35:55 +0000

    Add package dotted-notation/0.0.19/dotted_notation-0.0.19-py3-none-any.whl

---
+++ packages/dotted-notation/dotted_notation-0.0.19-py3-none-any.whl/tests/test_slice.py
@@ -0,0 +1,26 @@
+import dotted
+
+
+def test_slice_upsert():
+    m = dotted.update([], '[:]', ['hello'])
+    assert m == ['hello']
+
+
+def test_slice_append():
+    m = dotted.update(['hello', 'there'], '[+:]', ['bye'])
+    assert m == ['hello', 'there', 'bye']
+
+
+def test_slice_prepend():
+    m = dotted.update(['hello', 'there'], '[:0]', ['bye'])
+    assert m == ['bye', 'hello', 'there']
+
+
+def test_slice_default():
+    m = dotted.update({}, 'stuff[]', ['bye'])
+    assert m == {'stuff': ['bye']}
+
+
+def test_slice_match():
+    m = dotted.match('hello[]', 'hello[]')
+    assert m == 'hello[]'
