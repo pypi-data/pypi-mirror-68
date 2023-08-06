ccdf7f9c1a3b6010959aeb5beff895730c2868fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:12:44 +0000

    Add package ztext/0.0.1/zText-0.0.1.tar.gz

---
+++ packages/ztext/zText-0.0.1.tar.gz/zText-0.0.1/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="zText", # Replace with your own username
+    version="0.0.1",
+    author="Zack Dai",
+    author_email="author@example.com",
+    description="A small example package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypa/sampleproject",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    packages=["zText"],
+    install_requires=["IPython","bokeh","pandas","xlrd","gensim","spacy","textacy","textblob","pyvis","pyLDAvis"],
+    python_requires='>=3.6',
+)
