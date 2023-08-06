feb0bb75871e93c0dc38a4e2bb75b52acceb175a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:12:09 +0000

    Add package awshelper/1.3/awshelper-1.3.tar.gz

---
+++ packages/awshelper/awshelper-1.3.tar.gz/awshelper-1.3/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='awshelper',
+    version='1.3',
+    description='helps you run aws commands with sso-based credentials',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/trondhindenes/awshelper',
+    author='Trond Hindenes',
+    author_email='trond@hindenes.com',
+    license='MIT',
+    packages=['awshelper'],
+    zip_safe=False,
+    install_requires=[
+        'pytz'
+    ],
+    entry_points={
+        'console_scripts': ['awshelper=awshelper.util:main'],
+    },
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ],
+)
