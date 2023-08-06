4dea2ead5a59bf907810381a9caae82a9a5b88df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:46 +0000

    Add package wagtail-draftail-anchors/0.0.1/wagtail_draftail_anchors-0.0.1.tar.gz

---
+++ packages/wagtail-draftail-anchors/wagtail_draftail_anchors-0.0.1.tar.gz/wagtail_draftail_anchors-0.0.1/README.md
@@ -0,0 +1,8 @@
+# Wagtail Draftail Anchors
+
+Adds the ability to add and edit anchors in the Draftail rich text editor, as well as automatically adding
+(slug-form) anchor ids to all headings.
+
+## Installation
+
+Add `'wagtail_draftail_anchors'` to `INSTALLED_APPS` below `wagtail.admin`.
---
+++ packages/wagtail-draftail-anchors/wagtail_draftail_anchors-0.0.1.tar.gz/wagtail_draftail_anchors-0.0.1/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="wagtail_draftail_anchors",
+    version="0.0.1",
+    author="Jacob Topp-Mugglestone, Quoc Duan",
+    author_email="jacobtm@torchbox.com",
+    description="A Draftail extension to add anchor identifiers to rich text",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/jacobtoppm/wagtail_draftail_anchors",
+    packages=setuptools.find_packages(),
+    install_requires=["wagtail>=2.2",],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
