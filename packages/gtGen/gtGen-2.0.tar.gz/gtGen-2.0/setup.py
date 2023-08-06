3962b0aea7843c658064298093ecaa1093504321
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:17:23 +0000

    Add package gtGen/2.0/gtGen-2.0.tar.gz

---
+++ packages/gtGen/gtGen-2.0.tar.gz/gtGen-2.0/setup.py
@@ -0,0 +1,45 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+
+    long_description = fh.read()
+
+setuptools.setup(
+
+     name='gtGen',  
+
+     version='2.0',
+
+     scripts=['gtGen'] ,
+
+     author="Aravind Krishnan.B",
+
+     author_email="aravindk797@gmail.com",
+
+     description="GT_Generator Application",
+
+     long_description=long_description,
+
+   long_description_content_type="text/markdown",
+
+     url="",
+
+     packages=setuptools.find_packages(),
+	 install_requires=[
+			  'python-Levenshtein',
+			  'Pillow'
+	      ],
+     classifiers=[
+
+         "Programming Language :: Python :: 3",
+	     'Programming Language :: Python :: 3.4',
+	     'Programming Language :: Python :: 3.5',
+	     'Programming Language :: Python :: 3.6',
+	     'Programming Language :: Python :: 3.7',
+         "License :: OSI Approved :: MIT License",
+
+         "Operating System :: OS Independent",
+
+     ],
+
+ )
