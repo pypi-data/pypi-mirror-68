2556ad8a762e29c5405b4c7d6a24e7824c114512
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:02 +0000

    Add package metaproj/0.2/metaproj-0.2-py3-none-any.whl

---
+++ packages/metaproj/metaproj-0.2-py3-none-any.whl/metaproj/context.py
@@ -0,0 +1,27 @@
+import os
+from .pygen import PYGen
+from .cppgen import CPPGen
+from .vimgen import VIMGen
+
+class MetaProj:
+
+    def __init__(self, name='demo', projtype='python'):
+        self.name = name
+        self.projtype = projtype
+
+    def run(self):
+
+        if self.projtype == 'python':
+            PYGen().generate(self.name)
+        elif self.projtype == 'cpp':
+            CPPGen().generate(self.name)
+        elif self.projtype == 'vim':
+            VIMGen().generate(self.name)
+        else:
+            print("MetaProj: Unknown Project Type")
+
+    def setName(self, name):
+        self.name = name
+
+    def setType(self, projtype):
+        self.projtype = projtype
