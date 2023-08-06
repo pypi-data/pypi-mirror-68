57a5f3e7d1cc0c4413f22b3e6492293c423d8f73
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:04:45 +0000

    Add package iva-applications/6.3.0/iva_applications-6.3.0.tar.gz

---
+++ packages/iva-applications/iva_applications-6.3.0.tar.gz/iva_applications-6.3.0/src/iva_applications/resnet34/preprocess.py
@@ -0,0 +1,26 @@
+# -*- coding=utf-8 -*-
+"""RESNET34 preprocess."""
+import numpy as np
+from PIL import Image
+from iva_applications.imagenet.images_utils import crop_and_resize
+
+
+def image_to_tensor(image: Image) -> np.ndarray:
+    """
+    Realise preprocess for an input pillow image and convert it to numpy array.
+
+    Parameters
+    ----------
+    image
+        An input image to be processed.
+
+    Returns
+    -------
+    tensor
+        Preprocessed numpy array.
+    """
+    image = image.convert('RGB')
+    tensor = np.asarray(image)
+    tensor = crop_and_resize(tensor, 224)
+    assert tensor.shape == (224, 224, 3)
+    return tensor
