2dfe9df9e6fa69320536878d64043bd71f0e698c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:25:18 +0000

    Add package orbis-plugin-storage-html-pages/2.2.4/orbis_plugin_storage_html_pages-2.2.4.tar.gz

---
+++ packages/orbis-plugin-storage-html-pages/orbis_plugin_storage_html_pages-2.2.4.tar.gz/orbis_plugin_storage_html_pages-2.2.4/README.md
@@ -0,0 +1,1 @@
+# HTML Pages Storage Plugin for Orbis
---
+++ packages/orbis-plugin-storage-html-pages/orbis_plugin_storage_html_pages-2.2.4.tar.gz/orbis_plugin_storage_html_pages-2.2.4/orbis_plugin_storage_html_pages/templates/js_arrow_key_navigation.py
@@ -0,0 +1,28 @@
+js_arrow_key_navigation = """
+<!-- Arrow key navigation -->
+<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.1/jquery.min.js"></script>
+<script type="text/javascript">
+$(document).ready(function() {
+    {
+        document.onkeydown = function() {
+            {
+                var j = event.key
+                if (j == "ArrowRight")
+                    window.location = nextUrl
+                else if (j == "ArrowLeft")
+                    window.location = prevUrl
+            }
+        }
+    }
+});
+
+$(document).ready(function() {
+    {
+        var nextPage = $("#next_page_link")
+        var prevPage = $("#previous_page_link")
+        nextUrl = nextPage.attr("href")
+        prevUrl = prevPage.attr("href")
+    }
+});
+</script>
+"""
