2d1254d9ba7205bb055d31f243c7e85e1b41c6d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:24:56 +0000

    Add package ntscli-cloud-lib/0.0.1.dev52/ntscli_cloud_lib-0.0.1.dev52-py3-none-any.whl

---
+++ packages/ntscli-cloud-lib/ntscli_cloud_lib-0.0.1.dev52-py3-none-any.whl/ntscli_cloud_lib/__init__.py
@@ -0,0 +1,16 @@
+"""Main module."""
+import re
+
+
+def host_to_rae_name(userstring: str) -> str:
+    """
+    Return a RAE serial string from most things a user types
+
+    :param userstring: The RAE host/ip or connection configuration to connect to.
+    :return: None
+    """
+    pattern = r"r\d{7}"
+    foundparts = re.findall(pattern, userstring)
+    if len(foundparts) > 0:
+        return foundparts[0]
+    return ""
