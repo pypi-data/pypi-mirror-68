c202457b2d8fc83006c8ea19e625bc5a2df347ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:56 +0000

    Add package dsmr-data-logger/0.1.5/dsmr_data_logger-0.1.5-py3-none-any.whl

---
+++ packages/dsmr-data-logger/dsmr_data_logger-0.1.5-py3-none-any.whl/dsmr/cli.py
@@ -0,0 +1,20 @@
+import click
+
+from dsmr import __version__
+from dsmr.dsmr import DSMRReader
+
+@click.group()
+@click.version_option(version=__version__)
+def main():
+    pass
+
+@main.command()
+def log():
+    click.echo('log')
+
+@main.command()
+@click.option('-d', '--debug', is_flag=True)
+def read(debug):
+    clazz = DSMRReader(debug=debug)
+
+    clazz.read()
