36281ae999c07167645ec9979e08ed07ce7bbbae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:53:40 +0000

    Add package rigidanalytics-tracker/2.0.0/rigidanalytics_tracker-2.0.0.tar.gz

---
+++ packages/rigidanalytics-tracker/rigidanalytics_tracker-2.0.0.tar.gz/rigidanalytics_tracker-2.0.0/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="rigidanalytics_tracker",
+    version="2.0.0",
+    author="Shane Reustle",
+    author_email="shane@reustle.co",
+    description="Server-side analytics",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/reustleco/rigidanalytics-tracker/",
+    license="MIT",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        "blinker",
+        "urllib3",
+    ]
+)
