2d913c7cf6a99359b02d40ae5d2e3406e8eb1010
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:10:57 +0000

    Add package image-encoder/0.0.5/image-encoder-0.0.5.tar.gz

---
+++ packages/image-encoder/image-encoder-0.0.5.tar.gz/image-encoder-0.0.5/README.md
@@ -0,0 +1,1 @@
+# Image encoder for http
---
+++ packages/image-encoder/image-encoder-0.0.5.tar.gz/image-encoder-0.0.5/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="image-encoder", # Replace with your own username
+    version="0.0.5",
+    author="Jagath Jai Kumar",
+    author_email="jagath.jaikumar@gmail.com",
+    description="A simple way to encode and decode images for REST calls",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/jagath-jaikumar/image-encoder",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
