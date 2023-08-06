7d719106194d925cb37e68f4f856764a7a16d8f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:13:35 +0000

    Add package fontMath/0.6.0/fontMath-0.6.0.zip

---
+++ packages/fontMath/fontMath-0.6.0.zip/fontMath-0.6.0/README.md
@@ -0,0 +1,6 @@
+[![Build Status](https://travis-ci.org/robotools/fontMath.svg?branch=master)](https://travis-ci.org/robotools/fontMath)
+[![codecov](https://codecov.io/gh/robotools/fontMath/branch/master/graph/badge.svg)](https://codecov.io/gh/robotools/fontMath)
+
+# fontMath
+A collection of objects that implement fast font, glyph, etc. math.
+
---
+++ packages/fontMath/fontMath-0.6.0.zip/fontMath-0.6.0/setup.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+from __future__ import print_function
+from setuptools import setup, find_packages
+import sys
+
+needs_pytest = {'pytest', 'test'}.intersection(sys.argv)
+pytest_runner = ['pytest_runner'] if needs_pytest else []
+needs_wheel = {'bdist_wheel'}.intersection(sys.argv)
+wheel = ['wheel'] if needs_wheel else []
+
+# with open('README.rst', 'r') as f:
+#     long_description = f.read()
+
+setup(
+    name="fontMath",
+    version="0.6.0",
+    description="A set of objects for performing math operations on font data.",
+    # long_description=long_description,
+    author="Tal Leming",
+    author_email="tal@typesupply.com",
+    url="https://github.com/robotools/fontMath",
+    license="MIT",
+    package_dir={"": "Lib"},
+    packages=find_packages("Lib"),
+    test_suite="fontMath",
+    setup_requires=pytest_runner + wheel,
+    tests_require=[
+        'pytest>=3.0.3',
+    ],
+    install_requires=[
+        "fonttools>=3.32.0",
+    ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Topic :: Multimedia :: Graphics :: Editors :: Vector-Based',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+)
