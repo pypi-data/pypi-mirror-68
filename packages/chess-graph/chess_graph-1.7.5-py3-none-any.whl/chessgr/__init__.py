54f14759543dee129252473b1e503f63bdf29efe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:26:46 +0000

    Add package chess-graph/1.7.5/chess_graph-1.7.5-py3-none-any.whl

---
+++ packages/chess-graph/chess_graph-1.7.5-py3-none-any.whl/chessgr/__init__.py
@@ -0,0 +1,5 @@
+from chess_graph import chart
+
+def graph(database, depth=5, fragmentation_percentage=0.0032, should_defragment=False, custom_branching=False):
+
+    return chart.graph(database, depth, fragmentation_percentage, should_defragment, custom_branching)
