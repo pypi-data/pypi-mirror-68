93974c3eb99ec5aa876610b129530cf562b86766
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:41 +0000

    Add package xmltodict3/0.0.3/xmltodict3-0.0.3.tar.gz

---
+++ packages/xmltodict3/xmltodict3-0.0.3.tar.gz/xmltodict3-0.0.3/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='xmltodict3',
+    version='0.0.3',
+    description='An open-source library that is used '
+                'for converting XML to a python dictionary.',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/dart-neitro/xmltodict3',
+    author='Konstantin Neitro',
+    author_email='neitro88@gmail.com',
+    packages=find_packages(include=['xmltodict3']),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.5',
+)
