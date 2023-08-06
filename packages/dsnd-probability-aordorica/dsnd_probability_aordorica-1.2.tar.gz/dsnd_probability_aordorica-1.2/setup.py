edc2e470aa0708ef46125352e0f70273b721a0c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:33 +0000

    Add package dsnd-probability-aordorica/1.2/dsnd_probability_aordorica-1.2.tar.gz

---
+++ packages/dsnd-probability-aordorica/dsnd_probability_aordorica-1.2.tar.gz/dsnd_probability_aordorica-1.2/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='dsnd_probability_aordorica',
+      version='1.2',
+      description='Gaussian and Binomial  distributions',
+      packages=['dsnd_probability_aordorica'],
+      author = 'Armando Ordorica',
+      author_email = 'armandordorica@gmail.com',
+      zip_safe=False)
