7ecc5da57be59b706a08737a6c4994602528a586
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:22:19 +0000

    Add package distributions-Anshupriya2694/0.1/distributions_Anshupriya2694-0.1.tar.gz

---
+++ packages/distributions-Anshupriya2694/distributions_Anshupriya2694-0.1.tar.gz/distributions_Anshupriya2694-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributions_Anshupriya2694',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions_Anshupriya2694'],
+      zip_safe=False)
