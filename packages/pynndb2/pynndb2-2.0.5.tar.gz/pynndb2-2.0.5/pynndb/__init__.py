13c90b108ea8ab16c5a249105ed814a3edd30dd9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:09:30 +0000

    Add package pynndb2/2.0.5/pynndb2-2.0.5.tar.gz

---
+++ packages/pynndb2/pynndb2-2.0.5.tar.gz/pynndb2-2.0.5/pynndb/__init__.py
@@ -0,0 +1,40 @@
+#####################################################################################
+#
+#  Copyright (c) 2020 - Mad Penguin Consulting Ltd
+#
+#####################################################################################
+
+__version__ = '2.0.5'
+
+from .manager import Manager
+from .database import Database
+from .table import Table
+from .index import Index
+from .doc import Doc
+from .compression import CompressionType
+from .exceptions import IndexAlreadyExists, FailedToWriteMetadata, DocumentAlreadyExists, FailedToWriteData, \
+    DocumentDoesntExist, InvalidKeySpecifier, NoSuchIndex, NotDuplicateIndex, NoSuchTable, \
+    DuplicateKey, IndexWriteError, TableNotOpen, TrainingDataExists
+
+
+__all__ = [
+    Manager,
+    Database,
+    Table,
+    Index,
+    Doc,
+    CompressionType,
+    IndexAlreadyExists,
+    FailedToWriteMetadata,
+    DocumentAlreadyExists,
+    FailedToWriteData,
+    DocumentDoesntExist,
+    InvalidKeySpecifier,
+    NoSuchIndex,
+    NotDuplicateIndex,
+    NoSuchTable,
+    DuplicateKey,
+    IndexWriteError,
+    TableNotOpen,
+    TrainingDataExists
+]
