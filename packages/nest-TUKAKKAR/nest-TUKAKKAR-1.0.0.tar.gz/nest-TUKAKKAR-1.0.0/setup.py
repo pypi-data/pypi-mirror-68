c32237ecc56b9451f5a054fc017cdf5455c7d087
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:36 +0000

    Add package nest-TUKAKKAR/1.0.0/nest-TUKAKKAR-1.0.0.tar.gz

---
+++ packages/nest-TUKAKKAR/nest-TUKAKKAR-1.0.0.tar.gz/nest-TUKAKKAR-1.0.0/setup.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May  9 23:48:17 2020
+
+@author: TusharKakkar
+"""
+
+
+from distutils.core import setup
+setup(
+      name = 'nest-TUKAKKAR',
+      version = '1.0.0',
+      py_modules = ['nest-TUKAKKAR'],
+      author = 'Tushar',
+      author_email = 'tushar.kakkar22@gmail.com',
+      #url = 'http://www.headfirstlabs.com',
+      description = 'A simple printer of nested lists',
+)
