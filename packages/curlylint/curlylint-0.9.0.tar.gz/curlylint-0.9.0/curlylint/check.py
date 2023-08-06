51249496939ac86cf095607ec78070ce320f2fef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:23:35 +0000

    Add package curlylint/0.9.0/curlylint-0.9.0.tar.gz

---
+++ packages/curlylint/curlylint-0.9.0.tar.gz/curlylint-0.9.0/curlylint/check.py
@@ -0,0 +1,23 @@
+from curlylint.rules.indent.indent import INDENT, indent
+from curlylint.util import flatten
+
+checks = {INDENT: indent}
+
+
+def check_rule(file, code: str, options):
+    check = checks.get(code, None)
+    return check(file, options) if check else []
+
+
+def check_file(file, rules):
+    return set(
+        flatten(
+            check_rule(file, code, options)
+            for code, options in rules.items()
+            if options != "off"
+        )
+    )
+
+
+def check_files(files, rules):
+    return flatten(check_file(file, rules) for file in files)
