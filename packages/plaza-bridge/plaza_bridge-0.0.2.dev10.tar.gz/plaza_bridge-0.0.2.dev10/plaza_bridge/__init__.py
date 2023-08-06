ca64da3d85f5b77aad97d5ac079dd593c1a7d21d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:28:03 +0000

    Add package plaza-bridge/0.0.2.dev10/plaza_bridge-0.0.2.dev10.tar.gz

---
+++ packages/plaza-bridge/plaza_bridge-0.0.2.dev10.tar.gz/plaza_bridge-0.0.2.dev10/plaza_bridge/__init__.py
@@ -0,0 +1,8 @@
+from .bridge import PlazaBridge
+from .registration import MessageBasedServiceRegistration, FormBasedServiceRegistration
+from .blocks import (
+    BlockArgument,
+    CallbackBlockArgument,
+    VariableBlockArgument,
+    BlockContext,
+)
