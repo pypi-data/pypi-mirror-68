3b8c5edfc5e42b7ba3b7c670d2456775b2755632
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:04:27 +0000

    Add package hcolours/0.0.4/hcolours-0.0.4.tar.gz

---
+++ packages/hcolours/hcolours-0.0.4.tar.gz/hcolours-0.0.4/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='hcolours',
+    version='0.0.4',
+    description='a colours package',
+    license='MIT',
+    packages=['hcolours'],
+    author='Hs sH',
+    author_email='spi0003@boxhillhs.vic.edu.au',
+    keywords=['colours', 'hcolours', 'hs sh'],
+    url='https://pypi.org/project/hcolours/#files'
+)
