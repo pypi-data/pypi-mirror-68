1c19b352e3c7c80ec6ac9d3fc225db887897c85a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:11:37 +0000

    Add package spado/0.1.0/spado-0.1.0.tar.gz

---
+++ packages/spado/spado-0.1.0.tar.gz/spado-0.1.0/setup.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+modules = \
+['spado']
+install_requires = \
+['boto3>=1.13.8,<2.0.0']
+
+setup_kwargs = {
+    'name': 'spado',
+    'version': '0.1.0',
+    'description': 'Client simplifying and abstracting common operations for digital ocean spaces.',
+    'long_description': None,
+    'author': 'Alex Pedersen',
+    'author_email': 'me@alexpdr.dev',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'py_modules': modules,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
