f9c8e7094fb6ddaf90f5ee68754890b371277e00
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:23:36 +0000

    Add package dolfyn/0.12.1/dolfyn-0.12.1.tar.gz

---
+++ packages/dolfyn/dolfyn-0.12.1.tar.gz/dolfyn-0.12.1/dolfyn/adp/api.py
@@ -0,0 +1,3 @@
+from .base import binner
+from ..rdi.rotate import beam2inst, inst2earth, earth2principal
+from ..io.api import read, load
---
+++ packages/dolfyn/dolfyn-0.12.1.tar.gz/dolfyn-0.12.1/dolfyn/io/api.py
@@ -0,0 +1,41 @@
+from .nortek import read_nortek
+from .nortek2 import read_signature
+from .rdi import read_rdi
+from .base import WrongFileType as _WTF
+# These are included here for use in the API
+from .hdf5 import load
+
+
+def read(fname, userdata=True, nens=None):
+    """Read a binary Nortek (e.g., .VEC, .wpr, .ad2cp, etc.) or RDI
+    (.000, .PD0, etc.) data file.
+
+    Parameters
+    ----------
+    filename : string
+               Filename of Nortek file to read.
+
+    userdata : True, False, or string of userdata.json filename
+               (default ``True``) Whether to read the
+               '<base-filename>.userdata.json' file.
+
+    nens : None (default: read entire file), int, or
+           2-element tuple (start, stop)
+              Number of pings to read from the file
+
+    Returns
+    -------
+    dat : :class:`<~dolfyn.data.velocity.Velocity>`
+      A DOLfYN velocity data object.
+
+    """
+    # Loop over binary readers until we find one that works.
+    for func in [read_nortek, read_signature, read_rdi]:
+        try:
+            dat = func(fname, userdata=userdata, nens=nens)
+        except _WTF:
+            continue
+        else:
+            return dat
+    raise _WTF("Unable to find a suitable reader for "
+               "file {}.".format(fname))
