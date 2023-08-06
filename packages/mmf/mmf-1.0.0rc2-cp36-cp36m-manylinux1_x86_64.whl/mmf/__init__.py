1f479d332da7bb8e10966fc02110f4924a26a325
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:22:37 +0000

    Add package mmf/1.0.0rc2/mmf-1.0.0rc2-cp36-cp36m-manylinux1_x86_64.whl

---
+++ packages/mmf/mmf-1.0.0rc2-cp36-cp36m-manylinux1_x86_64.whl/mmf/__init__.py
@@ -0,0 +1,12 @@
+# Copyright (c) Facebook, Inc. and its affiliates.
+# isort:skip_file
+
+import mmf.utils
+import mmf.common
+import mmf.modules
+import mmf.modules.losses
+import mmf.modules.schedulers
+import mmf.modules.optimizers
+import mmf.modules.metrics
+import mmf.datasets
+import mmf.models
