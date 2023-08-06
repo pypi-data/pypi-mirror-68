38283bd99cb27115bec8c0cc28bf2ebb928e7552
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:34:15 +0000

    Add package nestervoja/1.2.0/nestervoja-1.2.0.tar.gz

---
+++ packages/nestervoja/nestervoja-1.2.0.tar.gz/nestervoja-1.2.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+name = 'nestervoja',
+version = '1.2.0',
+py_modules = ['nester'],
+author = 'voja14',
+author_email = 'voja14@ptt.rs',
+url = 'http://www.gethub.com/Vojkan78/Hello-World-',
+description = 'A simple printer of nested lists',
+)
