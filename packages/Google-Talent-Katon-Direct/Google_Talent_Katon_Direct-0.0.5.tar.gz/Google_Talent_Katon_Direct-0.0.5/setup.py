5db20e7c2822bd92f51407c9de8f8c5b2796b0dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:19:04 +0000

    Add package Google-Talent-Katon-Direct/0.0.5/Google_Talent_Katon_Direct-0.0.5.tar.gz

---
+++ packages/Google-Talent-Katon-Direct/Google_Talent_Katon_Direct-0.0.5.tar.gz/Google_Talent_Katon_Direct-0.0.5/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("readme.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="Google_Talent_Katon_Direct",
+    version="0.0.5",
+    author="Alex VanLaningham",
+    author_email="alexv@katondirect.com",
+    description="Simple way to use google talent cloud for basic functionality",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypa/sampleproject",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'google.cloud',
+        'six',
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
