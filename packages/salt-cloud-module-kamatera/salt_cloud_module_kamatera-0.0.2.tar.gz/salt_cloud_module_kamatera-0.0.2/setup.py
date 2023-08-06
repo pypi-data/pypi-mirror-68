d4c28c4b6da790d2fb43e24751d0572ebf870394
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:02:29 +0000

    Add package salt-cloud-module-kamatera/0.0.2/salt_cloud_module_kamatera-0.0.2.tar.gz

---
+++ packages/salt-cloud-module-kamatera/salt_cloud_module_kamatera-0.0.2.tar.gz/salt_cloud_module_kamatera-0.0.2/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+
+setup(
+    name="salt_cloud_module_kamatera",
+    version="0.0.2",
+    packages=find_packages(exclude=['tests',]),
+    install_requires=["salt>=2019.2.0"],
+    description='SaltStack Cloud module for managing Kamatera compute resources',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author='Kamatera',
+    url='https://github.com/Kamatera/salt-cloud-module-kamatera',
+    license='MIT',
+    entry_points={
+        'salt.loader': [
+            'cloud_dirs=salt_cloud_module_kamatera.loader:clouds_dirs',
+        ],
+    },
+)
