e7b9904c73f44ca190d8c05f5932a1d9771f27e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:25:05 +0000

    Add package mayank16maniac/0.1/mayank16maniac-0.1.tar.gz

---
+++ packages/mayank16maniac/mayank16maniac-0.1.tar.gz/mayank16maniac-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='mayank16maniac',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['mayank16maniac'],
+      zip_safe=False)
