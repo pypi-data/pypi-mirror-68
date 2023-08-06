6ea8751b3e869285f8fad7b7b3ec7c85ca9eb881
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:24 +0000

    Add package deeplearnt/0.0.1/deeplearnt-0.0.1.tar.gz

---
+++ packages/deeplearnt/deeplearnt-0.0.1.tar.gz/deeplearnt-0.0.1/setup.py
@@ -0,0 +1,10 @@
+import setuptools
+setuptools.setup(name='deeplearnt',
+      version='0.0.1',
+      description='clinical trial information retriver',
+      url='http://github.com/tongling/clinicaltrial',
+      author='lowe.li',
+      author_email='alilzg@aliyun.com',
+      license='MIT',
+      packages=setuptools.find_packages(),
+      zip_safe=False)
