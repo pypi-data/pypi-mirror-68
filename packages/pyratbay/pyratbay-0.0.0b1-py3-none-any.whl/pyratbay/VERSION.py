1bc533fd49227bfdae48bdac8683c881b27cc36a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:25:03 +0000

    Add package pyratbay/0.0.0b1/pyratbay-0.0.0b1-py3-none-any.whl

---
+++ packages/pyratbay/pyratbay-0.0.0b1-py3-none-any.whl/pyratbay/VERSION.py
@@ -0,0 +1,12 @@
+# Copyright (c) 2016-2020 Patricio Cubillos.
+# Pyrat Bay is open-source software under the GNU GPL-2.0 license (see LICENSE).
+
+# Software versioning:
+
+# Pyrat-Bay version:
+PBAY_VER  =  0  # Major version
+PBAY_MIN  =  0  # Minor version
+PBAY_REV  =  '0b1'  # Revision
+
+__version__ = '{}.{}.{}'.format(PBAY_VER, PBAY_MIN, PBAY_REV)
+
