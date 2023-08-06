17fedbaa085f584c408e1db745dd18a137c5171b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:20 +0000

    Add package edx-django-sites-extensions/2.5.1/edx_django_sites_extensions-2.5.1-py3-none-any.whl

---
+++ packages/edx-django-sites-extensions/edx_django_sites_extensions-2.5.1-py3-none-any.whl/django_sites_extensions/apps.py
@@ -0,0 +1,17 @@
+""" django_sites_extensions application configuration """
+
+from django.apps import AppConfig
+
+
+class DjangoSitesExtensionsConfig(AppConfig):
+    """ django_sites_extensions application configuration """
+    name = 'django_sites_extensions'
+    verbose_name = 'Django Sites Extensions'
+
+    # noinspection PyUnresolvedReferences
+    def ready(self):
+        """ Set up for django_sites_extensions app """
+        # pylint: disable=unused-variable
+        # pylint: disable=unused-import
+        from django_sites_extensions import models
+        from django_sites_extensions import signals
