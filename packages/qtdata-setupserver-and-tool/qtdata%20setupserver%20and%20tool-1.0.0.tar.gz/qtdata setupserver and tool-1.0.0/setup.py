78a082e3f3b8ab3692aa6d11f2a0f44fd50f5439
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:24:44 +0000

    Add package qtdata-setupserver-and-tool/1.0.0/qtdata%20setupserver%20and%20tool-1.0.0.tar.gz

---
+++ packages/qtdata-setupserver-and-tool/qtdata%20setupserver%20and%20tool-1.0.0.tar.gz/qtdata setupserver and tool-1.0.0/setup.py
@@ -0,0 +1,12 @@
+import setuptools
+from distutils.core import setup
+setup(
+    name = 'qtdata setupserver and tool',
+    version= "1.0.0",
+    py_modules=["qtdata_setupserver_and_tool"],
+    author="cai the si",
+    author_email="caithesi21041997@gmail.com",
+    url = '',
+    description="just ignore this module",
+    
+)
