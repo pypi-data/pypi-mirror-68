35216939ba58d25c1e06f22ff0fa775ada2a4a3b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:26:57 +0000

    Add package mundi-healthcare/0.1.1/mundi_healthcare-0.1.1-py2.py3-none-any.whl

---
+++ packages/mundi-healthcare/mundi_healthcare-0.1.1-py2.py3-none-any.whl/mundi_healthcare/__init__.py
@@ -0,0 +1,50 @@
+"""
+A Mundi plugin with healthcare statistics.
+"""
+__author__ = "Fábio Mendes"
+__version__ = "0.1.1"
+
+from .functions import hospital_capacity, hospital_capacity_public, icu_capacity, icu_capacity_public
+
+FUNCTIONS = {
+    "hospital_capacity": hospital_capacity,
+    "hospital_capacity_public": hospital_capacity_public,
+    "icu_capacity": icu_capacity,
+    "icu_capacity_public": icu_capacity_public,
+}
+
+
+def register(check_environ=False):
+    """
+    Enable plugin.
+
+    This is executed automatically
+    """
+    from mundi.loader import register
+    from mundi.types.region import REGION_PLUGINS
+
+    if check_environ:
+        import os
+        if os.environ.get("MUNDI_DEMOGRAPHY", "on").lower() in ("off", "false", "no"):
+            return
+
+    for k, v in FUNCTIONS.items():
+        register(k, v)
+
+    REGION_PLUGINS["hospital_capacity"] = lambda x: hospital_capacity(x.id)
+    REGION_PLUGINS["hospital_capacity_public"] = lambda x: hospital_capacity_public(x.id)
+    REGION_PLUGINS["icu_capacity"] = lambda x: icu_capacity(x.id)
+    REGION_PLUGINS["icu_capacity_public"] = lambda x: icu_capacity_public(x.id)
+
+
+def unregister():
+    """
+    Disable plugin.
+    """
+    from mundi.loader import unregister
+
+    for k, v in FUNCTIONS.items():
+        unregister(k, function=v)
+
+
+register(check_environ=True)
