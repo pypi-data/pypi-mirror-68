0aa6bce30d13b436d004cc8e3bb2417a67a9ea8f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:39 +0000

    Add package epicshop-ds-order-utils/0.1.0/epicshop_ds_order_utils-0.1.0.tar.gz

---
+++ packages/epicshop-ds-order-utils/epicshop_ds_order_utils-0.1.0.tar.gz/epicshop_ds_order_utils-0.1.0/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+name='epicshop_ds_order_utils',
+version='0.1.0',
+author='pleasefeedme',
+author_email='myfakeemail@gmail.com',
+packages=['epicshop_ds_order_utils'],
+scripts=[],
+url='http://pypi.python.org/pypi/epicshop_ds_order_utils',
+license='LICENSE.txt',
+description='Useful towel-related stuff.',
+install_requires=['requests'],
+)
