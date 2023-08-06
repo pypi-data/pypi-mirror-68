04a411584f438e19b5ac665b116303a9057489e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:11:11 +0000

    Add package invenio/3.3.0/invenio-3.3.0.tar.gz

---
+++ packages/invenio/invenio-3.3.0.tar.gz/invenio-3.3.0/invenio/version.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+#
+# This file is part of Invenio.
+# Copyright (C) 2015-2019 CERN.
+#
+# Invenio is free software; you can redistribute it and/or modify it
+# under the terms of the MIT License; see LICENSE file for more details.
+
+"""Version information for Invenio.
+
+This file is imported by ``invenio.__init__``,
+and parsed by ``setup.py``.
+"""
+
+__version__ = "3.3.0"
