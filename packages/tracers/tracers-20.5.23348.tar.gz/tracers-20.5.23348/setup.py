6cfd9cf4f03c80901f2de5bcdf16a18ad5fa0452
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:03:21 +0000

    Add package tracers/20.5.23348/tracers-20.5.23348.tar.gz

---
+++ packages/tracers/tracers-20.5.23348.tar.gz/tracers-20.5.23348/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+package_dir = \
+{'': 'src'}
+
+packages = \
+['tracers', 'tracers.function']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'tracers',
+    'version': '20.5.23348',
+    'description': 'Decorator-like performance tracing tool',
+    'long_description': '',
+    'author': 'Kevin Amado',
+    'author_email': 'kamadorueda@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://github.com/kamadorueda/tracers',
+    'package_dir': package_dir,
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
