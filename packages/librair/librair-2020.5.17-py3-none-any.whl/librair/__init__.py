cf60eed5925dd00916430d0aae0875ff6914fa97
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:02:47 +0000

    Add package librair/2020.5.17/librair-2020.5.17-py3-none-any.whl

---
+++ packages/librair/librair-2020.5.17-py3-none-any.whl/librair/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from . import interfaces
+from . import protocols
+from . import queries
+from . import schemas
+from . import services
+from . import endpoints
+from . import parsers
