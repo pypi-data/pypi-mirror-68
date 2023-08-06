e2e231ff13121b4dbbee63598aa01dde4c28ce0b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:09 +0000

    Add package biorxiv-retriever/0.20.0/biorxiv_retriever-0.20.0.tar.gz

---
+++ packages/biorxiv-retriever/biorxiv_retriever-0.20.0.tar.gz/biorxiv_retriever-0.20.0/setup.py
@@ -0,0 +1,36 @@
+from distutils.core import setup
+
+with open('README.md', encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+  name = 'biorxiv_retriever',
+  packages = ['biorxiv_retriever'],
+  version = '0.20.0',
+  license='MIT',
+  description = 'Simple retriever for Biorxiv articles',
+  long_description_content_type='text/markdown',
+  long_description=long_description,
+  author = 'Tal Schuster',
+  author_email = 'my.email@mit.edu',
+  url = 'https://github.com/TalSchuster/BiorxivRetriever/',
+  download_url = 'https://github.com/TalSchuster/BiorxivRetriever/biorxiv_retriever/v_0_20_0.tar.gz',
+  keywords = ['Biology', 'Rxivist', 'Biorxiv', 'retriever'],
+  install_requires=[
+          'tqdm',
+          'lxml',
+          'beautifulsoup4',
+      ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
+  ],
+)
