9b85875c02871ebbdfd568b94e90f6743e92b649
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:05 +0000

    Add package pycmc/0.0.2a0/pycmc-0.0.2a0.tar.gz

---
+++ packages/pycmc/pycmc-0.0.2a0.tar.gz/pycmc-0.0.2a0/pycmc/charts/deezer.py
@@ -0,0 +1,27 @@
+from .. import utilities
+
+deezer_music_url = f"/charts/deezer"
+
+
+def insights(date, country="US"):
+    """
+    # `insights`
+    Get the top 100 Deezer chart tracks for the given date
+
+    https://api.chartmetric.com/api/charts/deezer/
+
+    ## Parameters
+    - `date`:        string date in ISO format %Y-%m-%d
+    - `country`:     string country code
+
+    ## Returns            
+    list of dictionary of tracks on Deezer chart
+    """
+    urlhandle = f"{deezer_music_url}/"
+    params = {
+        "country_code": country,
+        "date": date,
+    }
+
+    data = utilities.RequestData(urlhandle, params)
+    return utilities.RequestGet(data)["data"]
---
+++ packages/pycmc/pycmc-0.0.2a0.tar.gz/pycmc-0.0.2a0/pycmc/charts/spotify.py
@@ -0,0 +1,63 @@
+"""
+Defines methods for obtaining Spotify chart data from the 
+chartmetric.com API.
+"""
+import requests
+import json
+from .. import utilities
+
+
+THURSDAY = 3
+SPOTIFY_CHARTS_URL = f"/charts/spotify"
+
+
+def tracks(date, country="US", viral=False):
+    """
+    # `tracks`
+    Get the top 200 tracks on Spotify chart for the given date.
+
+    https://api.chartmetric.com/api/charts/spotify
+
+    ## Parameters
+    - `date`:        string date in ISO format %Y-%m-%d
+    - `country`:     string two-letter country code
+    - `viral`:       if True return tracks from Spotify viral chart, otherwise return those from regional chart
+
+    ## Returns            
+    list of dictionary of tracks on Spotify chart
+    """
+
+    urlhandle = f"{SPOTIFY_CHARTS_URL}"
+    params = {
+        "date": date,
+        "country_code": country,
+        "type": "viral" if viral else "regional",
+        "interval": "daily",
+        "offset": 0,
+    }
+    data = utilities.RequestData(urlhandle, params)
+    return utilities.RequestGet(data)["data"]
+
+
+def freshfind(date):
+    """
+    # `freshfind`
+    Get the tracks from the Spotify Freshfind chart
+    for the given date.
+    Data available ONLY on Thursdays.
+
+    https://api.chartmetric.com/api/charts/spotify/freshfind
+
+    ## Parameters
+    - `date`:        string date in ISO format %Y-%m-%d, only Thursdays
+
+    ## Returns
+    list of dictionary of tracks on Spotify Freshfind
+    """
+    urlhandle = f"{SPOTIFY_CHARTS_URL}/freshfind"
+    params = {
+        "date": utilities.strWeekday(date, THURSDAY),
+    }
+
+    data = utilities.RequestData(urlhandle, params)
+    return utilities.RequestGet(data)
