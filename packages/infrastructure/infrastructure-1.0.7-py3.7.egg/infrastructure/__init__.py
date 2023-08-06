98e1711a4a378d65dfdcecb60c9b2c4298c0c8de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:27:22 +0000

    Add package infrastructure/1.0.7/infrastructure-1.0.7-py3.7.egg

---
+++ packages/infrastructure/infrastructure-1.0.7-py3.7.egg/infrastructure/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+# @Author: yongfanmao
+# @Date:   2020-05-18 21:02:33
+# @E-mail: maoyongfan@163.com
+# @Last Modified by:   yongfanmao
+# @Last Modified time: 2020-05-19 20:58:15
+VERSION = "1.0.6"
+from .remoteServer.ssh_server import ConnectServerDM
