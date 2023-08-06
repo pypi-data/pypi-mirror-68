79df1aea3306d9e77d9b176ec3511145a7150d44
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:22:29 +0000

    Add package TracFullBlog/0.1.6.5/TracFullBlog-0.1.6.5.tar.gz

---
+++ packages/TracFullBlog/TracFullBlog-0.1.6.5.tar.gz/TracFullBlog-0.1.6.5/setup.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+"""
+Trac plugin proving a full-featured, self-contained Blog.
+
+License: BSD
+
+(c) 2007 ::: www.CodeResort.com - BV Network AS (simon-code@bvnetwork.no)
+"""
+
+from setuptools import setup
+
+setup(name='TracFullBlog',
+      version='0.1.6.5',
+      packages=['tracfullblog'],
+      author='CodeResort.com = BV Network AS',
+      author_email='simon-code@bvnetwork.no',
+      keywords='trac blog',
+      description='Full-featured and self-contained Blog plugin for Trac.',
+      url='https://trac-hacks.org/wiki/FullBlogPlugin',
+      license='BSD',
+      classifiers=['Framework :: Trac'],
+      zip_safe = False,
+      extras_require={
+            'tags': 'TracTags>=0.6',
+            'spamfilter': 'TracSpamFilter>=0.2'},
+      entry_points={'trac.plugins': [
+            'tracfullblog.admin = tracfullblog.admin',
+            'tracfullblog.core = tracfullblog.core',
+            'tracfullblog.db = tracfullblog.db',
+            'tracfullblog.macros = tracfullblog.macros',
+            'tracfullblog.spamfilter = tracfullblog.spamfilter[spamfilter]',
+            'tracfullblog.tags = tracfullblog.tags[tags]',
+            'tracfullblog.web_ui = tracfullblog.web_ui']},
+      package_data={'tracfullblog' : ['htdocs/*.png',
+                                      'htdocs/css/*.css',
+                                      'htdocs/js/*.js',
+                                      'templates/*.html',
+                                      'templates/*.rss', ]},
+      exclude_package_data={'': ['tests/*']},
+      test_suite = 'tracfullblog.tests.test_suite',
+      tests_require = [],
+      install_requires = [])
---
+++ packages/TracFullBlog/TracFullBlog-0.1.6.5.tar.gz/TracFullBlog-0.1.6.5/tracfullblog/tests/model.py
@@ -0,0 +1,52 @@
+
+import datetime
+
+from tracfullblog.tests import FullBlogTestCaseTemplate
+from tracfullblog.model import *
+
+
+class GroupPostsByMonthTestCase(FullBlogTestCaseTemplate):
+
+    def test_many_posts(self):
+        # 2 posts in one period
+        one = BlogPost(self.env, 'one')
+        one.update_fields({'title': 'one', 'body': 'body', 'author': 'user'})
+        self.assertEquals([], one.save('user'))
+        two = BlogPost(self.env, 'two')
+        two.update_fields({'title': 'two', 'body': 'body', 'author': 'user'})
+        self.assertEquals([], two.save('user'))
+        grouped = group_posts_by_month(get_blog_posts(self.env))
+        self.assertEquals(1, len(grouped))
+        # Add 1 post in another period
+        three = BlogPost(self.env, 'three')
+        three.update_fields({'title': 'three', 'body': 'body', 'author': 'user',
+            'publish_time': three.publish_time - datetime.timedelta(days=-100)})
+        self.assertEquals([], three.save('user'))
+        grouped = group_posts_by_month(get_blog_posts(self.env))
+        self.assertEquals(2, len(grouped))
+        self.assertEquals(1, len(grouped[0][1]))
+        self.assertEquals(2, len(grouped[1][1]))
+        self.assertEquals(type(grouped[0][0]), datetime.datetime)
+        self.assertEquals((one.name, one.version, one.publish_time, one.author, one.title, one.body, []), grouped[1][1][0])
+
+    def test_no_posts(self):
+        grouped = group_posts_by_month(get_blog_posts(self.env))
+        self.assertEquals([], grouped)
+
+class GetBlogPostsTestCase(FullBlogTestCaseTemplate):
+
+    def test_get_by_category(self):
+        bp = BlogPost(self.env, 'one')
+        bp.update_fields({'title': 'one', 'body': 'body', 'author': 'user',
+                          'categories': 'about stuff'})
+        self.assertEquals([], bp.save('user'))
+        posts = get_blog_posts(self.env)
+        self.assertEquals(1, len(posts))
+        self.assertEquals('one', posts[0][0])
+        posts = get_blog_posts(self.env, category='non-existing')
+        self.assertEquals(0, len(posts))
+        posts = get_blog_posts(self.env, category='stuff')
+        self.assertEquals(1, len(posts))
+        self.assertEquals('one', posts[0][0])
+        self.assertEquals(get_blog_posts(self.env, category='about'),
+                          get_blog_posts(self.env, category='stuff'))
