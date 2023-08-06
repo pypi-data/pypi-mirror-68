75a32aebe8081d1cf3e8526ea1011a9c5c30f849
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:10:04 +0000

    Add package probdists-udacity-tarciso/0.1/probdists_udacity_tarciso-0.1.tar.gz

---
+++ packages/probdists-udacity-tarciso/probdists_udacity_tarciso-0.1.tar.gz/probdists_udacity_tarciso-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='probdists_udacity_tarciso',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['probdists_udacity_tarciso'],
+      author='Tarciso Braz',
+      author_email='tarciso.filho@ccc.ufcg.edu.br',
+      zip_safe=False)
