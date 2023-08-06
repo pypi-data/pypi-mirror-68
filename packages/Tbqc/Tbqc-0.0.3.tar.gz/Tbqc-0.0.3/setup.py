3edc99f810041f81a138217a94263f59fcddd81b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:01:12 +0000

    Add package Tbqc/0.0.3/Tbqc-0.0.3.tar.gz

---
+++ packages/Tbqc/Tbqc-0.0.3.tar.gz/Tbqc-0.0.3/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(name='Tbqc',
+      version='0.0.3',
+      description='Taiwan Bartender Quizdata Crawler',
+      url='https://github.com/dapingtai/Tw-Bartender-Quizdata-crawler/',
+      author='HungChang',
+      author_email='zero100x@gmail.com',
+      license='BSD-5',
+      packages=['Tbqc'],
+      python_requires='>=3.6',
+)
