280563a4f27368718e21792efea7dd1272f3be00
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:52:39 +0000

    Add package dnaApi/0.2/dnaApi-0.2.tar.gz

---
+++ packages/dnaApi/dnaApi-0.2.tar.gz/dnaApi-0.2/setup.py
@@ -0,0 +1,30 @@
+import setuptools
+with open("README.md", "r") as fh:
+	long_description = fh.read()
+setuptools.setup(
+	name = 'dnaApi',   
+	version = '0.2',      
+	license='MIT',        
+	description = 'This module will interact with Tor to get real time statistical and analytical information.', 
+	long_description=long_description,
+	long_description_content_type="text/markdown",
+	author = 'Narender Singh Yadav,Divya Gupta',                   
+	author_email = 'narenderyadav1999@gmail.com,2016ucp1472@mnit.ac.in',      
+	url = 'https://github.com/narendersinghyadav/dnaApi',     
+	keywords = ['TOR', 'ANALYSIS', 'STATISTICAL ANALYSIS TOR','TOR NETWORK ANALYSIS'],  
+	install_requires=[           
+		'ipaddress',
+		'binascii',
+		'psutil',
+	      ],
+	  classifiers=[
+	    'Development Status :: 3 - Alpha',      
+	    'Intended Audience :: Developers',      
+	    'Topic :: Software Development :: Build Tools',
+	    'License :: OSI Approved :: MIT License',   
+	    'Programming Language :: Python :: 3',      
+	    'Programming Language :: Python :: 3.4',
+	    'Programming Language :: Python :: 3.5',
+	    'Programming Language :: Python :: 3.6',
+	  ],
+	)
