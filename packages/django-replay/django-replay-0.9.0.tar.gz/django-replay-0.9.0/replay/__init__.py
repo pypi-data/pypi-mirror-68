b9b95d118584953f3266fbb6d1acbc9a739a7f7a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:20:52 +0000

    Add package django-replay/0.9.0/django-replay-0.9.0.tar.gz

---
+++ packages/django-replay/django-replay-0.9.0.tar.gz/django-replay-0.9.0/replay/__init__.py
@@ -0,0 +1,10 @@
+"""Django Replay
+
+"""
+
+__title__ = 'replay'
+__version__ = '0.9.0'
+__build__ = 0x000900
+__author__ = 'Grant Jenks'
+__license__ = 'Apache 2.0'
+__copyright__ = 'Copyright 2017 Grant Jenks'
