4a90c70a3db35fce0dc475df74f5860e49bc7b16
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:26:02 +0000

    Add package osha.oira/6.2.0/osha.oira-6.2.0.tar.gz

---
+++ packages/osha.oira/osha.oira-6.2.0.tar.gz/osha.oira-6.2.0/src/osha/oira/client/browser/webhelpers.py
@@ -0,0 +1,33 @@
+# coding=utf-8
+from euphorie.client.browser.webhelpers import WebHelpers
+from logging import getLogger
+from plone.namedfile.interfaces import INamedBlobImage
+from plone.memoize.instance import memoize
+
+log = getLogger(__name__)
+
+
+class OSHAWebHelpers(WebHelpers):
+
+    @memoize
+    def styles_override(self):
+
+        css = super(OSHAWebHelpers, self).styles_override
+        css += """
+#osc .miller-columns .browser .item .object-name {
+    color: inherit;
+}
+"""
+        return css
+
+    def is_image_small(self, context, fname="image", usecase="module"):
+        image = getattr(context, fname, None)
+        if image and INamedBlobImage.providedBy(image):
+            x, y = image.getImageSize()
+            if x < 1000 or y < 430:
+                return True
+
+    @property
+    @memoize
+    def show_completion_percentage(self):
+        return True
