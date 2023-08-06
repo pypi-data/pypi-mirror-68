38eb517cdfa2aa2d8e223a2e3a952c49a8e1b7f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:51:51 +0000

    Add package cpenv/0.5.1/cpenv-0.5.1-py2.py3-none-any.whl

---
+++ packages/cpenv/cpenv-0.5.1-py2.py3-none-any.whl/cpenv/__init__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+'''isort:skip_file'''
+__title__ = 'cpenv'
+__version__ = '0.5.1'
+__author__ = 'Dan Bradham'
+__email__ = 'danielbradham@gmail.com'
+__url__ = 'http://github.com/cpenv/cpenv'
+__license__ = 'MIT'
+__description__ = 'Cross-platform Python environment management.'
+
+
+# Local imports
+from .api import *
+from .repos import *
+from .resolver import *
+from .vendor import yaml
+from .versions import *
+
+
+# Initialize cpenv
+from .api import _init
+_init()
