ce4c1fe1cdb7e8776cc0f7afac8a66b426940f13
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:11:31 +0000

    Add package flask-msearch/0.2.8/flask-msearch-0.2.8.tar.gz

---
+++ packages/flask-msearch/flask-msearch-0.2.8.tar.gz/flask-msearch-0.2.8/flask_msearch/clickhouse_backend.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# ********************************************************************************
+# Copyright © 2020 jianglin
+# File Name: clickhouse_backend.py
+# Author: jianglin
+# Email: mail@honmaple.com
+# Created: 2020-04-30 09:56:22 (CST)
+# Last Update: Thursday 2020-04-30 09:56:23 (CST)
+#          By:
+# Description:
+# ********************************************************************************
