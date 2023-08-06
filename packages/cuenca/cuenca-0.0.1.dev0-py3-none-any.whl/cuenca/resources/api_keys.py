19d832512342825b73a591fcf07913d4ca327813
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:23:50 +0000

    Add package cuenca/0.0.1.dev0/cuenca-0.0.1.dev0-py3-none-any.whl

---
+++ packages/cuenca/cuenca-0.0.1.dev0-py3-none-any.whl/cuenca/resources/api_keys.py
@@ -0,0 +1,57 @@
+import datetime as dt
+from typing import ClassVar, Optional, Tuple
+
+from pydantic.dataclasses import dataclass
+
+from ..http import session
+from .base import Creatable, Queryable, Retrievable
+
+
+@dataclass
+class ApiKey(Creatable, Queryable, Retrievable):
+    _endpoint: ClassVar = '/api_keys'
+    _query_params: ClassVar = set()
+
+    id: str
+    secret: str
+    created_at: dt.datetime
+    deactivated_at: Optional[dt.datetime]
+
+    @property
+    def active(self) -> bool:
+        return (
+            self.deactivated_at is None
+            or self.deactivated_at > dt.datetime.utcnow()
+        )
+
+    @classmethod
+    def create(cls) -> 'ApiKey':
+        return super().create()
+
+    @classmethod
+    def roll(cls, minutes: int = 0) -> Tuple['ApiKey', 'ApiKey']:
+        """
+        1. create a new ApiKey
+        2. configure client with new ApiKey
+        3. deactivate prior ApiKey in a certain number of minutes
+        4. return both ApiKeys
+        """
+        old_id = session.auth[0]
+        new = cls.create()
+        # have to use the new key to deactivate the old key
+        session.configure(new.id, new.secret)
+        old = cls.deactivate(old_id, minutes)
+        return old, new
+
+    @classmethod
+    def deactivate(cls, api_key_id: str, minutes: int = 0) -> 'ApiKey':
+        """
+        deactivate an ApiKey in a certain number of minutes. If minutes is
+        negative, the API will treat it the same as 0. You can't deactivate
+        the same key with which the client is configured, since that'd risk
+        locking you out. The deactivated key is returned so that you have the
+        exact deactivated_at time.
+        """
+        url = cls._endpoint + f'/{api_key_id}'
+        resp = session.delete(url, dict(minutes=minutes))
+        return cls._from_dict(resp)
---
+++ packages/cuenca/cuenca-0.0.1.dev0-py3-none-any.whl/tests/resources/test_api_keys.py
@@ -0,0 +1,65 @@
+import datetime as dt
+
+import pytest
+
+from cuenca import ApiKey
+from cuenca.http import session
+
+
+@pytest.mark.vcr
+def test_api_keys_create():
+    api_key = ApiKey.create()
+    assert api_key.id is not None
+    assert api_key.secret is not None
+    assert api_key.active
+
+
+@pytest.mark.vcr
+def test_api_keys_retrieve():
+    id_key = 'PKUvRwK7imQK2JcjJV91iEzg=='
+    api_key: ApiKey = ApiKey.retrieve(id_key)
+    assert api_key.id == id_key
+    assert api_key.secret == '********'
+
+
+@pytest.mark.vcr
+def test_api_key_deactivate():
+    id_key = 'PKyyRnEL0XS6iHeSi2_8DDPA=='
+    api_key: ApiKey = ApiKey.retrieve(id_key)
+    assert api_key.active
+
+    disabled = ApiKey.deactivate(id_key, 0)
+    assert disabled.id == api_key.id
+    assert disabled.deactivated_at is not None
+    assert not disabled.active
+
+    assert api_key.active
+    api_key.refresh()
+    assert not api_key.active
+
+
+@pytest.mark.vcr
+def test_api_key_roll_keys():
+    session.configure(api_key='test', api_secret='test', sandbox=True)
+    old_keys, new_keys = ApiKey.roll(0)
+
+    assert old_keys.id == 'test'
+    assert not old_keys.active
+    assert new_keys.active
+    auth_key, auth_secret = session.auth
+    assert new_keys.id == auth_key
+
+
+def test_api_key_to_dict():
+    created = dt.datetime.utcnow()
+    api_key: ApiKey = ApiKey(
+        id='12345', secret='********', created_at=created, deactivated_at=None,
+    )
+    api_key_dict = dict(
+        id='12345',
+        secret='********',
+        created_at=created.isoformat(),
+        deactivated_at=None,
+    )
+
+    assert api_key_dict == api_key.to_dict()
