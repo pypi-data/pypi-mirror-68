aebd69d478e52b122b6fdaf9a383553ede1b321f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:13:49 +0000

    Add package libheap/0.5/libheap-0.5.tar.gz

---
+++ packages/libheap/libheap-0.5.tar.gz/libheap-0.5/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(name='libheap',
+      version='0.5',
+      description='a general heap library',
+      url='https://github.com/yutao-li/libheap',
+      download_url='https://github.com/yutao-li/libheap/archive/0.5.tar.gz',
+      author='masterlee',
+      author_email='870070392@qq.com',
+      packages=['libheap'],
+      license='MIT',
+      classifiers=[
+          'Development Status :: 3 - Alpha',
+          'License :: OSI Approved :: MIT License',
+          'Programming Language :: Python :: 3'])
