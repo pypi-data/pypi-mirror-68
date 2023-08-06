688a795bf4d046eed933df536282a286cd7b4059
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:53:57 +0000

    Add package waterch-tasker/0.0.3/waterch-tasker-0.0.3.tar.gz

---
+++ packages/waterch-tasker/waterch-tasker-0.0.3.tar.gz/waterch-tasker-0.0.3/setup.py
@@ -0,0 +1,46 @@
+from pip import main as pip_main
+
+pip_main(['install', '-r', 'requirements.txt'])
+
+from setuptools import setup, find_namespace_packages
+from markdown import markdown
+
+raw_version = open('VERSION').read().strip(' \n')
+
+setup(
+    name="waterch-tasker",
+    version=raw_version,
+    packages=find_namespace_packages(where="src"),
+    package_dir={"": "src"},
+    # Project uses reStructuredText, so ensure that the docutils get
+    # installed or upgraded on the target machine
+
+    package_data={
+        # If any package contains *.txt or *.rst files, include them:
+        "": ["*.txt", "*.rst"],
+        # And include any *.msg files found in the "hello" package, too:
+        # "hello": ["*.msg"],
+    },
+    # metadata to display on PyPI
+    author="Chen Runze",
+    author_email="chenrz925@icloud.com",
+    description="A scalable and extendable experiment task scheduler framework.",
+    long_description=markdown(open("README.md").read()),
+    url="https://github.com/chenrz925/waterch-tasker",  # project home page, if any
+    project_urls={
+        "Documentation": "https://waterch-tasker.readthedocs.io/zh_CN/latest/",
+        "Source Code": "https://github.com/chenrz925/waterch-tasker",
+    },
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python :: 3",
+    ],
+    entry_points={
+        "console_scripts": [
+            "waterch-tasker = waterch.tasker:launch",
+        ],
+    },
+    # could also include long_description, download_url, etc.
+    install_requires=open("requirements.txt").readlines(),
+)
