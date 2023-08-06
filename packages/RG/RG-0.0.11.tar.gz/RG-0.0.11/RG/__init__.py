8ed19d6e631a38e9221fa724b7127da1e65394c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:22:31 +0000

    Add package RG/0.0.11/RG-0.0.11.tar.gz

---
+++ packages/RG/RG-0.0.11.tar.gz/RG-0.0.11/RG/__init__.py
@@ -0,0 +1,21 @@
+from RG.xlsread import *
+from RG.regress import *
+from RG.R2 import *
+
+from RG.PCA import *
+from RG.PLS import *
+from RG.mbPLS import *
+from RG.LDA import *
+
+
+from RG.colorspectra import *
+
+from RG.SNV import *
+from RG.autoscale import *
+
+
+
+
+
+
+
