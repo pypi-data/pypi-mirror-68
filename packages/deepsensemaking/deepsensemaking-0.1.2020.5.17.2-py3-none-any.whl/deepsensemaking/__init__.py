04ed38c6eec6e009f5163b2adbdf7292a5db081a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:02:46 +0000

    Add package deepsensemaking/0.1.2020.5.17.2/deepsensemaking-0.1.2020.5.17.2-py3-none-any.whl

---
+++ packages/deepsensemaking/deepsensemaking-0.1.2020.5.17.2-py3-none-any.whl/deepsensemaking/__init__.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*
+
+"""
+deepsensemaking (dsm)
+
+
+"""
+
+from deepsensemaking.info import info
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    pass
---
+++ packages/deepsensemaking/deepsensemaking-0.1.2020.5.17.2-py3-none-any.whl/deepsensemaking/info/info.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*
+
+"""testing module """
+
+info = "deepsensemaking"
