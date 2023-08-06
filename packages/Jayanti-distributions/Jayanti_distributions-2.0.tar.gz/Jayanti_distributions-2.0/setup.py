0db6aedf6512497ed33e57a24cd5e467c7651c26
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:18 +0000

    Add package Jayanti-distributions/2.0/Jayanti_distributions-2.0.tar.gz

---
+++ packages/Jayanti-distributions/Jayanti_distributions-2.0.tar.gz/Jayanti_distributions-2.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='Jayanti_distributions',
+      version='2.0',
+      description='Gaussian distributions and Binomial Distributions',
+      packages=['Jayanti_distributions'],
+      author='Jayanti Vishnoi',
+      author_email='hasbrovish95@gmail.com',
+      zip_safe=False)
