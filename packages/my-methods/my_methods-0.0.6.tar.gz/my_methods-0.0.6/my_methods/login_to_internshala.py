36912b6b207e30cbf19507e0eb3be3da4960c2d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:09:43 +0000

    Add package my-methods/0.0.6/my_methods-0.0.6.tar.gz

---
+++ packages/my-methods/my_methods-0.0.6.tar.gz/my_methods-0.0.6/my_methods/login_to_internshala.py
@@ -0,0 +1,11 @@
+def login_to_internshala(browser):
+  from getpass import getpass
+  browser.get('https:/www.internshala.com')
+  button = browser.find_element_by_xpath("//button[@data-target = '#login-modal' and @data-toggle = 'modal']")
+  button.click()
+  email = getpass()
+  browser.find_element_by_id('modal_email').send_keys(email)
+  password = getpass()
+  browser.find_element_by_id('modal_password').send_keys(password)
+  browser.find_element_by_id('modal_login_submit').click()
+  return browser
