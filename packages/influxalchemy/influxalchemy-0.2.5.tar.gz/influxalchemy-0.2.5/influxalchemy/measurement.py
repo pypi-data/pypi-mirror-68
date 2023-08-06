1d64620a3a5aa64ec08280b5d57810e1dc31f2c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:01:40 +0000

    Add package influxalchemy/0.2.5/influxalchemy-0.2.5.tar.gz

---
+++ packages/influxalchemy/influxalchemy-0.2.5.tar.gz/influxalchemy-0.2.5/influxalchemy/measurement.py
@@ -0,0 +1,13 @@
+""" InfluxDB Measurement. """
+import six
+from . import meta
+
+
+# pylint: disable=too-few-public-methods
+class Measurement(six.with_metaclass(meta.MetaMeasurement)):
+    """ InfluxDB Measurement. """
+    # pylint: disable=no-init
+    @classmethod
+    def new(cls, name):
+        """ Generate new Measurement class. """
+        return type(name, (cls,), {"__measurement__": name})
