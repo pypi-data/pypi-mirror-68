774ee494f2384394bb47a505a5fb02efc7a8d232
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:53:32 +0000

    Add package wilight/0.0.72/wilight-0.0.72.tar.gz

---
+++ packages/wilight/wilight-0.0.72.tar.gz/wilight-0.0.72/setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+version = '0.0.72'
+
+setuptools.setup(
+    name="wilight",
+    version=version,
+    author="Leonardo Figueiro",
+    author_email="leoagfig@gmail.com",
+    description="Python client for WiLight",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/leofig-rj/python-wilight',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
