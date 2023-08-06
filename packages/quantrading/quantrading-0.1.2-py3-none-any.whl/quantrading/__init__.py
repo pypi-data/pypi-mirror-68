9f6f89ea450d05d504104cf19a086bdd1e0e1def
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:23:48 +0000

    Add package quantrading/0.1.2/quantrading-0.1.2-py3-none-any.whl

---
+++ packages/quantrading/quantrading-0.1.2-py3-none-any.whl/quantrading/__init__.py
@@ -0,0 +1,11 @@
+from time_series_utils import *
+from performance_utils import *
+import pandas as pd
+
+
+def generate_leverage_index(index_series: pd.Series, leverage: int, start_value=100) -> pd.Series:
+    pct_change = index_series.pct_change().mul(leverage)
+    new_index = (1 + pct_change).cumprod()
+    new_index.iloc[0] = 1
+    new_index = new_index * start_value
+    return new_index
