5fc9e4c594950ecccc2b8be2f9519e4bbae3fa6a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:03:53 +0000

    Add package piccolo/0.10.5/piccolo-0.10.5-py3-none-any.whl

---
+++ packages/piccolo/piccolo-0.10.5-py3-none-any.whl/tests/migrations/auto/test_migration_manager.py
@@ -0,0 +1,61 @@
+import asyncio
+
+from piccolo.apps.migrations.auto import MigrationManager
+
+from tests.base import DBTestCase
+
+
+class TestMigrationManager(DBTestCase):
+    def test_rename_column(self):
+        """
+        Test running a MigrationManager which contains a column rename
+        operation.
+        """
+        self.insert_row()
+
+        manager = MigrationManager()
+        manager.rename_column(
+            table_class_name="Band",
+            tablename="band",
+            old_column_name="name",
+            new_column_name="title",
+        )
+        asyncio.run(manager.run())
+
+        response = self.run_sync("SELECT * FROM band;")
+        self.assertTrue("title" in response[0].keys())
+        self.assertTrue("name" not in response[0].keys())
+
+    def test_raw_function(self):
+        """
+        Test adding raw functions to a MigrationManager.
+        """
+
+        class HasRun(Exception):
+            pass
+
+        def run():
+            raise HasRun("I was run!")
+
+        manager = MigrationManager()
+        manager.add_raw(run)
+
+        with self.assertRaises(HasRun):
+            asyncio.run(manager.run())
+
+    def test_raw_coroutine(self):
+        """
+        Test adding raw coroutines to a MigrationManager.
+        """
+
+        class HasRun(Exception):
+            pass
+
+        async def run():
+            raise HasRun("I was run!")
+
+        manager = MigrationManager()
+        manager.add_raw(run)
+
+        with self.assertRaises(HasRun):
+            asyncio.run(manager.run())
