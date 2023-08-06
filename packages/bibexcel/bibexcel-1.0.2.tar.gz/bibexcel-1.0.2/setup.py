0df90434667605ea15d10e5ab03abff9ce1dd16b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:26:44 +0000

    Add package bibexcel/1.0.2/bibexcel-1.0.2.tar.gz

---
+++ packages/bibexcel/bibexcel-1.0.2.tar.gz/bibexcel-1.0.2/setup.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='bibexcel',
+    version='1.0.2',
+    description='A python executable to get JSON and Excel data from a bibexcel text file',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='http://github.com/storopoli/bibexcel',
+    author='Jose Eduardo Storopoli',
+    author_email='thestoropoli@gmail.com',
+    license='MIT',
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'pandas',
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    zip_safe=False)
