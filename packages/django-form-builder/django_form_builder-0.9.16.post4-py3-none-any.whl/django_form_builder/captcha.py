bcb870d0386ee00aad6427f1069e8109f77a21f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:07:20 +0000

    Add package django-form-builder/0.9.16.post4/django_form_builder-0.9.16.post4-py3-none-any.whl

---
+++ packages/django-form-builder/django_form_builder-0.9.16.post4-py3-none-any.whl/django_form_builder/captcha.py
@@ -0,0 +1,36 @@
+import os
+import random
+import string
+
+from captcha.audio import AudioCaptcha
+from captcha.image import ImageCaptcha
+
+from django.conf import settings
+from . exceptions import AudioCaptchaLangPackNotFound
+
+
+def get_captcha(text=None, lang='en'):
+    fonts = getattr(settings, 'CAPTCHA_FONTS', None)
+    length = getattr(settings, 'CAPTCHA_LENGTH', 5)
+    voicedir = getattr(settings, 'CAPTCHA_VOICEDIR',
+                       os.path.join(os.path.abspath(os.path.dirname(__file__)),
+                                    'data', 'audio_captcha', lang))
+    # if audio for that language is not available
+    # fallback to default english
+    if not os.path.exists(voicedir):
+        voicedir = getattr(settings, 'CAPTCHA_VOICEDIR',
+                       os.path.join(os.path.abspath(os.path.dirname(__file__)),
+                                    'data', 'audio_captcha', 'en'))
+    image = ImageCaptcha(fonts=fonts)
+
+    try:
+        audio = AudioCaptcha(voicedir=voicedir)
+    except IndexError as e:
+        raise AudioCaptchaLangPackNotFound()
+
+    text = text or ''.join([random.choice(string.ascii_letters+string.hexdigits)
+                            for i in range(length)])
+
+    data_image = image.generate(text)
+    data_audio = audio.generate(text)
+    return data_image, data_audio
