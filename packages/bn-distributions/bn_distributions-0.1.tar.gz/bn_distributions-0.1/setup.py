f67b328bc7af1bacd6e0b98cf90582a481f01e3c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:13:35 +0000

    Add package bn-distributions/0.1/bn_distributions-0.1.tar.gz

---
+++ packages/bn-distributions/bn_distributions-0.1.tar.gz/bn_distributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='bn_distributions',
+      version='0.1',
+      description='Gaussian, General and Binomial distributions',
+      packages=['distributions'],
+      author = 'Gazal Gupta',
+      author_email = 'gazalgupta30@gmail.com',
+      zip_safe=False)
