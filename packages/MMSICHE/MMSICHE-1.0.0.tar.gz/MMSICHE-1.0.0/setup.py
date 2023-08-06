17c82bf92717abc9653359992085f56cd60f4768
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:03:37 +0000

    Add package MMSICHE/1.0.0/MMSICHE-1.0.0.tar.gz

---
+++ packages/MMSICHE/MMSICHE-1.0.0.tar.gz/MMSICHE-1.0.0/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup
+
+# reading long description from file
+with open('DESCRIPTION.txt') as file:
+    long_description = file.read()
+
+
+# specify requirements of your package here
+REQUIREMENTS = ['cv2', 'numpy', 'collections']
+
+# some more details
+CLASSIFIERS = [
+    "Programming Language :: Python :: 3",
+    "License :: OSI Approved :: MIT License",
+    "Operating System :: OS Independent",
+    ]
+
+# calling the setup function 
+setup(name='MMSICHE',
+      version='1.0.0',
+      description='Library that implements MMSICHE algorithm',
+      long_description=long_description,
+      url='https://github.com/abhinavbajpai2012/MMSICHE',
+      author='Abhinav Bajpai',
+      author_email='abhinavbajpai2012@gmail.com',
+      license='MIT',
+      packages=['MMSICHE'],
+      classifiers=CLASSIFIERS,
+      install_requires=REQUIREMENTS,
+      keywords='Image Processing - MMSICHE'
+      )
