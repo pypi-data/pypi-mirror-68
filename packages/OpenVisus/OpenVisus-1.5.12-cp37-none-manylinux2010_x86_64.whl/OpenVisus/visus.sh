83b7231af693044c7c289be0ded4c37bf69e6947
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:51 +0000

    Add package OpenVisus/1.5.12/OpenVisus-1.5.12-cp37-none-manylinux2010_x86_64.whl

---
+++ packages/OpenVisus/OpenVisus-1.5.12-cp37-none-manylinux2010_x86_64.whl/OpenVisus/visus.sh
@@ -0,0 +1,6 @@
+
+#!/bin/bash
+cd $(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+export PYTHONPATH=/root/OpenVisus/build/Release/OpenVisus:/usr/local/lib/python37.zip:/usr/local/lib/python3.7:/usr/local/lib/python3.7/lib-dynload:/usr/local/lib/python3.7/site-packages
+export LD_LIBRARY_PATH=/usr/local/lib:${LD_LIBRARY_PATH}
+bin/visus "$@"
