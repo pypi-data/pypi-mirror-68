2df11b3596e42a742f0ad7ffa190aacd2e7ac52a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:00:49 +0000

    Add package python-dependency-injection/0.0.1/python_dependency_injection-0.0.1-py3-none-any.whl

---
+++ packages/python-dependency-injection/python_dependency_injection-0.0.1-py3-none-any.whl/dependency_injection/test_dependency_injection/injected_class1.py
@@ -0,0 +1,8 @@
+class InjectedClass:
+
+    def __init__(self):
+        self.message: str = ' from InjectedClass1'
+        print(f'InjectedClass1 created - id=[{id(self)}]')
+
+    def foo(self, str1: str) -> str:
+        return str1 + self.message
