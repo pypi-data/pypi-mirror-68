b354520189e86adccccbd9e94a2448ffaeed1177
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:19 +0000

    Add package gocli/0.9.7/gocli-0.9.7.tar.gz

---
+++ packages/gocli/gocli-0.9.7.tar.gz/gocli-0.9.7/src/govcf/calculate_vaf/__init__.py
@@ -0,0 +1,21 @@
+# need to import all of the mappers so they get registered.
+
+from . import ao_ro_dp
+from . import ad_rd_dp
+from . import brli
+from . import clcbio
+from . import pheo
+from . import strelka
+from . import usc
+from .base import find
+
+__all__ = (
+    "strelka",
+    "pheo",
+    "clcbio",
+    "ao_ro_dp",
+    "ad_rd_dp",
+    "brli",
+    "usc",
+    "find",
+)
