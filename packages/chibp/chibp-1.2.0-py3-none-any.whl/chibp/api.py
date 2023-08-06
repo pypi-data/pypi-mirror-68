0c64140773ae5588400346cf8ec858c75c435b0e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:51:35 +0000

    Add package chibp/1.2.0/chibp-1.2.0-py3-none-any.whl

---
+++ packages/chibp/chibp-1.2.0-py3-none-any.whl/chibp/api.py
@@ -0,0 +1,54 @@
+import requests
+import urllib.parse
+
+def get(email, key):
+    if isinstance(email, str):
+        return requests.get(
+            f"https://haveibeenpwned.com/api/v3/breachedaccount/{urllib.parse.quote(email)}",
+            headers={"hibp-api-key": key}  
+        )
+    
+    if isinstance(email, list):
+        result = {}
+        for e in email:
+            r = requests.get(
+                f"https://haveibeenpwned.com/api/v3/breachedaccount/{urllib.parse.quote(e)}",
+                headers={"hibp-api-key": key}  
+            )
+            result[e] = r
+        return result
+    
+def get_full(email, key):
+    if isinstance(email, str):
+        return requests.get(
+            f"https://haveibeenpwned.com/api/v3/breachedaccount/{urllib.parse.quote(email)}?truncateResponse=false",
+             headers={"hibp-api-key": key}
+        )
+
+    if isinstance(email, list):
+        result = {}
+        for e in email:
+            r = requests.get(
+                f"https://haveibeenpwned.com/api/v3/breachedaccount/{urllib.parse.quote(e)}?truncateResponse=false",
+                headers={"hibp-api-key": key}
+            )
+            result[e] = r
+        return result
+
+def get_breaches(key):
+    return requests.get(
+        f"https://haveibeenpwned.com/api/v3/breaches",
+        headers={"hibp-api-key": key}
+    )
+
+def get_breach(site, key):
+    return requests.get(
+        f"https://haveibeenpwned.com/api/v3/breach/{site}",
+        headers={"hibp-api-key": key}
+    )
+
+def get_pastes(email, key):
+    return requests.get(
+        f"https://haveibeenpwned.com/api/v3/pasteaccount/{urllib.parse.quote(email)}",
+        headers={"hibp-api-key": key}
+    )
