c4c7fe7bb79d8013766dc551ceb7e0854ba941e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:25:48 +0000

    Add package iscc-cli/0.9.10/iscc_cli-0.9.10-py3-none-any.whl

---
+++ packages/iscc-cli/iscc_cli-0.9.10-py3-none-any.whl/iscc_cli/__init__.py
@@ -0,0 +1,18 @@
+import iscc_cli.monkeys
+import os
+import click
+
+
+__version__ = "0.9.10"
+APP_NAME = "iscc-cli"
+APP_DIR = click.get_app_dir(APP_NAME, roaming=False)
+os.makedirs(iscc_cli.APP_DIR, exist_ok=True)
+os.environ["TIKA_PATH"] = APP_DIR
+os.environ["TIKA_LOG_PATH"] = APP_DIR
+os.environ["TIKA_STARTUP_MAX_RETRY"] = "8"
+os.environ["LOGURU_AUTOINIT"] = "False"
+
+
+from iscc_cli.tika import tika
+
+tika.log.disabled = True
