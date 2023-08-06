6d9fc1113f841f1d0f817e77faf43963862259e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:10:48 +0000

    Add package syntext/2.4.0/syntext-2.4.0.tar.gz

---
+++ packages/syntext/syntext-2.4.0.tar.gz/syntext-2.4.0/syntext/__init__.py
@@ -0,0 +1,20 @@
+# ---------------------------------------------------------
+# Syntext: a lightweight, markdownish markup language.
+#
+# Author: Darren Mulholland <dmulholl@tcd.ie>
+# License: Public Domain
+# ---------------------------------------------------------
+
+# Package version number.
+__version__ = "2.4.0"
+
+from .interface import render
+from .interface import main
+from .utils import Error
+
+from . import tags
+from . import nodes
+from . import parsers
+from . import escapes
+from . import utils
+from . import shorthand
