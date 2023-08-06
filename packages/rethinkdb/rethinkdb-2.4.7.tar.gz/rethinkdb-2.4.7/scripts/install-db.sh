2ef4aad79d4813c9bc85a433aac1a92bca5d669a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:25:43 +0000

    Add package rethinkdb/2.4.7/rethinkdb-2.4.7.tar.gz

---
+++ packages/rethinkdb/rethinkdb-2.4.7.tar.gz/rethinkdb-2.4.7/scripts/install-db.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -e
+set -u
+
+export DISTRIB_CODENAME=$(lsb_release -sc)
+
+echo "deb https://download.rethinkdb.com/apt $DISTRIB_CODENAME main" | sudo tee /etc/apt/sources.list.d/rethinkdb.list
+wget -qO- https://download.rethinkdb.com/apt/pubkey.gpg | sudo apt-key add -
+
+sudo apt-get update --option Acquire::Retries=100 --option Acquire::http::Timeout="300"
+sudo apt-get install -y --option Acquire::Retries=100 --option Acquire::http::Timeout="300" rethinkdb
