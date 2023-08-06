4ebd2b5dd3369192b69f5523b17b45b0b1e6b970
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:11:05 +0000

    Add package metawards/0.12.0/metawards-0.12.0.tar.gz

---
+++ packages/metawards/metawards-0.12.0.tar.gz/metawards-0.12.0/src/metawards/app/__init__.py
@@ -0,0 +1,19 @@
+"""
+.. currentmodule:: metawards.app
+
+Functions
+=========
+
+.. autosummary::
+    :toctree: generated/
+
+    get_parallel_scheme
+    parse_args
+    get_hostfile
+    get_cores_per_node
+    get_threads_per_task
+    scoop_supervisor
+    mpi_supervisor
+    cli
+
+"""
