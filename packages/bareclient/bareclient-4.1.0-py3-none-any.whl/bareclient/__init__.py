526fba4e8f4b094d054e8d8aa0953716b0ec99b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:06:50 +0000

    Add package bareclient/4.1.0/bareclient-4.1.0-py3-none-any.whl

---
+++ packages/bareclient/bareclient-4.1.0-py3-none-any.whl/bareclient/__init__.py
@@ -0,0 +1,27 @@
+"""Exports from bareclient"""
+
+from .client import HttpClient
+from .session import HttpSession
+from .helpers import (
+    request,
+    get,
+    get_text,
+    get_json,
+    post,
+    post_text,
+    post_json
+)
+from .acgi import create_ssl_context_with_cert_chain
+
+__all__ = [
+    'HttpClient',
+    'HttpSession',
+    'request',
+    'get',
+    'get_text',
+    'get_json',
+    'post',
+    'post_text',
+    'post_json',
+    'create_ssl_context_with_cert_chain'
+]
