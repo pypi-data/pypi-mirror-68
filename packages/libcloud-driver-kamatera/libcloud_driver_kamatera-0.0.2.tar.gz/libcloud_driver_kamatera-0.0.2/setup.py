a1fc84c67144186d4899d2a9608b4d4827257bcb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:24:40 +0000

    Add package libcloud-driver-kamatera/0.0.2/libcloud_driver_kamatera-0.0.2.tar.gz

---
+++ packages/libcloud-driver-kamatera/libcloud_driver_kamatera-0.0.2.tar.gz/libcloud_driver_kamatera-0.0.2/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+
+setup(
+    name="libcloud_driver_kamatera",
+    version="0.0.2",
+    packages=find_packages(exclude=['tests',]),
+    install_requires=["apache-libcloud>=2.8.2"],
+    description='Apache Libcloud driver for managing Kamatera compute resources',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author='Kamatera',
+    url='https://github.com/Kamatera/libcloud-driver-kamatera',
+    license='MIT',
+)
