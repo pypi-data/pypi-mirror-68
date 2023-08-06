903b12fc53b365804ddebabf08572e5c85fcaa14
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:11:19 +0000

    Add package mcresources/1.2.1/mcresources-1.2.1.tar.gz

---
+++ packages/mcresources/mcresources-1.2.1.tar.gz/mcresources-1.2.1/mcresources/__init__.py
@@ -0,0 +1,7 @@
+#  Part of mcresources by Alex O'Neill
+#  Work under copyright. Licensed under MIT
+#  For more information see the project LICENSE file
+
+from mcresources.loot_tables import LootTables
+from mcresources.resource_manager import ResourceManager
+from mcresources.utils import clean_generated_resources
