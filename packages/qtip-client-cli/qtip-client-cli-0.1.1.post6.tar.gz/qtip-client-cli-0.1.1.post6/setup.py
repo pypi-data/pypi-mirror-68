4bbcb8ad0b7f797292e2b308ce036210489cd68e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:11:26 +0000

    Add package qtip-client-cli/0.1.1.post6/qtip-client-cli-0.1.1.post6.tar.gz

---
+++ packages/qtip-client-cli/qtip-client-cli-0.1.1.post6.tar.gz/qtip-client-cli-0.1.1.post6/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+print(find_packages())
+setup(
+    name='qtip-client-cli',
+    packages=find_packages(),
+    author='Shubham Naik',
+    author_email='shub@shub.club',
+    url='https://github.com/4shub/qtip',
+    version='0.1.1-rev6',
+    install_requires=[
+        'flask',
+        'requests'
+    ],
+    entry_points= {
+        "console_scripts": [
+            "qtip = qtip:main",
+        ]
+    },
+    setup_requires=[
+        'setuptools>=41.0.1',
+    ]
+)
