599cc7e08f21fa5c1e7f3771635fa73afccd7f42
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:25:15 +0000

    Add package orbis-plugin-aggregation-spotlight/2.2.4/orbis_plugin_aggregation_spotlight-2.2.4-py3-none-any.whl

---
+++ packages/orbis-plugin-aggregation-spotlight/orbis_plugin_aggregation_spotlight-2.2.4-py3-none-any.whl/orbis_plugin_aggregation_spotlight/__init__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+__name__ = "orbis_plugin_aggregation_spotlight"
+__version__ = "2.2.4"
+__author__ = "fabod"
+__year__ = "2020"
+__description__ = "The spotlight aggregation plugin for Orbis"
+__license__ = "GPL2"
+__min_python_version__ = "3.6"
+__requirements_file__ = "requirements.txt"
+__url__ = "https://github.com/orbis-eval/orbis_plugin_aggregation_spotlight"
+__type__ = "plugin"
+
+from .main import Main
