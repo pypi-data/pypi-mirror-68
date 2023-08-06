d975333298073e3f1c131d2510211c1d732b44fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:34 +0000

    Add package msticpy/0.5.0/msticpy-0.5.0-py3-none-any.whl

---
+++ packages/msticpy/msticpy-0.5.0-py3-none-any.whl/msticpy/__init__.py
@@ -0,0 +1,10 @@
+"""Placeholder for top level folder."""
+# flake8: noqa: F403
+from . import sectools
+from . import nbtools
+from . import data
+
+from ._version import VERSION
+from .common import pkg_config as settings
+
+__version__ = VERSION
