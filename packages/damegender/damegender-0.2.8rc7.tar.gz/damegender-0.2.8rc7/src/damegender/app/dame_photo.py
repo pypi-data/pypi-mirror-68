730a69f13bbf3a94a4fa8604f5f6c3040db7cfb3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:25:43 +0000

    Add package damegender/0.2.8rc7/damegender-0.2.8rc7.tar.gz

---
+++ packages/damegender/damegender-0.2.8rc7.tar.gz/damegender-0.2.8rc7/src/damegender/app/dame_photo.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+# Copyright (C) 2030  David Arroyo Menéndez
+
+# Author: David Arroyo Menéndez <davidam@gnu.org>
+# Maintainer: David Arroyo Menéndez <davidam@gnu.org>
+
+# This file is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3, or (at your option)
+# any later version.
+
+# This file is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with Damegender; see the file LICENSE.  If not, write to
+# the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+# Boston, MA 02110-1301 USA,
+
+
+import unidecode
+import unicodedata
+import re
+import os
+import csv
+from app.dame_photo import DamePhoto
+
+class DamePhoto():
+    
+    def highlightFace(net, frame, conf_threshold=0.7):
+        frameOpencvDnn=frame.copy()
+        frameHeight=frameOpencvDnn.shape[0]
+        frameWidth=frameOpencvDnn.shape[1]
+        blob=cv2.dnn.blobFromImage(frameOpencvDnn, 1.0, (300, 300), [104, 117, 123], True, False)
+
+        net.setInput(blob)
+        detections=net.forward()
+        faceBoxes=[]
+        for i in range(detections.shape[2]):
+            confidence=detections[0,0,i,2]
+            if confidence>conf_threshold:
+                x1=int(detections[0,0,i,3]*frameWidth)
+                y1=int(detections[0,0,i,4]*frameHeight)
+                x2=int(detections[0,0,i,5]*frameWidth)
+                y2=int(detections[0,0,i,6]*frameHeight)
+                faceBoxes.append([x1,y1,x2,y2])
+                cv2.rectangle(frameOpencvDnn, (x1,y1), (x2,y2), (0,255,0), int(round(frameHeight/150)), 8)
+    return frameOpencvDnn,faceBoxes
+
+    
