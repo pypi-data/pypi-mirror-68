5f176bf1b00c3ee00a77e32e6458494961bbf1bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:22:28 +0000

    Add package mavsdk/0.7.0/mavsdk-0.7.0-py3-none-any.whl

---
+++ packages/mavsdk/mavsdk-0.7.0-py3-none-any.whl/mavsdk/generated/__init__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+from .action import *
+from .calibration import *
+from .geofence import *
+from .gimbal import *
+from .camera import *
+from .core import *
+from .info import *
+from .mission import *
+from .mocap import *
+from .offboard import *
+from .param import *
+from .shell import *
+from .telemetry import *
