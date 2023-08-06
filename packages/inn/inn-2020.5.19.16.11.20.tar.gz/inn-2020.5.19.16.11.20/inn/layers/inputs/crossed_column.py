94c9718c9b5598b437dfb1793aa70643cda77d37
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:24:39 +0000

    Add package inn/2020.5.19.16.11.20/inn-2020.5.19.16.11.20.tar.gz

---
+++ packages/inn/inn-2020.5.19.16.11.20.tar.gz/inn-2020.5.19.16.11.20/inn/layers/inputs/crossed_column.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Project      : DeepNN.
+# @File         : crossed_column
+# @Time         : 2020/4/13 4:53 下午
+# @Author       : yuanjie
+# @Email        : yuanjie@xiaomi.com
+# @Software     : PyCharm
+# @Description  :
