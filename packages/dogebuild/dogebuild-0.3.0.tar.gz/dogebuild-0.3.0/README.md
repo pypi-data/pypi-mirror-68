4d66d6e4529f21bbbf41c7fb2a3c3f4534f02d93
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:56:41 +0000

    Add package dogebuild/0.3.0/dogebuild-0.3.0.tar.gz

---
+++ packages/dogebuild/dogebuild-0.3.0.tar.gz/dogebuild-0.3.0/README.md
@@ -0,0 +1,41 @@
+# dogebuild
+
+[![Build Status](https://travis-ci.org/dogebuild/dogebuild.svg?branch=master)](https://travis-ci.org/dogebuild/dogebuild)
+[![PyPI version](https://badge.fury.io/py/dogebuild.svg)](https://badge.fury.io/py/dogebuild)
+[![Documentation Status](https://readthedocs.org/projects/dogebuild/badge/?version=latest)](https://dogebuild.readthedocs.io/en/latest/?badge=latest)
+
+General purpose build manager mainly designed to build C++ projects which
+uses directed acyclic graph (DAG) to manage tasks dependencies.
+
+## Install
+
+```sh
+pip install dogebuild
+```
+
+## How to use
+
+Dogebuild uses `dogefile.py` to describe project structure and task DAG.
+An example of `dogefile.py`:
+
+```python
+from dogebuild_c.c_plugin import CPlugin, BinaryType
+from dogebuild.dependencies import dependencies, folder
+
+dependencies(
+    folder('../dependency')
+)
+
+CPlugin(
+    type=BinaryType.EXECUTABLE,
+    out='hello',
+    src_dir='.',
+    src=[
+        'main.c',
+    ],
+)
+```
+
+To run task simply pass task name as argument to doge script.
+`doge build` will run `build` task and all dependencies.
+You can also run multiple tasks: `doge clean build`.
---
+++ packages/dogebuild/dogebuild-0.3.0.tar.gz/dogebuild-0.3.0/dogebuild/common.py
@@ -0,0 +1,48 @@
+from typing import List, Dict
+import os
+import glob
+from itertools import chain
+
+
+DOGE_FILE = 'dogefile.py'
+
+
+def files(base_dir: str, include: List[str], exclude: List[str] = None):
+    if not exclude:
+        exclude = []
+
+    def expand_glob(base_dir, g):
+        return glob.iglob(os.path.join(base_dir, g), recursive=True)
+
+    return set(chain.from_iterable(map(lambda p: expand_glob(base_dir, p), include))) - set(chain.from_iterable(map(lambda p: expand_glob(base_dir, p), exclude)))
+
+
+def sanitize_name(name: str):
+    return name.replace('_', '-')
+
+
+def merge_dicts(*dicts: Dict[str, List]):
+    result = {}
+    for d in dicts:
+        for k, v in d.items():
+            if k not in result:
+                result[k] = []
+            result[k].extend(v)
+    return result
+
+
+class GlobalsContext:
+    def __init__(self, context: Dict):
+        self.context = context
+        self.saved = {}
+
+    def __enter__(self):
+        self.saved = globals()
+        globals().clear()
+        for k, v in self.context.items():
+            globals()[k] = v
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        globals().clear()
+        for k, v in self.saved.items():
+            globals()[k] = v
