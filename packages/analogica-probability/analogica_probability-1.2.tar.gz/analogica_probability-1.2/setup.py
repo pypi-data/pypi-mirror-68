8edcb06812d13659dff59d0bf7e9f0571eb3848d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:36:45 +0000

    Add package analogica-probability/1.2/analogica_probability-1.2.tar.gz

---
+++ packages/analogica-probability/analogica_probability-1.2.tar.gz/analogica_probability-1.2/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='analogica_probability',
+      version='1.2',
+      description='Gaussian distributions and Binomial Distribution',
+      packages=['analogica_probability'],
+      author = 'Kartikay Sharma',
+      author_email='skartikay15@gmail.com',
+      zip_safe=False)
