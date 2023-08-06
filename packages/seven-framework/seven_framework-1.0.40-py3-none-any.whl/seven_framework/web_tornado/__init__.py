d46263c72025e4cf5da964377deb76074acbaae7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:07:13 +0000

    Add package seven-framework/1.0.40/seven_framework-1.0.40-py3-none-any.whl

---
+++ packages/seven-framework/seven_framework-1.0.40-py3-none-any.whl/seven_framework/web_tornado/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+@Author: ChenXiaolei
+@Date: 2020-04-23 18:56:41
+@LastEditTime: 2020-05-12 17:49:30
+@LastEditors: ChenXiaolei
+@Description: 
+"""
+
+from . import base_handler
+from .base_handler import *
