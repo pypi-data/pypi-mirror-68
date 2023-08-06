8e77645826784f919628858cd698db6e491ffd9c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:24:23 +0000

    Add package mayank-prob/0.1/mayank_prob-0.1.tar.gz

---
+++ packages/mayank-prob/mayank_prob-0.1.tar.gz/mayank_prob-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='mayank_prob',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions'],
+      zip_safe=False)
