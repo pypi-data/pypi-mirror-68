284e5ef9d26bb7a417d2991a96d68aaf599441d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:12:50 +0000

    Add package actions-pipeline-example/0.1/actions-pipeline-example-0.1.tar.gz

---
+++ packages/actions-pipeline-example/actions-pipeline-example-0.1.tar.gz/actions-pipeline-example-0.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name = "actions-pipeline-example",
+    version = "0.1",
+    description = "Dummy project to test GitHub actions",
+    url = "https://github.com/cognitedata/actions-pipeline-example",
+    author = "Carlos",
+    author_email = "carlos.pereira@cognite.com",
+    packages = ["hello"],
+    entry_points =  {
+        "console_scripts": ["say-hello=hello.app:main"]
+    }
+)
