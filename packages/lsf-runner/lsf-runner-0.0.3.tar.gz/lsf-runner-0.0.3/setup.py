13eeec8608a9ae14c11c22ae21146f30a5a89114
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:27:42 +0000

    Add package lsf-runner/0.0.3/lsf-runner-0.0.3.tar.gz

---
+++ packages/lsf-runner/lsf-runner-0.0.3.tar.gz/lsf-runner-0.0.3/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+    
+setup(
+    name="lsf-runner",
+    version="0.0.3",
+    author="Sebastian Curi",
+    author_email="sebascuri@gmail.com",
+    description="A package to run experiments on lsf or linux clusters.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/sebascuri/runner",
+    license="MIT",
+    packages=find_packages(exclude=['docs']),
+    classifiers=[
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Build Tools",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires=[],
+    extras_require={
+        'test': [
+            'pytest>=5.0,<5.1',
+            'flake8>=3.7.8,<3.8',
+            'pydocstyle==4.0.0',
+            'pytest_cov>=2.7,<3',
+            'mypy>=0.740',
+        ],
+    },
+)
