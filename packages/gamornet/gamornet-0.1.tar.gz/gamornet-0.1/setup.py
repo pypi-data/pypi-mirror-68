bbe4071f37acc73a4187022b73f66a4eafd47f05
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:12:26 +0000

    Add package gamornet/0.1/gamornet-0.1.tar.gz

---
+++ packages/gamornet/gamornet-0.1.tar.gz/gamornet-0.1/setup.py
@@ -0,0 +1,42 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+	long_description = fh.read()
+
+setuptools.setup(
+	name="gamornet", # Replace with your own username
+	version="0.1",
+	author="Aritra Ghosh",
+	author_email="aritraghsh09@gmail.com",
+	description="A CNN to classify galaxies morphologically",
+	long_description=long_description,
+	long_description_content_type="text/markdown",
+	keywords="astrophysics astronomy galaxies convolutional neural networks morphological analysis morphology sdss candels",
+	url="http://gamornet.ghosharitra.com/",
+	project_urls={
+		"Source Code": "https://github.com/aritraghsh09/GaMorNet",
+		"Documentation": "https://gamornet.readthedocs.io/",
+	},
+	classifiers=[
+		"Programming Language :: Python :: 3",
+		"Development Status :: 4 - Beta",
+		"Environment :: GPU :: NVIDIA CUDA :: 10.1",
+		"License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+		"Intended Audience :: Science/Research",
+		"Topic :: Scientific/Engineering :: Astronomy",
+		"Topic :: Scientific/Engineering :: Physics",
+	],
+	packages=setuptools.find_packages(),
+	python_requires='~=3.1',
+	install_requires=[
+		"tensorflow-gpu ~=1.12",
+		"tflearn ~=0.3",
+		"keras ~=2.2",
+		"pandas >=1.0",
+		"wget >=3.2",
+		"numpy >=1.16",
+		"astropy >=3.2",
+		"matplotlib >=3.1",
+	],
+)
+
