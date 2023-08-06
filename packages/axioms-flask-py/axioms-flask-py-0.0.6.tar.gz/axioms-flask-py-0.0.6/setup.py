de380c47939b5c0f5ede0dd2f7ee5daba373a3d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:05 +0000

    Add package axioms-flask-py/0.0.6/axioms-flask-py-0.0.6.tar.gz

---
+++ packages/axioms-flask-py/axioms-flask-py-0.0.6.tar.gz/axioms-flask-py-0.0.6/setup.py
@@ -0,0 +1,38 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="axioms-flask-py",
+    version="0.0.6",
+    author="Axioms",
+    author_email="info@axioms.io",
+    description="Flask SDK for Axioms",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/axioms-io/axioms-flask-py",
+    license='MIT',
+    classifiers=[
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={'': 'src'},
+    packages=setuptools.find_packages(where='src'),
+    python_requires='>=3.7',
+    keywords='axioms authentication authorization iam authz authn jwt openid oauth2',
+    project_urls={
+        'Documentation': 'https://github.com/axioms-io/axioms-flask-py',
+        'Source': 'https://github.com/axioms-io/axioms-flask-py',
+        'Tracker': 'https://github.com/axioms-io/axioms-flask-py/issues',
+    },
+    install_requires=[
+        'pyjwt',
+        'jwcrypto',
+        'python-box',
+        'Flask',
+        'Flask-DotEnv',
+        'six'
+    ]
+)
