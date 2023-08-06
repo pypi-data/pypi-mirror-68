583c6c6971e8aa4b99c4c784b0060007ff24a2be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:12:25 +0000

    Add package RunDeckAPI/0.0.2/RunDeckAPI-0.0.2.tar.gz

---
+++ packages/RunDeckAPI/RunDeckAPI-0.0.2.tar.gz/RunDeckAPI-0.0.2/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="RunDeckAPI",
+    version="0.0.2",
+    author="Rory Murdock",
+    author_email="rory@itmatic.com.au",
+    description="A Library for interacting with RunDeck",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/rorymurdock/RunDeckAPI",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+          'reqrest',
+    ],
+)
