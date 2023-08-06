bccf793be58886afc4bd5a3b3a49bc2bc1981d9d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:18:59 +0000

    Add package physics-tenpy/0.6.1/physics_tenpy-0.6.1-py3.7-linux-x86_64.egg

---
+++ packages/physics-tenpy/physics_tenpy-0.6.1-py3.7-linux-x86_64.egg/tenpy/_version.py
@@ -0,0 +1,10 @@
+# THIS FILE IS GENERATED FROM setup.py
+# thus, it contains the version during compilation
+# Copyright 2018-2020 TeNPy Developers, GNU GPLv3
+version = '0.6.1'
+short_version = 'v' + version
+released = True
+full_version = '0.6.1'
+git_revision = 'c70ffd9d76f12a395d1213d123e3e6f48bf95619'
+numpy_version = '1.18.4'
+cython_version = '0.29.17'
