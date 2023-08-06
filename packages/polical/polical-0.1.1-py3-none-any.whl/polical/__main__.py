b681ff312367ff050a1539dc7e990d014099c31b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:19:40 +0000

    Add package polical/0.1.1/polical-0.1.1-py3-none-any.whl

---
+++ packages/polical/polical-0.1.1-py3-none-any.whl/polical/__main__.py
@@ -0,0 +1,27 @@
+from polical import TareasCSVToBD
+from polical import SendTaskToTrello
+from polical import SimpleIcsToCSV
+from polical import configuration
+import sys
+from polical import Get_Trello_MoodleEPN_Keys
+
+
+def main(argv):
+    if len(argv) == 2:
+        argument = argv[1]
+        if argument == "--addUser":
+            Get_Trello_MoodleEPN_Keys.onboard(True)
+    else:
+        users = None
+        while(users == None):
+            users = configuration.load_config_file("polical.yaml")
+            if users == None:
+                Get_Trello_MoodleEPN_Keys.onboard(True)
+        for user in users.keys():
+            SimpleIcsToCSV.convertICStoCSV(users[user]['calendar_url'])
+            TareasCSVToBD.LoadCSVTasktoDB(user, users[user])
+            SendTaskToTrello.SendTaskToTrello(user, users[user])
+
+
+if __name__ == "__main__":
+    main(sys.argv)
