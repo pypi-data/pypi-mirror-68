5a6f93c33b2dff30d2ec8220896168bda428ea91
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:27:46 +0000

    Add package efax/0.2.3/efax-0.2.3.tar.gz

---
+++ packages/efax/efax-0.2.3.tar.gz/efax-0.2.3/efax/exponential.py
@@ -0,0 +1,30 @@
+import jax.numpy as jnp
+from ipromise import implements
+
+from .exponential_family import ExponentialFamily
+from .tensors import RealTensor
+
+__all__ = ['Exponential']
+
+
+class Exponential(ExponentialFamily):
+
+    def __init__(self) -> None:
+        super().__init__(num_parameters=1)
+
+    # Implemented methods -----------------------------------------------------
+    @implements(ExponentialFamily)
+    def log_normalizer(self, q: RealTensor) -> RealTensor:
+        return -jnp.log(-q[..., 0])
+
+    @implements(ExponentialFamily)
+    def nat_to_exp(self, q: RealTensor) -> RealTensor:
+        return -1.0 / q
+
+    @implements(ExponentialFamily)
+    def exp_to_nat(self, p: RealTensor) -> RealTensor:
+        return -1.0 / p
+
+    @implements(ExponentialFamily)
+    def sufficient_statistics(self, x: RealTensor) -> RealTensor:
+        return x
---
+++ packages/efax/efax-0.2.3.tar.gz/efax-0.2.3/efax/poisson.py
@@ -0,0 +1,42 @@
+import jax.numpy as jnp
+import jax.scipy.special as jss
+import numpy as np
+from ipromise import implements, overrides
+
+from .exponential_family import ExponentialFamily
+from .tensors import RealTensor
+
+__all__ = ['Poisson']
+
+
+class Poisson(ExponentialFamily):
+
+    def __init__(self) -> None:
+        super().__init__(num_parameters=1)
+
+    # Implemented methods -----------------------------------------------------
+    @implements(ExponentialFamily)
+    def log_normalizer(self, q: RealTensor) -> RealTensor:
+        # pylint: disable=no-self-use
+        return jnp.exp(q)[..., 0]
+
+    @implements(ExponentialFamily)
+    def nat_to_exp(self, q: RealTensor) -> RealTensor:
+        return jnp.exp(q)
+
+    @implements(ExponentialFamily)
+    def exp_to_nat(self, p: RealTensor) -> RealTensor:
+        return jnp.log(p)
+
+    @implements(ExponentialFamily)
+    def sufficient_statistics(self, x: RealTensor) -> RealTensor:
+        return x[..., np.newaxis]
+
+    # Overridden methods ------------------------------------------------------
+    @overrides(ExponentialFamily)
+    def carrier_measure(self, x: RealTensor) -> RealTensor:
+        return -jss.gammaln(x + 1)
+
+    @overrides(ExponentialFamily)
+    def expected_carrier_measure(self, p: RealTensor) -> RealTensor:
+        raise NotImplementedError
