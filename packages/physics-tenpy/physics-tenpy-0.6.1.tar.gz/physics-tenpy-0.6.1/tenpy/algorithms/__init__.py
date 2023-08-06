35b1d502342478c35088e07a2c0e7f88bf2ae1c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:18:59 +0000

    Add package physics-tenpy/0.6.1/physics-tenpy-0.6.1.tar.gz

---
+++ packages/physics-tenpy/physics-tenpy-0.6.1.tar.gz/physics-tenpy-0.6.1/tenpy/algorithms/__init__.py
@@ -0,0 +1,25 @@
+"""A collection of algorithms such as TEBD and DMRG.
+
+.. rubric:: Submodules
+
+.. autosummary::
+    :toctree: .
+
+    truncation
+    dmrg
+    mps_sweeps
+    tebd
+    tdvp
+    purification_tebd
+    network_contractor
+    exact_diag
+"""
+# Copyright 2018-2020 TeNPy Developers, GNU GPLv3
+
+from . import truncation, dmrg, mps_sweeps, tebd, tdvp, exact_diag, purification_tebd, \
+    network_contractor
+
+__all__ = [
+    "truncation", "dmrg", "mps_sweeps", "tebd", "tdvp", "exact_diag", "purification_tebd",
+    "network_contractor"
+]
