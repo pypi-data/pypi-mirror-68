1870a455a609addd24cdc3ed8a21c7d88d7e32b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:10:31 +0000

    Add package mmuda-distributions/1.1/mmuda_distributions-1.1.tar.gz

---
+++ packages/mmuda-distributions/mmuda_distributions-1.1.tar.gz/mmuda_distributions-1.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='mmuda_distributions',
+      version='1.1',
+      description='Gaussian and binomial distributions',
+      packages=['mmuda_distributions'],
+      author='Maximilian Müller',
+      download_url = 'https://github.com/muellermax/mmuda_distributions/archive/1.1.tar.gz',
+      url = 'https://github.com/muellermax/',
+      keywords = ['binomial', 'gaussian', 'distribution', 'udacity'],
+      zip_safe=False)
