ce7bf6837a89cdf7785c1fef0225e045f4e4399e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:34:17 +0000

    Add package soln-ml/1.0.2/soln-ml-1.0.2.tar.gz

---
+++ packages/soln-ml/soln-ml-1.0.2.tar.gz/soln-ml-1.0.2/setup.py
@@ -0,0 +1,28 @@
+import pathlib
+from setuptools import setup, find_packages
+
+with open('requirements.txt') as fp:
+    install_reqs = [r.rstrip() for r in fp.readlines()
+                    if not r.startswith('#') and not r.startswith('git+')]
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+setup(name='soln-ml',
+      version='1.0.2',
+      long_description=README,
+      long_description_content_type="text/markdown",
+      description='Soln-ML: Towards Self-Learning AutoML System.',
+      author='AutoML Researcher @ DAIM',
+      author_email='liyang.cs@pku.edu.cn',
+      url='https://github.com/thomas-young-2013/soln-ml',
+      keywords='AutoML,machine learning',
+      packages=find_packages(exclude=['docs', 'examples', 'test']),
+      license='LICENSE.txt',
+      test_suite='nose.collector',
+      python_requires='>=3.5.*',
+      include_package_data=True,
+      install_requires=install_reqs)
