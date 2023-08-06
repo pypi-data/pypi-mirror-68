fe7dcde24b0497f5ca8879acbcdb08416592e6e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:25:50 +0000

    Add package silicone/0.1.0rc3/silicone-0.1.0rc3.tar.gz

---
+++ packages/silicone/silicone-0.1.0rc3.tar.gz/silicone-0.1.0rc3/src/silicone/multiple_infillers/__init__.py
@@ -0,0 +1,13 @@
+"""
+Multiple infillers
+
+Multiple infillers provide easy-to-use infiller options for the most common use-cases.
+"""
+
+from .decompose_collection_with_time_dep_ratio import (  # noqa: F401
+    DecomposeCollectionTimeDepRatio,
+)
+from .infill_all_required_emissions_for_openscm import (  # noqa: F401
+    infill_all_required_variables,
+)
+from .infill_composite_values import infill_composite_values  # noqa: F401
