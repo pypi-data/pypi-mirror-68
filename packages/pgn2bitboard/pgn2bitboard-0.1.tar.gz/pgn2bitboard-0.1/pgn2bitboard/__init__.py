ec72d693992f533c45174e6b084439699d5adae0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:25:04 +0000

    Add package pgn2bitboard/0.1/pgn2bitboard-0.1.tar.gz

---
+++ packages/pgn2bitboard/pgn2bitboard-0.1.tar.gz/pgn2bitboard-0.1/pgn2bitboard/__init__.py
@@ -0,0 +1,5 @@
+from pgn2bitboard.main import choosePositions
+from pgn2bitboard.main import winner
+from pgn2bitboard.main import pgn2fen
+from pgn2bitboard.main import fen2bitboard
+from pgn2bitboard.main import pgn2bitboard
---
+++ packages/pgn2bitboard/pgn2bitboard-0.1.tar.gz/pgn2bitboard-0.1/setup.py
@@ -0,0 +1,35 @@
+from distutils.core import setup
+setup(
+    name='pgn2bitboard',         # How you named your package folder (MyLib)
+    packages=['pgn2bitboard'],   # Chose the same as "name"
+    version='0.1',      # Start with a small number and increase it with every change you make
+    # Chose a license from here: https://help.github.com/articles/licensing-a-repository
+    license='MIT',
+    # Give a short description about your library
+    description='This library is used to process pgn files that contain several chess games and convert them to bitboards and labels.',
+    author='Anirudh',                   # Type in your name
+    author_email='anirudhganwal06@gmail.com',      # Type in your E-Mail
+    # Provide either the link to your github or to your website
+    url='https://github.com/anirudhganwal06/pgn2bitboard',
+    # I explain this later on
+    download_url='https://github.com/anirudhganwal06/pgn2bitboard/archive/v0.1.tar.gz',
+    # Keywords that define your package best
+    keywords=['pgn', 'bitboard', 'chess', 'chess-engine'],
+    install_requires=[            # I get to this in a second
+        'numpy',
+        'chess'
+    ],
+    classifiers=[
+        # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+        'Development Status :: 3 - Alpha',
+        # Define that your audience are developers
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',   # Again, pick a license
+        # Specify which python versions that you want to support
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+)
