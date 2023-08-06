50bfb72a84dc18064b9870ce815ba26a186e541d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:29 +0000

    Add package production-request/0.6.0/production_request-0.6.0-py2.py3-none-any.whl

---
+++ packages/production-request/production_request-0.6.0-py2.py3-none-any.whl/production_request/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls import (
+    url,
+)
+
+from production_request.views import (
+    save_client_log,
+)
+
+production_request_url = url(r'^production-request', save_client_log)
