c1887aacf62435339fdc3a4fc7a527d7a5bece86
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:02:22 +0000

    Add package calc2tex/0.0.1/calc2tex-0.0.1.tar.gz

---
+++ packages/calc2tex/calc2tex-0.0.1.tar.gz/calc2tex-0.0.1/README.md
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May 12 16:02:40 2020
+
+@author: stefa
+"""
+
+
---
+++ packages/calc2tex/calc2tex-0.0.1.tar.gz/calc2tex-0.0.1/calc2tex/settings.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Apr 19 09:14:34 2020
+
+@author: stefa
+"""
+
+
+#TODO Konstanten und anpassbare Sachen hier auslagern einheitendatenbank
+
+
+language = {"DE": 
+                {"table": 
+                     {"header": "Eingangsgrößen", "var": "Variable", "val": "Wert", "unit": "Einheit"}
+                },
+            "EN": 
+                {"table": 
+                     {"header": "input values", "var": "variable", "val": "value", "unit": "unit"}
+                }
+            }
+
+accuracy = 3
+
+mult_sign = "\\cdot" #others: \times, \star, \ast
