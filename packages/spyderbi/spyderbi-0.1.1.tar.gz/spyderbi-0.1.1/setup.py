054d8f3563ef39f61fb6402f881558e7130fef9f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:19:47 +0000

    Add package spyderbi/0.1.1/spyderbi-0.1.1.tar.gz

---
+++ packages/spyderbi/spyderbi-0.1.1.tar.gz/spyderbi-0.1.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup,find_packages
+
+setup(
+	name="spyderbi",
+	version='0.1.1',
+	keywords='bilibili',
+	description='a library for who want to use bilibili to research,maybe in the near future,if you find any problem.you can send email to',
+	packages=find_packages(),
+)
