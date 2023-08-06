7fde3207318afb2bb3f3a7d3177579e77163c9a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:19:40 +0000

    Add package tablib/2.0.0/tablib-2.0.0.tar.gz

---
+++ packages/tablib/tablib-2.0.0.tar.gz/tablib-2.0.0/setup.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+
+from setuptools import find_packages, setup
+
+setup(
+    name='tablib',
+    use_scm_version=True,
+    setup_requires=['setuptools_scm'],
+    description='Format agnostic tabular data library (XLS, JSON, YAML, CSV)',
+    long_description=(
+        open('README.md').read() + '\n\n' + open('HISTORY.md').read()
+    ),
+    long_description_content_type="text/markdown",
+    author='Kenneth Reitz',
+    author_email='me@kennethreitz.org',
+    maintainer='Jazzband',
+    maintainer_email='roadies@jazzband.co',
+    url='https://tablib.readthedocs.io',
+    project_urls={
+        "Documentation": "https://tablib.readthedocs.io",
+        "Source": "https://github.com/jazzband/tablib",
+    },
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+    license='MIT',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ],
+    python_requires='>=3.5',
+    extras_require={
+        'all': ['markuppy', 'odfpy', 'openpyxl>=2.6.0', 'pandas', 'pyyaml', 'tabulate', 'xlrd', 'xlwt'],
+        'cli': ['tabulate'],
+        'html': ['markuppy'],
+        'ods': ['odfpy'],
+        'pandas': ['pandas'],
+        'xls': ['xlrd', 'xlwt'],
+        'xlsx': ['openpyxl>=2.6.0'],
+        'yaml': ['pyyaml'],
+    },
+)
