1fcb0926370f960fb42f48be2c9f0c968fd9277b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:12:53 +0000

    Add package habu/0.1.36/habu-0.1.36.tar.gz

---
+++ packages/habu/habu-0.1.36.tar.gz/habu-0.1.36/habu/cli/cmd_cert_crtsh.py
@@ -0,0 +1,85 @@
+#!/usr/bin/env python3
+
+import json
+import logging
+import os
+import os.path
+import sys
+
+from pathlib import Path
+
+import click
+import requests
+import requests_cache
+
+from habu.lib.dns import query_bulk
+
+@click.command()
+@click.argument('domain')
+@click.option('-c', 'no_cache', is_flag=True, default=False, help='Disable cache')
+@click.option('-n', 'no_validate', is_flag=True, default=False, help='Disable DNS subdomain validation')
+@click.option('-v', 'verbose', is_flag=True, default=False, help='Verbose output')
+def cmd_cert_crtsh(domain, no_cache, no_validate, verbose):
+    """Downloads the certificate transparency logs for a domain
+    and check with DNS queries if each subdomain exists.
+
+    Uses multithreading to improve the performance of the DNS queries.
+
+    Example:
+
+    \b
+    $ sudo habu.crtsh securetia.com
+    [
+        "karma.securetia.com.",
+        "www.securetia.com."
+    ]
+    """
+
+    if verbose:
+        logging.basicConfig(level=logging.INFO, format='%(message)s')
+
+    if not no_cache:
+        homedir = Path(os.path.expanduser('~'))
+        requests_cache.install_cache(str((homedir / '.habu_requests_cache')), expire_after=3600)
+
+    subdomains = set()
+
+    if verbose:
+        print("Downloading subdomain list from https://crt.sh ...", file=sys.stderr)
+
+    req = requests.get("https://crt.sh/?q=%.{d}&output=json".format(d=domain))
+
+    if req.status_code != 200:
+        print("[X] Information not available!")
+        exit(1)
+
+    json_data = json.loads(req.text)
+
+    for data in json_data:
+        name = data['name_value'].lower()
+        if '*' not in name:
+            subdomains.add(name)
+
+    subdomains = list(subdomains)
+
+    if no_validate:
+        print(json.dumps(sorted(subdomains), indent=4))
+        return True
+
+    if verbose:
+        print("Validating subdomains against DNS servers ...", file=sys.stderr)
+
+    answers = query_bulk(subdomains)
+
+    validated = []
+
+    for answer in answers:
+        if answer:
+            validated.append(str(answer.qname))
+
+    print(json.dumps(sorted(validated), indent=4))
+    return True
+
+
+if __name__ == '__main__':
+    cmd_cert_crtsh()
---
+++ packages/habu/habu-0.1.36.tar.gz/habu-0.1.36/habu/cli/cmd_crack_luhn.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python3
+
+import sys
+
+import click
+
+
+def luhn_validate(number):
+    """ Source code from: https://en.wikipedia.org/wiki/Luhn_algorithm"""
+
+    sum = 0
+    parity = len(number) % 2
+    for i, digit in enumerate([int(x) for x in number]):
+        if i % 2 == parity:
+            digit *= 2
+            if digit > 9:
+                digit -= 9
+        sum += digit
+    return sum % 10 == 0
+
+
+@click.command()
+@click.argument('number')
+def cmd_crack_luhn(number):
+    """Having known values for a Luhn validated number, obtain the possible unknown numbers.
+
+    Numbers that use the Luhn algorithm for validation are Credit Cards, IMEI,
+    National Provider Identifier in the United States, Canadian Social
+    Insurance Numbers, Israel ID Numbers and Greek Social Security Numbers (ΑΜΚΑ).
+
+    The '-' characters are ignored.
+
+    Define the missing numbers with the 'x' character.
+
+    Reference: https://en.wikipedia.org/wiki/Luhn_algorithm
+
+    Example:
+
+    \b
+    $ habu.crack.luhn 4509-xxxx-3160-6445
+    """
+
+    number = number.replace('-', '')
+    unknown_count = number.count('x')
+
+    if not number.replace('x', '').isdigit():
+        print('Invalid format. Please, read the documentation.', file=sys.stderr)
+        sys.exit(1)
+
+    for n in range(10 ** unknown_count):
+        candidate = number
+        for item in '{:0{count}}'.format(n, count=unknown_count):
+            candidate = candidate.replace('x', item, 1)
+        if luhn_validate(candidate):
+            print(candidate)
+
+
+if __name__ == '__main__':
+    cmd_crack_luhn()
+
