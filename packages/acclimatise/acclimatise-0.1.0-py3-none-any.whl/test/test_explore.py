333b87587925decc9bfe3752787d3fca45cd70be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:16 +0000

    Add package acclimatise/0.1.0/acclimatise-0.1.0-py3-none-any.whl

---
+++ packages/acclimatise/acclimatise-0.1.0-py3-none-any.whl/test/test_explore.py
@@ -0,0 +1,28 @@
+import shutil
+
+import pytest
+
+from acclimatise import explore_command
+
+
+@pytest.mark.skipif(not shutil.which("bwa"), reason="bwa is not installed")
+def test_explore_bwa():
+    command = explore_command(["bwa"])
+    assert len(command.subcommands) == 14
+    assert len(command.positional) == 0
+
+    # Check that we parsed bwa mem correctly
+    mem = command.subcommands[1]
+    assert len(mem.positional) == 3
+    assert len(mem.subcommands) == 0
+    assert len(mem.named) >= 30
+
+
+@pytest.mark.skipif(
+    not shutil.which("podchecker"), reason="podchecker is not installed"
+)
+def test_explore_podchecker():
+    command = explore_command(["podchecker"])
+    assert len(command.subcommands) == 0
+    assert len(command.positional) == 1
+    assert len(command.named) == 2
