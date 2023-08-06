608f0d434b2992fb2de40822cd6b73bae6b317e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:10:43 +0000

    Add package searchconsole/0.0.3/searchconsole-0.0.3.tar.gz

---
+++ packages/searchconsole/searchconsole-0.0.3.tar.gz/searchconsole-0.0.3/auth/creds.py
@@ -0,0 +1,36 @@
+client_secrets = {
+    "installed": {
+        "client_id": "733940859455-h2arcrdjt6buk6cq2usg3g43usilpboh.apps.googleusercontent.com",
+        "client_secret": "VNYUx11cE9tzPF6WRA3QPntD",
+        "redirect_uris": [],
+        "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+        "token_uri": "https://accounts.google.com/o/oauth2/token"
+    }
+}
+
+credentials = {
+    "token": "ya29.Gl0kBf1OkvXDOHJ8HIzBsCtK6YuhPrvBoOAnIiwTJ-U-NWW9fLbC8yvYRGxlZOO9NjNE0I4f-OofhJLbKqpH6Ng6t7J8u6kOg-sWPO-e1IuxZUgbTKODx1NOsHAdCJM",
+    "client_id": "733940859455-h2arcrdjt6buk6cq2usg3g43usilpboh.apps.googleusercontent.com",
+    "client_secret": "VNYUx11cE9tzPF6WRA3QPntD",
+    "refresh_token": "1/_rQWynCiQ0G6zWV8kaDI4T1EKsIUwb7Y5RnzibrJvfo",
+    "token_expiry": "2017-12-16T19:09:55Z",
+    "token_uri": "https://accounts.google.com/o/oauth2/token",
+    "user_agent": None,
+    "revoke_uri": "https://accounts.google.com/o/oauth2/revoke",
+    "id_token": None,
+    "id_token_jwt": None,
+    "token_response": {
+        "access_token": "ya29.Gl0kBf1OkvXDOHJ8HIzBsCtK6YuhPrvBoOAnIiwTJ-U-NWW9fLbC8yvYRGxlZOO9NjNE0I4f-OofhJLbKqpH6Ng6t7J8u6kOg-sWPO-e1IuxZUgbTKODx1NOsHAdCJM",
+        "expires_in": 3600,
+        "token_type": "Bearer"
+    },
+    "scopes": [
+        "https://www.googleapis.com/auth/webmasters.readonly"
+    ],
+    "token_info_uri": "https://www.googleapis.com/oauth2/v3/tokeninfo",
+    "invalid": None,
+    "_class": "OAuth2Credentials",
+    "_module": "oauth2client.client"
+}
+
+webproperty_uri = 'https://www.asos.com/'
