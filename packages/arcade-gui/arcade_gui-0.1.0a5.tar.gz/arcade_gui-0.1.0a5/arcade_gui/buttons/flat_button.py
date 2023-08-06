55b2a0dc44699435979f5f76c2e726e1e29fa28e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:10:36 +0000

    Add package arcade-gui/0.1.0a5/arcade_gui-0.1.0a5.tar.gz

---
+++ packages/arcade-gui/arcade_gui-0.1.0a5.tar.gz/arcade_gui-0.1.0a5/arcade_gui/buttons/flat_button.py
@@ -0,0 +1,140 @@
+import arcade
+
+from arcade_gui import UIButton, UIView
+
+
+class UIGhostFlatButton(UIButton):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.style_classes.append('ghostflatbutton')
+
+    def on_draw(self):
+        """ Draw the button """
+        font_color = self.find_color('font_color')
+
+        normal_color = self.find_color('normal_color')
+        hover_color = self.find_color('hover_color')
+        pressed_color = self.find_color('pressed_color')
+
+        font_size = 20
+        button_height = font_size * 2
+        border_width = 3
+        border_x_offset = +2
+
+        if self.pressed:
+            arcade.draw_rectangle_filled(
+                center_x=self.center_x,
+                center_y=self.center_y + border_x_offset,
+                width=self.width,
+                height=button_height,
+                color=pressed_color,
+            )
+
+        elif self.hovered:
+            arcade.draw_rectangle_filled(
+                center_x=self.center_x,
+                center_y=self.center_y + border_x_offset,
+                width=self.width,
+                height=button_height,
+                color=hover_color,
+            )
+
+        arcade.draw_rectangle_outline(
+            center_x=self.center_x,
+            center_y=self.center_y + border_x_offset,
+            width=self.width,
+            height=button_height,
+            color=normal_color,
+            border_width=border_width,
+        )
+
+        arcade.draw_text(
+            self.text,
+            self.center_x,
+            self.center_y,
+            font_color,
+            font_size=font_size,
+            font_name=('calibri', 'arial'),
+            width=self.width, align="center",
+            anchor_x="center", anchor_y="center")
+
+
+class UIFlatButton(UIButton):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.style_classes.append('flatbutton')
+
+    def on_draw(self):
+        """ Draw the button """
+        font_color = self.find_color('font_color')
+
+        normal_color = self.find_color('normal_color')
+        hover_color = self.find_color('hover_color')
+        pressed_color = self.find_color('pressed_color')
+
+        font_size = 20
+        button_height = font_size * 2
+        border_x_offset = +2
+
+        if self.pressed:
+            arcade.draw_rectangle_filled(
+                center_x=self.center_x,
+                center_y=self.center_y + border_x_offset,
+                width=self.width,
+                height=button_height,
+                color=pressed_color,
+            )
+
+        elif self.hovered:
+            arcade.draw_rectangle_filled(
+                center_x=self.center_x,
+                center_y=self.center_y + border_x_offset,
+                width=self.width,
+                height=button_height,
+                color=hover_color,
+            )
+        else:
+            arcade.draw_rectangle_filled(
+                center_x=self.center_x,
+                center_y=self.center_y + border_x_offset,
+                width=self.width,
+                height=button_height,
+                color=normal_color,
+            )
+
+        arcade.draw_text(
+            self.text,
+            self.center_x,
+            self.center_y,
+            font_color,
+            font_size=font_size,
+            font_name=('calibri', 'arial'),
+            width=self.width, align="center",
+            anchor_x="center", anchor_y="center")
+
+
+if __name__ == '__main__':
+    view = UIView()
+    window = arcade.Window(height=200, width=600)
+    window.show_view(view)
+
+    view.add_ui_element(UIFlatButton(
+        'Hallo',
+        center_x=100,
+        center_y=100,
+        width=150,
+        height=20
+    ))
+    view.add_ui_element(UIGhostFlatButton(
+        'Hallo',
+        center_x=300,
+        center_y=100,
+        width=150,
+        height=20
+    ))
+
+    arcade.set_background_color(arcade.color.WHITE)
+    window.set_location(1200, 50)
+    arcade.run()
---
+++ packages/arcade-gui/arcade_gui-0.1.0a5.tar.gz/arcade_gui-0.1.0a5/arcade_gui/ui_style.py
@@ -0,0 +1,79 @@
+from pathlib import Path
+from typing import TYPE_CHECKING
+
+import yaml
+
+from arcade_gui.utils import MColor
+
+if TYPE_CHECKING:
+    from arcade_gui import UIElement
+
+
+def parse_color(color: str):
+    """
+    Parses the input string returning rgb int-tuple.
+
+    Supported formats:
+
+    * RGB ('r,g,b', 'r, g, b')
+    * HEX ('00ff00')
+    * Arcade colors ('BLUE', 'DARK_BLUE')
+
+    """
+    import arcade
+
+    if hasattr(arcade.color, color.upper()):
+        return getattr(arcade.color, color)
+    elif len(color) == 6 and ',' not in color:
+        return MColor.from_hex(color).rgb()
+    elif len(color.split(',')) == 3:
+        r, g, b = color.split(',')
+        r = int(r.strip())
+        g = int(g.strip())
+        b = int(b.strip())
+        return r, g, b
+    else:
+        return None
+
+
+class UIStyle:
+    """
+    Used as singleton in the UIView, style changes are applied by changing the values of the singleton.
+
+    Use `.load()` to update UIStyle instance from YAML-file
+
+
+    """
+
+    def __init__(self, data, *args, **kwargs):
+        self.style = data
+
+    def load(self, path: Path):
+        """
+        Load style from a file, overwriting existing data
+
+        :param path:
+        """
+        with path.open() as file:
+            self.style = yaml.safe_load(file)
+
+    def _get(self, ui_element: 'UIElement', attr):
+        element_style = getattr(ui_element, '_style', {})
+        if attr in element_style:
+            return element_style[attr]
+
+        style_classes = reversed(ui_element.style_classes + [ui_element.id])
+        for style_class in style_classes:
+            style_data = self.style.get(style_class, {})
+            attr_value = style_data.get(attr)
+            if attr_value:
+                return attr_value
+        else:
+            return None
+
+    def get_color(self, ui_element, attr):
+        value = self._get(ui_element, attr)
+        if value:
+            return parse_color(value)
+        else:
+            return None
