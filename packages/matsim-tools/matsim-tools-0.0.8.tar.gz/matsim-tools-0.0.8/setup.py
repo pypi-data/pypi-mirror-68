ce52f053d53e5646afe29374126c89c62908b980
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:25:24 +0000

    Add package matsim-tools/0.0.8/matsim-tools-0.0.8.tar.gz

---
+++ packages/matsim-tools/matsim-tools-0.0.8.tar.gz/matsim-tools-0.0.8/setup.py
@@ -0,0 +1,37 @@
+import pathlib
+
+from setuptools import setup
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+# This call to setup() does all the work
+setup(
+    version="0.0.8",
+    name="matsim-tools",
+    description="MATSim Agent-Based Transportation Simulation Framework - official python analysis tools",
+    long_description_content_type='text/markdown',
+    url="https://github.com/matsim-vsp/matsim-python-tools",
+    author="VSP-Berlin",
+    author_email="laudan@tu-berlin.de",
+    license="GPLv3",
+    classifiers=[
+        "License :: OSI Approved :: GNU General Public License (GPL)",
+        "Programming Language :: Python :: 3",
+    ],
+    packages=["matsim"],
+    install_requires=[
+        "protobuf >= 3.10.0",
+        "xopen",
+        "pandas", # "shapely", "geopandas >= 0.6.0"
+    ],
+    tests_require=[
+        "assertpy",
+        "pytest"
+    ],
+    entry_points={},
+    long_description=README,
+)
