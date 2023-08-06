c2ea06b435e2afa0e0aed03af641f8ad41ef2643
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:25:15 +0000

    Add package orbis-plugin-aggregation-monocle/2.2.4/orbis_plugin_aggregation_monocle-2.2.4-py3-none-any.whl

---
+++ packages/orbis-plugin-aggregation-monocle/orbis_plugin_aggregation_monocle-2.2.4-py3-none-any.whl/orbis_plugin_aggregation_monocle/__init__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+__name__ = "orbis_plugin_aggregation_monocle"
+__version__ = "2.2.4"
+__author__ = "fabod"
+__year__ = "2019"
+__description__ = "The monocle aggregation plugin for Orbis"
+__license__ = "GPL2"
+__min_python_version__ = "3.6"
+__requirements_file__ = "requirements.txt"
+__url__ = "https://github.com/orbis-eval/orbis_plugin_aggregation_monocle"
+__type__ = "plugin"
+
+from .main import Main
