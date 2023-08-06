ec630408816378c42e5bd0e3e5f57d3947b9e317
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:18:37 +0000

    Add package nestedhyperline/0.0.5/nestedhyperline-0.0.5.tar.gz

---
+++ packages/nestedhyperline/nestedhyperline-0.0.5.tar.gz/nestedhyperline-0.0.5/nestedhyperline/regressors/__init__.py
@@ -0,0 +1,8 @@
+"""
+Nested Cross-Validation for Bayesian Hyper-Parameter Optimized Linear Regularization
+https://github.com/nickkunz/nestedhyperline
+"""
+
+from nestedhyperline.regressors.elastic_ncv_regressor import elastic_ncv_regressor
+from nestedhyperline.regressors.lasso_ncv_regressor import lasso_ncv_regressor
+from nestedhyperline.regressors.ridge_ncv_regressor import ridge_ncv_regressor
