8a4664c71cc030df3f4eba1b61fb84db53fb1729
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:06:12 +0000

    Add package swh.objstorage/0.1.1/swh.objstorage-0.1.1-py3-none-any.whl

---
+++ packages/swh.objstorage/swh.objstorage-0.1.1-py3-none-any.whl/swh/objstorage/__init__.py
@@ -0,0 +1,12 @@
+# Copyright (C) 2020  The Software Heritage developers
+# See the AUTHORS file at the top-level directory of this distribution
+# License: GNU General Public License version 3, or any later version
+# See top-level LICENSE file for more information
+
+from typing import Iterable
+from pkgutil import extend_path
+
+__path__: Iterable[str] = extend_path(__path__, __name__)
+
+# for BW compat
+from swh.objstorage.factory import *  # noqa
