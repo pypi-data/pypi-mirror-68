e3a673feb30f6f51fafe2096e5b20f4d63d0e44a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:57 +0000

    Add package CoronaTest/1.8/CoronaTest-1.8-py3-none-any.whl

---
+++ packages/CoronaTest/CoronaTest-1.8-py3-none-any.whl/CoronaTest/test.py
@@ -0,0 +1,185 @@
+from pathlib import Path
+
+script_location = Path(__file__).absolute().parent
+ques_loc = script_location / "ques_pickle"
+sugg_loc = script_location / "sugg_pickle"
+
+from ConvertVal import converter
+import pickle
+with open(ques_loc, "rb") as p:
+    data_list = pickle.load(p)
+
+with open(sugg_loc, "rb") as p:
+    suggestions = pickle.load(p)
+
+def pick_ques(index):
+    import random
+    q = data_list[index].split("&&")
+    return q[random.randint(0,len(q)-1)]
+
+
+def get_prob(data):
+    
+    Fever103 = 25   #>103
+    Fever100 = 20   #>100
+    Fever99 = 15    #>=99
+
+    body_hot = 15
+    Dry_Cough = 10
+    Fatigue   = 7
+    Sputum_Production = 7
+    Shortness_Breath = 15
+    Shortness_Breath_Disease = 7
+    Pain = 10
+    Smoking = 6
+    Sore_throat = 15
+    Headache = 7
+    Chills = 7
+    Vomiting = 6
+    Nasal_Congestion = 6
+    Diarrhoea = 12
+    Past_Disease = 10
+    TRAVEL = 5
+    day = 20
+
+    sums = 0
+    fever_flag=0
+
+
+    
+    if fever_flag==1 and data[3]==1 and data[4]==0:
+        sums+=15
+    if fever_flag==1 and data[3]==1 and data[4]==1:
+        sums -= 10
+    if fever_flag==0 and data[5]==1:
+        sums+=body_hot
+        
+        
+        
+        
+    sums+= \
+    data[6]*Dry_Cough+     \
+    data[7]*Fatigue+  \
+    data[8]*Sputum_Production+    \
+    data[9]*Shortness_Breath+ \
+    data[10]*Shortness_Breath_Disease+    \
+    data[11]*Pain+     \
+    data[12]*Smoking+ \
+    data[13]*Sore_throat+     \
+    data[14]*Headache+    \
+    data[15]*Chills+    \
+    data[16]*Vomiting+    \
+    data[17]*Nasal_Congestion+    \
+    data[18]*Diarrhoea+     \
+    data[19]*Past_Disease+     \
+    data[20]*TRAVEL+    \
+    data[21]*day
+        
+    return sums
+
+
+def StartTest(i, data,status, flag, sugg_index, lst, check):
+    answer = 0
+    if check == 'sdk':
+        check = converter.convert(6, data)
+    else: check = converter.convert(i-1, data)
+        
+    if i==23:
+        lst[i] = answer
+        corona_mat = lst[1:3]+lst[4:]
+        result = get_prob(corona_mat)
+        if 0<result<=10:
+            sugg_index = 0
+        elif 10<result<=40:
+            sugg_index = 2
+        elif 40<result<=90:
+            sugg_index = 4
+        elif 90<result<=130:
+            sugg_index = 6
+        else:
+            sugg_index = 8
+        output = suggestions[sugg_index]
+        sugg_index+=1
+        i+=1
+        return i, output,status, flag, sugg_index,lst, check
+
+    
+            
+    if i==24:
+        if check == "tdk":
+            output = suggestions[sugg_index]
+        else:
+            output = "ধন্যবাদ"
+        i+=1
+        return i, output,False, flag, sugg_index,lst, check
+    
+    if check=="fdk":
+        answer = 0
+    elif check=="tdk":
+        answer = 1
+    elif check=="sdk" or check == "abcd":
+        answer = 0
+    else:
+        answer = int(check)
+    
+
+    if status == True and check != 'sdk':
+        if check == "fdk":
+            output = pick_ques(24)
+            i=50
+            status = False
+            return 50, output,status, flag, sugg_index,lst, check
+        output = pick_ques(i)
+        status = False
+        
+    elif check == 'sdk':
+        output = pick_ques(23)
+        if status == False:
+            i = i - 2
+        else:
+            i-=1
+        
+        status = True
+        
+    elif i == 3:
+        if check == "fdk":
+            lst[i] = answer
+            i = i + 3
+            output = pick_ques(i)
+        else:
+            output = pick_ques(i)
+            lst[i] = answer
+            flag = True
+    elif i == 5:
+        if check == "fdk":
+            lst[i] = answer
+            i = i + 2
+        else:
+            lst[i] = answer
+        output = pick_ques(i)
+            
+    elif i==7:
+        output = pick_ques(i)
+        lst[i] = answer
+    
+    elif i==6 and flag==False:
+        lst[i] = answer
+        output = pick_ques(i)
+        i+=1
+        
+    elif flag == True and i == 6:
+        lst[i] = answer
+        i += 1
+        output = pick_ques(i)
+        flag = False
+        
+        
+    else:
+        output = pick_ques(i)
+        lst[i] = answer
+
+    
+    
+
+    i = i + 1
+    return i, output,status, flag, sugg_index,lst, check
