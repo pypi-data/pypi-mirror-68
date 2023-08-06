184a31844401a01651678187acb0aaf4414405cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:23:28 +0000

    Add package djangocms-link/2.6.1/djangocms-link-2.6.1.tar.gz

---
+++ packages/djangocms-link/djangocms-link-2.6.1.tar.gz/djangocms-link-2.6.1/setup.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from setuptools import find_packages, setup
+
+from djangocms_link import __version__
+
+
+REQUIREMENTS = [
+    'django-cms>=3.4.5',
+    'django-filer>=1.5.0',
+    'djangocms-attributes-field>=0.4.0',
+]
+
+
+CLASSIFIERS = [
+    'Development Status :: 5 - Production/Stable',
+    'Environment :: Web Environment',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: BSD License',
+    'Operating System :: OS Independent',
+    'Programming Language :: Python',
+    'Programming Language :: Python :: 2',
+    'Programming Language :: Python :: 2.7',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
+    'Framework :: Django',
+    'Framework :: Django :: 1.11',
+    'Framework :: Django :: 2.1',
+    'Framework :: Django :: 2.2',
+    'Framework :: Django :: 3.0',
+    'Framework :: Django CMS',
+    'Framework :: Django CMS :: 3.4',
+    'Framework :: Django CMS :: 3.5',
+    'Framework :: Django CMS :: 3.6',
+    'Framework :: Django CMS :: 3.7',
+    'Topic :: Internet :: WWW/HTTP',
+    'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    'Topic :: Software Development',
+    'Topic :: Software Development :: Libraries',
+]
+
+
+setup(
+    name='djangocms-link',
+    version=__version__,
+    author='Divio AG',
+    author_email='info@divio.ch',
+    url='https://github.com/divio/djangocms-link',
+    license='BSD',
+    description='Adds a link plugin to django CMS',
+    long_description=open('README.rst').read(),
+    packages=find_packages(exclude=['tests']),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=REQUIREMENTS,
+    classifiers=CLASSIFIERS,
+    test_suite='tests.settings.run',
+)
