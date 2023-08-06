6e3b365432a90d7ef47be0103cb0254a5c8627fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:25:35 +0000

    Add package iotiumlib/20.5.13/iotiumlib-20.5.13-py3-none-any.whl

---
+++ packages/iotiumlib/iotiumlib-20.5.13-py3-none-any.whl/iotiumlib/requires/commonVariables.py
@@ -0,0 +1,24 @@
+"""
+// ========================================================================
+// Copyright (c) 2018-2019 Iotium, Inc.
+// ------------------------------------------------------------------------
+// All rights reserved.
+//
+// ========================================================================
+"""
+
+
+__author__ = "ioTium QA"
+__copyright__ = "Copyright (c) 2018-2019 by Iotium, Inc."
+__license__ = "All rights reserved."
+
+from iotiumlib import orch
+
+class commonVariables:
+
+    def __init__(self):
+        self.__orchip__ = orch.ip
+        self.__token__ = orch.token
+
+    def __getattr__(self, item):
+        return item
