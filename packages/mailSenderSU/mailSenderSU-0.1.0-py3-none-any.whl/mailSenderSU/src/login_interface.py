d61e96c897e731ee5aa16f7accf1c4f5ddb07135
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:22:35 +0000

    Add package mailSenderSU/0.1.0/mailSenderSU-0.1.0-py3-none-any.whl

---
+++ packages/mailSenderSU/mailSenderSU-0.1.0-py3-none-any.whl/mailSenderSU/src/login_interface.py
@@ -0,0 +1,76 @@
+"""
+Mail sender Sorbonne Université
+Login interface
+Quentin Deschamps, 2020
+"""
+import smtplib
+import tkinter as tk
+import tkinter.ttk as ttk
+from mailSenderSU.src.style import Style
+from mailSenderSU.src.message import show_login_error
+
+
+class LoginInterface:
+    """Interface de connexion au serveur"""
+    def __init__(self, window, style, path):
+        self.login = False
+        self.num_etudiant, self.password = "", ""
+
+        # Fenêtre
+        self.login_window = window
+        self.login_window.title("Authentification")
+        self.login_window.resizable(width=False, height=False)
+        self.login_window.focus_set()
+        self.login_window.bind("<Return>", self.return_connect)
+
+        # Style
+        self.style = Style(self.login_window, style, path)
+
+        # Gauche
+        self.frame_left = ttk.Frame(self.login_window)
+        self.frame_login = ttk.Frame(self.frame_left)
+        self.label_num_etudiant = ttk.Label(self.frame_login,
+                                            text="Numéro étudiant")
+        self.label_password = ttk.Label(self.frame_login, text="Mot de passe")
+        self.label_num_etudiant.grid(row=0, column=0, padx=5, pady=5)
+        self.label_password.grid(row=1, column=0, pady=5)
+
+        self.entry_num_etudiant = ttk.Entry(self.frame_login)
+        self.entry_password = ttk.Entry(self.frame_login, show='*')
+        self.entry_num_etudiant.grid(row=0, column=1, padx=5, pady=5)
+        self.entry_password.grid(row=1, column=1, padx=5, pady=5)
+        self.frame_login.grid(row=0, column=0, padx=5, pady=5)
+
+        self.button_login = ttk.Button(self.frame_left, text="Connexion",
+                                       command=self.connect)
+        self.button_login.grid(row=1, column=0, padx=5, pady=5)
+        self.frame_left.grid(row=0, column=0, padx=10, pady=10)
+
+        # Droite
+        try:
+            self.logo = tk.PhotoImage(file=self.style.image)
+            self.label_logo = tk.Label(self.login_window, image=self.logo)
+            self.label_logo.grid(row=0, column=1, padx=10, pady=10)
+        except Exception:
+            pass
+
+    def return_connect(self, event):
+        """Connexion au serveur avec la touche Enter"""
+        self.connect()
+
+    def connect(self, server="smtp.upmc.fr", port=587):
+        """Vérifie la validité du numéro étudiant et du mot de passe"""
+        num_etudiant = self.entry_num_etudiant.get()
+        password = self.entry_password.get()
+        try:
+            server = smtplib.SMTP(server, port)
+            server.starttls()
+            server.login(num_etudiant, password)
+            server.quit()
+        except Exception:
+            show_login_error()
+        else:
+            self.login = True
+            self.num_etudiant = num_etudiant
+            self.password = password
+            self.login_window.destroy()
---
+++ packages/mailSenderSU/mailSenderSU-0.1.0-py3-none-any.whl/mailSenderSU/src/mail_interface.py
@@ -0,0 +1,109 @@
+"""
+Mail sender Sorbonne Université
+Mail sender interface
+Quentin Deschamps, 2020
+"""
+import tkinter as tk
+import tkinter.ttk as ttk
+from mailSenderSU.src.send import send_mail
+from mailSenderSU.src.attachment import Attachment
+from mailSenderSU.src.style import Style
+import mailSenderSU.src.message as Message
+
+
+class MailSenderInterface:
+    """Interface du mail sender"""
+    def __init__(self, window, style, data, num_etudiant, password):
+        self.data = data
+        self.num_etudiant = num_etudiant
+        self.password = password
+
+        # Fenêtre
+        self.fenetre = window
+        self.fenetre.title("Mail Sender Sorbonne Université")
+        self.fenetre.resizable(width=False, height=False)
+
+        # Style
+        self.style = Style(self.fenetre, style, data.path)
+
+        # Partie du haut
+        self.frame = ttk.Frame(self.fenetre)
+        self.label_email_user = ttk.Label(self.frame, text="De")
+        self.entry_email_user = ttk.Combobox(self.frame, width=50,
+                                             values=data.list_email_user)
+        if data.list_email_user != []:
+            self.entry_email_user.current(0)
+        self.label_email_user.grid(row=0, column=0, padx=10, pady=5)
+        self.entry_email_user.grid(row=0, column=1, padx=10, pady=5)
+        self.label_email_send = ttk.Label(self.frame, text="À")
+        self.entry_email_send = ttk.Combobox(self.frame, width=50,
+                                             values=data.list_email_send)
+        self.label_email_send.grid(row=1, column=0, padx=10, pady=5)
+        self.entry_email_send.grid(row=1, column=1, padx=10, pady=5)
+        self.label_email_cc = ttk.Label(self.frame, text="Cc")
+        self.entry_email_cc = ttk.Combobox(self.frame, width=50,
+                                           values=data.list_email_send)
+        self.label_email_cc.grid(row=2, column=0, padx=10, pady=5)
+        self.entry_email_cc.grid(row=2, column=1, padx=10, pady=5)
+        self.label_subject = ttk.Label(self.frame, text="Objet")
+        self.entry_subject = ttk.Entry(self.frame, width=52)
+        self.label_subject.grid(row=3, column=0, padx=10, pady=5)
+        self.entry_subject.grid(row=3, column=1, padx=10, pady=5)
+        self.frame.grid(row=0, padx=10, pady=10)
+
+        # Corps du texte
+        self.entry_body = tk.Text(self.fenetre, height=10, width=60,
+                                  wrap=tk.WORD)
+        self.entry_body.grid(row=1, padx=10, pady=10)
+
+        # Boutons
+        self.button_frame = ttk.Frame(self.fenetre)
+        self.button_send = ttk.Button(self.button_frame, text="Envoyer",
+                                      command=self.send_message)
+        self.button_send.grid(row=0, column=0, padx=5, pady=5)
+
+        # Pièces jointes
+        self.attachment = Attachment(self.fenetre, self.button_frame)
+
+        self.button_frame.grid(row=3, padx=10, pady=5)
+
+    def check(self):
+        """Vérifie les adresses mail"""
+        email_user = self.entry_email_user.get()
+        if not email_user:
+            Message.show_email_user_miss()
+            return False
+        email_send = self.entry_email_send.get()
+        if not email_send:
+            Message.show_email_send_miss()
+            return False
+        if not ("sorbonne-universite.fr" in email_user
+                or "upmc.fr" in email_user):
+            Message.show_email_not_valid(email_user)
+            return False
+        subject = self.entry_subject.get()
+        if not subject:
+            Message.show_subject_miss()
+            return False
+        return True
+
+    def send_message(self):
+        """Lance l'envoi du mail"""
+        if self.check():
+            email_user = self.entry_email_user.get()
+            email_send = self.entry_email_send.get()
+            email_cc = self.entry_email_cc.get()
+            subject = self.entry_subject.get()
+            body = self.entry_body.get("1.0", "end")
+            list_attachment = self.attachment.list_attachment
+            if not email_cc:
+                email_cc = ""
+            is_sent, text = send_mail(email_user, email_send, email_cc,
+                                      subject, body, list_attachment,
+                                      self.num_etudiant, self.password,
+                                      self.data.signature)
+            if is_sent:
+                Message.show_send_mail()
+            else:
+                Message.show_send_mail_error()
+            self.data.maj_files(email_user, email_send, email_cc, text)
