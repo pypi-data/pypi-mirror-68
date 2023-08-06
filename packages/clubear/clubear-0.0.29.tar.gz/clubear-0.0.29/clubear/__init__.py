61d9ddf81e83bb625ce86004692f3eea1110261e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:37:06 +0000

    Add package clubear/0.0.29/clubear-0.0.29.tar.gz

---
+++ packages/clubear/clubear-0.0.29.tar.gz/clubear-0.0.29/clubear/__init__.py
@@ -0,0 +1,15 @@
+import time
+author="Hansheng Wang"
+version="2020-05-16 08:44:26"
+print("Welcome to clubear! A subsample-based massive data analysis toolkit by Hanson Wang (",version,")")
+from .dm import manager
+from .pp import pump
+from .pp import tank
+from .pp import save
+from .ck import check
+from .plt import plot
+from .md import model
+from .sf import shuffle
+from .sim import simulator
+from .fun import demo
+from .fun import ispump
