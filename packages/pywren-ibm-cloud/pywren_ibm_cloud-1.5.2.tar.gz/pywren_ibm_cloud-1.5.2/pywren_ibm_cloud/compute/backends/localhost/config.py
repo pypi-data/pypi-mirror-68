ea5dfbc6e73213b1134ccb165460ca4a74198ba6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:26:34 +0000

    Add package pywren-ibm-cloud/1.5.2/pywren_ibm_cloud-1.5.2.tar.gz

---
+++ packages/pywren-ibm-cloud/pywren_ibm_cloud-1.5.2.tar.gz/pywren_ibm_cloud-1.5.2/pywren_ibm_cloud/compute/backends/localhost/config.py
@@ -0,0 +1,31 @@
+import os
+import tempfile
+import multiprocessing
+from psutil import virtual_memory
+from pywren_ibm_cloud.config import LOGS_PREFIX
+
+
+RUNTIME_NAME_DEFAULT = 'localhost'
+RUNTIME_TIMEOUT_DEFAULT = 600  # 10 minutes
+
+TEMP = tempfile.gettempdir()
+STORAGE_BASE_DIR = os.path.join(TEMP)
+LOCAL_LOGS_DIR = os.path.join(STORAGE_BASE_DIR, LOGS_PREFIX)
+
+
+def load_config(config_data):
+    config_data['pywren']['runtime'] = RUNTIME_NAME_DEFAULT
+    mem = virtual_memory()
+    config_data['pywren']['runtime_memory'] = round(mem.total/1024/1024, 2)
+    if 'runtime_timeout' not in config_data['pywren']:
+        config_data['pywren']['runtime_timeout'] = RUNTIME_TIMEOUT_DEFAULT
+
+    if 'localhost' not in config_data:
+        config_data['localhost'] = {}
+
+    if 'workers' in config_data['pywren']:
+        config_data['localhost']['workers'] = config_data['pywren']['workers']
+    else:
+        total_cores = multiprocessing.cpu_count()
+        config_data['pywren']['workers'] = total_cores
+        config_data['localhost']['workers'] = total_cores
---
+++ packages/pywren-ibm-cloud/pywren_ibm_cloud-1.5.2.tar.gz/pywren_ibm_cloud-1.5.2/setup.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+#
+# (C) Copyright IBM Corp. 2020
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+from setuptools import setup, find_packages
+
+# how to get version info into the project
+exec(open('pywren_ibm_cloud/version.py').read())
+setup(
+    name='pywren_ibm_cloud',
+    version=__version__,
+    url='https://github.com/pywren/pywren-ibm-cloud',
+    author='Gil Vernik',
+    description='Run many jobs over IBM Cloud',
+    long_description="PyWren lets you transparently run your Python functions on IBM Cloud",
+    author_email='gilv@il.ibm.com',
+    packages=find_packages(),
+    install_requires=[
+        'Click', 'pandas', 'PyYAML', 'python-dateutil',
+        'pika==0.13.1', 'enum34', 'glob2', 'tqdm', 'lxml',
+        'tblib', 'docker', 'requests==2.22.0', 'seaborn',
+        'matplotlib', 'kubernetes', 'ibm-cos-sdk==2.6.0'
+    ],
+    include_package_data=True,
+    entry_points='''
+        [console_scripts]
+        pywren-ibm-cloud=pywren_ibm_cloud.cli.cli:cli
+    ''',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.5',
+)
