2088a3a38d6d589b18fbebb79b2b269bb1b7c106
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:13:05 +0000

    Add package semantyk/0.0.0.39/semantyk-0.0.0.39.tar.gz

---
+++ packages/semantyk/semantyk-0.0.0.39.tar.gz/semantyk-0.0.0.39/setup.py
@@ -0,0 +1,34 @@
+from setuptools import find_packages, setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    author = "Daniel Bakas Amuchastegui",    
+    author_email = "danielbakas@gmail.com",    
+    classifiers = [
+        "Development Status :: 1 - Planning",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+    ],
+    description = "Ideas Wonder.",    
+    install_requires = ['rdflib'],
+    keywords = 'semantyk',
+    long_description = long_description,
+    long_description_content_type = "text/markdown",
+    maintainer = "Semantyk",
+    name = "semantyk",
+    package_dir = {'': 'src'},
+    packages = find_packages(
+        exclude = {
+            'docs',
+            'tests*'
+        }
+    ),
+    url = "https://github.com/semantykcom/Semantyk",
+    version = "0.0.0.39"
+)
