7b09b0b5fef751dd754c3b70f41e3a07cbc781e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:03:26 +0000

    Add package unitypackage-extractor/1.0.0/unitypackage_extractor-1.0.0.tar.gz

---
+++ packages/unitypackage-extractor/unitypackage_extractor-1.0.0.tar.gz/unitypackage_extractor-1.0.0/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+setup(
+    name='unitypackage_extractor',
+    version='1.0.0',
+    description='Extractor for .unitypackage files',
+    long_description=open('README.md', 'r').read(),
+    long_description_content_type="text/markdown",
+    url='https://github.com/Cobertos/unitypackage_extractor/',
+    author='Cobertos',
+    author_email='me@cobertos.com',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Topic :: Software Development :: Build Tools',
+        'Topic :: System :: Filesystems'
+    ],
+    keywords='untiy unity3d unitypackage extract tar extractor',
+    packages=['unitypackage_extractor']
+)
---
+++ packages/unitypackage-extractor/unitypackage_extractor-1.0.0.tar.gz/unitypackage_extractor-1.0.0/unitypackage_extractor/extractor.py
@@ -0,0 +1,46 @@
+import tarfile
+import tempfile
+import sys
+import os
+import time
+import shutil
+
+def extractPackage(packagePath, outputPath=""):
+  """
+  Extracts a .unitypackage into the current directory
+  @param {string} packagePath The path to the .unitypackage
+  @param {string} [outputPath=""] Optional output path, otherwise will use cwd
+  """
+  with tempfile.TemporaryDirectory() as tmpDir:
+    # Unpack the whole thing in one go (faster than traversing the tar)
+    with tarfile.open(name=packagePath) as upkg:
+      upkg.extractall(tmpDir)
+
+    # Extract each file in tmpDir to final destination
+    for dirEntry in os.scandir(tmpDir):
+      assetEntryDir = f"{tmpDir}/{dirEntry.name}"
+      if not os.path.exists(f"{assetEntryDir}/pathname") or \
+          not os.path.exists(f"{assetEntryDir}/asset"):
+        continue #Doesn't have the required files to extract it
+
+      # Has the required info to extract
+      # Get the path to output to from /pathname
+      with open(f"{assetEntryDir}/pathname") as f:
+        pathname = f.readline()
+        pathname = pathname[:-1] if pathname[-1] == '\n' else pathname #Remove newline
+
+      #Extract to the pathname
+      print(f"Extracting '{dirEntry.name}' as '{pathname}'")
+      assetOutPath = os.path.join(outputPath, pathname)
+      os.makedirs(os.path.dirname(assetOutPath), exist_ok=True) #Make the dirs up to the given folder
+      shutil.move(f"{assetEntryDir}/asset", assetOutPath)
+
+def cli(args):
+  if not args:
+    raise TypeError("No .unitypackage path was given. \n\nUSAGE: unitypackage_extractor [XXX.unitypackage]")
+  startTime = time.time()
+  extractPackage(args[0], args[1] if len(args) > 1 else "")
+  print("--- Finished in %s seconds ---" % (time.time() - startTime))
+
+if __name__ == "__main__":
+  cli(sys.argv[1:])
