4b75874f578552d255ef21bb5d63e78f136e28bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:53:22 +0000

    Add package pyfmpcloud/1.0.2/pyfmpcloud-1.0.2-py3.7.egg

---
+++ packages/pyfmpcloud/pyfmpcloud-1.0.2-py3.7.egg/pyfmpcloud/settings.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+import configparser
+import sys
+site_packages = next(p for p in sys.path if 'pyfmpcloud' in p)
+
+cfile = site_packages + '/pyfmpcloud/config.ini'
+cfg = configparser.ConfigParser()
+cfg.read(cfile)
+#cfile = 'config.ini'
+
+def get_urlroot():
+    urlroot = cfg['API']['url_root']
+    return urlroot
+
+def get_urlrootfmp():
+    urlrootfmp = cfg['API']['url_root_fmp']
+    return urlrootfmp
+
+def get_apikey():
+    apikey = cfg['API']['api_key']
+    return apikey
+
+def set_apikey(apikey):
+    cfg['API']['api_key'] = apikey
+    with open(cfile, 'w') as configfile:
+        cfg.write(configfile)
