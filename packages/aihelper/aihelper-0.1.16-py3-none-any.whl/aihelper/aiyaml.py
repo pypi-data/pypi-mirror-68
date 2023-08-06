05fd29efae76c3931777fcd71248b5ff00272fd0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:14 +0000

    Add package aihelper/0.1.16/aihelper-0.1.16-py3-none-any.whl

---
+++ packages/aihelper/aihelper-0.1.16-py3-none-any.whl/aihelper/aiyaml.py
@@ -0,0 +1,16 @@
+import os
+
+def write_basic_yaml():
+    return {
+        "DIRECTORY": '',
+        "WAVE LENGTHS": [None],
+        "RMS": False,
+    }
+
+def write_yaml():
+    return {
+        "DIRECTORY": "",
+        "WAVE LENGTHS": [None, None, None, None],
+        "RMS": False
+    }
+
