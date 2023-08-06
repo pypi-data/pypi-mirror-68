619a7e919f7604f05978f7d5d01721fda81f30fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:07:11 +0000

    Add package djangopollapp/0.1.0/djangopollapp-0.1.0.tar.gz

---
+++ packages/djangopollapp/djangopollapp-0.1.0.tar.gz/djangopollapp-0.1.0/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='djangopollapp',
+    version='0.1.0',
+    author="Coder Bug",
+    author_email="thecoderbug@gmail.com",
+    description='A simple polls application',
+    packages=setuptools.find_packages(),
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/c0derbug/djangopollapp.git",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.5.2',
+)
