fba4365667840e31d41968756b7ffbda1ed65437
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:44 +0000

    Add package chaostoolkit-toxiproxy/0.3.0/chaostoolkit-toxiproxy-0.3.0.tar.gz

---
+++ packages/chaostoolkit-toxiproxy/chaostoolkit-toxiproxy-0.3.0.tar.gz/chaostoolkit-toxiproxy-0.3.0/CHANGELOG.md
@@ -0,0 +1,68 @@
+# Changelog
+
+## [Unreleased][]
+
+[Unreleased]: https://github.com/chaostoolkit-incubator/chaostoolkit-toxiproxy/compare/0.3.0...HEAD
+
+## [0.3.0][]
+
+[0.3.0]: https://github.com/chaostoolkit-incubator/chaostoolkit-toxiproxy/compare/0.2.1...0.3.0
+
+### Added
+
+- Use `toxiproxy_url` for defining toxiproxy-api base URL.
+
+## [0.2.1][]
+
+[0.2.1]: https://github.com/chaostoolkit-incubator/chaostoolkit-toxiproxy/compare/0.2.0...0.2.1
+
+### Added
+
+- Build Python 3.7 too
+- Specify readme format for Pypi
+
+## [0.2.0][]
+
+[0.2.0]: https://github.com/chaostoolkit-incubator/chaostoolkit-toxiproxy/compare/0.1.3...0.2.0
+
+### Changed
+
+- Fixed modify_proxy action: Does not error on a successful call and returns proper error message
+- Add support for [reset endpoint](https://github.com/Shopify/toxiproxy#endpoints)
+
+## [0.1.3][]
+
+[0.1.3]: https://github.com/chaostoolkit-incubator/chaostoolkit-toxiproxy/compare/0.1.2...0.1.3
+
+### Changed
+
+-   delint
+-   use the appropriate typing annotation `Any` instead of `any` which is a builtin
+    function
+
+## [0.1.2][]
+
+[0.1.2]: https://github.com/chaostoolkit-incubator/chaostoolkit-toxiproxy/compare/0.1.1...0.1.2
+
+Just so that we can re-upload to Pypi...
+
+## [0.1.1][]
+
+[0.1.1]: https://github.com/chaostoolkit-incubator/chaostoolkit-toxiproxy/compare/0.1.0...0.1.1
+
+### Added
+
+-   add `MANIFEST.in` for building a proper Python distribution package
+
+## [0.1.0][]
+
+[0.1.0]: https://github.com/chaostoolkit-incubator/chaostoolkit-toxiproxy/compare/51c126...0.1.0
+
+### Changed
+
+-   Module name to `chaostoxi`
+
+### Added
+
+-   Travis CI
+-   Initial version of code
---
+++ packages/chaostoolkit-toxiproxy/chaostoolkit-toxiproxy-0.3.0.tar.gz/chaostoolkit-toxiproxy-0.3.0/chaostoxi/toxiproxyapi.py
@@ -0,0 +1,101 @@
+# -*- coding: utf-8 -*-
+from typing import Any, Dict
+
+
+from chaoslib.types import Configuration
+from logzero import logger
+import requests
+
+
+def create_proxy(proxy_json: Dict[str, str],
+                 configuration: Configuration) -> Dict[str, Any]:
+    url = build_proxies_baseUrl(configuration)
+    response = requests.post(url, json=proxy_json)
+    if not response.ok:
+        logger.debug("Unable to create proxy, response code {} with {}".format(
+            response.status_code, response.text))
+        return None
+    return response.json()
+
+
+def modify_proxy(proxy_name: str, proxy_json: Dict[str, Any],
+                 configuration: Configuration) -> Dict[str, Any]:
+    base_url = build_proxies_baseUrl(configuration)
+    url = "{}/{}".format(base_url, proxy_name)
+    logger.debug("Toxiproxy server API located at {}".format(url))
+    response = requests.post(url, json=proxy_json)
+    if not response.ok:
+        logger.debug("Unable to modify proxy, response code {} with {}".format(
+            response.status_code, response.text))
+        return None
+    return response.json()
+
+
+def delete_proxy(proxy_name: str, configuration: Configuration) -> None:
+    base_url = build_proxies_baseUrl(configuration)
+    url = "{}/{}".format(base_url, proxy_name)
+    try:
+        response = requests.delete(url)
+        if not response.ok:
+            logger.warning(
+                "Unable to remove proxy from chaostoolkit with: {} received: "
+                "{}".format(url, response.status_code))
+    except Exception:
+        logger.warning(
+            "Unable to remove proxy from chaostoolkit with: {}".format(url))
+
+
+def read_proxy(proxy_name: str,
+               configuration: Configuration) -> Dict[str, Any]:
+    base_url = build_proxies_baseUrl(configuration)
+    url = "{}/{}".format(base_url, proxy_name)
+    response = requests.get(url)
+    if not response.ok:
+        return None
+    return response.json()
+
+
+def create_toxic(proxy_name: str, toxic_json: Dict[str, Any],
+                 configuration: Configuration) -> Dict[str, Any]:
+    base_url = build_proxies_baseUrl(configuration)
+    url = "{}/{}/toxics".format(base_url, proxy_name)
+    response = requests.post(url, json=toxic_json)
+    if not response.ok:
+        logger.error("Unable to create toxic for proxy: {}".format(proxy_name))
+        return None
+    return response.json()
+
+
+def delete_toxic(proxy_name: str, toxic_name,
+                 configuration: Configuration) -> int:
+    base_url = build_proxies_baseUrl(configuration)
+    url = "{}/{}/toxics/{}".format(base_url, proxy_name, toxic_name)
+    response = requests.delete(url)
+    return response.ok
+
+
+def reset(configuration: Configuration) -> int:
+    base_url = build_baseUrl(configuration)
+    url = "{}/reset".format(base_url)
+    response = requests.post(url)
+    return response.ok
+
+
+def build_baseUrl(configuration: Configuration) -> str:
+    """
+    Constructs toxiproxy baseURL using variables from configuration.
+    """
+    toxiproxy_host = configuration.get("toxiproxy_host")
+    toxiproxy_port = configuration.get("toxiproxy_port")
+    toxiproxy_url = configuration.get("toxiproxy_url")
+    if not toxiproxy_url:
+        if not toxiproxy_port:
+            toxiproxy_port = 8474
+        url = "http://{}:{}".format(toxiproxy_host, toxiproxy_port)
+    else:
+        url = toxiproxy_url
+    logger.debug("Calculated toxiproxy URL is: {}".format(url))
+    return url
+
+def build_proxies_baseUrl(configuration: Configuration) -> str:
+    return build_baseUrl(configuration)+"/proxies"
