aafcba0c7e077820b5419bb1a62b37be6cb862bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:59:52 +0000

    Add package aag-probability/1.9/aag_probability-1.9.tar.gz

---
+++ packages/aag-probability/aag_probability-1.9.tar.gz/aag_probability-1.9/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+    
+setup(name='aag_probability',
+      version='1.9',
+      description='Gaussian and Binomial distributions',
+      packages=['aag_distributions'],
+      url='https://github.com/alangordon258/lighthouse',
+      author='Alan Gordon',
+      author_email='alan_a_gordon@hotmail.com',
+      zip_safe=False)
