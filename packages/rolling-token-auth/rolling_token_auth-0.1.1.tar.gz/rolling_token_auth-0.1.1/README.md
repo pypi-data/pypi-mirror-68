7e9c5036e636789c4a7f57f2dde2a7467e63cce8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:56:48 +0000

    Add package rolling-token-auth/0.1.1/rolling_token_auth-0.1.1.tar.gz

---
+++ packages/rolling-token-auth/rolling_token_auth-0.1.1.tar.gz/rolling_token_auth-0.1.1/README.md
@@ -0,0 +1,25 @@
+# Rolling Token Authentication
+
+## Usage
+
+### Initialization
+```
+from rolling_token_auth import RollingTokenManager
+
+manager = RollingTokenManager("secret", interval=3600)
+```
+The `interval` defines how long a token is valid in seconds. Shorter = more secure.
+Both `secret` and `interval` must match between generation and verification.
+
+### Generation
+```
+manager.generate_token()
+```
+This generates a token for the current timestamp. Optionally a `offset` can be declared.
+
+
+## Verification
+```
+manager.is_valid(token)
+```
+This checks if the given token is valid for the current timestamp. The `manager.tolerance` parameter defines how many token from the past and future are still valid (default: 1 in each direction).
---
+++ packages/rolling-token-auth/rolling_token_auth-0.1.1.tar.gz/rolling_token_auth-0.1.1/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", 'r') as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name="rolling_token_auth",
+    version="0.1.1",
+    author="Alexander Eichhorn",
+    author_email="",
+    description="Rolling Token verificator & generator",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/alexeichhorn/python-rolling-token-auth",
+    install_requires=[
+        'cryptography>=2.9.0'
+    ],
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent"
+    ],
+    python_requires=">=3.6"
+)
