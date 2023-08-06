64e95d306b72b36b4c54cadaa7ae60909afb8622
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:01:10 +0000

    Add package gaubi-probability/0.1/gaubi_probability-0.1.tar.gz

---
+++ packages/gaubi-probability/gaubi_probability-0.1.tar.gz/gaubi_probability-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='gaubi_probability',
+      version='0.1',
+      description='Gaussian distributions and binomial distributions',
+      packages=['gaubi_probability'],
+      author = 'Matt Bray',
+      author_email = 'blazetolliver@icloud.com',
+      zip_safe=False)
