8cf0c8b8fe0d0518dee31b654634d5a0e91c2101
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:26:56 +0000

    Add package strix/0.5.1/strix-0.5.1-py3-none-any.whl

---
+++ packages/strix/strix-0.5.1-py3-none-any.whl/strix/__init__.py
@@ -0,0 +1,9 @@
+from strix.base_functions import *
+
+from strix.gca import GCA
+# from strix.file_formats import egf
+# from strix.file_formats import csv
+# from strix.file_formats import geo_json
+from strix.file_actions import str_file_read
+from strix.file_actions import text_writer
+from strix.file_actions import file_locate
