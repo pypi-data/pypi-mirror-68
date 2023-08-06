dc0f1d49d39a114615e496150ee7b80f1a4a0d9a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:22:00 +0000

    Add package reptile/1.0.1/reptile-1.0.1.tar.gz

---
+++ packages/reptile/reptile-1.0.1.tar.gz/reptile-1.0.1/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+setuptools.setup(
+    name="reptile",
+    version="1.0.1",
+    author="Alessandro",
+    url="https://github.com/alessandrosp/reptile",
+    description=(
+        "Reptile is a command-line interface for Python. Specifically, "
+        "Reptile helps with producing interactive REPL-like software."
+    ),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    keywords=(
+        "prompt-toolkit, cli, command-line, commandline, "
+        "command-line-interface, python-inquiry, inquirer, "
+        "reptile, REPL, prompt"
+    ),
+    install_requires=["prompt-toolkit>=3.0.5", "pygments>=2.6.1"],
+    python_requires=">=3.6",
+    packages=setuptools.find_packages(),
+)
