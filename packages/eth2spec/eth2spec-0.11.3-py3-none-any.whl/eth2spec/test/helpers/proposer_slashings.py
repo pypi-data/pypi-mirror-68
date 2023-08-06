24db9052775f6aacf0140294b737f9722c244bda
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:26:01 +0000

    Add package eth2spec/0.11.3/eth2spec-0.11.3-py3-none-any.whl

---
+++ packages/eth2spec/eth2spec-0.11.3-py3-none-any.whl/eth2spec/test/helpers/proposer_slashings.py
@@ -0,0 +1,33 @@
+from eth2spec.test.helpers.block_header import sign_block_header
+from eth2spec.test.helpers.keys import pubkey_to_privkey
+
+
+def get_valid_proposer_slashing(spec, state, signed_1=False, signed_2=False):
+    current_epoch = spec.get_current_epoch(state)
+    validator_index = spec.get_active_validator_indices(state, current_epoch)[-1]
+    privkey = pubkey_to_privkey[state.validators[validator_index].pubkey]
+    slot = state.slot
+
+    header_1 = spec.BeaconBlockHeader(
+        slot=slot,
+        proposer_index=validator_index,
+        parent_root=b'\x33' * 32,
+        state_root=b'\x44' * 32,
+        body_root=b'\x55' * 32,
+    )
+    header_2 = header_1.copy()
+    header_2.parent_root = b'\x99' * 32
+
+    if signed_1:
+        signed_header_1 = sign_block_header(spec, state, header_1, privkey)
+    else:
+        signed_header_1 = spec.SignedBeaconBlockHeader(message=header_1)
+    if signed_2:
+        signed_header_2 = sign_block_header(spec, state, header_2, privkey)
+    else:
+        signed_header_2 = spec.SignedBeaconBlockHeader(message=header_2)
+
+    return spec.ProposerSlashing(
+        signed_header_1=signed_header_1,
+        signed_header_2=signed_header_2,
+    )
