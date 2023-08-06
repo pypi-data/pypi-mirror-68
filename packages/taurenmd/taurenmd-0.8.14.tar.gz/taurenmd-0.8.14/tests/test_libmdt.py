575e94b609c89e6bac8fb0b72e32b3d131d667e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:10:37 +0000

    Add package taurenmd/0.8.14/taurenmd-0.8.14.tar.gz

---
+++ packages/taurenmd/taurenmd-0.8.14.tar.gz/taurenmd-0.8.14/tests/test_libmdt.py
@@ -0,0 +1,46 @@
+"""Test libmdt."""
+import mdtraj as md
+import pytest
+
+from taurenmd.libs import libmdt
+
+from . import toptest, toptest_cif, trajtest
+
+
+def test_load_traj_pdb():
+    """Test loading traj."""
+    traj = libmdt.load_traj(toptest, trajtest)
+    assert isinstance(traj, md.Trajectory)
+    assert len(traj) == 10
+
+
+def test_load_traj_pdb_2():
+    """Test loading traj."""
+    traj = libmdt.load_traj(toptest, [trajtest, trajtest])
+    assert isinstance(traj, md.Trajectory)
+    assert len(traj) == 20
+
+
+def test_load_traj_cif():
+    """Test loading traj."""
+    traj = libmdt.load_traj(toptest_cif, trajtest)
+    assert isinstance(traj, md.Trajectory)
+    assert len(traj) == 10
+
+
+def test_load_traj_cif_import_error():
+    """Test loading traj."""
+    libmdt.SIMTK = False
+    with pytest.raises(SystemExit) as err:
+        libmdt.load_traj(toptest_cif, trajtest)
+    assert err.type == SystemExit
+    assert err.value.code == 0
+
+
+def test_simtk_import_error():
+    """Test import error message."""
+    with pytest.raises(SystemExit) as err:
+        libmdt._log_simtkimport_error()
+    
+    assert err.type == SystemExit
+    assert err.value.code == 0
