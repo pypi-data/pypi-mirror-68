2997eab5459bd6a6abd4924288856b0cc0a90f71
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:19:45 +0000

    Add package taigapy/2.12.13/taigapy-2.12.13-py2.py3-none-any.whl

---
+++ packages/taigapy/taigapy-2.12.13-py2.py3-none-any.whl/taigapy/custom_exceptions.py
@@ -0,0 +1,26 @@
+class TaigaHttpException(Exception):
+    """Exception to retrieve a Bad Status code from Taiga"""
+
+
+class Taiga404Exception(TaigaHttpException):
+    """Exception to retrieve a NotFound returned by Taiga"""
+
+    pass
+
+
+class TaigaDeletedVersionException(TaigaHttpException):
+    """Exception to retrieve a deleted dataset version"""
+
+    pass
+
+
+class TaigaRawTypeException(Exception):
+    """Exception when we are trying to get a file from a Table or Matrix format, whereas it is Raw data"""
+
+    pass
+
+
+class TaigaClientConnectionException(Exception):
+    """Exception when we are unable to connect to Taiga"""
+
+    pass
