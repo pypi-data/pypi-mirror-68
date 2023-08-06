83da984f86766c58e87929e92bb166f65d388039
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:13:28 +0000

    Add package mvlearn/0.2.0/mvlearn-0.2.0-py3-none-any.whl

---
+++ packages/mvlearn/mvlearn-0.2.0-py3-none-any.whl/mvlearn/__init__.py
@@ -0,0 +1,10 @@
+import mvlearn.embed
+import mvlearn.cluster
+import mvlearn.construct
+import mvlearn.cotraining
+import mvlearn.datasets
+import mvlearn.utils
+import mvlearn.factorization
+import mvlearn.plotting
+
+__version__ = "0.2.0"
