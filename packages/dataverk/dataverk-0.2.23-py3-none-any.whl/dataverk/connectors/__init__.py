f015506cbdd7ecd5961cbe07d0c0ba218d55740b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:13:06 +0000

    Add package dataverk/0.2.23/dataverk-0.2.23-py3-none-any.whl

---
+++ packages/dataverk/dataverk-0.2.23-py3-none-any.whl/dataverk/connectors/__init__.py
@@ -0,0 +1,19 @@
+from dataverk.connectors.abc.base import DataverkBase
+from dataverk.connectors.jsonstat import JSONStatConnector
+from dataverk.connectors.google_storage import GoogleStorageConnector
+from dataverk.connectors.oracle import OracleConnector
+from dataverk.connectors.sqlite import SQLiteConnector
+from dataverk.connectors.kafka import KafkaConnector
+from dataverk.connectors.postgres import PostgresConnector
+from dataverk.connectors.s3 import S3Connector
+
+
+__all__ = [
+            'JSONStatConnector',
+            'S3Connector',
+            'GoogleStorageConnector',
+            'OracleConnector',
+            'SQLiteConnector',
+            'KafkaConnector',
+            'PostgresConnector',
+           ]
