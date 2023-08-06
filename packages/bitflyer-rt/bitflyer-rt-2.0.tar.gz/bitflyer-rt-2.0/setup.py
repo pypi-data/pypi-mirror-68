e1339d053e8f40813b66575056634b0c2284524c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:36:42 +0000

    Add package bitflyer-rt/2.0/bitflyer-rt-2.0.tar.gz

---
+++ packages/bitflyer-rt/bitflyer-rt-2.0.tar.gz/bitflyer-rt-2.0/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name='bitflyer-rt',
+    version='2.0',
+    description='Bitflyer Realtime and Rest API',
+    author='Philippe Remy',
+    license='MIT',
+    long_description_content_type='text/markdown',
+    long_description=open('README.md').read(),
+    packages=['bitflyer'],
+    install_requires=[
+        'websocket-client==0.47.0',
+        'pybitflyer==0.1.9'
+    ]
+)
