e5ad7529fbfa77673f16b0c2ff178b4871ad671b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:37 +0000

    Add package OpenVisus/1.5.12/OpenVisus-1.5.12-cp37-none-win_amd64.whl

---
+++ packages/OpenVisus/OpenVisus-1.5.12-cp37-none-win_amd64.whl/OpenVisus/visus.bat
@@ -0,0 +1,5 @@
+
+cd %~dp0
+set PATH=C:\hostedtoolcache\windows\Python\3.7.7\x64\python.exe\..;%PATH%;.\bin
+set PYTHONPATH=D:\a\OpenVisus\OpenVisus\build\Release\OpenVisus;C:\hostedtoolcache\windows\Python\3.7.7\x64\python37.zip;C:\hostedtoolcache\windows\Python\3.7.7\x64\DLLs;C:\hostedtoolcache\windows\Python\3.7.7\x64\lib;C:\hostedtoolcache\windows\Python\3.7.7\x64;C:\hostedtoolcache\windows\Python\3.7.7\x64\lib\site-packages;C:\hostedtoolcache\windows\Python\3.7.7\x64\lib\site-packages\win32;C:\hostedtoolcache\windows\Python\3.7.7\x64\lib\site-packages\win32\lib;C:\hostedtoolcache\windows\Python\3.7.7\x64\lib\site-packages\Pythonwin
+bin\visus.exe" %*
