87ab8ffd452d09f80ae60709a35c23f42eb791e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:57 +0000

    Add package RG/0.0.7/RG-0.0.7-py3-none-any.whl

---
+++ packages/RG/RG-0.0.7-py3-none-any.whl/RG/__init__.py
@@ -0,0 +1,21 @@
+from RG.xlsread import *
+from RG.regress import *
+from RG.R2 import *
+
+from RG.PCA import *
+from RG.PLS import *
+from RG.mbPLS import *
+from LDA import *
+
+
+from RG.colorspectra import *
+
+from RG.snv import *
+from RG.autoscale import *
+
+
+
+
+
+
+
