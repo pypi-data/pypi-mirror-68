71c27c187936f537408f7782732bae795f15828d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:27:14 +0000

    Add package hasy/0.1.1/hasy-0.1.1.tar.gz

---
+++ packages/hasy/hasy-0.1.1.tar.gz/hasy-0.1.1/setup.py
@@ -0,0 +1,10 @@
+# Third party modules
+from setuptools import setup
+
+setup(
+    package_data={"hasy": ["data/currencies.csv"]},
+    install_requires=["click"],
+    extras_require={
+        "all": ["tensorflow", "tflearn", "sklearn", "keras", "matplotlib"],
+    },
+)
