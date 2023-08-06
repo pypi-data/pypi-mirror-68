28582d8f8dd80f86f9c800cdd9678d1ee142116c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:02:28 +0000

    Add package lightfm-dataset-helper/0.1.2/lightfm_dataset_helper-0.1.2.tar.gz

---
+++ packages/lightfm-dataset-helper/lightfm_dataset_helper-0.1.2.tar.gz/lightfm_dataset_helper-0.1.2/tests/test_lightfm_dataset_helper.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+"""Tests for `lightfm_dataset_helper` package."""
+
+# from lightfm_dataset_helper import lightfm_dataset_helper
+
+
+from unittest import TestCase
+
+class TryTesting(TestCase):
+    def test_always_passes(self):
+        self.assertTrue(True)
+
+    # def test_always_fails(self):
+    #     self.assertTrue(False)
