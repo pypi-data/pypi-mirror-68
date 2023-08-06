ef445c6277a29974773c4d373b10af8f898297bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:07:11 +0000

    Add package semantyk/0.0.0.42/semantyk-0.0.0.42.tar.gz

---
+++ packages/semantyk/semantyk-0.0.0.42.tar.gz/semantyk-0.0.0.42/semantyk/das/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+##################################################
+#
+# das 
+# Package | Data Análisis and Statistics
+#
+# Author: Daniel Bakas Amuchastegui
+# March 18, 2020
+# 
+# Copyright © Semantyk 2020. All rights reserved.
+##################################################
