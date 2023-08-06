62e479816ba46e3d89c9904efd7bd419076db474
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:27:43 +0000

    Add package thesiqtpj/1.0.0/thesiqtpj-1.0.0.tar.gz

---
+++ packages/thesiqtpj/thesiqtpj-1.0.0.tar.gz/thesiqtpj-1.0.0/setup.py
@@ -0,0 +1,9 @@
+import setuptools
+setuptools.setup(
+    name = 'thesiqtpj',
+    version = '1.0.0',
+    author = 'Cai The Si',
+    description = 'my simple package',
+    author_email = 'caithesi21041997@gmail.com',
+    packages=setuptools.find_packages(),
+)
