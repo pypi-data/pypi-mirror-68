25b870d77050b9379e5125e6b6fb487d8a1a56a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:11:09 +0000

    Add package tap-intercom/0.0.3/tap-intercom-0.0.3.tar.gz

---
+++ packages/tap-intercom/tap-intercom-0.0.3.tar.gz/tap-intercom-0.0.3/setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(name='tap-intercom',
+      version='0.0.3',
+      description='Singer.io tap for extracting data from the Intercom API',
+      author='jeff.huth@bytecode.io',
+      classifiers=['Programming Language :: Python :: 3 :: Only'],
+      py_modules=['tap_intercom'],
+      install_requires=[
+          'backoff==1.8.0',
+          'requests==2.23.0',
+          'singer-python==5.9.0'
+      ],
+      entry_points='''
+          [console_scripts]
+          tap-intercom=tap_intercom:main
+      ''',
+      packages=find_packages(),
+      package_data={
+          'tap_intercom': [
+              'schemas/*.json',
+              'tests/*.py'
+          ]
+      })
