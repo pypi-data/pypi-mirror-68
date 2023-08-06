345c8ef253085dc5d9f25b28800305205325ec2a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:12:26 +0000

    Add package url-util/0.1.2/url-util-0.1.2.tar.gz

---
+++ packages/url-util/url-util-0.1.2.tar.gz/url-util-0.1.2/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['url_util']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['click>=7.1.2,<8.0.0', 'furl>=2.1.0,<3.0.0']
+
+setup_kwargs = {
+    'name': 'url-util',
+    'version': '0.1.2',
+    'description': '',
+    'long_description': None,
+    'author': 'Eyal Levin',
+    'author_email': 'eyalev@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.6,<4.0',
+}
+
+
+setup(**setup_kwargs)
