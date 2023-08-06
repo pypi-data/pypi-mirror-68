82757c1c30cf4a76383f69e08180aee14f879dc8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:25:14 +0000

    Add package DigiPathAI/0.1.5/DigiPathAI-0.1.5-py3-none-any.whl

---
+++ packages/DigiPathAI/DigiPathAI-0.1.5-py3-none-any.whl/DigiPathAI/static/js/dark-mode-switch.js
@@ -0,0 +1,48 @@
+function dark_on(){
+    $('nav').removeClass("navbar-light");
+    $('nav').addClass("navbar-dark");
+    //$('#get-segmentation-btn').removeClass('btn-success');
+    //$('#get-segmentation-btn').addClass('btn-dark');
+    $('table').addClass('table-dark');
+}
+function dark_off(){
+    $('nav').removeClass("navbar-dark");
+    $('nav').addClass("navbar-light");
+    //$('#get-segmentation-btn').removeClass('btn-dark');
+    //$('#get-segmentation-btn').addClass('btn-success');
+    $('table').removeClass('table-dark');
+}
+(function() {
+  var darkSwitch = document.getElementById("darkSwitch");
+  if (darkSwitch) {
+    initTheme();
+    darkSwitch.addEventListener("change", function(event) {
+      resetTheme();
+    });
+    function initTheme() {
+      var darkThemeSelected =
+        localStorage.getItem("darkSwitch") !== null &&
+        localStorage.getItem("darkSwitch") === "dark";
+      darkSwitch.checked = darkThemeSelected;
+      if(darkThemeSelected){
+            document.body.setAttribute("data-theme", "dark")
+            dark_on();
+        }
+        else{
+            document.body.removeAttribute("data-theme");
+            dark_off();
+        }
+    }
+    function resetTheme() {
+      if (darkSwitch.checked) {
+        document.body.setAttribute("data-theme", "dark");
+        dark_on();
+        localStorage.setItem("darkSwitch", "dark");
+      } else {
+        document.body.removeAttribute("data-theme");
+        dark_off();
+        localStorage.removeItem("darkSwitch");
+      }
+    }
+  }
+})();
