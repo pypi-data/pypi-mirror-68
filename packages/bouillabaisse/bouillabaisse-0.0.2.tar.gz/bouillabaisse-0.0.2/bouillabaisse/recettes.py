4adce018e308f1395cc1f5cecd1ea70b58774a6b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:13:27 +0000

    Add package bouillabaisse/0.0.2/bouillabaisse-0.0.2.tar.gz

---
+++ packages/bouillabaisse/bouillabaisse-0.0.2.tar.gz/bouillabaisse-0.0.2/bouillabaisse/recettes.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May 14 15:19:36 2020
+
+@author: AG
+"""
+
+
+class RecettesB():
+    """
+        différentes recettes de la bouillabaisse
+        
+        r = RecettesB()
+        print(r.recette)
+    """
+    def __init__(self):
+        """
+        une seule recette pour le moment
+        """
+        
+        self.recette = '''
+            Préparer les légumes : 
+            Peler, épépiner et concasser les tomates. Hacher les oignons, écraser l'ail. Emincer les blancs de poireaux. 
+            Mettre tous ces légumes dans une grande marmite. 
+            Vider les poissons, réserver les foies que l'on hachera. 
+            Couper les têtes et les queues, couper les pattes des cigales de mer. 
+            Ajouter les déchets des poissons (têtes et queues) et les cigales dans la marmite, ajouter 25 cl d'huile d'olive. 
+            Mettre également le fenouil, le laurier, le persil, les poivre en grains et la peau d'orange séchée. 
+            Couvrir et faire cuire 15 min à feu moyen en remuant de temps en temps. 
+            Faire bouillir 5 l d'eau à part. 
+            Mettre la marmite de légumes à vif et y verser l'eau bouillante. Saler, porter à ébullition et baisser le feu. 
+            Laisser mijoter 20 min. 
+            Au bout de ce temps, écumer passer la soupe au chinois en écrasant bien les légumes et morceaux de poisson. 
+            Rajouter le safran. 
+            Couper les poissons en tronçons égaux, ou couper les plus gros en 2 ou 3. 
+            Les faire cuire successivement dans la préparation précédente portée à ébullition sur feu vif : 
+            Le congre cuit environ 10 min, le Saint-pierre 6 min, la lotte 4 min, la rascasse 4 min, le grondin 3 min et la vive 3 min. 
+            Les poissons ne doivent pas être complètement cuits, mais juste à point. Les sortir du bouillon et réserver dès qu'ils sont cuits. 
+            Préparer la rouille : 
+            Faire tremper le pain dans le lait. 
+            Eplucher, dégermer et piler l'ail avec les piments rouges épépinés. Rajouter la mie de pain trempée et essorée puis l'huile, sans cesser de piler. Saler, poivrer et délayer avec 1 cuillère à soupe de bouillon. 
+            Pour servir la bouillabaisse : lier le bouillon avec les foies hachés des poissons en fouettant bien. 
+            Servir le bouillon lié dans les assiettes, et répartir les morceaux de poissons. 
+            Accompagner de tranches de pain de campagne frottées à l'ail et à l'huile et grillées, et de la rouille.
+            '''
---
+++ packages/bouillabaisse/bouillabaisse-0.0.2.tar.gz/bouillabaisse-0.0.2/setup.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+
+# minimal setup.py to install in develop mode
+
+
+
+from setuptools import setup, find_packages
+
+
+
+setup(
+
+    name="bouillabaisse",
+
+    packages=find_packages(),
+
+
+
+    # see https://semver.org/
+
+    # and https://pypi.org/project/semver/
+
+    version="0.0.2",
+
+
+
+    author="Alex Grumberg",
+
+    author_email="ag@gmail.com",
+
+    description="recettes de bouillabaisse",
+
+    #keywords=["Python", "package", "setup.py", "setuptools"],
+
+    url="https://github.com/flotpython/bidule",
+
+    license="Creative Commons BY-CC-ND 4.0",
+
+)
