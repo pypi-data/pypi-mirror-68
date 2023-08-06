f74e83888cb25d7bf96faccfa57367147682f613
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:11:20 +0000

    Add package pipelinex/0.2.7/pipelinex-0.2.7-py3-none-any.whl

---
+++ packages/pipelinex/pipelinex-0.2.7-py3-none-any.whl/pipelinex/framework/context/flexible_context.py
@@ -0,0 +1,28 @@
+import kedro
+from .pipelines_in_parameters_context import PipelinesInParametersContext
+from .only_missing_string_runner_context import (
+    OnlyMissingStringRunnerDefaultOptionContext,
+)
+from .catalog_sugar_context import CatalogSyntacticSugarContext
+from .mlflow_context import MLflowContext
+
+
+class BaseFlexibleContext(
+    PipelinesInParametersContext,
+    CatalogSyntacticSugarContext,
+    OnlyMissingStringRunnerDefaultOptionContext,
+):
+    project_name = "KedroProject"
+    project_version = kedro.__version__
+
+
+class FlexibleContext(MLflowContext, BaseFlexibleContext):
+    """ The Kedro context with flexible options """
+
+    pass
+
+
+class MLflowFlexibleContext(FlexibleContext):
+    """ Deprecated alias for FlexibleContext for backward-compatibility """
+
+    pass
