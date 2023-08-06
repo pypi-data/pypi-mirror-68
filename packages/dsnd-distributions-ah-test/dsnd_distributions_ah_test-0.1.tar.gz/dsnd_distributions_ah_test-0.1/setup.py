94bc27b8c8e9e62a1ab05b06e13c1ec3cc490168
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:10:57 +0000

    Add package dsnd-distributions-ah-test/0.1/dsnd_distributions_ah_test-0.1.tar.gz

---
+++ packages/dsnd-distributions-ah-test/dsnd_distributions_ah_test-0.1.tar.gz/dsnd_distributions_ah_test-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='dsnd_distributions_ah_test',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['dsnd_distributions_ah_test'],
+      author = 'Alejandro Hohmann',
+      author_email = 'alejandrohohmann@gmail.com',
+      zip_safe=False)
