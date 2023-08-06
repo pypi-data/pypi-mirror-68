1039571bf83476ed70f96a21f1a63cc281dd108d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:02:54 +0000

    Add package Multiple-dummies/1.0/Multiple_dummies-1.0.tar.gz

---
+++ packages/Multiple-dummies/Multiple_dummies-1.0.tar.gz/Multiple_dummies-1.0/setup.py
@@ -0,0 +1,16 @@
+
+from setuptools import setup
+
+setup(name = 'Multiple_dummies',
+      
+      version = '1.0',
+      
+      description = 'Create dummy columns for columns with multiple values',
+      
+      packages = ['Multiple_dummies'],
+      
+      author = 'Rajath Nagaraj',
+      
+      author_email= 'rnagara1@mtu.edu',
+      
+      zip_safe=False)
