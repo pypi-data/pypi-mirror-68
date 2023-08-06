2c41e20f727fc9497f12b40eeabed6e98b454155
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:49 +0000

    Add package aitoai/0.2.1/aitoai-0.2.1.tar.gz

---
+++ packages/aitoai/aitoai-0.2.1.tar.gz/aitoai-0.2.1/aito/cli/main_parser.py
@@ -0,0 +1,62 @@
+from typing import List
+
+import argcomplete
+
+from aito import __version__
+from aito.common.generic_utils import logging_config
+from .parser import ArgParser, ParseError
+from .sub_commands import *
+from typing import Dict, Optional
+import sys
+
+
+class MainParser(ArgParser):
+    _default_sub_commands = [
+        InferTableSchemaSubCommand(),
+        ConvertSubCommand(),
+        DatabaseSubCommand()
+    ]
+
+    def __init__(self, sub_commands: List[SubCommand] = None):
+        super().__init__()
+        self.prog = 'aito'
+        self.add_argument('-V', '--version', action='store_true', help='display the version of this tool')
+        self.add_argument('-v', '--verbose', action='store_true', help='display verbose messages')
+        self.add_argument('-q', '--quiet', action='store_true', help='display only error messages')
+        if not sub_commands:
+            sub_commands = self._default_sub_commands
+        self._sub_commands_map = {cmd.name: cmd for cmd in sub_commands}
+        sub_commands_subparsers = self.add_subparsers(
+            title='command',
+            dest='command',
+            parser_class=ArgParser,
+            metavar="<command>"
+        )
+        for sub_cmd in sub_commands:
+            sub_cmd_parser = sub_commands_subparsers.add_parser(sub_cmd.name, help=sub_cmd.help_message)
+            sub_cmd.build_parser(sub_cmd_parser)
+        argcomplete.autocomplete(self)
+
+    def parse_and_execute(self, parsed_args: Optional[Dict] = None):
+        if parsed_args is None:
+            parsed_args = vars(self.parse_args())
+        if parsed_args['version']:
+            print(__version__)
+            return 0
+        logging_level = 10 if parsed_args['verbose'] else 40 if parsed_args['quiet'] else 20
+        logging_config(level=logging_level)
+
+        sub_command_name = parsed_args['command']
+        if not sub_command_name:
+            self.error('the following arguments are required: <action>')
+
+        try:
+            self._sub_commands_map[sub_command_name].parse_and_execute(parsed_args)
+        except ParseError as e:
+            self.error(e.message)
+        return 0
+
+
+def main():
+    parser = MainParser()
+    sys.exit(parser.parse_and_execute())
---
+++ packages/aitoai/aitoai-0.2.1.tar.gz/aitoai-0.2.1/aito/cli/parser.py
@@ -0,0 +1,149 @@
+import sys
+from abc import ABC, abstractmethod
+from argparse import ArgumentParser, ArgumentTypeError, RawTextHelpFormatter
+from pathlib import Path
+from typing import Union, TextIO
+import logging
+
+
+LOG = logging.getLogger('Parser')
+
+
+class ArgParser(ArgumentParser):
+    def __init__(self, **kwargs):
+        super().__init__(formatter_class=RawTextHelpFormatter, **kwargs)
+
+    def error(self, message):
+        sys.stderr.write(f"error: {message}\n")
+        self.print_help()
+        sys.exit(2)
+
+    def add_aito_default_credentials_arguments(self):
+        args = self.add_argument_group("aito credential arguments")
+        args.add_argument(
+            '-e', '--use-env-file', type=PathArgType(must_exist=True), metavar='env-input-file',
+            help='set up the credentials using a .env file containing the required env variables'
+        )
+        args.add_argument('-i', '--instance-url', type=str, default='.env', help='specify aito instance url')
+        args.add_argument(
+            '-k', '--api-key', type=str, default='.env', help='specify aito read-write or read-only API key'
+        )
+        epilog_str = '''You must provide your Aito credentials to execute database operations.
+If no Aito credential flag is given, the following environment variables are used:
+  AITO_INSTANCE_URL, AITO_API_KEY
+'''
+        if not self.epilog:
+            self.epilog = epilog_str
+        else:
+            self.epilog += epilog_str
+
+    def add_sql_default_credentials_arguments(self, add_use_env_arg: bool = False):
+        """sql connection default arguments
+
+        :param add_use_env_arg: disable use env file to avoid conflict with database aito credentials arguments
+        :return:
+        """
+        args = self.add_argument_group('database connection arguments')
+        if add_use_env_arg:
+            args.add_argument('-e', '--use-env-file', type=PathArgType(must_exist=True), metavar='env-input-file',
+                              help='set up the credentials using a .env file containing the required env variables')
+
+        args.add_argument('--driver', '-D', type=str, help='the name of the ODBC driver', default='.env')
+        args.add_argument('--server', '-s', type=str, help='server to connect to', default='.env')
+        args.add_argument('--port', '-P', type=str, help='port to connect to', default='.env')
+        args.add_argument('--database', '-d', type=str, help='database to connect to', default='.env')
+        args.add_argument('--username', '-u', type=str, help='username for authentication', default='.env')
+        args.add_argument('--password', '-p', type=str, help='password for authentication', default='.env')
+
+        epilog_str = '''If no database credentials flag is given, the following environment variable are used: 
+  SQL_DRIVER, SQL_SERVER, SQL_PORT, SQL_DATABASE, SQL_USERNAME, SQL_PASSWORD
+'''
+        if not self.epilog:
+            self.epilog = epilog_str
+        else:
+            self.epilog += epilog_str
+
+    def add_csv_format_default_arguments(self):
+        self.add_argument('-d', '--delimiter', type=str, default=',', help="delimiter character(default: ',')")
+        self.add_argument('-p', '--decimal', type=str, default='.', help="decimal point character(default '.')")
+
+    def add_excel_format_default_arguments(self):
+        self.add_argument('-o', '--one-sheet', type=str, metavar='sheet-name', help='read a sheet by sheet name')
+
+
+class ParseError(Exception):
+    def __init__(self, message):
+        super().__init__(message)
+        self.message = message
+        LOG.debug(message, stack_info=True)
+
+
+class ArgType(ABC):
+    @abstractmethod
+    def __call__(self, string):
+        pass
+
+
+class PathArgType(ArgType):
+    """Customized PathType instead of argparse.FileType to handle close file more gracefully
+
+    """
+    def __init__(self, parent_must_exist: bool = False, must_exist: bool = False):
+        self.parent_must_exist = parent_must_exist
+        self.must_exist = must_exist
+
+    def __call__(self, string) -> Path:
+        try:
+            path = Path(string)
+        except Exception:
+            raise ArgumentTypeError(f'invalid path: {string}')
+        if self.parent_must_exist and not path.parent.exists():
+            raise ArgumentTypeError(f'{path.parent} does not exist')
+        if self.must_exist and not path.exists():
+            raise ArgumentTypeError(f'{path} does not exist')
+        return path
+
+
+class IOArgType(ArgType):
+    """Input Type Argument that parse into FilePathOrBuffer
+    """
+
+    def __init__(self, default: str = '-'):
+        """
+
+        :param default: special character to signal stdin and stdout
+        """
+        self.default = default
+
+    def __call__(self, string):
+        pass
+
+
+class InputArgType(IOArgType):
+    """parse into sys.stdin if match the default input or a must exist path
+    """
+    def __call__(self, string) -> Union[TextIO, Path]:
+        if string == self.default:
+            return sys.stdin
+        try:
+            path = Path(string)
+        except Exception:
+            raise ArgumentTypeError(f'invalid path: {string}')
+        if not path.exists():
+            raise ArgumentTypeError(f'{path} does not exist')
+        return path
+
+
+class OutputArgType(IOArgType):
+    """parse into sys.stdout if match the default input or a path whose parent must exist
+    """
+    def __call__(self, string) -> Union[TextIO, Path]:
+        if string == self.default:
+            return sys.stdout
+        try:
+            path = Path(string)
+        except Exception:
+            raise ArgumentTypeError(f'invalid path: {string}')
+        if not path.parent.exists():
+            raise ArgumentTypeError(f'{path.parent} does not exist')
+        return path
