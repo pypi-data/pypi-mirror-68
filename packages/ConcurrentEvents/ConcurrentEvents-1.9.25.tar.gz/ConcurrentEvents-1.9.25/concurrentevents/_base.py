8b0e3e7bd82724980c036e60a22b3aec56373523
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:34:56 +0000

    Add package ConcurrentEvents/1.9.25/ConcurrentEvents-1.9.25.tar.gz

---
+++ packages/ConcurrentEvents/ConcurrentEvents-1.9.25.tar.gz/ConcurrentEvents-1.9.25/concurrentevents/_base.py
@@ -0,0 +1,14 @@
+import logging
+
+threads = None
+
+event_manager = None
+
+# Handler Dictionary,
+#   key: Event Class Name
+#   value: List of function objects
+handlers = {}
+
+log = logging.getLogger('__main__')
+
+thread_monitor = {}
