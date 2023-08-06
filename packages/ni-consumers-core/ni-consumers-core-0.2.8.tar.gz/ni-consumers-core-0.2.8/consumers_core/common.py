45bb38052344d750358f549850b05e183b7d0ef8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:51:46 +0000

    Add package ni-consumers-core/0.2.8/ni-consumers-core-0.2.8.tar.gz

---
+++ packages/ni-consumers-core/ni-consumers-core-0.2.8.tar.gz/ni-consumers-core-0.2.8/consumers_core/common.py
@@ -0,0 +1,30 @@
+import requests
+
+
+def extract_informations(headers):
+    tags = headers['tags']
+    tags = list(dict.fromkeys(tags))
+
+    if 'operations' in headers:
+        operations = headers['operations']
+    else:
+        operations = []
+
+    pipeline_id = headers['pipeline']
+    step_id = headers['step']
+
+    return tags, operations, pipeline_id, step_id
+
+
+def resend(body, tags, operations, pipeline_id, step_id, consumer_id, core_url, error=False, reason=None):
+
+    url = f"{core_url}/finish"
+
+    with open("temp.jpeg", "wb") as image:
+        image.write(body)
+
+    with open("temp.jpeg", "rb") as image:
+        files = {"media": image}
+        requests.post(url, files=files,
+                      data={"tags": ",".join(tags), "operations": ",".join(operations), "pipeline_id": pipeline_id,
+                            "step_id": step_id, "consumer_id": consumer_id, "error": True, "reason": reason})
