22c8a3c1928de7ab9b2d580ad0ed1eec10a90a30
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:32 +0000

    Add package fenglei-wheel/0.0.3/fenglei-wheel-0.0.3.tar.gz

---
+++ packages/fenglei-wheel/fenglei-wheel-0.0.3.tar.gz/fenglei-wheel-0.0.3/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+import fenglei_wheel.sub
+
+
+def main():
+    print(fenglei_wheel.sub.d)
+
+
+if __name__ == '__main__':
+    main()
+
+setup(name='fenglei-wheel',
+      version='0.0.3',
+      author_email='673319230@qq.com',
+      description='none',
+      packages=['fenglei_wheel'],
+      install_requires=[],
+      entry_points={
+          'console_scripts': [
+              'a1=fenglei_wheel:a1',
+          ]
+      }
+    )
