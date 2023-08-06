372af89f19650bfe40f6bbaa8af897b520f53eac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:24:42 +0000

    Add package tinderdata/0.3.0/tinderdata-0.3.0-py3-none-any.whl

---
+++ packages/tinderdata/tinderdata-0.3.0-py3-none-any.whl/tinderdata/__init__.py
@@ -0,0 +1,11 @@
+"""
+tinderdata Library
+~~~~~~~~~~~~~~~~~~~
+tinderdata is a utility library, written in Python, to handle the json given by Tinder when requesting your usage data.
+It provides a high-level class with methods to print out insights, or plot more complicated ones.
+
+:copyright: (c) 2019 by Felix Soubelet.
+:license: MIT, see LICENSE for more details.
+"""
+
+from .tinder_data import TinderData
