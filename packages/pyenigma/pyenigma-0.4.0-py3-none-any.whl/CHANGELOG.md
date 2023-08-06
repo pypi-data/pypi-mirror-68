24cdfa4dc95086b1db77d0293ffb2af940fd3545
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:11:22 +0000

    Add package pyenigma/0.4.0/pyenigma-0.4.0-py3-none-any.whl

---
+++ packages/pyenigma/pyenigma-0.4.0-py3-none-any.whl/CHANGELOG.md
@@ -0,0 +1,50 @@
+## Release History
+
+
+### 0.4.0 (2020-05-17)
+
+* new: display the rotor output state
+  [#1](https://todo.sr.ht/~cedric/pyenigma/1);
+* fix: Rotor ROTOR_III_K name (was misspelled as ROTOR_II_K)
+  [1e682f93](https://git.sr.ht/~cedric/pyenigma/commit/1e682f93e92ad5864eecd6c2298279ab8a6d034d).
+
+
+### 0.3.0 (2020-05-11)
+
+* new: Ringstellung feature (a ring that shifts the letters on a given rotor);
+* fix: Rotor IV name (was misspelled as VI);
+* fix: allow en empty plugboard;
+* fix: the rotor must not advance if the character is not a letter.
+
+Thank you to [Rene Devichi](https://github.com/rene-d).
+
+
+### 0.2.4 (2020-01-27)
+
+* Python minimal version is now 3.6.
+
+
+### 0.2.3 (2019-12-30)
+
+* fix command line interface.
+
+
+### 0.2.2 (2019-12-30)
+
+* pyEnigma is ready for use with poetry.
+
+
+### 0.2.1 (2018-12-01)
+
+* dependencies upgraded for CVE-2018-18074;
+* some useless code removed.
+
+
+### 0.2 (2018-02-28)
+
+* pyEnigma is ready for use with pipenv and pipsi.
+
+
+### 0.1 (2017-03-15)
+
+* first release on pypi.
---
+++ packages/pyenigma/pyenigma-0.4.0-py3-none-any.whl/bin/enigma.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import sys
+
+from pyenigma.rotor import *
+from pyenigma.enigma import *
+
+"""A trivial and minimaliste CLI.
+"""
+
+
+def usage():
+    print("Usage:")
+    print('\techo "Hello World" | enigma ABC ref rotor1 rotor2 rotor3 plugboard')
+    print("\nExample:")
+    print(
+        '\t$ echo "Hello World" | enigma ABC A  I II III "AV BS CG DL FU HZ IN KM OW RX"'
+    )
+    print("\tQgqop Vyzxp")
+
+
+def main():
+    # Point of entry in execution mode
+    try:
+        key = sys.argv[1].upper()
+        ref = sys.argv[2].upper()
+        r1 = sys.argv[3].upper()
+        r2 = sys.argv[4].upper()
+        r3 = sys.argv[5].upper()
+        plugs = sys.argv[6].upper()
+        verbose = (sys.argv[7] if 7 < len(sys.argv) else '') in ['-v', '--verbose']
+    except:
+        usage()
+        exit()
+    raw = sys.stdin.read(-1)
+
+    rotors = {
+        "I": ROTOR_I,
+        "II": ROTOR_II,
+        "III": ROTOR_III,
+        "IV": ROTOR_IV,
+        "V": ROTOR_V,
+        "VI": ROTOR_VI,
+        "VII": ROTOR_VII,
+    }
+    reflectors = {
+        "A": ROTOR_Reflector_A,
+        "B": ROTOR_Reflector_B,
+        "C": ROTOR_Reflector_C,
+    }
+
+    if len(key) == 3:  # add the default ringstellung
+        key += "-AAA"
+
+    engr = Enigma(
+        reflectors[ref],
+        rotors[r1],
+        rotors[r2],
+        rotors[r3],
+        key=key[:3],
+        plugs=plugs,
+        ring=key[4:7],
+    )
+    res = engr.encipher(raw).strip()
+    print(res)
+    if verbose:
+        print(
+            '{}{}{} {} {} {} {} "{}"'.format(
+                engr.rotor1.state,
+                engr.rotor2.state,
+                engr.rotor3.state,
+                ref,
+                r1,
+                r2,
+                r3,
+                plugs,
+            ), file=sys.stderr
+        )
