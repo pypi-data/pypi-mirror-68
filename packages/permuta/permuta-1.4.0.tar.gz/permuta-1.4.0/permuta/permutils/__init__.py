16da0fe1e78c44f5a3a58bea9f88edfa94bd7906
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:12:09 +0000

    Add package permuta/1.4.0/permuta-1.4.0.tar.gz

---
+++ packages/permuta/permuta-1.4.0.tar.gz/permuta-1.4.0/permuta/permutils/__init__.py
@@ -0,0 +1,11 @@
+from .insertion_encodable import (
+    is_insertion_encodable,
+    is_insertion_encodable_maximum,
+    is_insertion_encodable_rightmost,
+)
+
+__all__ = [
+    "is_insertion_encodable",
+    "is_insertion_encodable_maximum",
+    "is_insertion_encodable_rightmost",
+]
