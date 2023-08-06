39c175d1f73438f3a78b0837194a9d224b33db8e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:03:05 +0000

    Add package py-gnuplot/0.1/py-gnuplot-0.1.tar.gz

---
+++ packages/py-gnuplot/py-gnuplot-0.1.tar.gz/py-gnuplot-0.1/pygnuplot/__init__.py
@@ -0,0 +1,13 @@
+# $Id: __init__.py,v 1.1.1.1 2020/05/17 10:24:41 ypguo Exp $
+
+"""gnuplot python wrapper."""
+
+__author__ = 'Yongping Guo <guoyoooping@163.com>'
+__copyright__ = 'Copyright (c) 2020 Yongping guo'
+__license__ = 'GPL'
+__url__ = 'http://www.gnuplot.info'
+__version__ = '0.1'
+
+#from gnuplotpy import *
+#import gnuplotpy
+__all__ = ['gnuplot', 'pyplot'] 
