312db3a2c727ef5bb0643e35e1b8c793c50ecde0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:52:28 +0000

    Add package batcave/38.1.2/BatCave-38.1.2.tar.gz

---
+++ packages/batcave/BatCave-38.1.2.tar.gz/BatCave-38.1.2/batcave/__init__.py
@@ -0,0 +1,19 @@
+﻿'Module initializer and generic utilities'
+__all__ = ('__title__', '__summary__', '__uri__',
+           '__version__', '__buildname__', '__builddate__',
+           '__author__', '__email__',
+           '__license__', '__copyright__')
+
+__title__ = 'BatCave'
+__summary__ = 'Python Programming Toolkit'
+__uri__ = 'https://gitlab.com/arisilon/batcave/'
+
+__version__ = '38.1.2'
+__buildname__ = 'BatCave_38.1.2_0'
+__builddate__ = '2020-05-19 02:09:58.447953'
+
+__author__ = 'Jeff Smith'
+__email__ = 'web@pobox.com'
+
+__license__ = 'MIT'
+__copyright__ = 'Copyright (c) 2019 Jeffery G. Smith'
---
+++ packages/batcave/BatCave-38.1.2.tar.gz/BatCave-38.1.2/setup.py
@@ -0,0 +1,58 @@
+'Setuptools setup script for BatCave'
+# cSpell:ignore aarch, psutil pywin
+
+from pathlib import Path
+from setuptools import find_packages, setup  # Always prefer setuptools over distutils
+
+import batcave
+from batcave.fileutil import slurp
+from batcave.sysutil import chmod, S_775
+
+# The files need to be writable
+chmod(Path.cwd(), S_775, True)
+
+setup(
+    name=batcave.__title__,
+    version=batcave.__version__,
+
+    description=batcave.__summary__,
+    long_description=''.join(slurp(Path(__file__).parent / 'README.md')),
+    keywords='python programming utilities',
+
+    author=batcave.__author__,
+    author_email=batcave.__email__,
+    license=batcave.__license__,
+
+    url=batcave.__uri__,
+    # project_urls={
+    #     'Documentation': 'https://gitlab.com/arisilon/batcave/',
+    # },
+
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'License :: OSI Approved :: MIT License',
+
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3.6',
+
+        'Intended Audience :: Developers',
+        'Topic :: Software Development',
+        'Natural Language :: English',
+    ],
+
+    python_requires='>=3.6',
+    packages=find_packages(),
+    install_requires=['docker >= 4.0',
+                      'GitPython >= 3.0',
+                      'google-cloud',
+                      'kubernetes >= 10.0',
+                      'PyQt5 >= 5.13; platform_machine != "aarch64"',
+                      'psutil >= 5.6',
+                      'pywin32 >= 225; sys_platform == "win32"',
+                      'requests >= 2.22',
+                      'WMI >= 1.4, < 1.5'],
+    extras_require={
+        'dev': ['setuptools', 'twine', 'wheel', 'xmlrunner'],
+        'test': [],
+    }
+)
