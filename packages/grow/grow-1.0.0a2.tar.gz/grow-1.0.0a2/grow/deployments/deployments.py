1ecf09aa70cb6029208396bb59b5add7230df471
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:22:44 +0000

    Add package grow/1.0.0a2/grow-1.0.0a2.tar.gz

---
+++ packages/grow/grow-1.0.0a2.tar.gz/grow-1.0.0a2/grow/deployments/deployments.py
@@ -0,0 +1,44 @@
+from .destinations import git_destination
+from .destinations import local
+from .destinations import scp
+from .destinations import webreview_destination
+from protorpc import protojson
+import json
+
+_destination_kinds_to_classes = None
+
+_builtins = (
+    webreview_destination.WebReviewDestination,
+    webreview_destination.LegacyJetwayDestination,
+    git_destination.GitDestination,
+    local.LocalDestination,
+    scp.ScpDestination)
+
+
+def register_destination(class_obj):
+    _destination_kinds_to_classes[class_obj.KIND] = class_obj
+
+
+def register_builtins():
+    global _destination_kinds_to_classes
+    if _destination_kinds_to_classes is None:
+        _destination_kinds_to_classes = {}
+    for builtin in _builtins:
+        register_destination(builtin)
+
+
+def make_deployment(kind, config, name='default'):
+    if _destination_kinds_to_classes is None:
+        register_builtins()
+    class_obj = _destination_kinds_to_classes.get(kind)
+    if class_obj is None:
+        raise ValueError('No configuration exists for "{}".'.format(kind))
+    if isinstance(config, dict):
+        config = json.dumps(config)
+        config = config_from_json(class_obj, config)
+    return class_obj(config, name=name)
+
+
+def config_from_json(deployment_class, content):
+    config_class = deployment_class.Config
+    return protojson.decode_message(config_class, content)
