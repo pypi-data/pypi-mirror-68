e5a4fc68e51276ad85f5ef614d84c6e70f485dcb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:24:26 +0000

    Add package pycellfit/0.0.6/pycellfit-0.0.6-py3-none-any.whl

---
+++ packages/pycellfit/pycellfit-0.0.6-py3-none-any.whl/pycellfit/pycellfit.py
@@ -0,0 +1,15 @@
+def hello():
+    """
+    simple hello world function to test initial commit
+    """
+
+    print("Hello World!")
+    return "hello new"
+
+def hello2():
+    """
+    simple hello world2 function to test initial commit
+    """
+
+    print("Hello World!2")
+    return "hello new"
