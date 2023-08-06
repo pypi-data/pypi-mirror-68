437b90003a7765162eee1f5f369490eb7c234401
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:37:31 +0000

    Add package rfhub2/0.20/rfhub2-0.20.tar.gz

---
+++ packages/rfhub2/rfhub2-0.20.tar.gz/rfhub2-0.20/rfhub2/static/precache-manifest.99d46d3ed6a6223314825bbbeedce890.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "087c5d5c2891a1276f62816863f4a315",
+    "url": "/static/index.html"
+  },
+  {
+    "revision": "42d411adfd65262b89d0",
+    "url": "/static/static/css/main.fc37acba.chunk.css"
+  },
+  {
+    "revision": "67f55e51f90cfd77b4a2",
+    "url": "/static/static/js/2.eaaa614f.chunk.js"
+  },
+  {
+    "revision": "42d411adfd65262b89d0",
+    "url": "/static/static/js/main.1ff5d20b.chunk.js"
+  },
+  {
+    "revision": "517784e95ca40345faae",
+    "url": "/static/static/js/runtime~main.e6c9c1d2.js"
+  }
+]);
