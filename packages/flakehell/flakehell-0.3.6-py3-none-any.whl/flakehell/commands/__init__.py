42e039cb5a9b9d62ce408402c8566fd74a6aeec8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:25:35 +0000

    Add package flakehell/0.3.6/flakehell-0.3.6-py3-none-any.whl

---
+++ packages/flakehell/flakehell-0.3.6-py3-none-any.whl/flakehell/commands/__init__.py
@@ -0,0 +1,30 @@
+from types import MappingProxyType
+
+from ._baseline import baseline_command
+from ._code import code_command
+from ._codes import codes_command
+from ._lint import lint_command
+from ._missed import missed_command
+from ._plugins import plugins_command
+
+
+__all__ = [
+    'COMMANDS',
+
+    'baseline_command',
+    'code_command',
+    'codes_command',
+    'lint_command',
+    'missed_command',
+    'plugins_command',
+]
+
+
+COMMANDS = MappingProxyType(dict(
+    baseline=baseline_command,
+    code=code_command,
+    codes=codes_command,
+    lint=lint_command,
+    missed=missed_command,
+    plugins=plugins_command,
+))
