21a4bc2f28056578d4b65fcad102e9a991d38ab3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:10:03 +0000

    Add package feynplots/0.0.1/feynplots-0.0.1.tar.gz

---
+++ packages/feynplots/feynplots-0.0.1.tar.gz/feynplots-0.0.1/feynplots/__init__.py
@@ -0,0 +1,5 @@
+from .main import model_chart
+
+__all__ = [
+    'model_chart'
+]
---
+++ packages/feynplots/feynplots-0.0.1.tar.gz/feynplots-0.0.1/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+# Layout & format inspired from https://github.com/navdeep-G/samplemod/blob/master/setup.py
+
+
+
+with open('README.md') as f:
+    readme = f.read()
+
+with open('LICENSE') as f:
+    license = f.read()
+
+requirements = ['feyn == 1.1.*']
+
+setup(
+    name='feynplots',
+    version='0.0.1',
+    description='Charting Feyn graphs',
+    long_description=readme,
+    author='Chris Cave',
+    author_email='chris.cave@abzu.ai',
+    url='https://github.com/chriscave/cave-plots',
+    license=license,
+    packages=find_packages(),
+    install_requires=requirements
+)
