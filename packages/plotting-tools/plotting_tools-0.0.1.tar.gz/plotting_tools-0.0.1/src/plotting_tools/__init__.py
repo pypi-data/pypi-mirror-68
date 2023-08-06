75a287138d277e05cbe3e470ed46f36149417f7e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:11:17 +0000

    Add package plotting-tools/0.0.1/plotting_tools-0.0.1.tar.gz

---
+++ packages/plotting-tools/plotting_tools-0.0.1.tar.gz/plotting_tools-0.0.1/src/plotting_tools/__init__.py
@@ -0,0 +1,14 @@
+"""Top-level package for Python Plotting Tools."""
+
+# Do not edit this string manually, always use bumpversion
+# Details in CONTRIBUTING.md
+__version__ = "0.0.1"
+
+__author__ = "Lara Kassab"
+__email__ = "larakassab01@gmail.com"
+
+__license__ = "MIT"
+__copyright__ = "Copyright (c) 2020, Lara Kassab"
+
+
+from .example import Example  # noqa: F401
