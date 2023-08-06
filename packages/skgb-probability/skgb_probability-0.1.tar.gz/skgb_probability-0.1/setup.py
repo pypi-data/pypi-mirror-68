35c079efce8b4002a154c4ad1430c3b2f83f7812
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:03:22 +0000

    Add package skgb-probability/0.1/skgb_probability-0.1.tar.gz

---
+++ packages/skgb-probability/skgb_probability-0.1.tar.gz/skgb_probability-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='skgb_probability',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['skgb_probability'],
+      author = 'Sumit Kumar',
+      author_email = 'sksumit068@gmail.com',
+      zip_safe=False)
