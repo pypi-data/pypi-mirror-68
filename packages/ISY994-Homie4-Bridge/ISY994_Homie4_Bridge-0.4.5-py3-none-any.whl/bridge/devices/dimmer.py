9574d5983d114b4a2430f1ef0d181b44d01e15d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:56 +0000

    Add package ISY994-Homie4-Bridge/0.4.5/ISY994_Homie4_Bridge-0.4.5-py3-none-any.whl

---
+++ packages/ISY994-Homie4-Bridge/ISY994_Homie4_Bridge-0.4.5-py3-none-any.whl/bridge/devices/dimmer.py
@@ -0,0 +1,33 @@
+#! /usr/bin/env python
+
+from homie.device_dimmer import Device_Dimmer
+from .base import Base
+
+from homie.node.property.property_string import Property_String
+
+class Dimmer(Base,Device_Dimmer):
+
+    def __init__(self, isy_device=None,homie_settings=None, mqtt_settings=None):
+
+        Base.__init__ (self,isy_device)
+
+        Device_Dimmer.__init__ (self,self.get_homie_device_id(), isy_device.name, homie_settings, mqtt_settings)
+        
+        node = self.get_node ('dimmer')
+        self.paddle = Property_String(node,'paddle-action','Paddle Action')
+        node.add_property(self.paddle)
+
+        level = self.isy_device.get_property('level')
+        if level is not None:
+            self.property_change('level',level)
+
+    def property_change(self,property_,value):
+        if property_ == 'level':
+            self.update_dimmer(value)
+        elif property_ == 'paddle_action':
+            self.paddle.value = value
+
+        Base.property_change (self,property_,value)
+
+    def set_dimmer(self,level):
+        self.isy_device.set_level (level)
