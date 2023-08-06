92c4e7de44ef27a8aa857402208256f3ca3238cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:27:33 +0000

    Add package Adyen/2.3.0/Adyen-2.3.0.tar.gz

---
+++ packages/Adyen/Adyen-2.3.0.tar.gz/Adyen-2.3.0/Adyen/settings.py
@@ -0,0 +1,13 @@
+# Those constants are used from the library only
+BASE_PAL_URL = "https://pal-{}.adyen.com/pal/servlet"
+BASE_HPP_URL = "https://{}.adyen.com/hpp"
+ENDPOINT_CHECKOUT_TEST = "https://checkout-test.adyen.com"
+ENDPOINT_CHECKOUT_LIVE_SUFFIX = "https://{}-checkout-live" \
+                                ".adyenpayments.com/checkout"
+API_CHECKOUT_VERSION = "v49"
+API_CHECKOUT_UTILITY_VERSION = "v1"
+API_RECURRING_VERSION = "v25"
+API_PAYMENT_VERSION = "v49"
+API_PAYOUT_VERSION = "v30"
+LIB_VERSION = "2.3.0"
+LIB_NAME = "adyen-python-api-library"
