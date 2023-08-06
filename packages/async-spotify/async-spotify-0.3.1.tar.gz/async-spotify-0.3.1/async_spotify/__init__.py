c7a13c931b3e79fdec93bc1ace24db14c23d9954
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:17:22 +0000

    Add package async-spotify/0.3.1/async-spotify-0.3.1.tar.gz

---
+++ packages/async-spotify/async-spotify-0.3.1.tar.gz/async-spotify-0.3.1/async_spotify/__init__.py
@@ -0,0 +1,23 @@
+"""
+Asynchronous spotify api wrapper
+"""
+
+# ##################################################################################################
+#  Copyright (c) 2020. HuiiBuh                                                                     #
+#  This file (__init__.py) is part of AsyncSpotify which is released under MIT.                    #
+#  You are not allowed to use this code or this file for another project without                   #
+#  linking to the original source.                                                                 #
+# ##################################################################################################
+
+
+__title__ = 'async_spotify'
+__author__ = 'HuiiBuh'
+__version__ = '0.3.1'
+__license__ = 'MIT'
+
+from .api.spotify_api_preferences import SpotifyApiPreferences
+from .api.spotify_api_client import SpotifyApiClient
+from .authentification.spotify_authorization_token import SpotifyAuthorisationToken
+from .authentification.spotify_cookies import SpotifyCookie
+from .spotify_errors import SpotifyError, RateLimitExceeded, TokenExpired, SpotifyAPIError, SpotifyBaseError
+from .token_renew_class import TokenRenewClass
