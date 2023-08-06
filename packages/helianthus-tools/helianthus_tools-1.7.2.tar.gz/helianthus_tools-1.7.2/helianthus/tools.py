901c8a981e1fea095902f095cba06657aab3d974
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:57 +0000

    Add package helianthus-tools/1.7.2/helianthus_tools-1.7.2.tar.gz

---
+++ packages/helianthus-tools/helianthus_tools-1.7.2.tar.gz/helianthus_tools-1.7.2/helianthus/tools.py
@@ -0,0 +1,16 @@
+# -*- coding: UTF-8 -*-
+
+def f2x(filepath):
+	"""
+	file to \x
+	worked all python version
+    return: bytes
+	"""
+    import os
+    result = b""
+    with open(filepath, "rb") as f:
+        for _ in range(os.path.getsize(filepath)):
+            item = str(hex(ord(f.read(1))))
+            item = r"\x" + item[2:].rjust(2, "0").upper()
+            result += item
+    return result
