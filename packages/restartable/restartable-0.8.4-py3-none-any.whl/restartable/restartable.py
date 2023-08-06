656b72797178d707f0c3651691ac911cd4986a11
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:12:29 +0000

    Add package restartable/0.8.4/restartable-0.8.4-py3-none-any.whl

---
+++ packages/restartable/restartable-0.8.4-py3-none-any.whl/restartable/restartable.py
@@ -0,0 +1,165 @@
+#!/usr/bin/env python3
+#
+# Copyright 2019 Ricardo Branco <rbranco@suse.de>
+# MIT License
+#
+"""
+list restartable programs or services using deleted libraries
+"""
+
+import os
+import re
+import sys
+from argparse import ArgumentParser
+
+from restartable.procfs import Proc, ProcPid
+from restartable import __version__
+
+
+USAGE = """%s [OPTIONS]
+
+List running processes using files deleted by recent upgrades
+
+Options:
+    -h, --help      Get help
+    -V, --version   Show version and exit
+    -P, --proc PROC_DIRECTORY
+    -s, --short
+         Create a short table not showing the deleted files. Given twice,
+         show only processes which are associated with a system service.
+         Given three times, list the associated system service names only.
+    -v, --verbose   Show the complete command line
+""" % os.path.basename(sys.argv[0])
+
+# Ignore deleted files in these directories
+IGNORE = (
+    '/dev',
+    '/run',
+    '/ ',
+)
+
+# Regular expression to find systemd service unit in /proc/<pid>/cgroup
+SYSTEMD_REGEX = r"\d+:name=systemd:/system\.slice/(?:.*/)?(.*)\.service$"
+
+# Regular expression to match scripting languages
+SCRIPT_REGEX = r"((perl|python|(ruby\.)?ruby)(\d?(\.\d)?)|(a|ba|c|da|fi|k|pdk|tc|z)?sh)$"
+
+FORMAT_STRING = "%s\t%s\t%s\t%-20s\t%20s\t%s"
+
+DELETED = " (deleted)"
+
+opts = None
+services = set()
+
+
+def guess_command(proc):
+    """
+    Guess the command being run
+    """
+    if opts.verbose:
+        # cmdline is empty if zombie, but zombies have void proc.maps
+        if not proc.cmdline[0].startswith('/') and proc.exe:
+            # Use full path
+            exe = proc.exe
+            if exe.endswith(DELETED):
+                exe = exe[:-len(DELETED)]  # Strip " (deleted)"
+            if os.path.basename(proc.cmdline[0]) == os.path.basename(exe):
+                return " ".join([exe] + proc.cmdline[1:])
+        return " ".join(proc.cmdline)
+    cmdline = proc.status.Name
+    # The command may be truncated to 15 chars
+    #   in /proc/<pid>/{comm,stat,status}
+    # Also, kernel usermode helpers use "none"
+    if (len(cmdline) == 15 or cmdline == "none") and proc.cmdline[0]:
+        cmdline = proc.cmdline[0]
+    # If running a script, get the name of the script instead of the interpreter
+    if re.match(SCRIPT_REGEX, os.path.basename(cmdline.split()[0])):
+        # Skip options
+        for arg in proc.cmdline[1:]:
+            if not arg.startswith('-'):
+                if (arg.startswith('/') and os.path.isfile(arg)
+                        or os.path.isfile(os.path.join(proc.cwd, arg))):
+                    cmdline = arg
+                    break
+    if cmdline.startswith('/'):
+        return os.path.basename(cmdline)
+    return cmdline.split()[0]
+
+
+def print_info(proc, deleted):
+    """
+    Print information
+    """
+    try:
+        service = re.findall(SYSTEMD_REGEX, proc.cgroup, re.MULTILINE)[0]
+    except IndexError:
+        if opts.short > 1:
+            return
+        service = "-"
+    if opts.short > 2:
+        services.add(service)
+    else:
+        uid = proc.status.Uid.real
+        username = uid.name
+        cmdline = guess_command(proc)
+        print(FORMAT_STRING % (
+            proc.pid, proc.status.PPid, uid, username, service, cmdline))
+    if not opts.short:
+        for path in sorted(deleted):
+            print("\t%s" % path)
+
+
+def main():
+    """
+    Main function
+    """
+    argparser = ArgumentParser(usage=USAGE, add_help=False)
+    argparser.add_argument('-h', '--help', action='store_true')
+    argparser.add_argument('-P', '--proc', default='/proc')
+    argparser.add_argument('-s', '--short', action='count', default=0)
+    argparser.add_argument('-v', '--verbose', action='store_true')
+    argparser.add_argument('-V', '--version', action='store_true')
+    global opts
+    opts = argparser.parse_args()
+    if opts.help:
+        print(USAGE)
+        sys.exit(0)
+    elif opts.version:
+        print(__version__)
+        sys.exit(0)
+
+    if os.geteuid() != 0:
+        print("WARN: Run this program as root", file=sys.stderr)
+
+    if not os.path.isdir(opts.proc):
+        print("ERROR: %s: No such directory" % opts.proc, file=sys.stderr)
+        sys.exit(1)
+
+    if opts.short < 3:
+        print(FORMAT_STRING % (
+            "PID", "PPID", "UID", "User", "Service", "Command"))
+    for pid in Proc().pids():
+        try:
+            with ProcPid(pid, proc=opts.proc) as proc:
+                # Get deleted executable mappings
+                deleted = {
+                    _['pathname'][:-len(DELETED)]
+                    for _ in proc.maps
+                    if (_['pathname']
+                        and 'x' in _['perms']
+                        and _['pathname'].endswith(DELETED)
+                        and not _['pathname'].startswith(IGNORE))
+                }
+                if deleted:
+                    print_info(proc, deleted)
+        except OSError:
+            pass
+    if opts.short > 2:
+        print("\n".join(sorted(services)))
+
+
+if __name__ == "__main__":
+    try:
+        main()
+    except KeyboardInterrupt:
+        sys.exit(1)
---
+++ packages/restartable/restartable-0.8.4-py3-none-any.whl/restartable/utils.py
@@ -0,0 +1,274 @@
+#
+# Copyright 2019,2020 Ricardo Branco <rbranco@suse.de>
+# MIT License
+#
+"""
+Module for AttrDict
+"""
+import json
+import os
+import stat
+import threading
+from collections import UserDict, UserString
+from datetime import datetime
+from ipaddress import ip_address, IPv4Address, IPv6Address
+from socket import htonl
+from weakref import WeakValueDictionary
+from pwd import getpwuid
+from grp import getgrgid
+from sys import byteorder
+
+
+def try_int(string):
+    """
+    Return an integer if possible, else string
+    """
+    # Ignore octal & hexadecimal
+    if len(string) > 1 and string[0] == '0' and string.isdigit():
+        return string
+    try:
+        return int(string)
+    except ValueError:
+        return string
+
+
+def sorted_alnum(list_):
+    """
+    Returns a list sorted in-place alphanumerically.
+    Useful for directories, like /proc, that contain pids and other files
+    """
+    # Sort alphabetically
+    list_.sort()
+    # Sort numerically
+    list_.sort(key=lambda x: int(x) if x.isdigit() else float('inf'))
+    return list_
+
+
+class Property:
+    """
+    Simple cached-property class decorator that works with partialmethod
+    """
+    def __init__(self, fget=None, name=None):
+        self.fget = fget
+        self.name = fget.__name__ if name is None else name
+        self.lock = threading.RLock()
+
+    def __get__(self, obj, objtype=None):
+        if obj is None:
+            return self
+        if self.name not in obj:
+            with self.lock:
+                if self.name not in obj:
+                    if callable(self.fget):
+                        obj[self.name] = self.fget(obj)
+                    else:   # partialmethod
+                        obj[self.name] = self.fget.func(obj, *self.fget.args, **self.fget.keywords)
+        return obj[self.name]
+
+    def __set__(self, obj, value):
+        raise AttributeError
+
+
+class Singleton:    # pylint: disable=no-member
+    """
+    Singleton decorator to avoid having multiple objects handling the same args
+    """
+    def __new__(cls, klass):
+        if issubclass(klass, int):
+            dict_ = dict()
+        else:
+            # We must use WeakValueDictionary() to let the instances be garbage-collected
+            dict_ = WeakValueDictionary()
+        _dict = dict(cls.__dict__, **{'cls': klass, 'instances': dict_})
+        singleton = type(klass.__name__, cls.__bases__, _dict)
+        obj = super().__new__(singleton)
+        obj.lock = threading.RLock()
+        return obj
+
+    def __instancecheck__(self, other):
+        return isinstance(other, self.cls)
+
+    def __call__(self, *args, **kwargs):
+        key = (args, frozenset(kwargs.items()))
+        if key not in self.instances:
+            with self.lock:
+                if key not in self.instances:
+                    instance = self.cls.__call__(*args, **kwargs)
+                    self.instances[key] = instance
+        return self.instances[key]
+
+
+@Singleton
+class Uid(int):
+    """
+    Class to hold user ID's
+    """
+    def __init__(self, uid):
+        super().__init__()
+        self.uid = int(uid)
+        self._name = ""
+
+    @property
+    def name(self):
+        if not self._name:
+            try:
+                self._name = getpwuid(self.uid).pw_name
+            except KeyError:
+                self._name = str(self.uid)
+        return self._name
+
+
+@Singleton
+class Gid(int):
+    """
+    Class to hold user ID's
+    """
+    def __init__(self, gid):
+        super().__init__()
+        self.gid = int(gid)
+        self._name = ""
+
+    @property
+    def name(self):
+        if not self._name:
+            try:
+                self._name = getgrgid(self.gid).gr_name
+            except KeyError:
+                self._name = str(self.gid)
+        return self._name
+
+
+class Time(UserString):
+    """
+    Class for time objects
+    """
+    def __init__(self, arg):
+        super().__init__(arg)
+        self.datetime = datetime.fromtimestamp(float(self.data))
+        self.data = self.datetime.ctime()
+
+
+@Singleton
+class IPAddr(UserString):
+    """
+    Class for IPv4/6 address objects
+    """
+    def __init__(self, arg):
+        super().__init__(arg)
+        try:
+            self.ip_address = ip_address(self.data)
+        except ValueError:
+            if len(self.data) == 8:
+                self.ip_address = IPv4Address(htonl(int(self.data, base=16)))
+            elif byteorder == "big":
+                self.ip_address = IPv6Address(int(self.data, base=16))
+            elif byteorder == "little":
+                self.ip_address = IPv6Address(
+                    htonl(int(self.data[:8], base=16)) << 24
+                    | htonl(int(self.data[8:16], base=16)) << 16
+                    | htonl(int(self.data[16:24], base=16)) << 8
+                    | htonl(int(self.data[24:], base=16)))
+        self.data = self.ip_address.compressed
+
+
+class Pathname(UserString):
+    """
+    Class for pathnames
+    """
+    def __new__(cls, arg):
+        if arg is None:
+            return None
+        return super().__new__(cls)
+
+    def __init__(self, arg):
+        super().__init__(arg)
+        self.raw = self.data
+        # TODO: There are lots of funky characters that can mess with the terminal  # pylint: disable=fixme
+        self.data = self.data.replace("\r", "\\r").replace("\n", "\\n")
+
+
+class AttrDict(UserDict):
+    """
+    Class for accessing dictionary keys with attributes
+    """
+    def __getattr__(self, attr):
+        if attr.startswith('__') and attr.endswith('__'):
+            raise AttributeError    # Make help() work
+        try:
+            return self.__getitem__(attr)
+        except KeyError as e:
+            raise AttributeError(e)
+
+    def __delattr__(self, attr):
+        self.__delitem__(attr)
+
+
+class FSDict(AttrDict):
+    """
+    Class for capturing a directory structure into a dictionary
+    """
+    _dir_fd = None
+    _path = None
+    _handler = None
+
+    def __init__(self, path="", dir_fd=None, handler=None):
+        if dir_fd is not None:
+            self._dir_fd = dir_fd
+        self._path = path
+        self._handler = handler
+        super().__init__()
+
+    def __repr__(self):
+        return "%s(path=%s, dir_fd=%s, handler=%s)" % (
+            type(self).__name__, self._path if self._path else '""', self._dir_fd, self._handler)
+
+    def __getitem__(self, item):
+        value = super().__getitem__(item)
+        super().__setitem__(item, value)
+        return value
+
+    def _opener(self, path, flags):
+        return os.open(path, flags, dir_fd=self._dir_fd)
+
+    def _lsdir(self, path):
+        """
+        Returns os.listdir() on path
+        """
+        if not path:
+            return sorted_alnum(os.listdir(self._dir_fd))
+        dir_fd = os.open(path, os.O_RDONLY | os.O_DIRECTORY, dir_fd=self._dir_fd)
+        try:
+            listing = os.listdir(dir_fd)
+        except OSError as err:
+            raise err
+        finally:
+            os.close(dir_fd)
+        return sorted_alnum(listing)
+
+    def __missing__(self, path):
+        """
+        Get contents from file, symlink or directory
+        """
+        if self._handler:
+            return self._handler(path)
+        path = os.path.join(self._path, path)
+        mode = os.lstat(path, dir_fd=self._dir_fd).st_mode
+        if stat.S_ISLNK(mode):
+            return os.readlink(path, dir_fd=self._dir_fd)
+        if stat.S_ISREG(mode):
+            with open(path, opener=self._opener) as file:
+                return file.read()
+        if stat.S_ISDIR(mode):
+            return FSDict(path=path, dir_fd=self._dir_fd)
+        return None
+
+
+class CustomJSONEncoder(json.JSONEncoder):
+    """
+    JSON Encoder for the objects defined here
+    Use like this: json.dumps(obj, cls=CustomJSONEncoder)
+    """
+    def default(self, obj):     # pylint: disable=method-hidden,arguments-differ
+        if isinstance(obj, (IPAddr, Uid, Gid, Time, AttrDict, Pathname)):
+            return obj.data
+        return json.JSONEncoder.default(self, obj)
