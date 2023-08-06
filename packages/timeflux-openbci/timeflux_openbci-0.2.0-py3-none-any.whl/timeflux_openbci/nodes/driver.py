6472b10c2525dba5fad8a8f8b651a586392fa7a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:11:10 +0000

    Add package timeflux-openbci/0.2.0/timeflux_openbci-0.2.0-py3-none-any.whl

---
+++ packages/timeflux-openbci/timeflux_openbci-0.2.0-py3-none-any.whl/timeflux_openbci/nodes/driver.py
@@ -0,0 +1,27 @@
+from timeflux_brainflow.nodes.driver import BrainFlow
+
+
+class OpenBCI(BrainFlow):
+
+    """OpenBCI driver.
+
+    This plugin provides a unified interface for all OpenBCI boards.
+
+    Attributes:
+        o (Port): Default output, provides DataFrame.
+
+    Args:
+        board (string): The OpenBCI board type.
+            Allowed values: ``synthetic``, ``cyton``, ``ganglion``, ``cyton_daisy``,
+            ``ganglion_wifi``, ``cyton_wifi``, ``cyton_daisy_wifi``
+        channels (list): The EEG channel labels.
+            If not set, incrementing numbers will be used.
+        debug (boolean): Print debug messages.
+        **kwargs: The parameters specific for each board.
+            Allowed arguments: ``serial_port``, ``mac_address``, ``ip_address``,
+            ``ip_port``, ``ip_protocol``.
+
+    Example:
+        .. literalinclude:: /../examples/synthetic.yaml
+           :language: yaml
+    """
