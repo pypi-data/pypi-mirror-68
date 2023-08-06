a48a2b0226f1ab0abb437c421f226f7d1cbfd1b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:07 +0000

    Add package staxapp/1.0.1/staxapp-1.0.1-py3-none-any.whl

---
+++ packages/staxapp/staxapp-1.0.1-py3-none-any.whl/staxapp/exceptions.py
@@ -0,0 +1,39 @@
+import logging
+from json.decoder import JSONDecodeError
+
+
+class ApiException(Exception):
+    def __init__(self, message, response, detail=""):
+        try:
+            if response.json().get("Error"):
+                logging.error(f"{response.status_code}: {response.json()}")
+                self.message = f"Api Exception: {response.status_code} -{detail} {response.json()['Error']}"
+            else:
+                logging.error(f"{response.status_code}: {response.json()}")
+                self.message = f"Api Exception:{detail} {message}"
+        except JSONDecodeError:
+            if response.content:
+                logging.error(f"{response.status_code}: {response.content}")
+            else:
+                logging.error(f"{response.status_code}: {message}")
+            self.message = f"Api Exception:{detail} {message}"
+
+    def __str__(self):
+        return self.message
+
+
+class ValidationException(Exception):
+    def __init__(self, message):
+        self.message = message
+
+
+class InvalidCredentialsException(Exception):
+    def __init__(self, message, detail=""):
+        logging.error(message)
+        prefix = f"InvalidCredentialsException: "
+        if detail:
+            prefix = f"{prefix}{detail} - "
+        self.message = f"{prefix}{message}"
+
+    def __str__(self):
+        return self.message
