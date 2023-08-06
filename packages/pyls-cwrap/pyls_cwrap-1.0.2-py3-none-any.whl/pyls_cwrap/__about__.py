16ec0ce3b30df56eddc5f6ddebfb3d71d52cd4cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:51:34 +0000

    Add package pyls-cwrap/1.0.2/pyls_cwrap-1.0.2-py3-none-any.whl

---
+++ packages/pyls-cwrap/pyls_cwrap-1.0.2-py3-none-any.whl/pyls_cwrap/__about__.py
@@ -0,0 +1,10 @@
+__title__ = "pyls-cwrap"
+__version__ = "1.0.2"
+
+__summary__ = "cwrap plugin for python-language-server"
+__uri__ = "https://github.com/daizutabi/pyls-cwrap"
+__author__ = "daizutabi"
+__email__ = "daizutabi@gmail.com"
+
+__license__ = "MIT"
+__copyright__ = "Copyright 2019 %s" % __author__
