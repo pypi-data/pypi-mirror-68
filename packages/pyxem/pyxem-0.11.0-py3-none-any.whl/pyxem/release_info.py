c3b862234f8e5fc2eb8009585bd48fe1c39d84f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:12:52 +0000

    Add package pyxem/0.11.0/pyxem-0.11.0-py3-none-any.whl

---
+++ packages/pyxem/pyxem-0.11.0-py3-none-any.whl/pyxem/release_info.py
@@ -0,0 +1,32 @@
+name = "pyxem"
+version = "0.11.0"
+author = "Duncan Johnstone, Phillip Crout"
+copyright = "copyright 2016-2020, the pyxem developers"
+credits = [
+    "Duncan Johnstone",
+    "Phillip Crout",
+    "Joonatan Laulainen",
+    "Simon Høgås",
+    "Ben Martineau",
+    "Tina Bergh",
+    "Stef Smeets",
+    "Carter Francis",
+    "Eirik Opheim",
+    "Eric Prestat",
+    "Sean Collins",
+    "Tom Furnival",
+    "Mohsen Danaie",
+    "Håkon Wiik Ånes",
+    "Tiarnan Doherty",
+    "Affan Iqbal",
+    "Jedrzej Morzy",
+    "Tomas Ostasevicius",
+    "Rob Tovey",
+    "Matt von Lany",
+    "Tim Poon",
+    "Endre Jacobsen",
+]
+license = "GPLv3"
+maintainer = "Duncan Johnstone, Phillip Crout"
+email = "pyxem.team@gmail.com"
+status = "development"
