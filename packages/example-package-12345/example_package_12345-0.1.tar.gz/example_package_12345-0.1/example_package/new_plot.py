b7cd00c81856f1d7d10987107a889933fcc1b9b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:33 +0000

    Add package example-package-12345/0.1/example_package_12345-0.1.tar.gz

---
+++ packages/example-package-12345/example_package_12345-0.1.tar.gz/example_package_12345-0.1/example_package/new_plot.py
@@ -0,0 +1,33 @@
+
+dna_linker_length = 52
+"""bp, from so-and-so paper [links](work-like-this.com)"""
+
+def b_function(x):
+    """
+    Some docs for a function.
+
+    Parameters
+    ----------
+    x : float
+        The number that will be squared.
+
+    Returns
+    -------
+    The square of the number.
+    """
+    pass
+
+def a_function(x):
+    """
+    Some docs for a function.
+
+    Parameters
+    ----------
+    x : float
+        The number that will be squared.
+
+    Returns
+    -------
+    The square of the number.
+    """
+    return x**4
---
+++ packages/example-package-12345/example_package_12345-0.1.tar.gz/example_package_12345-0.1/setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='example_package_12345',
+      version="0.1",
+      description='Example package',
+      long_description="Example package",
+      author='Bruno Beltran',
+      author_email='brunobeltran0@gmail.com',
+      packages=['example_package'],
+      license='MIT',
+      classifiers=[
+          'Development Status :: 2 - Pre-Alpha',
+          'Intended Audience :: Developers',
+          'Intended Audience :: Science/Research',
+          'License :: OSI Approved :: MIT License',
+          'Topic :: Utilities'
+      ],
+      keywords='parameter scanning scientific search testing',
+      url='https://github.com/brunobeltran/example_package',
+)
