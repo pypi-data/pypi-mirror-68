35c5e1a4f66d3a9c7e24f4ba81171da62e90661b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:20:40 +0000

    Add package venvipy/0.2.8/venvipy-0.2.8.tar.gz

---
+++ packages/venvipy/venvipy-0.2.8.tar.gz/venvipy-0.2.8/README.rst
@@ -0,0 +1,168 @@
+.. image:: https://raw.githubusercontent.com/sinusphi/venvipy/master/img/cover_slim.png
+
+**A GUI for managing multiple Python virtual environments**
+
+.. image:: https://img.shields.io/badge/pypi-v0.2.8-blue?logo=pypi&logoColor=FFE873
+    :target: https://pypi.org/project/venvipy/0.2.8/#description
+
+.. image:: https://img.shields.io/badge/python-3.7+-blue?logo=python&logoColor=FFE873
+    :target: https://www.python.org/downloads
+
+.. image:: https://img.shields.io/badge/pyqt-5.14.0-darkgreen
+    :target: https://pypi.org/project/PyQt5
+
+.. image:: https://img.shields.io/pypi/dm/venvipy?color=brightgreen
+    :target: https://pypistats.org/packages/venvipy
+
+.. image:: https://img.shields.io/badge/platform-linux-darkblue?logo=linux&logoColor=FFE873
+    :target: https://www.linux.org/pages/download
+
+.. image:: https://img.shields.io/badge/code%20style-black-000000
+    :target: https://github.com/psf/black
+
+.. image:: https://img.shields.io/badge/license-MIT-darkviolet
+    :target: https://github.com/sinusphi/venvipy/blob/master/LICENSE
+
+.. image:: https://img.shields.io/travis/sinusphi/venvipy/master?label=Travis%20CI&logo=travis
+    :target: https://travis-ci.org/sinusphi/venvipy
+
+
+Introduction
+------------
+
+*VenviPy* is a user friendly graphical interface for creating customized
+virtual environments or modifing any existing Python environment (that
+supports the built-in venv) quick and easy.
+
+It provides useful features like a wizard, that guides the user through
+the creation process, a table that shows an overview over installed
+environments in a specific directory and a collection of context menu
+actions like listing detailed information about an environment and much
+more.
+
+**The main menu:**
+
+.. image:: https://raw.githubusercontent.com/sinusphi/venvipy/master/img/screen-1.png
+
+**The wizard:**
+
+.. image:: https://raw.githubusercontent.com/sinusphi/venvipy/master/img/screen-2.png
+
+**Output when running pip commands:**
+
+.. image:: https://raw.githubusercontent.com/sinusphi/venvipy/master/img/screen-3.png
+
+
+Features
+--------
+
+*  Create virtual environments from any Python version (3.3+) which is
+   properly build or installed on your system
+*  Install and update Pip with one click
+*  Clone an environment from a requirements file
+*  Search and install packages from `PyPI <https://pypi.org/>`__
+*  Generate requirements from an existing environment
+*  List detailed information about installed packages
+*  Show dependency tree (using
+   `pipdeptree <https://pypi.org/project/pipdeptree/#description>`__ package)
+*  Open a project's `PyPI <https://pypi.org/>`__ website in your browser
+*  Install packages from local projects and from repository urls 
+   *(currently git only)*
+*  Modify any environment by adding or removing packages *(comming
+   soon)*
+
+
+Prerequisits
+------------
+
+Primarily *VenviPy* is aimed at \*NIX systems (maybe a Windows port could
+come sometime in the future)
+
+If you want to run *VenviPy* using your operating system's Python (3.7+)
+you will have to make sure that the two packages ``python3-venv`` and
+``python3-pip`` are installed, because in this case the operating system's
+venv and pip will be used to perform the commands.
+
+
+Installation
+------------
+
+You can install the latest version of *VenviPy* via:
+
+.. code-block:: bash
+
+    $ pip install venvipy
+
+or:
+
+.. code-block:: bash
+
+    $ pip install git+https://github.com/sinusphi/venvipy.git
+
+Now you can launch the main menu via:
+
+.. code-block:: bash
+
+    $ venvipy
+
+Or if you just want to quickly create a new virtual environment use
+the wizard standalone:
+
+.. code-block:: bash
+
+    $ venvipy-wizard
+
+
+Running from source
+-------------------
+
+If running *VenviPy* from source the recommended way is to use a virtual
+environment. First clone or download the source repository. Then open a
+terminal and run:
+
+.. code-block:: bash
+
+    $ python3.x -m venv [your_env_name]
+
+Change to the created directory and run:
+
+.. code-block:: bash
+
+    $ source bin/activate
+
+The easiest way to install the required packages is to use the
+`requirements.txt <https://github.com/sinusphi/venvipy/blob/master/requirements.txt>`__
+from the repository. Navigate to the downloaded repo and run:
+
+.. code-block:: bash
+
+    $ (your_env_name) pip install --requirement requirements.txt
+
+Or install the `PyQt5 <https://pypi.org/project/PyQt5>`__ package by
+running the following command:
+
+.. code-block:: bash
+
+    $ (your_env_name) pip install PyQt5==5.14.0 PyQt5-sip
+
+Finally inside the repo cd into the ``venvipy/`` folder and run:
+
+.. code-block:: bash
+
+    $ (your_env_name) python venvi.py
+
+
+Known issues
+------------
+
+Sometimes it happens that when starting the creation process the wizard page 
+freezes. Restarting *VenviPy* fixes this. 
+
+
+Contributing
+------------
+
+Contributions are welcomed, as well as `Pull
+requests <https://github.com/sinusphi/venvipy/pulls>`__, `bug
+reports <https://github.com/sinusphi/venvipy/issues>`__, and `feature
+requests <https://github.com/sinusphi/venvipy/issues>`__.
---
+++ packages/venvipy/venvipy-0.2.8.tar.gz/venvipy-0.2.8/venvipy/get_data.py
@@ -0,0 +1,281 @@
+# -*- coding: utf-8 -*-
+"""
+This module provides all the necessary data.
+"""
+import xmlrpc.client
+import shutil
+import csv
+import sys
+import os
+from subprocess import Popen, PIPE
+from dataclasses import dataclass
+
+__version__ = "0.2.8"
+
+
+
+#]===========================================================================[#
+#] FIND PYTHON 3 INSTALLATIONS [#============================================[#
+#]===========================================================================[#
+
+@dataclass
+class PythonInfo:
+    """_"""
+    py_version: str
+    py_path: str
+
+
+def get_python_version(py_path):
+    """
+    Return Python version.
+    """
+    res = Popen(
+        [py_path, "-V"],
+        stdout=PIPE,
+        text="utf-8"
+    )
+    out, _ = res.communicate()
+    python_version = out.strip()
+    return python_version
+
+
+def get_python_installs():
+    """
+    Get the available python versions installed.
+    """
+    versions = ["3.9", "3.8", "3.7", "3.6", "3.5", "3.4", "3.3"]
+    py_info_list = []
+    csv_file = os.path.expanduser("~/.venvipy/py-installs")
+
+    if not os.path.exists(os.path.expanduser("~/.venvipy")):
+        os.mkdir(os.path.expanduser("~/.venvipy"))
+
+    with open(csv_file, "w", newline="") as cf:
+        fields = ["PYTHON_VERSION", "PYTHON_PATH"]
+        writer = csv.DictWriter(
+            cf, delimiter=",", quoting=csv.QUOTE_ALL, fieldnames=fields
+        )
+        writer.writeheader()
+        for i, version in enumerate(versions):
+            python_path = shutil.which(f"python{version}")
+            if python_path is not None:
+                python_version = get_python_version(python_path)
+                py_info = PythonInfo(python_version, python_path)
+                py_info_list.append(py_info)
+                writer.writerow({
+                    "PYTHON_VERSION": py_info.py_version,
+                    "PYTHON_PATH": py_info.py_path
+                })
+    add_sys_python()
+
+
+def add_sys_python():
+    """
+    Add the system python if running VenviPy in a
+    virtual environment.
+    """
+    csv_file = os.path.expanduser("~/.venvipy/py-installs")
+    super_python = os.path.realpath(sys.executable)
+
+    if not os.path.isfile(csv_file):
+        get_python_installs()
+
+    with open(csv_file, "a", newline="") as cf:
+        fields = ["PYTHON_VERSION", "PYTHON_PATH"]
+        writer = csv.DictWriter(
+            cf, delimiter=",", quoting=csv.QUOTE_ALL, fieldnames=fields
+        )
+        writer.writerow({
+            "PYTHON_VERSION": get_python_version(super_python),
+            "PYTHON_PATH": super_python
+        })
+
+
+#]===========================================================================[#
+#] GET VENVS [#==============================================================[#
+#]===========================================================================[#
+
+@dataclass
+class VenvInfo:
+    """_"""
+    venv_name: str
+    venv_version: str
+    system_site_packages: str
+    is_on_system: str
+
+
+def get_venvs(path):
+    """
+    Get the available virtual environments
+    from the specified folder.
+    """
+    # return an emtpty list if directory doesn't exist
+    if not os.path.isdir(path):
+        return []
+
+    venv_info_list = []
+
+    for i, venv in enumerate(os.listdir(path)):
+        # build path to venv directory
+        valid_venv = os.path.join(path, venv)
+        if not os.path.isdir(valid_venv):
+            continue
+
+        # build path to pyvenv.cfg file
+        cfg_file = os.path.join(valid_venv, "pyvenv.cfg")
+        if not os.path.isfile(cfg_file):
+            continue
+
+        venv_name = os.path.basename(valid_venv)
+        version_found = get_pyvenv_cfg(cfg_file, line=2)
+        system_site_packages = get_pyvenv_cfg(cfg_file, line=1)
+        is_on_system = is_installed(cfg_file)
+
+        venv_info = VenvInfo(
+            venv_name, version_found, system_site_packages, is_on_system
+        )
+        venv_info_list.append(venv_info)
+
+    return venv_info_list
+
+
+def is_installed(cfg_file):
+    """
+    Check if a Python version is installed.
+    """
+    with open(cfg_file, "r") as f:
+        lines = f.readlines()
+
+    python_path = lines[0][7:].strip()  # e.g. /usr/local/bin
+    python_version = lines[2][10:13].strip()  # e.g. 3.8
+    target = f"{python_path}/python{python_version}"
+    csv_file = os.path.expanduser("~/.venvipy/py-installs")
+
+    if not os.path.isfile(csv_file):
+        get_python_installs()
+
+    with open(csv_file, newline="") as cf:
+        reader = csv.DictReader(cf, delimiter=",")
+        for info in reader:
+            if target == info["PYTHON_PATH"]:
+                return "yes"
+        return "no"
+
+
+def get_pyvenv_cfg(cfg_file, line):
+    """
+    Get the required info of a virtual environment
+    by reading `pyvenv.cfg` file.
+    """
+    with open(cfg_file, "r") as f:
+        lines = f.readlines()
+
+    if line == 1:
+        system_site_packages = lines[1][31:]  # true | false
+        if "true" in system_site_packages:
+            return "global"
+        if "false" in system_site_packages:
+            return "isolated"
+    if line == 2:
+        python_version = lines[2][10:]  # e.g. 3.8.2
+        return f"Python {python_version}".strip()
+    return "N/A"
+
+
+def get_active_dir_str():
+    """
+    Get the default venv directory string from `active` file.
+    """
+    active_file = os.path.expanduser("~/.venvipy/active")
+
+    if not os.path.exists(os.path.expanduser("~/.venvipy")):
+        os.mkdir(os.path.expanduser("~/.venvipy"))
+
+    if os.path.exists(active_file):
+        with open(active_file, "r") as f:
+            active_dir = f.read()
+            return active_dir
+    else:
+        with open(active_file, "w+") as f:
+            active_dir = f.write("")
+            return active_dir
+    return []
+
+
+def get_active_dir():
+    """
+    Get the active venv directory path string from `active`
+    file and pass it to `get_venvs()`.
+    """
+    active_dir = get_active_dir_str()
+    return get_venvs(active_dir)
+
+
+#]===========================================================================[#
+#] GET INFOS FROM PYTHON PACKAGE INDEX [#====================================[#
+#]===========================================================================[#
+
+@dataclass
+class ModuleInfo:
+    """_"""
+    mod_name: str
+    mod_version: str
+    mod_summary: str
+
+
+def get_module_infos(name):
+    """
+    Get module name, version and description
+    from [PyPI](https://pypi.org/pypi).
+    """
+    client = xmlrpc.client.ServerProxy("https://pypi.org/pypi")
+    search_result = client.search({"name": name})
+
+    module_info_list = []
+
+    for i, mod in enumerate(search_result):
+        mod_name = mod["name"]
+        mod_version = mod["version"]
+        mod_summary = mod["summary"]
+
+        mod_info = ModuleInfo(mod_name, mod_version, mod_summary)
+        module_info_list.append(mod_info)
+
+    return module_info_list[::-1]
+
+
+
+if __name__ == "__main__":
+    #pass
+
+    get_python_installs()
+
+
+    #]=======================================================================[#
+
+    #test_cfg_file = "/mnt/SQ-Core/coding/.virtualenvs/.OLD/pgsqltest/pyvenv.cfg"
+    #test_cfg_file = "/mnt/SQ-Core/coding/.virtualenvs/pyqt-dev-build/pyvenv.cfg"
+
+    #print(is_installed(test_cfg_file))
+
+    #]=======================================================================[#
+
+    #for python in get_python_installs():
+        #print(python.py_version, python.py_path)
+
+    #]=======================================================================[#
+
+    # venv in get_active_dir():
+        #print(venv.name, venv.version)
+
+    #]=======================================================================[#
+
+    #test_module = "test"
+
+    #for item in get_module_infos(test_mod):
+        #print(item.mod_name, item.mod_version, item.mod_summary)
+
+    #if not get_module_infos(test_module):
+        #print("No modules found!")
+
+    #print(get_active_dir())
