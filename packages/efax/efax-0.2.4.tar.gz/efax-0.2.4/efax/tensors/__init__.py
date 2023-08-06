6ad14ba118158b0c7f3392acffa716cc449a35a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:27:50 +0000

    Add package efax/0.2.4/efax-0.2.4.tar.gz

---
+++ packages/efax/efax-0.2.4.tar.gz/efax-0.2.4/efax/tensors/__init__.py
@@ -0,0 +1,11 @@
+"""
+This package provides abstract interfaces:
+* tensor manipulation (TensorManipulator),
+* interval arithmetic (Extrema), and
+* slicing and broadcasting (Matching).
+
+The interface is implemented using cpu and gpu routines.
+"""
+from .annotations import *
+from .dtypes import *
+from .tools import *
