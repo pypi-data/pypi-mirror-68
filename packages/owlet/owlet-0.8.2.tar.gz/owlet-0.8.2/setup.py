b9c5936355eaf516d6508f6fdf4d2a646e1cfd49
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:52:25 +0000

    Add package owlet/0.8.2/owlet-0.8.2.tar.gz

---
+++ packages/owlet/owlet-0.8.2.tar.gz/owlet-0.8.2/setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="owlet",
+    version="0.8.2",
+    author="HFM3",
+    # author_email="author@example.com",
+    description="A Geospatial Python Package for Field Researchers",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/HFM3/owlet',
+    packages=setuptools.find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Science/Research",
+        "Topic :: Scientific/Engineering :: GIS",
+        "Programming Language :: Python :: 3.8",
+        "Operating System :: OS Independent",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Natural Language :: English",
+    ],
+    python_requires='>=3.8',
+    install_requires=['strix>=0.7.2']
+)
+
+# https://github.com/pypa/sampleproject/blob/master/setup.py
