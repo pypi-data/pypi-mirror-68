0ffa1f0d707eb226f1ce68782c11f1dc58944668
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:25:49 +0000

    Add package metad/0.0.0/metad-0.0.0.tar.gz

---
+++ packages/metad/metad-0.0.0.tar.gz/metad-0.0.0/setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+setup(
+    author="MIT Data To AI Lab",
+    author_email='dailabmit@gmail.com',
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ],
+    license="MIT license",
+    include_package_data=True,
+    name='metad',
+    version='0.0.0',
+    zip_safe=False,
+)
