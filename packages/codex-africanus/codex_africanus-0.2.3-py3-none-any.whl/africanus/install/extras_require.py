9c2a8eb761e85bbbb61f552dea284b2ec4bb28ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:26:38 +0000

    Add package codex-africanus/0.2.3/codex_africanus-0.2.3-py3-none-any.whl

---
+++ packages/codex-africanus/codex_africanus-0.2.3-py3-none-any.whl/africanus/install/extras_require.py
@@ -0,0 +1,9 @@
+extras_require = {
+   'cuda': ['cupy >= 5.0.0', 'jinja2 >= 2.10'],
+   'dask': ['dask[array] >= 1.1.0'],
+   'jax': ['jax == 0.1.27', 'jaxlib == 0.1.14'],
+   'scipy': ['scipy >= 1.0.0'],
+   'astropy': ['astropy >= 3.0'],
+   'python-casacore': ['python-casacore >= 3.2.0'],
+   'testing': ['pytest', 'flaky', 'pytest-flake8 >= 1.0.6'],
+}
