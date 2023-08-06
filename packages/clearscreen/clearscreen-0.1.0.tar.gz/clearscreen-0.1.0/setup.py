ec3f5021c39df53726afadcb545ed2fb72a6f967
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:10:58 +0000

    Add package clearscreen/0.1.0/clearscreen-0.1.0.tar.gz

---
+++ packages/clearscreen/clearscreen-0.1.0.tar.gz/clearscreen-0.1.0/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['clearscreen', 'clearscreen.art']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['colored>=1.4.2,<2.0.0']
+
+entry_points = \
+{'console_scripts': ['cls = clearscreen.cli:main']}
+
+setup_kwargs = {
+    'name': 'clearscreen',
+    'version': '0.1.0',
+    'description': '',
+    'long_description': None,
+    'author': 'Tim Simpson',
+    'author_email': 'timsimpson4@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.6,<4.0',
+}
+
+
+setup(**setup_kwargs)
