47a200cfea7f3557cf8bd5b9d8936cd382f634e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:06:46 +0000

    Add package bleak/0.6.2/bleak-0.6.2.tar.gz

---
+++ packages/bleak/bleak-0.6.2.tar.gz/bleak-0.6.2/tests/test_imports.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Tests for `bleak` package."""
+
+import os
+import platform
+
+import pytest
+
+_IS_CI = os.environ.get("CI", "false").lower() == "true"
+_IS_AZURE_PIPELINES = os.environ.get("SYSTEM_HOSTTYPE", "") == "build"
+
+if platform.system() != "Darwin":
+    _OS = os.environ.get("AGENT_OS").lower()
+else:
+    _OS = platform.system()
+
+
+@pytest.mark.skipif(
+    condition=(_IS_CI or _IS_AZURE_PIPELINES) and (_OS in ("linux", "darwin")),
+    reason="""Cannot run on Azure Pipelines with
+    Ubuntu 16.04 or macOS installed.""",
+)
+def test_import():
+    """Test by importing the client and assert correct client by OS."""
+    if platform.system() == "Linux":
+        from bleak import BleakClient
+
+        assert BleakClient.__name__ == "BleakClientBlueZDBus"
+    elif platform.system() == "Windows":
+        from bleak import BleakClient
+
+        assert BleakClient.__name__ == "BleakClientDotNet"
+    elif platform.system() == "Darwin":
+        from bleak import BleakClient
+
+        assert BleakClient.__name__ == "BleakClientCoreBluetooth"
