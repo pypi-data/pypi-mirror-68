761d8c9b71d0883a19231eaa68a4dc34274be0a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:13:12 +0000

    Add package katcp/0.7.1/katcp-0.7.1.tar.gz

---
+++ packages/katcp/katcp-0.7.1.tar.gz/katcp-0.7.1/katcp/test/__init__.py
@@ -0,0 +1,7 @@
+# __init__.py
+# -*- coding: utf8 -*-
+# vim:fileencoding=utf8 ai ts=4 sts=4 et sw=4
+# Copyright 2009 National Research Foundation (South African Radio Astronomy Observatory)
+# BSD license - see LICENSE for details
+
+"""Tests for the katcp package."""
---
+++ packages/katcp/katcp-0.7.1.tar.gz/katcp-0.7.1/scripts/__init__.py
@@ -0,0 +1,8 @@
+# Copyright 2008 National Research Foundation (South African Radio Astronomy Observatory)
+# BSD license - see LICENSE for details
+
+"""The empty module is here so that pylint can examine the scripts
+
+   @author Robert Crida <robert.crida@ska.ac.za>
+   @date 2008-10-10
+   """
