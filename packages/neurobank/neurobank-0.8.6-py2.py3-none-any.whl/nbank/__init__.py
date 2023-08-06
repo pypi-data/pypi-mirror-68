1c76dec64e07afd9a7c04c0f9772c407285b9cde
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:50 +0000

    Add package neurobank/0.8.6/neurobank-0.8.6-py2.py3-none-any.whl

---
+++ packages/neurobank/neurobank-0.8.6-py2.py3-none-any.whl/nbank/__init__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+# -*- mode: python -*-
+"""neurobank data management system
+
+Copyright (C) 2013 Dan Meliza <dan@meliza.org>
+Created Mon Nov 25 08:43:19 2013
+"""
+from __future__ import absolute_import
+
+__version__ = '0.8.6'
+
+from nbank.core import default_registry, deposit, search, find, get, describe, get_path_or_url, verify
+
+# Variables:
+# End:
