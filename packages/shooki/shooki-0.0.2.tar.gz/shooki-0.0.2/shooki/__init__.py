10a4bf6711fff435c9cb4894f79455d863b0428e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:32 +0000

    Add package shooki/0.0.2/shooki-0.0.2.tar.gz

---
+++ packages/shooki/shooki-0.0.2.tar.gz/shooki-0.0.2/shooki/__init__.py
@@ -0,0 +1,11 @@
+from shooki.shooki import tag_maker
+
+html = tag_maker('html')
+head = tag_maker('head')
+body = tag_maker('body')
+h1 = tag_maker('h1')
+h2 = tag_maker('h2')
+div = tag_maker('div')
+script = tag_maker('script')
+link = tag_maker('link')
+title = tag_maker('title')
