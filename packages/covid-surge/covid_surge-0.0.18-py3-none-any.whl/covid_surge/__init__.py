532a5b845cf763e4f453dcdb2b9c20255e65e0c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:22:57 +0000

    Add package covid-surge/0.0.18/covid_surge-0.0.18-py3-none-any.whl

---
+++ packages/covid-surge/covid_surge-0.0.18-py3-none-any.whl/covid_surge/__init__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# This file is part of the COVID-surge application.
+# https://github/dpploy/covid-surge
+
+from .src.covid_surge_main import Surge
+from .src.help import color_map
