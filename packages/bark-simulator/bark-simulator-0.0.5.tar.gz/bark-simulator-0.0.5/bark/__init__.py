1c69b77c7483bfbcc721b976f21869d96590af54
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:02:42 +0000

    Add package bark-simulator/0.0.5/bark-simulator-0.0.5.tar.gz

---
+++ packages/bark-simulator/bark-simulator-0.0.5.tar.gz/bark-simulator-0.0.5/bark/__init__.py
@@ -0,0 +1,9 @@
+#from .pybark import core
+#import shutil
+#shutil.copyfile('bark/pybark/core.so','bark/core.so')
+# import os
+# src = os.path.abspath('bark/pybark/core.so')
+# dst = os.path.abspath('bark/core.so')
+# os.symlink(src, dst)
+
+#__all__ = ["core"]
