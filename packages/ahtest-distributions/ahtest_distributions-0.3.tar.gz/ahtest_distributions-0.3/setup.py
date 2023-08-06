c63ea5a819d6612da9047fe906f834851b66e5f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:22:41 +0000

    Add package ahtest-distributions/0.3/ahtest_distributions-0.3.tar.gz

---
+++ packages/ahtest-distributions/ahtest_distributions-0.3.tar.gz/ahtest_distributions-0.3/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='ahtest_distributions',
+      version='0.3',
+      description='Gaussian distributions',
+      packages=['ahtest_distributions'],
+      author='Ahsan',
+      author_email='ahsan@ahsan.com',
+      zip_safe=False)
