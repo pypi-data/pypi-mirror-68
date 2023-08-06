10b86733f5b114b79ced204a0a616e51034cd9b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:11 +0000

    Add package distkv/0.18.5/distkv-0.18.5.tar.gz

---
+++ packages/distkv/distkv-0.18.5.tar.gz/distkv-0.18.5/distkv/command/client/log.py
@@ -0,0 +1,57 @@
+# command line interface
+
+import asyncclick as click
+
+from distkv.util import yprint
+
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+@main.group(short_help="Manage logging.")  # pylint: disable=undefined-variable
+@click.pass_obj
+async def cli(obj):
+    """
+    This subcommand controls a server's logging.
+    """
+    pass
+
+
+@cli.command()
+@click.option("-i", "--incremental", is_flag=True, help="Don't write the initial state")
+@click.argument("path", nargs=1)
+@click.pass_obj
+async def dest(obj, path, incremental):
+    """
+    Log changes to a file.
+
+    Any previously open log (on the server you talk to) is closed as soon
+    as the new one is opened and ready.
+    """
+    res = await obj.client._request("log", path=path, fetch=not incremental)
+    if obj.meta:
+        yprint(res, stream=obj.stdout)
+
+
+@cli.command()
+@click.argument("path", nargs=1)
+@click.pass_obj
+async def save(obj, path):
+    """
+    Write the server's current state to a file.
+    """
+    res = await obj.client._request("save", path=path)
+    if obj.meta:
+        yprint(res, stream=obj.stdout)
+
+
+@cli.command()
+@click.pass_obj
+async def stop(obj):
+    """
+    Stop logging changes.
+    """
+    res = await obj.client._request("log")  # no path == stop
+    if obj.meta:
+        yprint(res, stream=obj.stdout)
