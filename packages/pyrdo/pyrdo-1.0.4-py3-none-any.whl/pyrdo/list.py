262c3ff46635fc7c7a9bd00051adcd39dffab6d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:00:51 +0000

    Add package pyrdo/1.0.4/pyrdo-1.0.4-py3-none-any.whl

---
+++ packages/pyrdo/pyrdo-1.0.4-py3-none-any.whl/pyrdo/list.py
@@ -0,0 +1,29 @@
+# 定义dict_keys_list
+def dict_keys_list(dict):
+    res =  [i for i in dict.keys()]
+    return res
+# 定义dict_values_list
+def dict_values_list(dict):
+    res = [i for i in dict.values()]
+    return res
+def paste(list:dict(type=list,help="一维数组"),sep:dict(type=str,help="seperater default value is |")="|")->str:
+	return sep.join(list)
+# 元组转化为列表
+def tuple_as_list(tuple):
+	return(list(tuple))
+
+
+if __name__ == "__main__":
+	mydata = [1,2,3,4,5]
+	myarr = [['A1'],['B1']]
+	text = "|".join(['a',"b","c"])
+	text2 = paste(['a','b','c'],"|")
+	print(text2)
+	print(type(text))
+	print(paste.__annotations__)
+	a = ["我是胡立磊"]
+	b = "我是e"
+	mydata2 =[['1','tom'],['2','jack']]
+	tupledata = (1,2,3)
+	listdata = tuple_as_list(tupledata)
+	print(listdata)
