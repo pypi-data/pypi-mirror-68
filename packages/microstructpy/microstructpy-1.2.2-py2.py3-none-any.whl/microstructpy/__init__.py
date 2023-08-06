b4f514c29260f82ae9dde14112c3686173750924
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:10:44 +0000

    Add package microstructpy/1.2.2/microstructpy-1.2.2-py2.py3-none-any.whl

---
+++ packages/microstructpy/microstructpy-1.2.2-py2.py3-none-any.whl/microstructpy/__init__.py
@@ -0,0 +1,9 @@
+import microstructpy.cli
+import microstructpy.geometry
+import microstructpy.meshing
+import microstructpy.seeding
+import microstructpy.verification
+from microstructpy._vs import _ver_str
+
+__version__ = '1.2.2'
+__version__ = _ver_str(__version__)
