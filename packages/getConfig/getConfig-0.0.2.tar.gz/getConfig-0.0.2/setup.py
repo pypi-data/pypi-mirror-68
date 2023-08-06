659db7d3e8fcf28209e48ef81facf85e83dd850c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:35:44 +0000

    Add package getConfig/0.0.2/getConfig-0.0.2.tar.gz

---
+++ packages/getConfig/getConfig-0.0.2.tar.gz/getConfig-0.0.2/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="getConfig", # Replace with your own username
+    version="0.0.2",
+    author="Alex-Martin Lakovski",
+    author_email="amkl17mz@gmail.com",
+    description="Python module for reading variables from configuration files like cfg, INI, XML, JSON.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/AlexMartin17/getConfig",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
