736d7bf9469d3f6862b300c4a4b6155231e28b36
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:34:22 +0000

    Add package brainrender/0.4.0.0/brainrender-0.4.0.0-py3-none-any.whl

---
+++ packages/brainrender/brainrender-0.4.0.0-py3-none-any.whl/Examples/basic/adding_labels.py
@@ -0,0 +1,22 @@
+"""
+    This tutorial shows how to use flags (strings of text that appear when you mose hoover over an actor)
+    to add more information about the actors in your rendering. 
+"""
+
+
+
+import brainrender
+brainrender.SHADER_STYLE = 'cartoon'
+from brainrender.scene import Scene
+
+# Create a scene
+scene = Scene()
+
+# add_brain_regions can be used to add labels directly
+scene.add_brain_regions('VAL', add_labels=True)
+
+# you can also use scene.add_actor_label
+mos = scene.add_brain_regions('MOs')
+scene.add_actor_label(mos, 'MOs', size=400, color='blackboard')
+
+scene.render()
