7d445f8ed80a2eeade9ca0f3d852afea68431a5b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:09:38 +0000

    Add package pdf-to-scan/0.0.3/pdf-to-scan-0.0.3.tar.gz

---
+++ packages/pdf-to-scan/pdf-to-scan-0.0.3.tar.gz/pdf-to-scan-0.0.3/setup.py
@@ -0,0 +1,32 @@
+import setuptools
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name="pdf-to-scan",
+    version="0.0.3",
+    author="Apurv Mishra",
+    author_email="me@apurvmishra.com",
+    description="A small script to make your pdfs seem like scanned",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/apurvmishra99/pdf-to-scan",
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'Click',
+        'ghostscript',
+        'Wand'
+    ],
+    entry_points='''
+        [console_scripts]
+        pdf-to-scan=pdf_to_scan.make_pdfs_look_scanned:convert
+    ''',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
