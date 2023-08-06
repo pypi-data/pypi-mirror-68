22a375c9bda031def4cc9493fc72e2cb7e7e0673
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:02:09 +0000

    Add package marcel/0.6.7/marcel-0.6.7-py3-none-any.whl

---
+++ packages/marcel/marcel-0.6.7-py3-none-any.whl/test/test_api2.py
@@ -0,0 +1,39 @@
+from marcel.api import *
+
+# run(remote('jao', gen(3)))
+
+for x, y in fork(3, gen(3)):
+    print(f'{x}, {y}')
+
+# run(ls())
+#
+# # run
+# run(gen(3))
+#
+# # gather
+# print(gather(gen(3)))
+#
+# # first
+# print(first(gen(start=100, count=10)))
+#
+# # iterator
+# for x in gen(3, start=5):
+#     print(x)
+#
+# for ext, count, size in (ls('/home/jao/git/marcel', file=True, recursive=True)
+#                          | map(lambda f: (f.suffix.lower(), 1, f.size))
+#                          | red(None, r_plus, r_plus)
+#                          | sort(lambda ext, count, size: -size)
+#                          | head(10)):
+#     print(f'{ext}: {size / count}')
+#
+# for size, pid, commandline in (ps(command='python')
+#                                | sort(lambda p: p.VmRSS)
+#                                | map(lambda p: (p.VmRSS, p.pid, p.commandline))):
+#     print(f'{size} -- {pid}: {commandline}')
+#
+# for x in gen(3, -1) | map(lambda x: 1/x):
+#     print(x)
+#
+# # # first with exception
+# # print(first(map(lambda: 1 / 0)))
