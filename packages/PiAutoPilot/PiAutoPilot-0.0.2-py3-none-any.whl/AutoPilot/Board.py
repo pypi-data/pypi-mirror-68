67f26d7059b8215e4063aca34956abb320f4c5b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:10:27 +0000

    Add package PiAutoPilot/0.0.2/PiAutoPilot-0.0.2-py3-none-any.whl

---
+++ packages/PiAutoPilot/PiAutoPilot-0.0.2-py3-none-any.whl/AutoPilot/Board.py
@@ -0,0 +1,7 @@
+import time
+
+class Board(object):
+  def __init__(self, **kwargs):
+    """ Board(port)
+    """
+    
---
+++ packages/PiAutoPilot/PiAutoPilot-0.0.2-py3-none-any.whl/tests/test_board.py
@@ -0,0 +1,12 @@
+
+from AutoPilot.Board import Board
+import pytest
+import sys
+import fake_rpi
+import time
+
+
+class TestBoardh():
+    def test_dummy(self):
+        assert 1 == 1
+   
