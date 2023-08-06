83abf21d9f7afa5d84a76d534c1421f865e24002
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:12:47 +0000

    Add package ip-address/1.3.1/ip_address-1.3.1.tar.gz

---
+++ packages/ip-address/ip_address-1.3.1.tar.gz/ip_address-1.3.1/setup.py
@@ -0,0 +1,31 @@
+import setuptools
+
+with open('README.md') as f:
+    README = f.read()
+
+setuptools.setup(
+    author="Thomas Dewitte",
+    author_email="thomasdewittecontact@gmail.com",
+
+    name='ip_address',
+    version='1.3.1',
+    license="MIT",
+    url='https://github.com/dewittethomas/ip-address',
+    python_requires='>= 3.5',
+    
+    description='A simple tool to get your ip-address',
+    long_description=README,
+    long_description_content_type="text/markdown",
+
+    package_dir={"ip_address": "ip_address"},
+    install_requires=["requests>=2.22.0"],
+    
+    packages=setuptools.find_packages(),
+
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3'
+    ]
+)
