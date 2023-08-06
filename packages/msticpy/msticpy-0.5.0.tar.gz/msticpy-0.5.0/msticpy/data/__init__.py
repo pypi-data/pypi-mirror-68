50804dc9007336bacb8a0dd20ee4c72b4924b4f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:34 +0000

    Add package msticpy/0.5.0/msticpy-0.5.0.tar.gz

---
+++ packages/msticpy/msticpy-0.5.0.tar.gz/msticpy-0.5.0/msticpy/data/__init__.py
@@ -0,0 +1,8 @@
+"""Data sub-package."""
+# flake8: noqa: F403
+from .data_providers import QueryProvider
+from .azure_data import AzureData
+
+from .._version import VERSION
+
+__version__ = VERSION
