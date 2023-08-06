c68c7bec2966703c9363374033aeda9a77ac36a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:50:25 +0000

    Add package x-bot/0.0.5/x-bot-0.0.5.tar.gz

---
+++ packages/x-bot/x-bot-0.0.5.tar.gz/x-bot-0.0.5/xbot/constants.py
@@ -0,0 +1,25 @@
+import enum
+DEFAULT_OUTPUT_FILENAME = "gen__xbot.py"
+GENERATED_COMMENT = '"""__GENERATED__ cross platform generated function"""'
+
+class TEMPLATES(enum.Enum):
+    """
+    These are the supported templates, i.e. the classes
+    of function that xbot can generate
+    """
+
+    # REPLY is basic get arguments, 
+    # process them and send reply template
+    REPLY = 'reply.py'
+
+class LIBRARIES(enum.Enum):
+    """
+    These are the supported messaing platform APIs
+    wrapper to translate between
+    """
+
+    # https://github.com/Rapptz/discord.py
+    DISCORD_PY = 'discord'
+
+    # https://github.com/python-telegram-bot/python-telegram-bot
+    PYTHON_TELEGRAM_BOT = 'telegram'
