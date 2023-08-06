a1bc7fb05bd3539c838e19d08a226939b3bffdef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:55:22 +0000

    Add package azureml-interpret/1.5.0/azureml_interpret-1.5.0-py3-none-any.whl

---
+++ packages/azureml-interpret/azureml_interpret-1.5.0-py3-none-any.whl/azureml/interpret/common/storage_policy/__init__.py
@@ -0,0 +1,9 @@
+# ---------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# ---------------------------------------------------------
+
+"""Defines storage policy."""
+
+from ._storage_policy import _storage_policy as storage_policy
+
+__all__ = ['storage_policy']
