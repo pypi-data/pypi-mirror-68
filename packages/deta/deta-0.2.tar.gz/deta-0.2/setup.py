9fb6b09f677388e07fee1224a03d0f04f78064eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:53 +0000

    Add package deta/0.2/deta-0.2.tar.gz

---
+++ packages/deta/deta-0.2.tar.gz/deta-0.2/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name="deta",
+    version="0.2",
+    description="Deta lib for Python 3",
+    url="http://github.com/deta/deta-python",
+    author="Mustafa Abdelhai",
+    author_email="mustafa@deta.sh",
+    license="MIT",
+    packages=["deta"],
+)
