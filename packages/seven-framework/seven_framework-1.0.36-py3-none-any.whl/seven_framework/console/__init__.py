40504c23e66c068a151b45083695b22630fdf4a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:13:06 +0000

    Add package seven-framework/1.0.36/seven_framework-1.0.36-py3-none-any.whl

---
+++ packages/seven-framework/seven_framework-1.0.36-py3-none-any.whl/seven_framework/console/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+@Author: ChenXiaolei
+@Date: 2020-05-09 20:44:50
+@LastEditTime: 2020-05-09 20:44:59
+@LastEditors: ChenXiaolei
+@Description: 
+"""
+from .base_console import *
