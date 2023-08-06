6f9453a751a06e6fe0e67218e6bf040a48ef9c78
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:23:09 +0000

    Add package polysdk/0.0.7/polysdk-0.0.7-py3-none-any.whl

---
+++ packages/polysdk/polysdk-0.0.7-py3-none-any.whl/polysdk/models/unmaskeddata.py
@@ -0,0 +1,11 @@
+class UnmaskedData:
+    __text = ''
+
+    def __init__(self, text):
+        if text is '':
+            raise Exception("text is empty.")
+
+        self.__text = text
+
+    def get_text(self):
+        return self.__text;
