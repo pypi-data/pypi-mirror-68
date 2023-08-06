2bdb3fe7d40397ba5850c2970b50afd197635fba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:11:15 +0000

    Add package rpi-ws281x-mock/0.2.2/rpi_ws281x_mock-0.2.2-py2.py3-none-any.whl

---
+++ packages/rpi-ws281x-mock/rpi_ws281x_mock-0.2.2-py2.py3-none-any.whl/rpi_ws281x/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for rpi-ws281x mock."""
+
+__author__ = """Harm van Zeeland"""
+__email__ = "harm.van.zeeland@gmail.com"
+__version__ = "__version__ = '0.2.2'"
+
+from .rpi_ws281x_mock import PixelStrip, Adafruit_NeoPixel, Color, ws
