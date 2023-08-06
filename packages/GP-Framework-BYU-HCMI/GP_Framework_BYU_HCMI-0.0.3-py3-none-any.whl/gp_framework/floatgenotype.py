93e273adfac7e802bc8835e5f3df82bedd82c96d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:36:29 +0000

    Add package GP-Framework-BYU-HCMI/0.0.3/GP_Framework_BYU_HCMI-0.0.3-py3-none-any.whl

---
+++ packages/GP-Framework-BYU-HCMI/GP_Framework_BYU_HCMI-0.0.3-py3-none-any.whl/gp_framework/floatgenotype.py
@@ -0,0 +1,38 @@
+from typing import List
+import numpy as np
+
+
+class FloatGenotype:
+    def __init__(self, floats: List[float]):
+        self._floats = floats
+        hashcode = 0
+        for float_ in floats:
+            hashcode += int(100 * float_)
+        hashcode /= 10
+        self._hashcode = hashcode
+
+    def _mutate_floats(self, mutation_factor: float) -> List[float]:
+        return [f + np.random.uniform(-mutation_factor, mutation_factor, 1)[0] for f in self._floats]
+
+    def mutate(self, mutation_factor: float):
+        self._floats = self._mutate_floats(mutation_factor)
+
+    def make_mutated_copy(self, mutation_factor):
+        return FloatGenotype(self._mutate_floats(mutation_factor))
+
+    def __getitem__(self, item):
+        return self._floats[item]
+
+    def __len__(self):
+        return len(self._floats)
+
+    def __eq__(self, other):
+        if not isinstance(other, FloatGenotype):
+            return False
+        return self._floats == other._floats
+
+    def __ne__(self, other):
+        return not self.__eq__(other)
+
+    def __hash__(self):
+        return self._hashcode
