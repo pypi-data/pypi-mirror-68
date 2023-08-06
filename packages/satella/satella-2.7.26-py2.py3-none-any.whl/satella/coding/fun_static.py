0922682ea14f22ccd57a2a9a0636fac80d56867e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:11:53 +0000

    Add package satella/2.7.26/satella-2.7.26-py2.py3-none-any.whl

---
+++ packages/satella/satella-2.7.26-py2.py3-none-any.whl/satella/coding/fun_static.py
@@ -0,0 +1,23 @@
+def static_var(var_name: str, starting_value=None):
+    """
+    Declare a static variable for given function
+
+    Use it like:
+
+    >>> @static_var('counter', 2)
+    >>> def count():
+    >>>     count.counter += 1
+
+    or:
+
+    >>> class MyClass:
+    >>>     @static_var('counter', 2)
+    >>>     def count(self):
+    >>>         MyClass.count.counter += 1
+    """
+
+    def decorate(func):
+        setattr(func, var_name, starting_value)
+        return func
+
+    return decorate
---
+++ packages/satella/satella-2.7.26-py2.py3-none-any.whl/satella/posix/__init__.py
@@ -0,0 +1,50 @@
+"""
+POSIX things
+"""
+
+import os
+import sys
+
+from .daemon import daemonize
+from .pidlock import PIDFileLock
+from satella.exceptions import LockIsHeld
+from .signals import hang_until_sig
+
+__all__ = [
+    'daemonize',
+    'PIDFileLock', 'hang_until_sig',
+    'is_running_as_root',
+    'suicide'
+]
+
+
+def is_running_as_root() -> bool:
+    """
+    Is this process running as root?
+
+    Checks whether EUID is 0
+
+    :return: bool
+    :raises OSError: called on Windows!
+    """
+
+    if sys.platform.startswith('win'):
+        raise OSError('Routine unavailable on Windows!')
+
+    return os.geteuid() == 0
+
+
+def suicide(kill_entire_pg: bool = True) -> None:
+    """
+    Kill self.
+
+    :param kill_entire_pg: whether to kill entire PG if a session leader
+    :raises NotImplementedError: called on Windows!
+    """
+
+    if sys.platform.startswith('win'):
+        raise NotImplementedError('suicide() not yet supported on Windows! (see issue #37)')
+
+    mypid = os.getpid()
+    kill = os.killpg if kill_entire_pg and os.getpgid(0) == mypid else os.kill
+    kill(mypid, 9)
