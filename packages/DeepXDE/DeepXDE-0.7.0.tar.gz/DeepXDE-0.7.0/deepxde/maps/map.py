8f4c64bc2572504bd3492556db007b6224c3e5b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:13:02 +0000

    Add package DeepXDE/0.7.0/DeepXDE-0.7.0.tar.gz

---
+++ packages/DeepXDE/DeepXDE-0.7.0.tar.gz/DeepXDE-0.7.0/deepxde/maps/map.py
@@ -0,0 +1,64 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+from ..backend import tf
+from ..utils import make_dict, timing
+
+
+class Map(object):
+    """Map base class."""
+
+    def __init__(self):
+        if not hasattr(self, "regularizer"):
+            self.regularizer = None
+
+        self.training = tf.placeholder(tf.bool)
+        self.dropout = tf.placeholder(tf.bool)
+        self.data_id = tf.placeholder(tf.uint8)  # 0: train data, 1: test data
+
+        # The property will be set upon call of self.build()
+        self._built = False
+
+    @property
+    def built(self):
+        return self._built
+
+    @built.setter
+    def built(self, value):
+        self._built = value
+
+    @property
+    def inputs(self):
+        """Mapping inputs."""
+
+    @property
+    def outputs(self):
+        """Mapping outputs."""
+
+    @property
+    def targets(self):
+        """Targets of the mapping outputs."""
+
+    def feed_dict(self, training, dropout, data_id, inputs, targets=None):
+        """Construct a feed_dict to feed values to TensorFlow placeholders."""
+        feed_dict = {
+            self.training: training,
+            self.dropout: dropout,
+            self.data_id: data_id,
+        }
+        feed_dict.update(self._feed_dict_inputs(inputs))
+        if targets is not None:
+            feed_dict.update(self._feed_dict_targets(targets))
+        return feed_dict
+
+    def _feed_dict_inputs(self, inputs):
+        return make_dict(self.inputs, inputs)
+
+    def _feed_dict_targets(self, targets):
+        return make_dict(self.targets, targets)
+
+    @timing
+    def build(self):
+        """Construct the mapping."""
+        self.built = True
