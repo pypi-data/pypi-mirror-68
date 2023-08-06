7be02619f6c5dad7480f95d090e2ca37e8c6fc19
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:25:28 +0000

    Add package kpytrends/4.7.4/kpytrends-4.7.4.tar.gz

---
+++ packages/kpytrends/kpytrends-4.7.4.tar.gz/kpytrends-4.7.4/setup.py
@@ -0,0 +1,35 @@
+import io
+import os
+
+from setuptools import setup
+
+dir = os.path.dirname(__file__)
+
+with io.open(os.path.join(dir, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='kpytrends',
+    version='4.7.4',
+    description='Pseudo API for Google Trends',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/kkristof200/pytrends',
+    author=['John Hogue', 'Burton DeWilde'],
+    author_email='dreyco676@gmail.com',
+    license='Apache 2.0',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'License :: OSI Approved :: MIT License'
+    ],
+    install_requires=["pandas", "requests"],
+    keywords='google trends api search',
+    packages=['pytrends'],
+)
