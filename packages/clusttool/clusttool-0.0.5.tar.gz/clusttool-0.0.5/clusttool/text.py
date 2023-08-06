4026d5faa95cad780b5263d59426c5a9ea4ec167
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:04:14 +0000

    Add package clusttool/0.0.5/clusttool-0.0.5.tar.gz

---
+++ packages/clusttool/clusttool-0.0.5.tar.gz/clusttool-0.0.5/clusttool/text.py
@@ -0,0 +1,23 @@
+def joke():
+	"""
+	this is some info on myfunc
+	"""
+	return (u'Wenn ist das Nunst\u00fcck git und Slotermeyer? Ja! ... '
+		u'Beiherhund das Oder die Flipperwaldt gersput.')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
