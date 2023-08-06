f05663fb81c3deab12319f968581135623d4ec54
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:02:34 +0000

    Add package grayskull/0.5.0/grayskull-0.5.0.tar.gz

---
+++ packages/grayskull/grayskull-0.5.0.tar.gz/grayskull-0.5.0/tests/cli/test_cli_cmds.py
@@ -0,0 +1,31 @@
+import pytest
+
+import grayskull
+from grayskull.__main__ import main
+
+
+def test_version(capsys):
+    main(["--version"])
+    captured = capsys.readouterr()
+    assert captured.out.strip() == grayskull.__version__
+
+
+def test_pypi_cmd(tmpdir):
+    out_folder = tmpdir.mkdir("out")
+    main(
+        ["pypi", "pytest=5.3.2", "-o", str(out_folder), "-m", "m1", "m2", "--download"]
+    )
+    pytest_folder = out_folder / "pytest"
+    assert pytest_folder.isdir()
+
+    recipe_file = pytest_folder / "meta.yaml"
+    assert recipe_file.isfile()
+    assert (pytest_folder / "pypi.json").isfile()
+    assert (pytest_folder / "pytest-5.3.2.tar.gz").isfile()
+
+
+@pytest.mark.parametrize("option", ["--heman", "--shera"])
+def test_easter(capsys, option):
+    main([option])
+    captured = capsys.readouterr()
+    assert "By the power of Grayskull..." in captured.out.strip()
