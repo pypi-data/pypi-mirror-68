a8bf567645b141816e192079f981c62e32a54c55
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:44 +0000

    Add package flumine/1.4.0/flumine-1.4.0.tar.gz

---
+++ packages/flumine/flumine-1.4.0.tar.gz/flumine-1.4.0/flumine/events/events.py
@@ -0,0 +1,91 @@
+import datetime
+from enum import Enum
+
+
+class EventType(Enum):
+    TERMINATOR = "Terminator"
+    # betfair objects
+    MARKET_CATALOGUE = "MarketCatalogue"
+    MARKET_BOOK = "MarketBook"
+    RAW_DATA = "Raw streaming data"
+    CURRENT_ORDERS = "CurrentOrders"
+    CLEARED_MARKETS = "ClearedMarkets"
+    CLEARED_ORDERS = "ClearedOrders"
+    # flumine objects
+    ORDER_PACKAGE = "Order package"
+    CLOSE_MARKET = "Closed market"
+    STRATEGY_RESET = "Strategy reset"
+    CUSTOM_EVENT = "Custom event"
+    NEW_DAY = "New day"
+
+
+class QueueType(Enum):
+    HANDLER = "Handler queue"
+    ACCOUNT = "Account queue"
+    LOGGING = "Logging queue"
+
+
+class BaseEvent:  # todo __slots__?
+    EVENT_TYPE = None
+    QUEUE_TYPE = None
+
+    def __init__(self, event):
+        self._time_created = datetime.datetime.utcnow()
+        self.event = event
+
+    @property
+    def elapsed_seconds(self):
+        return (datetime.datetime.utcnow() - self._time_created).total_seconds()
+
+    def __str__(self):
+        return "<{0} [{1}]>".format(self.EVENT_TYPE.name, self.QUEUE_TYPE.name)
+
+
+class MarketCatalogueEvent(BaseEvent):
+    EVENT_TYPE = EventType.MARKET_CATALOGUE
+    QUEUE_TYPE = QueueType.HANDLER
+
+
+class MarketBookEvent(BaseEvent):
+    EVENT_TYPE = EventType.MARKET_BOOK
+    QUEUE_TYPE = QueueType.HANDLER
+
+
+class RawDataEvent(BaseEvent):
+    EVENT_TYPE = EventType.RAW_DATA
+    QUEUE_TYPE = QueueType.HANDLER
+
+
+class CurrentOrdersEvent(BaseEvent):
+    EVENT_TYPE = EventType.CURRENT_ORDERS
+    QUEUE_TYPE = QueueType.HANDLER
+
+
+class ClearedMarketsEvent(BaseEvent):
+    EVENT_TYPE = EventType.CLEARED_MARKETS
+    QUEUE_TYPE = QueueType.HANDLER
+
+
+class ClearedOrdersEvent(BaseEvent):
+    EVENT_TYPE = EventType.CLEARED_ORDERS
+    QUEUE_TYPE = QueueType.HANDLER
+
+
+class CloseMarketEvent(BaseEvent):
+    EVENT_TYPE = EventType.CLOSE_MARKET
+    QUEUE_TYPE = QueueType.HANDLER
+
+
+class StrategyResetEvent(BaseEvent):
+    EVENT_TYPE = EventType.STRATEGY_RESET
+    QUEUE_TYPE = QueueType.HANDLER
+
+
+class CustomEvent(BaseEvent):
+    EVENT_TYPE = EventType.CUSTOM_EVENT
+    QUEUE_TYPE = QueueType.HANDLER
+
+
+class NewDayEvent(BaseEvent):
+    EVENT_TYPE = EventType.NEW_DAY
+    QUEUE_TYPE = QueueType.HANDLER
---
+++ packages/flumine/flumine-1.4.0.tar.gz/flumine-1.4.0/flumine/worker.py
@@ -0,0 +1,99 @@
+import time
+import threading
+import logging
+import queue
+from betfairlightweight import BetfairError, filters
+
+from .events.events import MarketCatalogueEvent
+from .utils import chunks
+
+logger = logging.getLogger(__name__)
+
+
+class BackgroundWorker(threading.Thread):
+    def __init__(
+        self,
+        interval: int,
+        function,
+        func_args: tuple = None,
+        func_kwargs: dict = None,
+        start_delay: int = 0,
+        **kwargs
+    ):
+        threading.Thread.__init__(self, daemon=True, **kwargs)
+        self.interval = interval
+        self.function = function
+        self.func_args = func_args if func_args is not None else []
+        self.func_kwargs = func_kwargs if func_kwargs is not None else {}
+        self.start_delay = start_delay
+
+    def run(self) -> None:
+        time.sleep(self.start_delay)
+        while self.is_alive():
+            logger.debug(
+                "BackgroundWorker {0} executing".format(self.name),
+                extra={"name": self.name, "function": self.function},
+            )
+            try:
+                self.function(*self.func_args, **self.func_kwargs)
+            except Exception as e:
+                logger.error(
+                    "Error in BackgroundWorker {0}: {1}".format(self.name, e),
+                    extra={"worker_name": self.name, "function": self.function},
+                )
+            time.sleep(self.interval)
+
+
+def keep_alive(client) -> None:
+    """ Attempt keep alive if required or
+    login if keep alive failed
+    """
+    if client.betting_client.session_token:
+        try:
+            resp = client.keep_alive()
+            if resp is None or resp.status == "SUCCESS":
+                return
+        except BetfairError as e:
+            logger.error(
+                "keep_alive error",
+                exc_info=True,
+                extra={"trading_function": "keep_alive", "response": e},
+            )
+    # attempt login
+    try:
+        client.login()
+    except BetfairError as e:
+        logger.error(
+            "login error",
+            exc_info=True,
+            extra={"trading_function": "login", "response": e},
+        )
+
+
+def poll_market_catalogue(client, markets, handler_queue: queue.Queue) -> None:
+    live_markets = list(markets.markets.keys())
+    for market_ids in chunks(live_markets, 100):
+        try:
+            market_catalogues = client.betting_client.betting.list_market_catalogue(
+                filter=filters.market_filter(market_ids=market_ids),
+                max_results=100,
+                market_projection=[
+                    "COMPETITION",
+                    "EVENT",
+                    "EVENT_TYPE",
+                    "RUNNER_DESCRIPTION",
+                    "RUNNER_METADATA",
+                    "MARKET_START_TIME",
+                    "MARKET_DESCRIPTION",
+                ],
+            )
+        except BetfairError as e:
+            logger.error(
+                "poll_market_catalogue error",
+                exc_info=True,
+                extra={"trading_function": "list_market_catalogue", "response": e},
+            )
+            continue
+
+        if market_catalogues:
+            handler_queue.put(MarketCatalogueEvent(market_catalogues))
