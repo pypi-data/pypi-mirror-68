c92c03810f8ab9ba00cdc22a1c1eed0ca48cad29
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:21 +0000

    Add package py-ed25519-bindings/0.1.0/py_ed25519_bindings-0.1.0.tar.gz

---
+++ packages/py-ed25519-bindings/py_ed25519_bindings-0.1.0.tar.gz/tests.py
@@ -0,0 +1,26 @@
+import unittest
+
+import bip39
+import ed25519
+
+
+class MyTestCase(unittest.TestCase):
+    def test_sign_and_verify_message(self):
+
+        message = b"test"
+
+        # Get private and public key from seed
+        seed = bip39.bip39_to_mini_secret('daughter song common combine misery cotton audit morning stuff weasel flee field','')
+        private_key, public_key = ed25519.ed_from_seed(bytes(seed))
+
+        # Generate signature
+        signature = ed25519.ed_sign(public_key, private_key, message)
+        print(signature.hex())
+
+        # Verify message with signature
+        if ed25519.ed_verify(signature, message, public_key):
+            print('Verified')
+
+
+if __name__ == '__main__':
+    unittest.main()
