43b282bf9f2067731b2b12d7dd78dd4c3ed5e39f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:22:42 +0000

    Add package quickumls/1.4.0.post1/quickumls-1.4.0.post1.tar.gz

---
+++ packages/quickumls/quickumls-1.4.0.post1.tar.gz/quickumls-1.4.0.post1/quickumls/about.py
@@ -0,0 +1,12 @@
+# inspired from:
+# https://python-packaging-user-guide.readthedocs.org/en/latest/single_source_version/
+# https://github.com/pypa/warehouse/blob/master/warehouse/__about__.py
+# https://github.com/explosion/spaCy/blob/master/spacy/about.py
+
+__title__ = 'quickumls'
+__version__ = '1.4.0r1'
+__author__ = 'Luca Soldaini'
+__email__ = 'luca@ir.cs.georgetown.edu'
+__license__ = 'MIT'
+__uri__ = "https://github.com/Georgetown-IR-Lab/QuickUMLS"
+__copyright__ = '2014-2020, Georgetown University Information Retrieval Lab'
