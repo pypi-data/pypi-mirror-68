d3c79274c32055eb5b2169783415eabc38b57691
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:03:12 +0000

    Add package py-gnuplot/0.2/py-gnuplot-0.2.tar.gz

---
+++ packages/py-gnuplot/py-gnuplot-0.2.tar.gz/py-gnuplot-0.2/setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+import pygnuplot
+
+setup(name='py-gnuplot',
+      version='0.2',
+      author='Yongping Guo',
+      author_email='guoyoooping@163.com',
+      url='http://www.gnuplot.info',
+      license='GPL',
+      description='A Python plot tools based on Gnuplot',
+      packages=[ 'pygnuplot' ],
+      )
