79ab178f3978716fa973234131eee005481dd036
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:50 +0000

    Add package django-ripple/0.2.20/django-ripple-0.2.20.tar.gz

---
+++ packages/django-ripple/django-ripple-0.2.20.tar.gz/django-ripple-0.2.20/ripple/context.py
@@ -0,0 +1,12 @@
+from django.conf import settings
+
+
+def GlobalConfig(request):
+    return {'GA_ACCOUNT_ID': settings.GA_ACCOUNT_ID,
+            'ALLOW_ROBOTS': settings.ALLOW_ROBOTS,
+            'SITE_URL': settings.SITE_URL,
+            'HJID': settings.HJID,
+            'HJSV': settings.HJSV,
+            'DEBUG': ('false', 'true')[settings.DEBUG],
+            'FEATURES': ','.join(["'%s'" % i for i in request.session.get('features', [])])
+            }
