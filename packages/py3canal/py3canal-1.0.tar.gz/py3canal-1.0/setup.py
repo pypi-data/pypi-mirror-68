3d8039c0ed23ed0fdfb52093648ae0142b6d62b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:14:51 +0000

    Add package py3canal/1.0/py3canal-1.0.tar.gz

---
+++ packages/py3canal/py3canal-1.0.tar.gz/py3canal-1.0/setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+    name="py3canal",
+    version="1.0",
+    packages=["pycanal", "pycanal.protocol"],
+    author="vallee",
+    author_email="xph_wangly10@163.com",
+    license="MIT",
+    url="https://github.com/vallee11/canal-python"
+)
