f2b9b16f9ed9e820c1bcf00e7c9297d2d9bbfa21
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:10:08 +0000

    Add package gino-admin/0.0.8/gino_admin-0.0.8-py3-none-any.whl

---
+++ packages/gino-admin/gino_admin-0.0.8-py3-none-any.whl/gino_admin/config.py
@@ -0,0 +1,33 @@
+from expiring_dict import ExpiringDict
+from passlib.hash import pbkdf2_sha256
+
+
+class App:
+    """ class to store links to main app data app.config and DB"""
+
+    config = {}
+    db = None
+
+
+class Config:
+    """ Gino Admin Panel settings """
+
+    URL_PREFIX = "/admin"
+    app = App
+    hash_method = pbkdf2_sha256.encrypt
+    models = {}
+    sessions = ExpiringDict(ttl=3600)
+    # upload from csv config
+    upload_dir = "files/"
+    max_file_size = 10485760
+    allowed_file_types = ["csv"]
+    datetime_str_formats = [
+        "%Y-%m-%dT%H:%M:%S.%f",
+        "%Y-%m-%dT%H:%M:%S",
+        "%Y-%m-%d %H:%M:%S.%f",
+        "%m-%d-%yT%H:%M:%S.%f",
+        "%m-%d-%y %H:%M:%S",
+        "%m-%d-%yT%H:%M:%S",
+    ]
+    presets_folder = "presets"
+    composite_csv_settings = {}
