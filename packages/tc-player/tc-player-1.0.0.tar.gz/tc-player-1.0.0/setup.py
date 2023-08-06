f7e774e1fc7e02a22a29e810498d137f51b4346d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:13:16 +0000

    Add package tc-player/1.0.0/tc-player-1.0.0.tar.gz

---
+++ packages/tc-player/tc-player-1.0.0.tar.gz/tc-player-1.0.0/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+with open('README.rst') as readme_file:
+    README = readme_file.read()
+
+setup(
+    name='tc-player',
+    description='A Time Clickers automaton.',
+    url='https://github.com/colons/tc',
+    author='colons',
+    author_email='pypi@colons.co',
+    version='1.0.0',
+    license='LICENSE',
+    platforms=['any'],
+    packages=['tc'],
+    scripts=['scripts/tc'],
+    long_description=README,
+    classifiers=[
+        'Operating System :: OS Independent',
+    ],
+    install_requires=[
+        'selenium<4',
+    ],
+    include_package_data=True,
+)
