346aab944a9d0f592af421b060aee8773d54bc5c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:27:04 +0000

    Add package dummy-poetry-repo/0.1.1/dummy-poetry-repo-0.1.1.tar.gz

---
+++ packages/dummy-poetry-repo/dummy-poetry-repo-0.1.1.tar.gz/dummy-poetry-repo-0.1.1/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['dummy_poetry_repo']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'dummy-poetry-repo',
+    'version': '0.1.1',
+    'description': "Just a repository for testing out different stuff so I don't have to clutter my offiail repos",
+    'long_description': None,
+    'author': 'Lovecraftian Horror',
+    'author_email': 'LovecraftianHorror@pm.me',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
