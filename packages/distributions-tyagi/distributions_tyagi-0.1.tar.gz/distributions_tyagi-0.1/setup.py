ffbb3fd7bf0348ebcf7ebb7c273004d918355e3d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:20:34 +0000

    Add package distributions-tyagi/0.1/distributions_tyagi-0.1.tar.gz

---
+++ packages/distributions-tyagi/distributions_tyagi-0.1.tar.gz/distributions_tyagi-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='distributions_tyagi',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions_tyagi'],
+      author='Sarthak Tyagi',
+      author_email='thesarthaktyagi@gmail.com',
+      zip_safe=False)
