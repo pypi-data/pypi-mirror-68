ac48653b6bd1334a726405df133b28a29b11d41e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:01:50 +0000

    Add package judge0api/0.1.19/judge0api-0.1.19.tar.gz

---
+++ packages/judge0api/judge0api-0.1.19.tar.gz/judge0api-0.1.19/setup.py
@@ -0,0 +1,50 @@
+import io
+import os
+import re
+
+from setuptools import find_packages
+from setuptools import setup
+
+
+def read(filename):
+    filename = os.path.join(os.path.dirname(__file__), filename)
+    text_type = type(u"")
+    with io.open(filename, mode="r", encoding='utf-8') as fd:
+        return re.sub(text_type(r':[a-z]+:`~?(.*?)`'), text_type(r'``\1``'), fd.read())
+
+
+setup(
+    name="judge0api",
+    version="0.1.19",
+    url="https://github.com/vCra/judge0api",
+    license='MIT',
+
+    author="Aaron Walker",
+    author_email="aaron@vcra.io",
+
+    description="A Python API for interacting with the Judge0 REST API",
+    long_description=read("README.rst"),
+
+    packages=find_packages(exclude=('tests',)),
+
+    install_requires=[
+        'requests'
+    ],
+    setup_requires=[
+        'pytest-runner',
+    ],
+    tests_require=[
+        'pytest',
+        'pytest-cov'
+    ],
+
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ],
+)
