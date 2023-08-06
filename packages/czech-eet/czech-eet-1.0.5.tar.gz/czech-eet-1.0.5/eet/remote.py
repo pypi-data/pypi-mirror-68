133b7c3bdc05a058450cb92cc07c42a539999996
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:37 +0000

    Add package czech-eet/1.0.5/czech-eet-1.0.5.tar.gz

---
+++ packages/czech-eet/czech-eet-1.0.5.tar.gz/czech-eet-1.0.5/eet/remote.py
@@ -0,0 +1,46 @@
+from . import invoices, binding, types
+
+from urllib import request, error
+
+
+class Scheduler:
+
+    PLAYGROUND_ENDPOINT = "https://pg.eet.cz:443/eet/services/EETServiceSOAP/v3/"
+    PRODUCTION_ENDPOINT = "https://eet.cz:443/eet/services/EETServiceSOAP/v3/"
+
+    def __init__(self):
+        self._queue = []
+
+    def process(self, invoice):
+        resp = self.send(invoice)
+        if not resp:
+            invoice.Hlavicka["prvni_zaslani"] = types.boolean(False)
+            self._queue.append(invoice)
+        return resp
+    
+    def dispatch(self):
+        queue = []
+        for invoice in self._queue:
+            resp = self.send(invoice)
+            if not resp:
+                queue.append(invoice)
+        self._queue = queue
+
+    @staticmethod
+    def send(invoice):
+        xml = invoice.build()
+        endpoint = Scheduler.PRODUCTION_ENDPOINT if invoice.prod() else Scheduler.PLAYGROUND_ENDPOINT
+
+        try:
+            resp = Scheduler._request(endpoint, xml)
+            resp_data = binding.Soap.parse_response(resp, not invoice.prod())
+            return invoices.Factory.Response(invoice.codes(), resp_data)
+        except error.URLError:
+            return invoices.Factory.Response(invoice.codes(), binding.Odpoved())
+    
+    @staticmethod
+    def _request(endpoint, data):
+        req = request.Request(endpoint, data, method="POST")
+        with request.urlopen(req, timeout=3) as response:
+            return response.read()
+        
---
+++ packages/czech-eet/czech-eet-1.0.5.tar.gz/czech-eet-1.0.5/setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="czech-eet",
+    version="1.0.5",
+    author="Keombre",
+    author_email="keombre8@gmail.com",
+    description="API for EET",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/keombre/python-eet",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires=[
+        "cryptography",
+        "lxml"
+    ],
+    include_package_data=True,
+    package_data={
+        '': ['eet/soap_template.xml'],
+    }
+)
