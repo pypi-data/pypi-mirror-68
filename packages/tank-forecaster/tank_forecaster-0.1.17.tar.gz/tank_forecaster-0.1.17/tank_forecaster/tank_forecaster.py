f2a2825007c2a3b77448b22480d37fcc4756423e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:19:56 +0000

    Add package tank-forecaster/0.1.17/tank_forecaster-0.1.17.tar.gz

---
+++ packages/tank-forecaster/tank_forecaster-0.1.17.tar.gz/tank_forecaster-0.1.17/tank_forecaster/tank_forecaster.py
@@ -0,0 +1,69 @@
+# live function
+import numpy as np
+import pandas as pd
+import statsmodels.api as sm
+from datetime import timedelta
+
+DEFAULT_INTERVAL = timedelta(minutes=10)
+DEFAULT_PERIODS = 6 * 48  # 10 mins * 6 * 48 = 2 days
+DEFAULT_MODEL_OPTIONS = {"type": "areg"}
+
+def remove_outlier(df, col_name = 'y', low = -2, high = 250):
+    return(df[(df[col_name] > low) & (df[col_name] < high)])
+
+# dictionary in, df out
+def clean(dict_inputs):
+    df = pd.DataFrame(dict_inputs)
+    df.read_time = pd.to_datetime(df.read_time) 
+    delta = df.volume.diff()
+    delta = delta.rename('delta')
+    df = pd.concat([df, delta], axis = 1)
+    cols = [0,1,2,6]
+    df = df.drop(df.columns[cols], axis = 1)
+    df = pd.DataFrame(df, columns = ['read_time', 'delta'])
+    df.columns = ['ds','y']
+    
+    df = remove_outlier(df, col_name = 'y')
+    
+    # still need to set slightly negative liftings to zero
+    
+    return(df)
+
+def generate_future_periods(df, periods):
+    start_0 = df.iloc[0, 0]
+    return pd.date_range(start=start_0, freq="10min", periods=periods)
+
+def sarimax_mod(df, prediction_count):
+    mod = sm.tsa.SARIMAX(df.y, order=(8, 0, 0))
+    res = mod.fit()
+    return res.get_prediction(start=0, end=prediction_count).summary_frame()
+
+# df in, dictionary out
+def forecast(
+    df, interval=DEFAULT_INTERVAL, periods=DEFAULT_PERIODS, model_options=None
+):
+
+    model_options = model_options or DEFAULT_MODEL_OPTIONS
+        
+    interval_granularity_ratio = interval / timedelta(minutes=10)
+    periods = interval_granularity_ratio * periods
+
+    future = generate_future_periods(df, periods=periods)
+
+    predictions = sarimax_mod(df, len(future))
+    
+    out = pd.concat([pd.Series(future), predictions], axis = 1)
+    out.columns = ["ds", "yhat", "se", "lower", "upper"]
+
+    for field in ["yhat", "lower", "upper"]:
+        out.loc[out[field] < 0, field] = 0
+        out[field] = (
+            out[field].groupby(out.index // interval_granularity_ratio).cumsum()
+        )
+
+    output_rows_to_keep = (out.index + 1) % interval_granularity_ratio == 0
+
+    return {
+        "forecast_status": "success",
+        "data": out[output_rows_to_keep].to_dict(orient="records"),
+    }
