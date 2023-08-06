fddaa4ccaff70c6fdab72f7cc3aa2958ce48bcf1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:03:28 +0000

    Add package eaven-distributions/0.2/eaven_distributions-0.2.tar.gz

---
+++ packages/eaven-distributions/eaven_distributions-0.2.tar.gz/eaven_distributions-0.2/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='eaven_distributions',
+      version='0.2',
+      description='Gaussian and Binomial distributions',
+      packages=['eaven_distributions'],
+      author = 'Eaven Khaw',
+      author_email = 'cykhaw@hotmail.com',
+      zip_safe=False)
