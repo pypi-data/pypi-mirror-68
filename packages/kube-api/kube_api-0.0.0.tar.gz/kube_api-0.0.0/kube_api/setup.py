358df621901b74da18055ae56f6b10d056df500f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:14:15 +0000

    Add package kube-api/0.0.0/kube_api-0.0.0.tar.gz

---
+++ packages/kube-api/kube_api-0.0.0.tar.gz/kube_api-0.0.0/kube_api/setup.py
@@ -0,0 +1,41 @@
+"""Build Script for setuptools
+
+This build script must be executed outside of the Aries directory.
+
+See Also: https://packaging.python.org/tutorials/packaging-projects/
+"""
+import setuptools
+import os
+
+package_name = "kube_api"
+package_description = "A simple Kubernetes Python API"
+package_url = "https://github.com/labdave/kube_api"
+
+with open(os.path.join(package_name, "README.md"), "r") as fh:
+    long_description = fh.read()
+
+with open(os.path.join(package_name, "requirements.txt"), "r") as f:
+    requirements = f.read().split("\n")
+    requirements = [r.strip() for r in requirements if r.strip()]
+
+release_version = os.popen("cd %s && git tag | tail -1" % package_name).read()
+commit_version = os.popen("cd %s && git rev-list --count master" % package_name).read()
+
+setuptools.setup(
+    name=package_name,
+    version="%s%s" % (release_version, commit_version),
+    author="Qiu Qin",
+    author_email="qiuosier@gmail.com",
+    description=package_description,
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url=package_url,
+    packages=setuptools.find_packages(),
+    install_requires=requirements,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
