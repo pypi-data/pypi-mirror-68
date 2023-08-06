6bd238d5fcaa5f3dfbc50071637c814ac03ddabe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:12:10 +0000

    Add package shaclgen/0.2.5/shaclgen-0.2.5.tar.gz

---
+++ packages/shaclgen/shaclgen-0.2.5.tar.gz/shaclgen-0.2.5/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+#%%
+from setuptools import setup
+
+
+with open('README.rst') as readme:
+    l_description = readme.read()
+with open('requirements.txt') as reqs:
+    requirements = reqs.read()
+
+setup(
+    name = 'shaclgen',
+    version = '0.2.5',
+    packages = ['shaclgen'],
+    package_data = {'shaclgen': ['prefixes/*.json']},
+    description='Shacl graph generator',
+    long_description=l_description,
+    author='Alexis Keely',
+    url='https://github.com/alexiskeely/shaclgen',
+    author_email='alexiskm@uw.com',
+    install_requires=requirements,
+    keywords=['Linked Data', 'Semantic Web', 'Python',
+              'SHACL', 'Shapes', 'Schema', 'Validate'],
+    license='MIT',
+    classifiers=[
+                'Development Status :: 4 - Beta',
+                'Programming Language :: Python :: 3',
+
+    ],
+    entry_points = {
+        'console_scripts': [
+            'shaclgen = shaclgen.__main__:main'
+        ]
+    })
